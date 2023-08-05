cff7e0c10dd9258b3b717647a42ca59325572f79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:48 +0000

    Add package quanario/1.0.75/quanario-1.0.75.tar.gz

---
+++ packages/quanario/quanario-1.0.75.tar.gz/quanario-1.0.75/quanario/__init__.py
@@ -0,0 +1,9 @@
+from .bot import Bot
+from .send import Send
+from .upload import Upload
+from .message_extensions.keyboard import *
+from .message_extensions.keyboard import *
+
+
+# if __name__ == '__main__':
+#     pass
