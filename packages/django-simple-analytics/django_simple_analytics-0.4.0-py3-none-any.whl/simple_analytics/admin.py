aab86c810e03b095075726d44697ba1179111d64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:59 +0000

    Add package django-simple-analytics/0.4.0/django_simple_analytics-0.4.0-py3-none-any.whl

---
+++ packages/django-simple-analytics/django_simple_analytics-0.4.0-py3-none-any.whl/simple_analytics/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+
+from simple_analytics.models import VisitPerPage
+
+
+@admin.register(VisitPerPage)
+class PageAnalyticsAdmin(admin.ModelAdmin):  # type: ignore[type-arg]
+    list_display = (
+        "page",
+        "method",
+        "date",
+        "view_count",
+        "username",
+        "exists",
+        "origin",
+    )
+
+    list_filter = (
+        "exists",
+        "date",
+        "method",
+        "origin",
+        "username",
+        "page",
+    )
