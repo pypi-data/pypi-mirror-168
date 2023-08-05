2ac7aade57a27ac7891d4b05a1abf663c1f93cb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:59 +0000

    Add package distributionsgaussianbinomialbyToheeb/1.0/distributionsgaussianbinomialbyToheeb-1.0.tar.gz

---
+++ packages/distributionsgaussianbinomialbyToheeb/distributionsgaussianbinomialbyToheeb-1.0.tar.gz/distributionsgaussianbinomialbyToheeb-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributionsgaussianbinomialbyToheeb',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['distributionsgaussianbinomialbyToheeb'],
+      author = 'Toheeb B',
+      zip_safe=False,
+       install_requires=['requests'])
