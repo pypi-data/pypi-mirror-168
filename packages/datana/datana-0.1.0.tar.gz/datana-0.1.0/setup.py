7fbb3486a69b8bcaac957f42ea1529d0ca46fd6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:02 +0000

    Add package datana/0.1.0/datana-0.1.0.tar.gz

---
+++ packages/datana/datana-0.1.0.tar.gz/datana-0.1.0/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+	name = 'datana',
+	version = '0.1.0',
+	author = 'MasterLegend',
+	description = 'datana pakage',
+	url = 'https://github.com/MasterLegend/datana',
+	py_modules = ['datana.spectrum', 'datana.figure'],
+)
