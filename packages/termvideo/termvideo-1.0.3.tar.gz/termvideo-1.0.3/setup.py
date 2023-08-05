9dec6ca401ed29e43a44a3795769fa1e54f79a23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:46 +0000

    Add package termvideo/1.0.3/termvideo-1.0.3.tar.gz

---
+++ packages/termvideo/termvideo-1.0.3.tar.gz/termvideo-1.0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='termvideo',
+    version='1.0.3',
+    scripts=['termvideo.py'],
+    author='Om Duggineni',
+    install_requires=['numpy', 'opencv-python'],
+    license="LGPLv3",
+    entry_points = {
+        'console_scripts': ['termvideo=termvideo:main'],
+    }
+)
