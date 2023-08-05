56878118fe7eeae7143d57c4a73b21aa98034e1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:35 +0000

    Add package imaginAIry/1.3.0/imaginAIry-1.3.0-py3-none-any.whl

---
+++ packages/imaginAIry/imaginAIry-1.3.0-py3-none-any.whl/imaginairy/__init__.py
@@ -0,0 +1,14 @@
+import os.path
+
+# tells pytorch to allow MPS usage (for Mac M1 compatibility)
+os.putenv("PYTORCH_ENABLE_MPS_FALLBACK", "1")
+
+from .api import imagine, imagine_image_files  # noqa
+from .schema import (  # noqa
+    ImaginePrompt,
+    ImagineResult,
+    LazyLoadingImage,
+    WeightedPrompt,
+)
+
+PKG_ROOT = os.path.dirname(__file__)
---
+++ packages/imaginAIry/imaginAIry-1.3.0-py3-none-any.whl/imaginairy/safety.py
@@ -0,0 +1,26 @@
+from functools import lru_cache
+
+import numpy as np
+from diffusers.pipelines.stable_diffusion.safety_checker import (
+    StableDiffusionSafetyChecker,
+)
+from transformers import AutoFeatureExtractor
+
+
+@lru_cache()
+def safety_models():
+    safety_model_id = "CompVis/stable-diffusion-safety-checker"
+    safety_feature_extractor = AutoFeatureExtractor.from_pretrained(safety_model_id)
+    safety_checker = StableDiffusionSafetyChecker.from_pretrained(safety_model_id)
+    return safety_feature_extractor, safety_checker
+
+
+def is_nsfw(img, x_sample):
+    safety_feature_extractor, safety_checker = safety_models()
+    safety_checker_input = safety_feature_extractor([img], return_tensors="pt")
+    clip_input = safety_checker_input.pixel_values
+
+    _, has_nsfw_concept = safety_checker(
+        images=[np.empty((2, 2))], clip_input=clip_input
+    )
+    return has_nsfw_concept[0]
