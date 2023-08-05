7a40c323bfd0a4ebf2118ea81c39df554b598a62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:34 +0000

    Add package quanario/1.0.78/quanario-1.0.78.tar.gz

---
+++ packages/quanario/quanario-1.0.78.tar.gz/quanario-1.0.78/quanario/__init__.py
@@ -0,0 +1,11 @@
+import os.path
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from .bot import Bot
+from .send import Send
+from .upload import Upload
+from .message_extensions.keyboard import *
+from .message_extensions.keyboard import *
+
