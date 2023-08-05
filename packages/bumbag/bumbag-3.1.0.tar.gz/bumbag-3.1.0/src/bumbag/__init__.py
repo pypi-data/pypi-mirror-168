4c6fba05199889cc77728f54812cb83c42dc355b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:14 +0000

    Add package bumbag/3.1.0/bumbag-3.1.0.tar.gz

---
+++ packages/bumbag/bumbag-3.1.0.tar.gz/bumbag-3.1.0/src/bumbag/__init__.py
@@ -0,0 +1,10 @@
+from importlib.metadata import version
+
+__version__ = version("bumbag")
+
+from .core import *
+from .io import *
+from .math import *
+from .random import *
+from .string import *
+from .time import *
