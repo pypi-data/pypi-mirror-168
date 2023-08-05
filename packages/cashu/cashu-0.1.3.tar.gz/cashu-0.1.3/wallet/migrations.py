b444016400955fd7d33add0f3a58c02b4413c1a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:21 +0000

    Add package cashu/0.1.3/cashu-0.1.3.tar.gz

---
+++ packages/cashu/cashu-0.1.3.tar.gz/cashu-0.1.3/wallet/migrations.py
@@ -0,0 +1,70 @@
+from core.db import Database
+
+
+async def m000_create_migrations_table(db):
+    await db.execute(
+        """
+    CREATE TABLE IF NOT EXISTS dbversions (
+        db TEXT PRIMARY KEY,
+        version INT NOT NULL
+    )
+    """
+    )
+
+
+async def m001_initial(db: Database):
+    await db.execute(
+        """
+            CREATE TABLE IF NOT EXISTS proofs (
+                amount INTEGER NOT NULL,
+                C TEXT NOT NULL,
+                secret TEXT NOT NULL,
+
+                UNIQUE (secret)
+
+            );
+        """
+    )
+
+    await db.execute(
+        """
+            CREATE TABLE IF NOT EXISTS proofs_used (
+                amount INTEGER NOT NULL,
+                C TEXT NOT NULL,
+                secret TEXT NOT NULL,
+
+                UNIQUE (secret)
+
+            );
+        """
+    )
+
+    await db.execute(
+        """
+        CREATE VIEW IF NOT EXISTS balance AS
+        SELECT COALESCE(SUM(s), 0) AS balance FROM (
+            SELECT SUM(amount) AS s
+            FROM proofs
+            WHERE amount > 0
+        );
+    """
+    )
+
+    await db.execute(
+        """
+        CREATE VIEW IF NOT EXISTS balance_used AS
+        SELECT COALESCE(SUM(s), 0) AS used FROM (
+            SELECT SUM(amount) AS s
+            FROM proofs_used
+            WHERE amount > 0
+        );
+    """
+    )
+
+
+async def m002_add_proofs_reserved(db):
+    """
+    Column for marking proofs as reserved when they are being sent.
+    """
+
+    await db.execute("ALTER TABLE proofs ADD COLUMN reserved BOOL")
