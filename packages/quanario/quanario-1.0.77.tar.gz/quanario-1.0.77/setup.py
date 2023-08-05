28dacd6bee79e605cc86da1659c02b68422c16bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:14 +0000

    Add package quanario/1.0.77/quanario-1.0.77.tar.gz

---
+++ packages/quanario/quanario-1.0.77.tar.gz/quanario-1.0.77/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+
+setuptools.setup(
+    name='quanario',
+    version='1.0.77',
+    author='Stepan-Coder',
+    author_email='stepan.borodin2016@bk.ru',
+    description='Module for creating VK bots...',
+    long_description='Quanario_VK - Module for the development of chatbots in the social network VKontakte',
+    # long_description_content_type='text/markdown',
+    url='https://github.com/Stepan-coder/Quanario_VK',
+    download_url='https://github.com/Stepan-coder/Quanario_VK/archive/refs/heads/v1.0.77.zip',
+    license='CC-BY-NC',
+    packages=['quanario'],
+    install_requires=['vk-api', 'asyncio', 'ffmpeg', 'pydub', 'prettytable', 'urllib3'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    # python_requires='>=3.5',
+)
