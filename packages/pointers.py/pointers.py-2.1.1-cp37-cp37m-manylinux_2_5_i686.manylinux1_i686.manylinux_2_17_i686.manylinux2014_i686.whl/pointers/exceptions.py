e231fa4af9d5da4a0bb8c0b54590c74258279e5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:31 +0000

    Add package pointers.py/2.1.1/pointers.py-2.1.1-cp37-cp37m-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/pointers.py/pointers.py-2.1.1-cp37-cp37m-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl/pointers/exceptions.py
@@ -0,0 +1,44 @@
+__all__ = (
+    "AllocationError",
+    "DereferenceError",
+    "FreedMemoryError",
+    "InvalidSizeError",
+    "InvalidBindingParameter",
+    "NullPointerError",
+)
+
+
+class AllocationError(Exception):
+    """Raised when a memory allocation fails."""
+
+    pass
+
+
+class DereferenceError(Exception):
+    """Raised when dereferencing an address fails."""  # noqa
+
+    pass
+
+
+class FreedMemoryError(Exception):
+    """Raised when trying to perform an operation on freed memory."""
+
+    pass
+
+
+class InvalidSizeError(Exception):
+    """Raised when trying to move an object of the wrong size to an allocation."""  # noqa
+
+    pass
+
+
+class InvalidBindingParameter(Exception):
+    """Raised when an invalid type is passed to a binding."""
+
+    pass
+
+
+class NullPointerError(Exception):
+    """Raised when a pointer is null."""
+
+    pass
