0a21edfe86597eda84617d090e8e0579ea464e3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:49 +0000

    Add package realfakepg/0.0.0/realfakepg-0.0.0-py3-none-any.whl

---
+++ packages/realfakepg/realfakepg-0.0.0-py3-none-any.whl/realfakepg/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "0.0.0"
+
+__author__ = "Nick Timkovich"
+__email__ = "prometheus235@gmail.com"
+
+__license__ = "Apache Software License 2.0"
+__description__ = "Real Fake Databases! ...for PostgreSQL"
+__url__ = "https://github.com/nicktimko/realfakepg"
