d2b36d917c29ae5d349513721f758446973f45dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:19 +0000

    Add package dsnd-probability-das/0.1/dsnd_probability_das-0.1.tar.gz

---
+++ packages/dsnd-probability-das/dsnd_probability_das-0.1.tar.gz/dsnd_probability_das-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_das',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dsnd_probability_das'],
+      author= 'Dipak Sutar',
+      author_email= 'sutardipak@nomail.com',
+      zip_safe=False)
