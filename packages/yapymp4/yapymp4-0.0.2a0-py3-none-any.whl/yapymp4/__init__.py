0bc7856b9fd71eb07e46c92fd78a4b9b755ff08d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:51 +0000

    Add package yapymp4/0.0.2a0/yapymp4-0.0.2a0-py3-none-any.whl

---
+++ packages/yapymp4/yapymp4-0.0.2a0-py3-none-any.whl/yapymp4/__init__.py
@@ -0,0 +1,6 @@
+
+from .utils import _read_atoms_from_mp4, _read_table_or_header_data
+from .mp4reader import MP4Atom , MP4Data
+from .version import __version__ # created during deployment.
+
+import os
