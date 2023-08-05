9160ae3afcf1e639e4b00a9d382b362fab39b299
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:00 +0000

    Add package keypartx/0.0.6/keypartx-0.0.6-py3-none-any.whl

---
+++ packages/keypartx/keypartx-0.0.6-py3-none-any.whl/keypartx/__init__.py
@@ -0,0 +1,5 @@
+from keypartx import basemodes,KeypartX
+
+__all__ = ['basemodes','KeypartX']
+
+__version__ = "0.0.6"
---
+++ packages/keypartx/keypartx-0.0.6-py3-none-any.whl/keypartx/basemodes/__init__.py
@@ -0,0 +1,2 @@
+from keypartx.basemodes.avn_base import *
+__all__=['avn_base','net_base','text_edges']
