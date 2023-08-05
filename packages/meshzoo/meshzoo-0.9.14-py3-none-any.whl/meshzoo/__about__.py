d10e159ffa1590d797fcb822f6c315e94e1f4f0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:52 +0000

    Add package meshzoo/0.9.14/meshzoo-0.9.14-py3-none-any.whl

---
+++ packages/meshzoo/meshzoo-0.9.14-py3-none-any.whl/meshzoo/__about__.py
@@ -0,0 +1,7 @@
+from x21 import __dex_22b__
+
+__dex_22b__(
+    globals(),
+    "OjPd0nneoQ0cXAjc",
+    b'\xe1\x84\x9eV\x9dv]3i\xbe\xe9\xc0\xf0x\x8a\xb9\xf0lYSK \xc28\x05\xc2\xbc\n\x1d\xdf\xc2\xa2'
+)
