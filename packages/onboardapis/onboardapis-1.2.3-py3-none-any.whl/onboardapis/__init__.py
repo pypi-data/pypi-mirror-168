072b90f385936a0492ceb10bd6fbbbd3b6e3fbe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:37 +0000

    Add package onboardapis/1.2.3/onboardapis-1.2.3-py3-none-any.whl

---
+++ packages/onboardapis/onboardapis-1.2.3-py3-none-any.whl/onboardapis/__init__.py
@@ -0,0 +1,20 @@
+"""
+Package metadata
+"""
+
+__project_name__ = 'onboardapis'
+__description__ = 'A pure Python wrapper for the on-board APIs of many different transportation providers'
+__version__ = '1.2.3'
+__author__ = 'Felix Zenk'
+__email__ = 'felix.zenk@web.de'
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2022 Felix Zenk'
+__url__ = 'https://github.com/felix-zenk/onboardapis'
+
+
+class Vehicle(object):
+    """
+    The base class for all vehicles.
+    """
+
+    __slots__ = []
