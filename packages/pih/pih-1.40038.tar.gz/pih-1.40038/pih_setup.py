45e4eb1892e1e0c2fc6e1a1b1fae2ba8822c58da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:14 +0000

    Add package pih/1.40038/pih-1.40038.tar.gz

---
+++ packages/pih/pih-1.40038.tar.gz/pih-1.40038/pih_setup.py
@@ -0,0 +1,62 @@
+import sys
+from setuptools import setup
+
+sys.path.append("//pih/facade")
+from pih.pih import PIH
+
+#for facade
+#prettytable
+#colorama
+#protobuf
+#grpcio (six)
+#pyperclip
+#win32com -> pywin32 (!!!)
+
+#MC
+#pywhatkit
+
+#for orion
+#myssql
+
+#for log
+#telegram_send
+
+#for dc2
+#docs:
+    #mailmerge (pip install docx-mailmerge)
+    #xlsxwriter
+    #xlrd
+    #python-barcode
+    #Pillow
+#ad:
+    #pyad
+    #pywin32 (!!!)
+#transliterate
+
+#for printer (dc2)
+#pysnmp
+
+#for polibase
+#cx_Oracle
+
+#########################################################################################################
+"""
+1. python pih_setup.py sdist --dist-dir pih_dist bdist_wheel --dist-dir pih_dist build --build-base pih_build
+2. twine upload pih_dist/*
+"""
+
+# This call to setup() does all the work
+setup(
+    name="pih",
+    version=f"{PIH.version}",
+    description="PIH library",
+    long_description_content_type="text/markdown",
+    url="https://pacifichosp.com/",
+    author="Nikita Karachentsev",
+    author_email="it@pacifichosp.com",
+    license="MIT",
+    classifiers=[],
+    packages=["pih"],
+    include_package_data=True,
+    install_requires=["prettytable", "colorama", "protobuf", "grpcio"]
+)
