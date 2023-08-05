83027562a854bdfbac370d2f3a47d1cfb76f3ffb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:40 +0000

    Add package MeineUtils/0.0.5/MeineUtils-0.0.5.tar.gz

---
+++ packages/MeineUtils/MeineUtils-0.0.5.tar.gz/MeineUtils-0.0.5/MeineUtils/__init__.py
@@ -0,0 +1,19 @@
+from MeineUtils.General import (
+    _list, 
+    mkdir, 
+    touch, 
+    TXT, 
+    CSV, 
+    flatten_list, 
+    path_join, 
+    split
+)
+
+from MeineUtils.Math import (
+    ReLU_base,
+    ReLU,
+    PReLU,
+    LeakyReLU,
+    isfloat,
+    isfloatfromstring
+)
