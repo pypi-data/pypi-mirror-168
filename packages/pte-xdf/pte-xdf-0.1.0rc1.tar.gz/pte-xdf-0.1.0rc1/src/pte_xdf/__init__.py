77fdfc502004135d519149a0bf04593f346c4ac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:05:51 +0000

    Add package pte-xdf/0.1.0rc1/pte-xdf-0.1.0rc1.tar.gz

---
+++ packages/pte-xdf/pte-xdf-0.1.0rc1.tar.gz/pte-xdf-0.1.0rc1/src/pte_xdf/__init__.py
@@ -0,0 +1,16 @@
+"""A package for loading XDF files recorded with TMSi amplifiers.
+
+This package exposes the following functions:
+
+- `read_raw_xdf(
+    xdf_fname: PathLike,
+    stream_picks: int | str = 1,
+    scale: int | float = 1e-6,
+    verbose: VerbosityLevel = True,
+)` - Reads XDF files (*.xdf) recorded with TMSi amplifiers.
+
+"""
+
+__version__ = "0.1.0.rc1"
+
+from .xdf import read_raw_xdf
