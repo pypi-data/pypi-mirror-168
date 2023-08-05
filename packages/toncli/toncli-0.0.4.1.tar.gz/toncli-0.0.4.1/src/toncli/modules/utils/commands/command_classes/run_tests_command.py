d6eb1dc7d6b2f9d36873d702e71f6219e4b50d88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:14 +0000

    Add package toncli/0.0.4.1/toncli-0.0.4.1.tar.gz

---
+++ packages/toncli/toncli-0.0.4.1.tar.gz/toncli-0.0.4.1/src/toncli/modules/utils/commands/command_classes/run_tests_command.py
@@ -0,0 +1,15 @@
+from argparse import ArgumentParser
+import sys
+from toncli.modules.utils.test.tests import TestsRunner
+from toncli.modules.utils.system.argparse_fix import argv_fix
+
+class RunTestsCommand():
+    def __init__(self, string_kwargs, parser: ArgumentParser):
+        _, kwargs = argv_fix(sys.argv, string_kwargs)
+        args = parser.parse_args(['run_tests', *kwargs])
+
+        test_runner = TestsRunner()
+        test_runner.run(args.contracts.split() if args.contracts else None,
+                        verbose=args.verbose,
+                        output_results=args.output_results,
+                        run_tests_old_way=args.old)
