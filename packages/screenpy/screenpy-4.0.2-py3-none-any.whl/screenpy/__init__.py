58bbb15ce440bebd586a14b8addd8f168ed75c1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:03 +0000

    Add package screenpy/4.0.2/screenpy-4.0.2-py3-none-any.whl

---
+++ packages/screenpy/screenpy-4.0.2-py3-none-any.whl/screenpy/__init__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+#                 ____                           ____
+#                / ___|  ___ _ __ ___  ___ _ __ |  _ \ _   _
+#                \___ \ / __| '__/ _ \/ _ \ '_ \| |_) | | | |
+#                 ___) | (__| | |  __/  __/ | | |  __/| |_| |
+#                |____/ \___|_|  \___|\___|_| |_|_|    \__, |
+#                                                      |___/
+
+"""
+                                  ScreenPy
+                                                                      FADE IN:
+INT. SITEPACKAGES DIRECTORY
+
+ScreenPy is a composition-based test framework. It is inspired by the
+SerenityBDD library for Java.
+
+:copyright: (c) 2019–2022 by Perry Goy.
+:license: MIT, see LICENSE for more details.
+"""
+
+
+from .actor import Actor
+from .director import Director
+from .given_when_then import and_, given, given_that, then, when
+
+# Natural-language-enabling syntactic sugar
+AnActor = Actor
+
+
+__all__ = [
+    "Actor",
+    "AnActor",
+    "Director",
+    "and_",
+    "given",
+    "given_that",
+    "then",
+    "when",
+]
---
+++ packages/screenpy/screenpy-4.0.2-py3-none-any.whl/screenpy/actions/see.py
@@ -0,0 +1,65 @@
+"""
+Make an assertion using a Question and a Resolution.
+"""
+
+from typing import Any, Union
+
+from hamcrest import assert_that
+
+from screenpy import Actor
+from screenpy.pacing import aside, beat
+from screenpy.protocols import Answerable, ErrorKeeper
+from screenpy.resolutions import BaseResolution
+from screenpy.speech_tools import get_additive_description
+
+
+class See:
+    """See if a value or the answer to a Question matches the Resolution.
+
+    This is a very important Action in ScreenPy. It is the way to perform
+    test assertions. For more information, see the documentation for
+    :ref:`Questions` and :ref:`Resolutions`.
+
+    Examples::
+
+        the_actor.should(
+            See(TheText.of_the(WELCOME_MESSAGE), ReadsExactly("Welcome!")),
+        )
+
+        the_actor.should(
+            See.the(list_of_items, ContainsTheItem("juice extractor")),
+        )
+    """
+
+    @classmethod
+    def the(cls, question: Union[Answerable, Any], resolution: BaseResolution) -> "See":
+        """Supply the Question (or value) and Resolution to test."""
+        return cls(question, resolution)
+
+    def describe(self) -> str:
+        """Describe the Action in present tense."""
+        return f"See if {self.question_to_log} is {self.resolution_to_log}."
+
+    @beat("{} sees if {question_to_log} is {resolution_to_log}.")
+    def perform_as(self, the_actor: Actor) -> None:
+        """Direct the Actor to make an observation."""
+        if isinstance(self.question, Answerable):
+            value: object = self.question.answered_by(the_actor)
+        else:
+            # must be a value instead of a question!
+            value = self.question
+            aside(f"the actual value is: {value}")
+
+        reason = ""
+        if isinstance(self.question, ErrorKeeper):
+            reason = f"{self.question.caught_exception}"
+
+        assert_that(value, self.resolution, reason)
+
+    def __init__(
+        self, question: Union[Answerable, Any], resolution: BaseResolution
+    ) -> None:
+        self.question = question
+        self.question_to_log = get_additive_description(question)
+        self.resolution = resolution
+        self.resolution_to_log = resolution.get_line()
