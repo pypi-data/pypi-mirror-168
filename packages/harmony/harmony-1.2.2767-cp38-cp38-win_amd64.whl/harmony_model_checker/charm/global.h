69675f7d3515c63750b73faeffcfad0079e2241c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:52 +0000

    Add package harmony/1.2.2767/harmony-1.2.2767-cp38-cp38-win_amd64.whl

---
+++ packages/harmony/harmony-1.2.2767-cp38-cp38-win_amd64.whl/harmony_model_checker/charm/global.h
@@ -0,0 +1,30 @@
+#ifndef SRC_GLOBAL_H
+#define SRC_GLOBAL_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#define new_alloc(t)	(t *) calloc(1, sizeof(t))
+
+#define CALLTYPE_BITS          4
+#define CALLTYPE_MASK          ((1 << CALLTYPE_BITS) - 1)
+#define CALLTYPE_PROCESS       1
+#define CALLTYPE_NORMAL        2
+#define CALLTYPE_INTERRUPT     3
+
+#define PRI_HVAL            PRIx64
+typedef uint64_t            hvalue_t;
+
+void panic(char *s);
+unsigned long to_ulong(const char *p, int len);
+double gettime();
+
+#define CHUNKSIZE   (1 << 12)
+
+struct allocator {
+    void *(*alloc)(void *ctx, unsigned int size, bool zero);
+    void *ctx;
+    unsigned int worker;        // identifies worker thread
+};
+
+#endif // SRC_GLOBAL_H
