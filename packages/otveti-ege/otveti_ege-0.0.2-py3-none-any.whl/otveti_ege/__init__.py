2374a7335a16bb83b0dfe09efb424e3d2321e0ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:44 +0000

    Add package otveti-ege/0.0.2/otveti_ege-0.0.2-py3-none-any.whl

---
+++ packages/otveti-ege/otveti_ege-0.0.2-py3-none-any.whl/otveti_ege/__init__.py
@@ -0,0 +1,11 @@
+"""
+:authors: BlueSpy
+:license: OSI Approved :: MIT License
+:copyright: (c) 2022 BlueSpy
+"""
+
+import otveti_ege.py
+
+__author__ = 'BlueSpy'
+__version__ = '0.0.1'
+__email__ = 'alexej.rutskij@gmail.com'
