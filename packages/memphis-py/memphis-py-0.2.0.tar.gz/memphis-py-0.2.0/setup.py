4bebad082f99d2442e0d8f71cacec24d601f76af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:18 +0000

    Add package memphis-py/0.2.0/memphis-py-0.2.0.tar.gz

---
+++ packages/memphis-py/memphis-py-0.2.0.tar.gz/memphis-py-0.2.0/setup.py
@@ -0,0 +1,33 @@
+
+from distutils.core import setup
+setup(
+    name='memphis-py',
+    packages=['memphis'],
+    version='0.2.0',
+    license='GPL',
+    description='A powerful messaging platform for modern developers',
+    long_description='file: README.md',
+    long_description_content_type='text/markdown; charset=UTF-8; variant=GFM',
+    readme="README.md",
+    author='Memphis.dev',
+    author_email='team@memphis.dev',
+    url='https://github.com/memphisdev/memphis.py',
+    download_url='https://github.com/memphisdev/memphis.py/archive/refs/tags/v0.2.0.tar.gz',
+    keywords=['message broker', 'devtool', 'streaming', 'data'],
+    install_requires=[
+        'asyncio',
+        'nats-py'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+)
