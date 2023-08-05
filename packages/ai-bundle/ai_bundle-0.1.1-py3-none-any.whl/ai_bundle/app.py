6a67df272bd58c426ffcc004da5d40140b302fda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:10 +0000

    Add package ai-bundle/0.1.1/ai_bundle-0.1.1-py3-none-any.whl

---
+++ packages/ai-bundle/ai_bundle-0.1.1-py3-none-any.whl/ai_bundle/app.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Interface",
+    description="This is a cool bundle interface/"
+    " based on the best practices.  "
+    "Lets dp it together.",
+    version="1.0.0",
+)
