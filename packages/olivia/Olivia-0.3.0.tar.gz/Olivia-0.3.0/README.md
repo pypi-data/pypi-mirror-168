242ee7b3ce827603190f5ec7304104967a6317a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:10 +0000

    Add package olivia/0.3.0/Olivia-0.3.0.tar.gz

---
+++ packages/olivia/Olivia-0.3.0.tar.gz/Olivia-0.3.0/README.md
@@ -0,0 +1,28 @@
+# Olivia
+
+**Uma simples, amigavel e poderosa assistente para requisições HTTP**
+
+- Olivia utiliza a biblioteca *requests*, tornando sua execução mais intuitiva.
+
+## Instalação
+    pip install olivia
+
+## Métodos
+Na versão *0.3.0*, Olivia assite aos métodos *POST, GET, DELETE*
+
+# Modo de Usar
+## POST
+
+``` python
+
+from olivia import Olivia
+
+infos = {
+    "rua": "Verde",
+    "numero": "29"
+}
+
+dados = Olivia("http://url@url.com", infos)
+dados.olivia_salva()
+
+```
---
+++ packages/olivia/Olivia-0.3.0.tar.gz/Olivia-0.3.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['olivia']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.28.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'olivia',
+    'version': '0.3.0',
+    'description': 'Assistente para facilitar requisições HTTP',
+    'long_description': '# Olivia\n\n**Uma simples, amigavel e poderosa assistente para requisições HTTP**\n\n- Olivia utiliza a biblioteca *requests*, tornando sua execução mais intuitiva.\n\n## Instalação\n    pip install olivia\n\n## Métodos\nNa versão *0.3.0*, Olivia assite aos métodos *POST, GET, DELETE*\n\n# Modo de Usar\n## POST\n\n``` python\n\nfrom olivia import Olivia\n\ninfos = {\n    "rua": "Verde",\n    "numero": "29"\n}\n\ndados = Olivia("http://url@url.com", infos)\ndados.olivia_salva()\n\n```\n',
+    'author': 'matheus',
+    'author_email': 'matheus.mmn@hotmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
