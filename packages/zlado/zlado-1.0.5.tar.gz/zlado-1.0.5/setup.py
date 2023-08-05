41bebb045f7352a9e3cd4176429e8d1aa428af02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:19 +0000

    Add package zlado/1.0.5/zlado-1.0.5.tar.gz

---
+++ packages/zlado/zlado-1.0.5.tar.gz/zlado-1.0.5/setup.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='zlado',
+    version='1.0.5',
+    packages=find_packages(),  # 包含所有的py文件
+    package_data={
+        # @see https://www.cnblogs.com/babykick/archive/2012/01/18/2325702.html
+        '': ['*.ini']
+    },
+    # 将数据文件也打包
+    include_package_data=True,
+    install_requires=[
+        'click',
+        'aliyun-python-sdk-ecs'
+    ],
+    entry_points={
+        'console_scripts': [
+            'ado = zlado.ado:cli',
+        ]
+    }
+)
