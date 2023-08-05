eb45c120068b6d1ed603bf341d23180c17727292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:42 +0000

    Add package directory2docker/0.0.6/directory2docker-0.0.6.tar.gz

---
+++ packages/directory2docker/directory2docker-0.0.6.tar.gz/directory2docker-0.0.6/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+from setuptools import setup, find_packages
+
+setuptools.setup(
+    name="directory2docker",
+    version="0.0.6",
+    description="Python Package Boilerplate",
+    long_description=open("README.md").read().strip(),
+    author="Sachin Chandra",
+    author_email="sachin@netbook.ai",
+    py_modules=["directory2docker"],
+    install_requires=[],
+    license="MIT License",
+    zip_safe=False,
+    keywords="Package python enviroment as docker",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
