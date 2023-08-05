a467c9bfdf1d8cdfe7fe616a25d26432f5816859
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:36 +0000

    Add package AutomaticDemuxer/1.33/AutomaticDemuxer-1.33.tar.gz

---
+++ packages/AutomaticDemuxer/AutomaticDemuxer-1.33.tar.gz/AutomaticDemuxer-1.33/automatic_demuxer/format_extensions.py
@@ -0,0 +1,15 @@
+mkv_video_formats_conversion = {
+    "V_MS/VFW/FOURCC": "avi",
+    "V_UNCOMPRESSED": "raw",
+    "V_MPEG4/ISO/AVC": "h264",
+    "V_MPEGH/ISO/HEVC": "hevc",
+    "V_MPEG4/MS/V3": "mp4",
+    "V_MPEG1": "mpg",
+    "V_MPEG2": "mpg",
+    "V_REAL/": "rm",
+    "V_QUICKTIME": "mov",
+    "V_THEORA": "ogv",
+    "V_PRORES": "mov",
+    "V_VP": "ivf",
+    "V_DIRAC": "drc",
+}
