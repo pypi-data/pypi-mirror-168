b4f5bbcf5e1e628ee4e7e49938029054354e3b59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:45 +0000

    Add package snapatac2/2.1.0/snapatac2-2.1.0-cp37-cp37m-macosx_10_9_x86_64.whl

---
+++ packages/snapatac2/snapatac2-2.1.0-cp37-cp37m-macosx_10_9_x86_64.whl/snapatac2/__init__.py
@@ -0,0 +1,15 @@
+from ._version import __version__
+from ._io import read_10x_mtx
+from . import preprocessing as pp
+from . import tools as tl
+from . import plotting as pl
+from . import export as ex
+
+from snapatac2._snapatac2 import (
+    AnnData, AnnDataSet, read, read_mtx, create_dataset, read_dataset,
+    LinkData, NodeData,
+)
+
+import sys
+sys.modules.update({f'{__name__}.{m}': globals()[m] for m in ['pp', 'tl', 'pl', 'ex']})
+del sys
