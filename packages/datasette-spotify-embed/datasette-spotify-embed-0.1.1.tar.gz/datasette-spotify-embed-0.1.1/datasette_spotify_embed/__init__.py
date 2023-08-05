3c2284c969ef4bc69b54d5d7b7beefa4c459c226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:37 +0000

    Add package datasette-spotify-embed/0.1.1/datasette-spotify-embed-0.1.1.tar.gz

---
+++ packages/datasette-spotify-embed/datasette-spotify-embed-0.1.1.tar.gz/datasette-spotify-embed-0.1.1/datasette_spotify_embed/__init__.py
@@ -0,0 +1,18 @@
+from datasette import hookimpl
+from markupsafe import Markup, escape
+
+
+@hookimpl
+def render_cell(value):
+    if not isinstance(value, str):
+        return
+    if value.startswith("spotify:track:"):
+        uri = escape(value.split(":")[-1])
+        iframe = (
+            f'<iframe style="border-radius:12px" '
+            f'src="https://open.spotify.com/embed/track/{uri}?theme=0" '
+            'width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; '
+            'clipboard-write; encrypted-media; fullscreen; picture-in-picture">'
+            "</iframe>"
+        )
+        return Markup(iframe)
