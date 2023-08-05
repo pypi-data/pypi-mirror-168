e92651d22fc5d189461e9348bb75b0b064c72450
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:25 +0000

    Add package pyobjc-framework-Quartz/8.5.1/pyobjc-framework-Quartz-8.5.1.tar.gz

---
+++ packages/pyobjc-framework-Quartz/pyobjc-framework-Quartz-8.5.1.tar.gz/pyobjc-framework-Quartz-8.5.1/PyObjCTest/test_pdfpage.py
@@ -0,0 +1,18 @@
+from PyObjCTools.TestSupport import TestCase
+import Quartz
+
+
+class TestPDFPage(TestCase):
+    def test_enum_types(self):
+        self.assertIsEnumType(Quartz.PDFDisplayBox)
+
+    def testConstants(self):
+        self.assertEqual(Quartz.kPDFDisplayBoxMediaBox, 0)
+        self.assertEqual(Quartz.kPDFDisplayBoxCropBox, 1)
+        self.assertEqual(Quartz.kPDFDisplayBoxBleedBox, 2)
+        self.assertEqual(Quartz.kPDFDisplayBoxTrimBox, 3)
+        self.assertEqual(Quartz.kPDFDisplayBoxArtBox, 4)
+
+    def testMethods(self):
+        self.assertResultIsBOOL(Quartz.PDFPage.displaysAnnotations)
+        self.assertArgIsBOOL(Quartz.PDFPage.setDisplaysAnnotations_, 0)
