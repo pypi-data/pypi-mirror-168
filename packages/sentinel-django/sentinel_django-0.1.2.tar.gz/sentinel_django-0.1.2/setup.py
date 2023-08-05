cefbcd08ab33f37bd65fd51b52ea5d998333e182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:04 +0000

    Add package sentinel-django/0.1.2/sentinel_django-0.1.2.tar.gz

---
+++ packages/sentinel-django/sentinel_django-0.1.2.tar.gz/sentinel_django-0.1.2/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['sentinel_django']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Django==4.0.6',
+ 'asgiref==3.5.2',
+ 'certifi==2022.6.15',
+ 'charset-normalizer==2.1.0',
+ 'idna==3.3',
+ 'requests==2.28.1',
+ 'sqlparse==0.4.2',
+ 'tzdata==2022.1',
+ 'urllib3==1.26.11']
+
+setup_kwargs = {
+    'name': 'sentinel-django',
+    'version': '0.1.2',
+    'description': 'Error Detection Package for Sentinel',
+    'long_description': 'None',
+    'author': 'Niranjan',
+    'author_email': 'niranjannb7777@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
