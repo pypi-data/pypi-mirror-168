33cab79e7efada0eebe9d977af41813db285c5bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:55 +0000

    Add package cz-legacy/0.1.7/cz_legacy-0.1.7.tar.gz

---
+++ packages/cz-legacy/cz_legacy-0.1.7.tar.gz/cz_legacy-0.1.7/cz_legacy/__init__.py
@@ -0,0 +1,9 @@
+"""cz_legacy."""
+
+from .cz_legacy import _LegacyCz as discover_this  # noqa: N813
+
+__version__ = '0.1.7'
+__pkg_name__ = 'cz_legacy'
+
+__all__ = ('discover_this',)
+"""Make the _LegacyCz class discoverable by commitizen."""
