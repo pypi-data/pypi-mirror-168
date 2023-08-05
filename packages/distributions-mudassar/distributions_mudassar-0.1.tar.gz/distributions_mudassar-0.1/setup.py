e27bc28ec0e56f0125b4b1460dd0f670cd91ebe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:44 +0000

    Add package distributions-mudassar/0.1/distributions_mudassar-0.1.tar.gz

---
+++ packages/distributions-mudassar/distributions_mudassar-0.1.tar.gz/distributions_mudassar-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_mudassar',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_mudassar'],
+      author= 'Mudassar Hanif',
+      author_email= 'mudassarhanif2@gmail.com',
+      zip_safe=False)
