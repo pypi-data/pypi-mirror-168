265ce60ee254ca5c5c72e667571b5bba36c13614
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:01 +0000

    Add package phantom-types/0.18.0/phantom-types-0.18.0.tar.gz

---
+++ packages/phantom-types/phantom-types-0.18.0.tar.gz/phantom-types-0.18.0/src/phantom/__init__.py
@@ -0,0 +1,34 @@
+"""
+Use ``Phantom`` to create arbitrary phantom types using boolean predicates.
+
+.. code-block:: python
+
+    from phantom import Phantom
+
+
+    def is_big(value: int) -> bool:
+        return value > 5
+
+
+    class Big(int, Phantom, predicate=is_big):
+        ...
+
+
+    assert isinstance(10, Big)  # this passes
+"""
+from ._base import BoundError
+from ._base import Phantom
+from ._base import PhantomBase
+from ._base import PhantomMeta
+from ._base import get_bound_parser
+from .predicates import Predicate
+
+__version__ = "0.18.0"
+__all__ = (
+    "BoundError",
+    "Phantom",
+    "PhantomBase",
+    "PhantomMeta",
+    "get_bound_parser",
+    "Predicate",
+)
---
+++ packages/phantom-types/phantom-types-0.18.0.tar.gz/phantom-types-0.18.0/src/phantom/datetime.py
@@ -0,0 +1,45 @@
+"""
+Types for narrowing on the builtin datetime types.
+"""
+
+import datetime
+
+from . import Phantom
+from .predicates.datetime import is_tz_aware
+from .predicates.datetime import is_tz_naive
+from .schema import Schema
+
+
+class TZAware(datetime.datetime, Phantom, predicate=is_tz_aware):
+    """
+    A type for helping ensure that ``datetime`` objects are always timezone aware.
+
+    >>> isinstance(datetime.datetime.now(), TZAware)
+    False
+    >>> isinstance(datetime.datetime.now(tz=datetime.timezone.utc), TZAware)
+    True
+    """
+
+    @classmethod
+    def __schema__(cls) -> Schema:
+        return {
+            **super().__schema__(),  # type: ignore[misc]
+            "description": "A date-time with timezone data.",
+        }
+
+
+class TZNaive(datetime.datetime, Phantom, predicate=is_tz_naive):
+    """
+    >>> isinstance(datetime.datetime.now(), TZNaive)
+    True
+    >>> isinstance(datetime.datetime.now(tz=datetime.timezone.utc), TZNaive)
+    False
+    """
+
+    @classmethod
+    def __schema__(cls) -> Schema:
+        return {
+            **super().__schema__(),  # type: ignore[misc]
+            "description": "A date-time without timezone data.",
+            "format": "date-time-naive",
+        }
