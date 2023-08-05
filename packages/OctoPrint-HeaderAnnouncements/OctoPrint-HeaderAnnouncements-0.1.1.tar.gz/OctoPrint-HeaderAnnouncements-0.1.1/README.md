8c117fb9dc005ce2b35e26adb6eec964aa3db8e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:56 +0000

    Add package OctoPrint-HeaderAnnouncements/0.1.1/OctoPrint-HeaderAnnouncements-0.1.1.tar.gz

---
+++ packages/OctoPrint-HeaderAnnouncements/OctoPrint-HeaderAnnouncements-0.1.1.tar.gz/OctoPrint-HeaderAnnouncements-0.1.1/README.md
@@ -0,0 +1,13 @@
+# OctoPrint-HeaderAnnouncements
+In a shared environment like a maker space or lab, it can be useful to be able to notify users of upcoming changes, maintenance, procedures, etc.  This plugin allows you to set a header that will be displayed at the top of the page for users to see.
+
+## Setup
+
+Install via the bundled [Plugin Manager](https://docs.octoprint.org/en/master/bundledplugins/pluginmanager.html)
+or manually using this URL:
+
+    https://github.com/wwsean08/OctoPrint-HeaderAnnouncements/archive/main.zip
+
+## Configuration
+
+* Announcement: The announcement to display.  If you update this after someone has loaded the page, they will receive the new announcement within 1 minute replacing the existing one.
---
+++ packages/OctoPrint-HeaderAnnouncements/OctoPrint-HeaderAnnouncements-0.1.1.tar.gz/OctoPrint-HeaderAnnouncements-0.1.1/octoprint_HeaderAnnouncements/static/js/HeaderAnnouncements.js
@@ -0,0 +1,50 @@
+/*
+ * View model for OctoPrint-HeaderAnnouncements
+ *
+ * Author: Sean Smith
+ * License: MIT
+ */
+$(function () {
+    function HeaderannouncementsViewModel(parameters) {
+        let self = this
+        const announcement = document.getElementById('header_announcement')
+        const announcement_msg = document.getElementById('header_announcement_msg')
+        const navbar = document.getElementById('navbar')
+        navbar.after(announcement)
+
+        self.settings = parameters[0]
+        self.settings.onStartupComplete = function () {
+            const text = self.settings.settings.plugins.HeaderAnnouncements.announcementText()
+            if (text !== "") {
+                announcement_msg.innerHTML = text
+                announcement.style.display = "flex"
+            }
+        }
+
+        self.onDataUpdaterPluginMessage = function (plugin, data) {
+            if (plugin !== "HeaderAnnouncements") {
+                return
+            }
+            console.log(data)
+            const text = data.announcementText
+            announcement_msg.innerHTML = text
+            if (text !== "") {
+                announcement.style.display = "flex"
+            } else {
+                announcement.style.display = "none"
+            }
+        }
+    }
+
+    /* view model class, parameters for constructor, container to bind to
+     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
+     * and a full list of the available options.
+     */
+    OCTOPRINT_VIEWMODELS.push({
+        construct: HeaderannouncementsViewModel,
+        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
+        dependencies: ["settingsViewModel"],
+        // Elements to bind to, e.g. #settings_plugin_HeaderAnnouncements, #tab_plugin_HeaderAnnouncements, ...
+        elements: [ /* ... */]
+    });
+});
