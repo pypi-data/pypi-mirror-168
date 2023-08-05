968984b8ca8c34f048908437839dbf633fe33199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:40 +0000

    Add package superior6564/0.0.8/superior6564-0.0.8.tar.gz

---
+++ packages/superior6564/superior6564-0.0.8.tar.gz/superior6564-0.0.8/superior6564/__init__.py
@@ -0,0 +1,20 @@
+"""
+:authors: Superior_6564
+:license: Apache License, Version 2.0, see LICENSE file
+:copyright: (c) 2022 Superior_6564
+"""
+
+
+# import degget
+# import games_help
+# import packages
+
+
+# from degget import show
+# from games_help import gen_ru_words
+# from packages import install, pip_upgrade, required
+
+
+__author__ = 'Superior_6564'
+__version__ = '0.0.8'
+__email__ = 'vip.klochayil@gmail.com'
