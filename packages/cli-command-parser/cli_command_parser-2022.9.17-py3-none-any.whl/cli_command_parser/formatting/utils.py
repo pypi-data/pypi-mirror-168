acd772035e1f2101260be2374c834351ba320788
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:21 +0000

    Add package cli-command-parser/2022.9.17/cli_command_parser-2022.9.17-py3-none-any.whl

---
+++ packages/cli-command-parser/cli_command_parser-2022.9.17-py3-none-any.whl/cli_command_parser/formatting/utils.py
@@ -0,0 +1,130 @@
+"""
+Utils for usage / help text formatters
+
+:author: Doug Skrypa
+"""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Optional, Any, Collection, Sequence, Iterator, Iterable, Tuple, List
+
+try:
+    from wcwidth import wcswidth
+except ImportError:
+    wcswidth = len
+
+from ..compat import WCTextWrapper
+from ..config import ShowDefaults
+from ..context import ctx
+from ..utils import _NotSet
+
+if TYPE_CHECKING:
+    from ..typing import Bool, Strs, OptStrs
+
+__all__ = ['format_help_entry', 'line_iter']
+
+
+def format_help_entry(
+    usage: Strs,
+    description: OptStrs,
+    lpad: int = 2,
+    tw_offset: int = 0,
+    prefix: str = '',
+    cont_indent: int = 2,
+) -> str:
+    config = ctx.config
+    usage_width = max(config.min_usage_column_width, config.usage_column_width - tw_offset - 2)
+    after_pad_width = usage_width - lpad
+    term_width = ctx.terminal_width - tw_offset
+    pad_prefix = prefix + ' ' * (lpad - len(prefix)) if prefix else ' ' * lpad
+
+    usage = tuple(
+        _indented(_norm_column((usage,) if isinstance(usage, str) else usage, term_width, cont_indent), cont_indent)
+    )
+    if not description:
+        return '\n'.join(f'{pad_prefix}{line}' for line in usage)
+
+    description_lines = [''] * _description_start_line(usage, after_pad_width)
+    description_lines.extend(_norm_column(_single_line_strs(description), term_width - usage_width - 2))
+    format_row = f'{pad_prefix}{{:<{after_pad_width}s}}  {{}}'.format
+    return '\n'.join(format_row(*row).rstrip() for row in line_iter((usage, description_lines)))
+
+
+def _description_start_line(usage: Iterable[str], max_usage_width: int) -> int:
+    widths = [wcswidth(line) for line in usage]
+    if max(widths, default=0) <= max_usage_width:
+        return 0
+
+    widths.reverse()
+    line = len(widths)
+    for width in widths:
+        if width > max_usage_width:
+            break
+        line -= 1
+    return line
+
+
+def _single_line_strs(lines: Strs) -> List[str]:
+    if isinstance(lines, str):
+        lines = (lines,)
+    return [line for full_line in lines for line in full_line.splitlines()]
+
+
+def _norm_column(lines: Sequence[str], column_width: int, cont_indent: int = 0) -> Sequence[str]:
+    max_width = max(map(wcswidth, lines)) + cont_indent
+    if max_width <= column_width:
+        return lines
+
+    tw = WCTextWrapper(column_width, break_long_words=True, break_on_hyphens=True)
+    fixed = []
+    for line in lines:
+        if wcswidth(line) + cont_indent >= column_width:
+            fixed.extend(tw.wrap(line))
+        else:
+            fixed.append(line)
+
+    return fixed
+
+
+def _indented(lines: Iterable[str], cont_indent: int = 2) -> Iterator[str]:
+    i_lines = iter(lines)
+    yield next(i_lines)  # pylint: disable=R1708
+    prefix = ' ' * cont_indent
+    for line in i_lines:
+        yield prefix + line
+
+
+def _should_add_default(default: Any, help_text: Optional[str], param_show_default: Optional[Bool]) -> bool:
+    if default is _NotSet:
+        return False
+    elif param_show_default is not None:
+        return param_show_default
+    sd = ctx.config.show_defaults
+    if sd._value_ < 2 or (sd & ShowDefaults.MISSING and help_text and 'default:' in help_text):
+        return False
+    elif sd & ShowDefaults.ANY:
+        return True
+    elif sd & ShowDefaults.NON_EMPTY:
+        return bool(default) or not (default is None or isinstance(default, Collection))
+    else:
+        return bool(default)
+
+
+def line_iter(columns: Sequence[Strs]) -> Iterator[Tuple[str, ...]]:
+    """More complicated than what would be necessary for just 2 columns, but this will scale to handle 3+"""
+    exhausted = 0
+    column_count = len(columns)
+
+    def _iter(column: Strs) -> Iterator[str]:
+        nonlocal exhausted
+        yield from column.splitlines() if isinstance(column, str) else column
+        exhausted += 1
+        while True:
+            yield ''
+
+    column_iters = tuple(_iter(c) for c in columns)
+    while True:
+        row = tuple(next(ci) for ci in column_iters)  # pylint: disable=R1708
+        if exhausted == column_count:  # `while exhausted < column_count:` always results in 1 extra row
+            break
+        yield row
---
+++ packages/cli-command-parser/cli_command_parser-2022.9.17-py3-none-any.whl/cli_command_parser/utils.py
@@ -0,0 +1,213 @@
+"""
+Utilities for extracting types from annotations, finding / storing program metadata, and other misc utilities.
+
+:author: Doug Skrypa
+"""
+
+from __future__ import annotations
+
+from collections.abc import Collection, Iterable
+from enum import Flag
+from inspect import isclass
+from shutil import get_terminal_size
+from time import monotonic
+from typing import Any, Callable, Union, Optional, TypeVar, get_type_hints, List
+
+try:
+    from typing import get_origin, get_args as _get_args  # pylint: disable=C0412
+except ImportError:  # Added in 3.8; the versions from 3.8 are copied here
+    from .compat import get_origin, _get_args
+
+try:
+    from types import NoneType
+except ImportError:  # Added in 3.10
+    NoneType = type(None)
+
+from .compat import decompose_flag, missing_flag
+
+FlagEnum = TypeVar('FlagEnum', bound='FixedFlag')
+_NotSet = object()
+
+
+# region Text Processing / Formatting
+
+
+def camel_to_snake_case(text: str, delim: str = '_') -> str:
+    return ''.join(f'{delim}{c}' if i and c.isupper() else c for i, c in enumerate(text)).lower()
+
+
+def short_repr(obj: Any, max_len: int = 100, sep: str = '...', func: Callable[[Any], str] = repr) -> str:
+    obj_repr = func(obj)
+    if len(obj_repr) > max_len:
+        part_len = (max_len - len(sep)) // 2
+        return '{}{}{}'.format(obj_repr[:part_len], sep, obj_repr[-part_len:])
+    return obj_repr
+
+
+def _parse_tree_target_repr(target) -> str:
+    try:
+        return target.__name__
+    except AttributeError:
+        return repr(target)
+
+
+# endregion
+
+# region Annotation Inspection
+
+
+def get_descriptor_value_type(command_cls: type, attr: str) -> Optional[type]:
+    try:
+        annotation = get_type_hints(command_cls)[attr]
+    except KeyError:
+        return None
+
+    return get_annotation_value_type(annotation)
+
+
+def get_annotation_value_type(annotation) -> Optional[type]:
+    origin = get_origin(annotation)
+    # Note on get_origin return values:
+    # get_origin(List[str]) -> list
+    # get_origin(List) -> list
+    # get_origin(list[str]) -> list
+    # get_origin(list) -> None
+    if origin is None and isinstance(annotation, type):
+        return annotation
+    elif isclass(origin) and issubclass(origin, (Collection, Iterable)):
+        return _type_from_collection(origin, annotation)
+    elif origin is Union:
+        return _type_from_union(annotation)
+    return None
+
+
+def get_args(annotation) -> tuple:
+    """
+    Wrapper around :func:`python:typing.get_args` for 3.7~8 compatibility, to make it behave more like it does in 3.9+
+    """
+    if getattr(annotation, '_special', False):  # 3.7-3.8 generic collection alias with no content types
+        return ()
+    return _get_args(annotation)
+
+
+def _type_from_union(annotation) -> Optional[type]:
+    args = get_args(annotation)
+    # Note: Unions of a single argument return the argument; i.e., Union[T] returns T, so the len can never be 1
+    if len(args) == 2 and NoneType in args:
+        arg = args[0] if args[1] is NoneType else args[1]
+    else:
+        return None
+
+    origin = get_origin(arg)
+    if origin is None and isinstance(arg, type):
+        return arg
+    elif isclass(origin) and issubclass(origin, (Collection, Iterable)):
+        return _type_from_collection(origin, arg)
+    else:
+        return None
+
+
+def _type_from_collection(origin, annotation) -> Optional[type]:
+    args = get_args(annotation)
+    try:
+        annotation = args[0]
+    except IndexError:  # The annotation was a collection with no content types specified
+        return origin
+
+    n_args = len(args)
+    if n_args > 1 and (origin is not tuple or not ((n_args == 2 and args[1] is Ellipsis) or len(set(args)) == 1)):
+        return None
+
+    origin = get_origin(annotation)
+    if origin is None and isinstance(annotation, type):
+        return annotation
+    elif origin is Union:
+        return _type_from_union(annotation)
+    else:
+        return None
+
+
+# endregion
+
+
+class MissingMixin:
+    @classmethod
+    def _missing_(cls, value):
+        if isinstance(value, str):
+            try:
+                return cls._member_map_[value.upper()]  # noqa
+            except KeyError:
+                pass
+        return super()._missing_(value)  # noqa
+
+
+class FixedFlag(Flag):
+    """Extends Flag to work around breaking changes in 3.11 for repr, missing, and pseudo-members."""
+
+    def __repr__(self) -> str:
+        # In 3.11, this needs to be declared in the parent of a Flag that actually has members - it breaks if it is
+        # defined in a mixin or the class with members.
+        names = '|'.join(part._name_ for part in self._decompose())
+        return f'<{self.__class__.__name__}:{names}>'
+
+    @classmethod
+    def _missing_(cls, value) -> FlagEnum:
+        if isinstance(value, str):
+            if value.startswith(('!', '~')):
+                invert = True
+                value = value[1:]
+            else:
+                invert = False
+
+            try:
+                member = cls._missing_str(value)
+            except KeyError:
+                expected = ', '.join(cls._member_map_)
+                raise ValueError(f'Invalid {cls.__name__} value={value!r} - expected one of {expected}') from None
+            else:
+                return ~member if invert else member  # pylint: disable=E1130
+
+        return missing_flag(cls, value)
+
+    @classmethod
+    def _missing_str(cls, value: str) -> FlagEnum:
+        try:
+            return cls._member_map_[value.upper()]  # noqa
+        except KeyError:
+            pass
+        if '|' in value:
+            tmp = cls(0)
+            for part in map(str.strip, value.split('|')):
+                if not part:
+                    continue
+                try:
+                    tmp |= cls._member_map_[part.upper()]
+                except KeyError:
+                    break
+            else:
+                if tmp._value_ != 0:
+                    return tmp
+
+        raise KeyError
+
+    def _decompose(self) -> List[FlagEnum]:
+        return decompose_flag(self.__class__, self._value_, self._name_)[0]
+
+    def __lt__(self, other: FlagEnum) -> bool:
+        return self._value_ < other._value_
+
+
+class Terminal:  # pylint: disable=R0903
+    __slots__ = ('_cache_time', '_last_time', '_width')
+
+    def __init__(self, cache_time: float = 1):
+        self._cache_time = cache_time
+        self._last_time = 0
+        self._width = 80
+
+    @property
+    def width(self) -> int:
+        if monotonic() - self._last_time > self._cache_time:
+            self._width = get_terminal_size()[0]
+            self._last_time = monotonic()
+        return self._width
