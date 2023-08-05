2eb68d6bcabfb96df3df158d7648f42164448ce0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:43 +0000

    Add package django-qiyu-utils/0.4.4/django_qiyu_utils-0.4.4-py3-none-any.whl

---
+++ packages/django-qiyu-utils/django_qiyu_utils-0.4.4-py3-none-any.whl/django_qiyu_utils/http_header.py
@@ -0,0 +1,22 @@
+"""
+从 HTTP Header 中获取的信息
+"""
+import warnings
+from typing import Optional
+
+from django.http import HttpRequest
+
+__all__ = ["get_client_ip"]
+
+
+def get_client_ip(request: HttpRequest) -> Optional[str]:
+    """
+    获取用户的 IP 地址
+    """
+    # https://github.com/un33k/django-ipware
+    warnings.warn("use django-ipware plugin replace this", DeprecationWarning)
+
+    x: str = request.META.get("HTTP_X_FORWARDED_FOR", None)
+    if x:
+        return x.split(",")[0]
+    return request.META.get("REMOTE_ADDR", None)
