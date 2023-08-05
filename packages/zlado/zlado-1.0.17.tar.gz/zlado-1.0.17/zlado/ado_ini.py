c9559faa45bfdd94109f601e9abd24499f821aac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:55 +0000

    Add package zlado/1.0.17/zlado-1.0.17.tar.gz

---
+++ packages/zlado/zlado-1.0.17.tar.gz/zlado-1.0.17/zlado/ado_ini.py
@@ -0,0 +1,27 @@
+# -*-coding=utf-8-*-
+
+import os
+import ConfigParser
+
+ini_file = os.path.join(os.path.expanduser('~'), '.ado/config.ini')
+ini_file_dir=os.path.dirname(ini_file)
+
+if not os.path.exists(ini_file_dir):
+    os.makedirs(ini_file_dir)
+
+if not os.path.exists(ini_file):
+    cfg = ConfigParser.RawConfigParser()
+    cfg.add_section('default')
+    # cfg.set('default','accesskeyid','')
+    cfg.set('default','accesskeyid',input('请输入accesskeyid:'))
+    cfg.set('default','accesskeysecret',input('accesskeysecret:'))
+    cfg.set('default','regionids',input('请输入regionids(","分隔):'))
+    print('需要在配置文件中设置相应值,配置文件：',ini_file)
+    with open (ini_file,'+w') as f:
+        cfg.write(f)
+
+def read_cfg():
+    cfg = ConfigParser.RawConfigParser()
+    cfg.read(ini_file)
+    return cfg
+
