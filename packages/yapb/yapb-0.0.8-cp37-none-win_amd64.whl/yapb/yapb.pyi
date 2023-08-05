ffe3a5475073956c57023a926f69564299e27167
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:17 +0000

    Add package yapb/0.0.8/yapb-0.0.8-cp37-none-win_amd64.whl

---
+++ packages/yapb/yapb-0.0.8-cp37-none-win_amd64.whl/yapb/yapb.pyi
@@ -0,0 +1,30 @@
+from typing import Optional
+
+class ProgressBar:
+    length: int
+    current: int
+    message: str
+    description: str
+    character: str
+    head: str
+    done: bool
+    max_length: int
+
+    def __init__(
+        self,
+        length: int,
+        message: str,
+        description: str,
+        max_length: Optional[int] = 88,
+    ) -> None:
+        """Initialize the progress bar."""
+        ...
+    def get_length(self) -> int:
+        """Get the length of the progress bar."""
+        ...
+    def update(self, message: Optional[str], description: Optional[str]) -> None:
+        """Update the progress bar."""
+        ...
+    def render(self) -> None:
+        """Render the progress bar."""
+        ...
