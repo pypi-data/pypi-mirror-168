871a8c3b327deb7b799674ec972f457284ca2367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:45 +0000

    Add package pyettj/0.2.2/pyettj-0.2.2.tar.gz

---
+++ packages/pyettj/pyettj-0.2.2.tar.gz/pyettj-0.2.2/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# _*_ coding:utf-8 _*_
+from setuptools import setup
+import os
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(this_directory, 'README_pypi.md'), encoding="utf-8") as f:
+    long_description = f.read()
+
+with open(os.path.join(this_directory,'version.py'), encoding="utf-8") as f:
+    version = f.read()
+
+version = version.split("=")[-1].split("print")[0].replace('"','').strip()
+
+packages = ['pyettj']
+
+package_data = \
+{'': ['*'],
+ 'pyettj': ['exemplo/*', 'media/*']}
+
+install_requires = \
+['beautifulsoup4>=4.9.3,<5.0.0',
+ 'lxml>=4.6.3,<5.0.0',
+ 'matplotlib>=3.4.2,<4.0.0',
+ 'pandas>=1.2.4,<2.0.0',
+ 'requests>=2.25.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'pyettj',
+    'version': version,
+    'description': '"Capturar dados das estruturas a termo de algumas taxas de juros (ettj) brasileiras."',
+    'long_description':long_description,
+    'long_description_content_type':'text/markdown',
+    'author': 'Rafael Rodrigues',
+    'author_email': 'rafael.rafarod@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/pyettj/pyettj-0.2.2.tar.gz/pyettj-0.2.2/tests/test_ettj.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+import pytest, sys
+
+sys.path.append("./pyettj/")
+sys.path.append("./pyettj/pyettj/")
+
+from ettj import get_ettj, plot_ettj, listar_dias_uteis
+from modelo_ettj import get_ettj_anbima, svensson
+
+data = "2021/05/18"
+
+class TestClass():
+
+    def test_ettj(self):
+        self.ettj_dataframe = get_ettj(data)
+        self.curvas = self.ettj_dataframe.columns.tolist()[1:]
+        self.curva = self.curvas[2] #selic
+        return self.ettj_dataframe, self.curva
+
+    def test_listar_dias_uteis(self):
+        de = "2021/05/13"
+        ate = data
+        datas=listar_dias_uteis(de, ate)
+        with pytest.raises(Exception) as error4:
+            de = 12
+            ate = "2021/05/13"
+            datas=listar_dias_uteis(de, ate)
+        assert str(error4.value) == "O parametro data deve ser em formato string, exemplo: '18/05/2021'"
+        with pytest.raises(Exception) as error5:
+            de = "13/05/2021"
+            ate = 34
+            datas=listar_dias_uteis(de, ate)
+        assert str(error5.value) == "O parametro data deve ser em formato string, exemplo: '18/05/2021'"
+
+    def test_plot_ettj(self):
+        self.ettj_dataframe, self.curva = self.test_ettj()
+        plot_ettj(self.ettj_dataframe.drop(self.ettj_dataframe.columns[0],axis=1), self.curva, data)
+
+    def test_raises(self):
+        with pytest.raises(Exception) as error1:
+            get_ettj(18)
+        assert str(error1.value) == "O parametro data deve ser em formato string, exemplo: '18/05/2021'"
+        with pytest.raises(Exception) as error2:
+            get_ettj("teste")
+        assert str(error2.value) == "O parametro data deve ser em formato string, exemplo: '18/05/2021'" 
+
+    def test_svensson(self):
+        taxa = svensson(0.13, 0.03, 0.03, 0.03, 1.97, 0.93, 1/252)
+        assert round(taxa,2) == 0.16
+
+    def get_ettj_anbima(self):
+        data = "15/09/2022"
+        parametros_curva, ettj, taxa, erros = get_ettj_anbima(data)
+        assert not parametros_curva.empty
+        assert not ettj.empty
+        assert not taxa.empty
+        assert not erros.empty
+
+    def test_ettj2(self):
+        data = "15/09/2022"
+        curva = "pre"
+        ettj_pre = get_ettj(data, curva)
+        assert not ettj_pre.empty
+
+TestClass()
