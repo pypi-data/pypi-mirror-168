7b5587bd2779413bad97a1142701ea5aff73ed98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:35 +0000

    Add package pwnvasive/0.1/pwnvasive-0.1.tar.gz

---
+++ packages/pwnvasive/pwnvasive-0.1.tar.gz/pwnvasive-0.1/setup.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name = 'pwnvasive',
+    version = '0.1',
+    packages=['pwnvasive'],
+    scripts = [ 'bin/pwnvasive' ],
+
+    # Metadata
+    author = 'Philippe BIONDI',
+    author_email = 'phil@secdev.org',
+    description = 'Semi-automatic discovery and lateralization tool',
+    install_requires = [
+        "asyncssh",
+        "aiocmd",
+        "graphviz",
+        "bcrypt",
+    ],
+    license = 'GPLv2',
+    # keywords = '',
+
+)
