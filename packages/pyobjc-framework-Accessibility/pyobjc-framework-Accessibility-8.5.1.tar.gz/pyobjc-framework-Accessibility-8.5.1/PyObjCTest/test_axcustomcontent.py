90e1f35fc48ca84e9400ebf16352072570140405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:55 +0000

    Add package pyobjc-framework-Accessibility/8.5.1/pyobjc-framework-Accessibility-8.5.1.tar.gz

---
+++ packages/pyobjc-framework-Accessibility/pyobjc-framework-Accessibility-8.5.1.tar.gz/pyobjc-framework-Accessibility-8.5.1/PyObjCTest/test_axcustomcontent.py
@@ -0,0 +1,16 @@
+from PyObjCTools.TestSupport import TestCase
+
+import Accessibility
+import objc
+
+
+class TestAXCustomContent(TestCase):
+    def test_enum_types(self):
+        self.assertIsEnumType(Accessibility.AXCustomContentImportance)
+
+    def test_constants(self):
+        self.assertEqual(Accessibility.AXCustomContentImportanceDefault, 0)
+        self.assertEqual(Accessibility.AXCustomContentImportanceHigh, 1)
+
+    def test_protocols(self):
+        objc.protocolNamed("AXCustomContentProvider")
