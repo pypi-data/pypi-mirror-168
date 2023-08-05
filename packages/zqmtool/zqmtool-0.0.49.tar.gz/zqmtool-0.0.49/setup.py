e0e45b70313f53ad25c9c563402eaa6f3d7627b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:38 +0000

    Add package zqmtool/0.0.49/zqmtool-0.0.49.tar.gz

---
+++ packages/zqmtool/zqmtool-0.0.49.tar.gz/zqmtool-0.0.49/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+    
+setup(
+    name="zqmtool",
+    version="0.0.49",
+    author="qiming_zou",
+    author_email="zqm1028@gmail.com",
+    description="qiming zou's toolbox",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # url=" ,
+    # project_urls=\{\},
+    # package_dir={"": "src/"},
+    packages=find_packages('src'),
+    # ext_modules=cythonize(["src/zqmtool/*.py"]),
+    python_requires=">=3.6",
+)
