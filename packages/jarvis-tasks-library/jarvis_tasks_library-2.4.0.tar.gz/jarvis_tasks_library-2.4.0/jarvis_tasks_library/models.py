3a636e0af376800701b20c90ab11a8598abbade9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:54 +0000

    Add package jarvis-tasks-library/2.4.0/jarvis_tasks_library-2.4.0.tar.gz

---
+++ packages/jarvis-tasks-library/jarvis_tasks_library-2.4.0.tar.gz/jarvis_tasks_library-2.4.0/jarvis_tasks_library/models.py
@@ -0,0 +1,13 @@
+from typing import Optional
+
+from pydantic import BaseModel  # pylint:disable=no-name-in-module
+
+
+class Task(BaseModel):
+    id: Optional[str]
+    name: Optional[str]
+    time: Optional[str]
+    active: Optional[bool]
+    is_one_time: Optional[bool]
+    notify: Optional[bool]
+    weekdays: str = '0123456'
