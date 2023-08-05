79281f894b8afa7914ec8d5a4df5305aa2ea1a93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:29 +0000

    Add package measuresoftgram/3.1.6/measuresoftgram-3.1.6.tar.gz

---
+++ packages/measuresoftgram/measuresoftgram-3.1.6.tar.gz/measuresoftgram-3.1.6/src/clients/service_client.py
@@ -0,0 +1,35 @@
+import requests
+from requests.adapters import HTTPAdapter, Retry
+
+
+class ServiceClient:
+    @staticmethod
+    def configure_session(session):
+        errors = [500, 501, 502, 503, 504, 505, 506, 507, 508, 509, 510, 511]
+        retries = Retry(total=5, backoff_factor=5, status_forcelist=errors)
+        # session.mount("http://", HTTPAdapter(max_retries=retries))
+        session.mount("https://", HTTPAdapter(max_retries=retries))
+
+    @staticmethod
+    def make_get_request(url):
+        with requests.Session() as session:
+            ServiceClient.configure_session(session)
+            return session.get(url)
+
+    @staticmethod
+    def make_post_request(url, payload):
+        with requests.Session() as session:
+            ServiceClient.configure_session(session)
+            return session.post(url, json=payload)
+
+    @staticmethod
+    def get_entity(url):
+        return ServiceClient.make_get_request(url)
+
+    @staticmethod
+    def import_file(url, payload):
+        return ServiceClient.make_post_request(url, payload)
+
+    @staticmethod
+    def calculate_entity(url, payload):
+        return ServiceClient.make_post_request(url, payload)
---
+++ packages/measuresoftgram/measuresoftgram-3.1.6.tar.gz/measuresoftgram-3.1.6/src/config/settings.py
@@ -0,0 +1,26 @@
+import os
+
+SERVICE_URL = os.getenv("SERVICE_URL", "https://measuresoftgram-service.herokuapp.com/")
+
+BASE_URL = "http://172.20.0.2:5000/"
+
+AVAILABLE_ENTITIES = [
+    "metrics",
+    "measures",
+    "subcharacteristics",
+    "characteristics",
+    # "sqc",
+]
+
+SUPPORTED_FORMATS = [
+    "json",
+    "tabular",
+]
+
+AVAILABLE_IMPORTS = [
+    "sonarqube"
+]
+
+AVAILABLE_GEN_FORMATS = [
+    "csv"
+]
