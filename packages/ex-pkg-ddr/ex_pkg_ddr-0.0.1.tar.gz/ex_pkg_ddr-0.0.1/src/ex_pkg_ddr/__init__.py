7847bb9bca077d9e2960b82c31e2c6120466a216
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:37 +0000

    Add package ex-pkg-ddr/0.0.1/ex_pkg_ddr-0.0.1.tar.gz

---
+++ packages/ex-pkg-ddr/ex_pkg_ddr-0.0.1.tar.gz/ex_pkg_ddr-0.0.1/src/ex_pkg_ddr/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 16 23:09:12 2022
+
+@author: GTCRL
+"""
+
---
+++ packages/ex-pkg-ddr/ex_pkg_ddr-0.0.1.tar.gz/ex_pkg_ddr-0.0.1/src/ex_pkg_ddr/examples.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 16 23:09:54 2022
+
+@author: GTCRL
+"""
+
+def add_one(number):
+    return number + 1
