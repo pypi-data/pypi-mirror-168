39aee83bf85bf5be05e66d332900ad10ab82d435
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:57 +0000

    Add package igwrapper/0.1.3/igwrapper-0.1.3.tar.gz

---
+++ packages/igwrapper/igwrapper-0.1.3.tar.gz/igwrapper-0.1.3/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['igwrapper', 'igwrapper.clients', 'igwrapper.models']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Brotli>=1.0.9,<2.0.0',
+ 'aiodns>=3.0.0,<4.0.0',
+ 'aiohttp>=3.8.1,<4.0.0',
+ 'lightstreamer-client-lib>=1.0.0b2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'igwrapper',
+    'version': '0.1.3',
+    'description': '',
+    'long_description': '',
+    'author': 'sohaib94',
+    'author_email': 'sohaib.ashraf94@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
