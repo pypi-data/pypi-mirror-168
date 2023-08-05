f92b7d63f6e74a0003d9a66e173a8778340747aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:54 +0000

    Add package leopardo/0.0.4/leopardo-0.0.4.tar.gz

---
+++ packages/leopardo/leopardo-0.0.4.tar.gz/leopardo-0.0.4/leopardo/__main__.py
@@ -0,0 +1,14 @@
+# ------------------------------------------------------------------------------
+#  leopardo [Configurable fast-access toolset]
+#  (C) 2022. A. Shavykin <0.delameter@gmail.com>
+# ------------------------------------------------------------------------------
+
+from .app import App
+
+
+def main():
+    (App()).run()
+
+
+if __name__ == '__main__':
+    main()
