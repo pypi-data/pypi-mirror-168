3a92a9caebbaee9f2af73e3f95cf0d9d1bcca531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:32 +0000

    Add package pyobjc-framework-Virtualization/8.5.1/pyobjc-framework-Virtualization-8.5.1.tar.gz

---
+++ packages/pyobjc-framework-Virtualization/pyobjc-framework-Virtualization-8.5.1.tar.gz/pyobjc-framework-Virtualization-8.5.1/PyObjCTest/test_vzmacosrestoreimage.py
@@ -0,0 +1,19 @@
+from PyObjCTools.TestSupport import TestCase, min_os_level, arch_only
+
+import Virtualization
+
+
+class TestVZMacOSRestoreImage(TestCase):
+    @min_os_level("12.0")
+    @arch_only("arm64")
+    def test_methods(self):
+        self.assertArgIsBlock(
+            Virtualization.VZMacOSRestoreImage.loadFileURL_completionHandler_,
+            1,
+            b"v@@",
+        )
+        self.assertArgIsBlock(
+            Virtualization.VZMacOSRestoreImage.fetchLatestSupportedWithCompletionHandler_,
+            0,
+            b"v@@",
+        )
