fdfe3e50c220134c2e128bdee0d1ab4083367f60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:30 +0000

    Add package fourequalsten-hint/1.0.2/fourequalsten_hint-1.0.2.tar.gz

---
+++ packages/fourequalsten-hint/fourequalsten_hint-1.0.2.tar.gz/fourequalsten_hint-1.0.2/README.md
@@ -0,0 +1,103 @@
+# 4=10's Solution Generator
+
+***by Cheewy***
+
+## Description : 
+
+This tiny program in Python allows you to quickly and simply calculate every possible solutions to the famous *4=10* game.</br>
+I had a lot of fun developping it, I hope you will enjoy it.</br>
+
+*Note : The code don't have comments, but if you want some, just send me an email and I will do it.*
+
+## DISCLAIMER BEFORE USING IT
+
+*Use this program sparingly, or only when you are **REALLY** stuck and you have no more daily solution left.*</br>
+*Using this program too much will ruin your entire game experience, and will be very insulting to the game developpers.*</br>
+*I am **NOT** responsible for any bad game experience.*</br>
+
+## Installation :
+
+**1. Use pip to install the package**
+
+For Unix/macOS :
+```
+python3 -m pip install fourequalsten_hint
+```
+For Windows :
+```
+py -m pip install fourequalsten_hint
+```
+**2. Import the package in your program</br>**
+
+Code to write down in your file *before using the commands of the package* :</br>
+```
+import fourequalsten_hint as feth
+```
+
+## Utilisation Guide :
+
+**If you just want the answers :**
+
+1. Create a python file anywhere on your computer (ex: ```test.py```)
+2. In this file, write down the following code :
+```
+import fourequalsten_hint as feth
+feth.fancy_solve()
+```
+3. Execute the program and follow the instructions in the terminal</br></br>
+
+**If you want to use the functions in your program :**
+
+```
+feth.solve(input_nb,input_op,all_solutions)
+```
+
+*Note : Documentation of the functions are right underneath*
+
+## Documentation
+- ```feth.solve(input_nb,input_op,all_solutions)```</br></br>
+  The ```solve()``` function takes 3 parameters :
+    - **input_nb** (str) : String made of 4 digits (the ones in the enigma)</br>
+      *Ex :* ```"1234"```</br></br>
+    - **input_op** (str) : String made of the banned operators</br>
+        Working chars :
+        - '+' for plus
+        - '-' for minus
+        - '*' for multiplication
+        - '/' for division
+        - Empty string for none : ""</br>
+      *Ex :* ```"+"```, ```"-+"```, ```""```</br></br>
+    - **all_solutions** (int) : 0 or 1 depending of the informations you want in return</br>
+        - 0 : Returns one possible solution in the form of a string</br>
+          *Ex :* ```"(1+2+3)+4"```</br>
+        - 1 : Returns every possible solutions in the form of a list of strings</br>
+          *Ex :* ```["(1+2+3)+4", ... , "4+3+2+1"]```
+      
+  *Note : If there's no result, the function will return an empty list*</br></br>
+  
+- ```feth.fancy_solve()```</br></br>
+  The ```fancy_solve()``` function takes no parameters, and will return nothing.</br>
+  It is made of multiple affordable inputs, prints and os.clear to give to the user a casual and simple use of the program.
+  
+## License
+
+[MIT License](https://github.com/CheewyOFF/fourequalsten_hint/blob/main/LICENSE)
+
+## Credits :
+
+### Fourequalsten_hint
+
+Developper : Cheewy</br>
+Email : cheewy.perso@gmail.com
+
+### 4=10
+
+Developper : Sveinn Steinarsson</br>
+Website : [fourequalsten.app](https://fourequalsten.app)</br>
+Email : fourequalsten@fourequalsten.app</br>
+Youtube Channel : [svenstone](https://www.youtube.com/user/svenstone)</br>
+
+***Buy Sveinn a Coffee ♥ :*** [Click here](https://www.buymeacoffee.com/steinarsson)
+
+Download game on Google Play Store : [Click here](https://play.google.com/store/apps/details?id=app.fourequalsten.fourequalsten_app&hl=en_US&gl=US")</br>
+Download game on Apple Play Store : [Click here](https://apps.apple.com/us/app/4-10/id1609871477)
---
+++ packages/fourequalsten-hint/fourequalsten_hint-1.0.2.tar.gz/fourequalsten_hint-1.0.2/src/fourequalsten_hint/feth.py
@@ -0,0 +1,417 @@
+def fancy_solve():
+    import os
+
+    missing_operator = []
+    current_nb = []
+
+    cont = "1"
+    while(cont == "1"):
+        os.system('cls')
+        input_str = str(input("Enter the 4 enigma's digits : "))
+        while(decompose(input_str,0) == []):
+            input_str = str(input("Enter the 4 enigma's digits : "))
+
+        tmp_digits = input_str
+        current_nb = decompose(input_str,0)
+
+        input_str = str(input("Enter the banned operators (Press Enter if there's none) : "))
+        while(decompose(input_str,1) == []):
+            input_str = str(input("Enter the banned operators (Press Enter if there's none) : "))
+
+        tmp_banop = input_str
+        missing_operator = decompose(input_str,1)
+
+        operator_list = ["+","-","*","/"]
+
+        if missing_operator != ["none"]:
+            for i in missing_operator:
+                operator_list.remove(i)
+
+        for k in range(len(current_nb)):
+            current_nb[k] = int(current_nb[k])
+        pattern = {"par_ou1" : 0, "num1" : -1, "op1" : 0, "par_ou2" : 0, "num2" : -1, "par_fe1" : 0, "op2" : 0, "par_ou3" : 0, "num3"  : -1, "par_fe2" : 0, "op3" : 0, "num4" : -1, "par_fe3" : 0}
+
+        solution = research(pattern,current_nb,operator_list)
+        os.system('cls')
+        print("Digits : " + tmp_digits)
+        if(missing_operator == ["none"]):
+            print("Banned operators : none")
+        else:
+            print("Banned operators : " + tmp_banop)
+        if(solution == []):
+            print("No solution found. You enter wrong values or there is no solution.")
+        else:
+            print("We found [ " + str(len(solution)) + " ] solutions !")
+            if(len(solution) == 1):
+                print("Here it is :")
+                print("")
+                print(solution[0])
+            else:
+                print("Here is one : ")
+                print("")
+                print(solution[0])
+                print("")
+                print("Would you like to see all the answers ?")
+
+                display_all = str(input("(Press Enter to skip, 0 to display all possible solutions) : "))
+                while(display_all != "" and display_all != "0"):
+                    display_all = str(input("(Press Enter to skip, 0 to display all possible solutions) : "))
+
+                print("")
+                if(display_all == "0"):
+                    for k in range(len(solution)):
+                        print("Solution n°"+str(k+1)+" : "+solution[k])
+
+        print("")
+        print("")
+        print("")
+                
+        cont = str(input("Would you like to continue ? (1 for YES, 0 for NO) : "))
+        while(cont not in ["1","0"]):
+            print("Please enter either 0 or 1.")
+            cont = str(input("Would you like to continue ? (1 for YES, 0 for NO) : "))
+
+def solve(input_nb,input_op,all_solutions):
+    current_nb = decompose(input_nb,0)
+    missing_operator = decompose(input_op,1)
+
+    if(current_nb != [] and missing_operator != []):
+        operator_list = ["+","-","*","/"]
+
+        if missing_operator != ["none"]:
+            for i in missing_operator:
+                operator_list.remove(i)
+
+        for k in range(len(current_nb)):
+            current_nb[k] = int(current_nb[k])
+        pattern = {"par_ou1" : 0, "num1" : -1, "op1" : 0, "par_ou2" : 0, "num2" : -1, "par_fe1" : 0, "op2" : 0, "par_ou3" : 0, "num3"  : -1, "par_fe2" : 0, "op3" : 0, "num4" : -1, "par_fe3" : 0}
+
+        solution = research(pattern,current_nb,operator_list)
+
+        if(all_solutions == 0 and solution != []):
+            return solution[0]
+        elif(all_solutions == 1 and solution != []):
+            return solution
+        else:
+            return []
+
+def decompose(input,x):
+    number_list = ["0","1","2","3","4","5","6","7","8","9"]
+    operator_list = ["+","-","*","/"]
+    allowed_char = number_list + operator_list
+    res = []
+
+    if(type(input) != str):
+        print("ERROR : Wrong type ! You need to enter a string.")
+    else:
+        if(x == 0):
+            if(len(input) != 4):
+                print("ERROR : You need to enter 4 numbers.")
+            else:
+                inc = 1
+                valid = 1
+                for i in input:
+                    if(i not in number_list):
+                        valid = 0
+                        print("ERROR : The character n°"+str(inc)+" is invalid.")
+                    inc += 1
+                if(valid == 1):
+                    res = [input[0],input[1],input[2],input[3]]
+        elif(x == 1):
+            if(len(input)>len(operator_list)-1):
+                print("ERROR : You banned too many operators !")
+            else:
+                inc = 1
+                valid = 1
+                for i in input:
+                    if(i not in operator_list):
+                        valid = 0
+                        print("ERROR : The character n°"+str(inc)+" is invalid.")
+                    inc += 1
+                if(valid == 1):
+                    if(len(input) == 0):
+                        res = ["none"]
+                    else:
+                        for i in input:
+                            res.append(i)
+        else:
+            print("ERROR : The input is neither 4 numbers or the list of banned operators.")
+    
+    return res
+
+def research(pattern,current_nb,operator_list):
+    nb_chiffre = {}
+
+    for i in current_nb:
+        nb_chiffre[i] = current_nb.count(i)
+
+    if(len(nb_chiffre) == 4):
+        nb_db = 24
+    elif(len(nb_chiffre) == 3):
+        nb_db = [3,1,0,0]
+    elif(len(nb_chiffre) == 2):
+        if(nb_chiffre[current_nb[0]] == 2):
+            nb_db = [0,2,0,0]
+        else:
+            nb_db = [1,0,1,0]
+    elif(len(nb_chiffre) == 1):
+        nb_db = [0,0,0,1]
+
+    nb_par = 0
+    nb_signe = 0
+    nb_chiffre = 0
+    no_swap = 0
+    solution = []
+
+    operator_equ = {}
+
+    for k in range(len(operator_list)):
+        operator_equ[k] = operator_list[k]
+
+    while(nb_par <= 5):
+        nb_chiffre = 0
+        while(nb_chiffre < 24):
+            no_swap = 0
+            nb_signe = 0
+            if(nb_par == 0 and nb_signe == 0 and nb_chiffre == 0):
+                pattern["num1"] = current_nb[0]
+                pattern["num2"] = current_nb[1]
+                pattern["num3"] = current_nb[2]
+                pattern["num4"] = current_nb[3]
+            else:
+                if(nb_chiffre%6 == 0):
+                    if(pattern["num1"] != pattern["num" + str((nb_chiffre//6)+1)] or nb_chiffre == 0):
+                        pattern["num1"], pattern["num" + str((nb_chiffre//6)+1)] = pattern["num" + str((nb_chiffre//6)+1)], pattern["num1"]
+                    else:
+                        no_swap = 1
+                else:
+                    if(nb_chiffre%2 == 1):
+                        if(pattern["num3"] != pattern["num4"]):
+                            pattern["num3"], pattern["num4"] = pattern["num4"], pattern["num3"]
+                        else:
+                            no_swap = 1
+                    else:
+                        if(pattern["num2"] != pattern["num4"]):
+                            pattern["num2"], pattern["num4"] = pattern["num4"], pattern["num2"]
+                        else:
+                            no_swap = 1
+
+            while(nb_signe < len(operator_list)**3):
+                pattern["op1"] = operator_equ[nb_signe//len(operator_list)**2]
+                pattern["op2"] = operator_equ[(nb_signe//len(operator_list))%len(operator_list)]
+                pattern["op3"] = operator_equ[nb_signe%len(operator_list)]
+                if(no_swap == 0):
+                    res = calcul(pattern)
+                    if(res != "unsolved" and res != "invalid"):
+                        solution.append(res)
+                nb_signe += 1
+            nb_chiffre += 1
+        nb_par += 1
+        pattern = par_pattern(nb_par,pattern)
+
+    return solution
+
+def valid_pattern(pattern):
+    if(pattern["op1"] == 0 or pattern["op2"] == 0 or pattern["op3"] == 0):
+        res = False
+    else:
+        res = True
+
+    return res
+
+def add_par(x,y,pattern):
+    if(x in [0,1,2,3]):
+        if(x == 1):
+            pattern["par_ou1"] = 1
+            pattern["par_ou2"] = 0
+            pattern["par_ou3"] = 0
+
+            if(y == 1):
+                pattern["par_fe1"] = 1
+                pattern["par_fe2"] = 0
+                pattern["par_fe3"] = 0
+            elif(y == 2):
+                pattern["par_fe1"] = 0
+                pattern["par_fe2"] = 1
+                pattern["par_fe3"] = 0
+        elif(x == 2):
+            pattern["par_ou1"] = 0
+            pattern["par_ou2"] = 1
+            pattern["par_ou3"] = 0
+
+            if(y == 1):
+                pattern["par_fe1"] = 0
+                pattern["par_fe2"] = 1
+                pattern["par_fe3"] = 0
+            elif(y == 2):
+                pattern["par_fe1"] = 0
+                pattern["par_fe2"] = 0
+                pattern["par_fe3"] = 1
+        elif(x == 3):
+            pattern["par_ou1"] = 0
+            pattern["par_ou2"] = 0
+            pattern["par_ou3"] = 1
+
+            pattern["par_fe1"] = 0
+            pattern["par_fe2"] = 0
+            pattern["par_fe3"] = 1
+        else:
+            pattern["par_ou1"] = 0
+            pattern["par_ou2"] = 0
+            pattern["par_ou3"] = 0
+
+            pattern["par_fe1"] = 0
+            pattern["par_fe2"] = 0
+            pattern["par_fe3"] = 0
+
+    return pattern
+
+def str_calcul(pattern):
+    str_calcul = str(pattern["num1"]) + str(pattern["op1"]) + str(pattern["num2"]) + str(pattern["op2"]) + str(pattern["num3"]) + str(pattern["op3"]) + str(pattern["num4"])
+    if(pattern["par_ou1"] == 1):
+        str_calcul = "(" + str_calcul
+        if(pattern["par_fe1"] == 1):
+            str_calcul = str_calcul[:4] + ")" + str_calcul[4:]
+        else:
+            str_calcul = str_calcul[:6] + ")" + str_calcul[6:]
+    elif(pattern["par_ou2"] == 1):
+        str_calcul = str_calcul[:2] + "(" + str_calcul[2:]
+        if(pattern["par_fe2"] == 1):
+            str_calcul = str_calcul[:6] + ")" + str_calcul[6:]
+        else:
+            str_calcul = str_calcul + ")"
+    elif(pattern["par_ou3"] == 1):
+        str_calcul = str_calcul[:4] + "(" + str_calcul[4:] + ")"
+    
+    return str_calcul
+
+def calcul(pattern):
+    if(valid_pattern):
+        calcul_res = 0
+        tmp = 0
+
+        if(pattern["par_ou1"] == 1):
+            if(pattern["par_fe1"] == 1):
+                calcul_res = calcul_op(pattern["num1"],pattern["num2"],pattern["op1"])
+                if(prior_op(pattern["op2"],pattern["op3"]) == 1):
+                    calcul_res = calcul_op(calcul_res,pattern["num3"],pattern["op2"])
+                    calcul_res = calcul_op(calcul_res,pattern["num4"],pattern["op3"])
+                else:
+                    tmp = calcul_op(pattern["num3"],pattern["num4"],pattern["op3"])
+                    calcul_res = calcul_op(calcul_res,tmp,pattern["op2"])
+            else:
+                if(prior_op(pattern["op1"],pattern["op2"]) == 1):
+                    calcul_res = calcul_op(pattern["num1"],pattern["num2"],pattern["op1"])
+                    calcul_res = calcul_op(calcul_res,pattern["num3"],pattern["op2"])
+                    calcul_res = calcul_op(calcul_res,pattern["num4"],pattern["op3"])
+                else:
+                    calcul_res = calcul_op(pattern["num2"],pattern["num3"],pattern["op2"])
+                    calcul_res = calcul_op(calcul_res,pattern["num1"],pattern["op1"])
+                    calcul_res = calcul_op(calcul_res,pattern["num4"],pattern["op3"])
+        elif(pattern["par_ou2"] == 1):
+            if(pattern["par_fe2"] == 1):
+                calcul_res = calcul_op(pattern["num2"],pattern["num3"],pattern["op2"])
+                if(prior_op(pattern["op1"],pattern["op3"]) == 1):
+                    calcul_res = calcul_op(pattern["num1"],calcul_res,pattern["op1"])
+                    calcul_res = calcul_op(calcul_res,pattern["num4"],pattern["op3"])
+                else:
+                    calcul_res = calcul_op(calcul_res,pattern["num4"],pattern["op3"])
+                    calcul_res = calcul_op(pattern["num1"],calcul_res,pattern["op1"])
+            else:
+                if(prior_op(pattern["op2"],pattern["op3"]) == 1):
+                    calcul_res = calcul_op(pattern["num2"],pattern["num3"],pattern["op2"])
+                    calcul_res = calcul_op(calcul_res,pattern["num4"],pattern["op3"])
+                    calcul_res = calcul_op(pattern["num1"],calcul_res,pattern["op1"])
+                else:
+                    calcul_res = calcul_op(pattern["num3"],pattern["num4"],pattern["op3"])
+                    calcul_res = calcul_op(pattern["num2"],calcul_res,pattern["op2"])
+                    calcul_res = calcul_op(pattern["num1"],calcul_res,pattern["op1"])
+                
+        elif(pattern["par_ou3"] == 1):
+            calcul_res = calcul_op(pattern["num3"],pattern["num4"],pattern["op3"])
+            if(prior_op(pattern["op1"],pattern["op2"]) == 1):
+                tmp = calcul_op(pattern["num1"],pattern["num2"],pattern["op1"])
+                calcul_res = calcul_op(tmp,calcul_res,pattern["op2"])
+            else:
+                calcul_res = calcul_op(pattern["num2"],calcul_res,pattern["op2"])
+                calcul_res = calcul_op(pattern["num1"],calcul_res,pattern["op1"])
+        else:
+            if(prior_op(pattern["op1"],pattern["op2"]) == 1):
+                if(prior_op(pattern["op1"],pattern["op3"]) == 1):
+                    calcul_res = calcul_op(pattern["num1"],pattern["num2"],pattern["op1"])
+                    if(prior_op(pattern["op2"],pattern["op3"]) == 1):
+                        calcul_res = calcul_op(calcul_res,pattern["num3"],pattern["op2"])
+                        calcul_res = calcul_op(calcul_res,pattern["num4"],pattern["op3"])
+                    else:
+                        tmp = calcul_op(pattern["num3"],pattern["num4"],pattern["op3"])
+                        calcul_res = calcul_op(calcul_res,tmp,pattern["op2"])
+            elif(prior_op(pattern["op2"],pattern["op1"]) == 1):
+                if(prior_op(pattern["op2"],pattern["op3"]) == 1):
+                    calcul_res = calcul_op(pattern["num2"],pattern["num3"],pattern["op2"])
+                    if(prior_op(pattern["op1"],pattern["op3"]) == 1):
+                        calcul_res = calcul_op(pattern["num1"],calcul_res,pattern["op1"])
+                        calcul_res = calcul_op(calcul_res,pattern["num4"],pattern["op3"])
+                    else:
+                        calcul_res = calcul_op(calcul_res,pattern["num4"],pattern["op3"])
+                        calcul_res = calcul_op(pattern["num1"],calcul_res,pattern["op1"])
+            else:
+                if(prior_op(pattern["op1"],pattern["op2"]) == 1):
+                    tmp = calcul_op(pattern["num1"],pattern["num2"],pattern["op1"])
+                    calcul_res = calcul_op(tmp,calcul_res,pattern["op2"])
+                else:
+                    calcul_res = calcul_op(pattern["num2"],calcul_res,pattern["op2"])
+                    calcul_res = calcul_op(pattern["num1"],calcul_res,pattern["op1"])
+
+        if(calcul_res == 10):
+            res = str_calcul(pattern)
+        else:
+            res = "unsolved"
+
+    else:
+        res = "invalid"
+
+    return res
+
+def calcul_op(x,y,op):
+    if(op == "+"):
+        return x+y
+    elif(op == "-"):
+        return x-y
+    elif(op == "*"):
+        return x*y
+    elif(op == "/"):
+        if(y != 0):
+            return x/y
+        else:
+            return 9999
+    else:
+        print("ERROR : Invalid operator !")
+        return False
+
+def prior_op(op1,op2):
+    def_prior = ["*","/","-","+"]
+    if(op1 in def_prior and op2 in def_prior):
+        id_op1 = def_prior.index(op1)
+        id_op2 = def_prior.index(op2)
+
+        if(id_op1 <= id_op2):
+            return 1
+        else:
+            return 2
+    else:
+        print("ERROR : One of the two operators given are invalid !")
+        return False
+
+def par_pattern(x,pattern):
+    if(x == 1):
+        pattern = add_par(1,1,pattern)   
+    elif(x == 2):
+        pattern = add_par(1,2,pattern)
+    elif(x == 3):
+        pattern = add_par(2,1,pattern) 
+    elif(x == 4):
+        pattern = add_par(2,2,pattern) 
+    elif(x == 5):
+        pattern = add_par(3,1,pattern) 
+
+    return pattern
