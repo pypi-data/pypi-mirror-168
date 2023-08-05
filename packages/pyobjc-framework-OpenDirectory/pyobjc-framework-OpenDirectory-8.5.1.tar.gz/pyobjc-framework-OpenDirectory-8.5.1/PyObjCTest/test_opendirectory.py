050ce5f77991071e10b819189031a191bfcf6d96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:17 +0000

    Add package pyobjc-framework-OpenDirectory/8.5.1/pyobjc-framework-OpenDirectory-8.5.1.tar.gz

---
+++ packages/pyobjc-framework-OpenDirectory/pyobjc-framework-OpenDirectory-8.5.1.tar.gz/pyobjc-framework-OpenDirectory-8.5.1/PyObjCTest/test_opendirectory.py
@@ -0,0 +1,26 @@
+import OpenDirectory
+from PyObjCTools.TestSupport import TestCase
+import objc
+
+
+class TestOpenDirectory(TestCase):
+    def testConstants(self):
+        self.assertIsInstance(OpenDirectory.ODFrameworkErrorDomain, str)
+
+    def testProtocols(self):
+        objc.protocolNamed("ODQueryDelegate")
+
+    def testIntegration(self):
+        import CFOpenDirectory
+
+        for nm in dir(CFOpenDirectory):
+            with self.subTest(nm):
+                cfod = getattr(CFOpenDirectory, nm)
+                od = getattr(OpenDirectory, nm)
+
+                self.assertIs(cfod, od, f"{nm!r}, {type(cfod)}, {type(od)}")
+
+
+class TestCallableMetadata(TestCase):
+    def test_callable_metadata_is_sane(self):
+        self.assertCallableMetadataIsSane(OpenDirectory)
