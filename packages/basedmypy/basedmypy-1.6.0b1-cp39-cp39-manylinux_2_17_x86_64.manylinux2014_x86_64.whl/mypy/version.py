3ccc1b7690fd5585e4d3cd000cba671d90968504
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:36 +0000

    Add package basedmypy/1.6.0b1/basedmypy-1.6.0b1-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/basedmypy/basedmypy-1.6.0b1-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/mypy/version.py
@@ -0,0 +1,4 @@
+from mypy.versionutil import VersionInfo
+__version__ = "0.980"
+based_version_info = VersionInfo(major=1, minor=6, patch=0, release_level='beta', iteration=1, mypy_version='0.980', mypy_release_level='final')
+__based_version__ = "1.6.0b1"
