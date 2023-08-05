a3d586cf91aaa98d4a0376a6532ec3f483b2403d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:36 +0000

    Add package scikit-tabtrans/0.2/scikit-tabtrans-0.2.tar.gz

---
+++ packages/scikit-tabtrans/scikit-tabtrans-0.2.tar.gz/scikit-tabtrans-0.2/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['scikit_tab_trans']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['headers_workaround>=0.18,<0.19',
+ 'joblib>=1.2.0,<2.0.0',
+ 'pytorch-widedeep>=1.2.0,<2.0.0',
+ 'scikit-learn>=1.1.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'scikit-tabtrans',
+    'version': '0.2',
+    'description': 'TabTransformer ready for Scikit learn.',
+    'long_description': None,
+    'author': 'Christopher Lemke',
+    'author_email': 'chris@syhbl.mozmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/chrislemke/scikit-TabTrans',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<3.11',
+}
+
+
+setup(**setup_kwargs)
