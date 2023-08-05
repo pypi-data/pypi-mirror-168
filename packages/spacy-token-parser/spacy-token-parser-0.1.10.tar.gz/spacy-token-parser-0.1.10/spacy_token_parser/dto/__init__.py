e9177f27acb76db506d5a075b66ca50d4eb98af8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:45 +0000

    Add package spacy-token-parser/0.1.10/spacy-token-parser-0.1.10.tar.gz

---
+++ packages/spacy-token-parser/spacy-token-parser-0.1.10.tar.gz/spacy-token-parser-0.1.10/spacy_token_parser/dto/__init__.py
@@ -0,0 +1,6 @@
+from .normalizer import Normalizer
+from .stemmer import Stemmer
+from .project_types import DependencyGraph
+from .project_types import InputTokens
+from .project_types import ParseInputTokensResult
+from .project_types import ParseResults
