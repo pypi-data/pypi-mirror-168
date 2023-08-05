2a8d6294ada3571315472932629a499a5a2a3836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:11 +0000

    Add package hsir/0.1/hsir-0.1-py3-none-any.whl

---
+++ packages/hsir/hsir-0.1-py3-none-any.whl/hsiboard/box.py
@@ -0,0 +1,92 @@
+import cv2
+import numpy as np
+
+mapbbpox = {
+    'Bottom Right': 'br',
+    'Bottom Left': 'bl',
+    'Top Right': 'ur',
+    'Top Left': 'ul',
+}
+
+def get_border_box_pt(h, w, size, thickness, pos):
+    if pos == 'br':
+        return (w-size-thickness, h-size-thickness)
+    if pos == 'ur':
+        return (w-size-thickness, 0)
+    if pos == 'ul':
+        return (0, 0)
+    if pos == 'bl':
+        return (0, h-size-thickness)
+
+def addbox(img, pt, size=100,
+           bbsize=200, bbpos='br',
+           color=(255, 0, 0),
+           thickness=1,
+           bbthickness=2):
+    H, W = img.shape[0], img.shape[1]
+
+    ptb = get_border_box_pt(H, W, bbsize, bbthickness, pos=bbpos)
+    crop_img = img[pt[1]:pt[1]+size, pt[0]:pt[0]+size]
+    crop_img = cv2.resize(crop_img, (bbsize, bbsize))
+    img[ptb[1]:ptb[1]+bbsize, ptb[0]:ptb[0]+bbsize] = crop_img
+
+    # big box
+    pt1 = ptb
+    pt2 = (pt1[0]+bbsize, pt1[1]+bbsize)
+    cv2.rectangle(img, pt1, pt2, color, bbthickness)
+
+    # small box
+    pt1 = pt
+    pt2 = (pt1[0]+size, pt1[1]+size)
+    cv2.rectangle(img, pt1, pt2, color, thickness)
+
+    return img
+
+
+def convert_color(arr, cmap='viridis', vmin=0, vmax=0.1):
+    import matplotlib.cm as cm
+    sm = cm.ScalarMappable(cmap=cmap)
+    sm.set_clim(vmin, vmax)
+    rgba = sm.to_rgba(arr, alpha=1)
+    return np.array(rgba[:, :, :3])
+
+
+def addbox_with_diff(input, gt, pt, vmax=0.1, size=100,
+                     color=(255, 0, 0),
+                     thickness=1,
+                     bbthickness=2,
+                     sep=5):
+    if len(input.shape) == 2:
+        input = np.expand_dims(input, -1)
+    if len(gt.shape) == 2:
+        gt = np.expand_dims(gt, -1)
+        
+    H, W = input.shape[:2]
+    C = 3
+    
+    out = np.zeros((H, W + H // 2 + sep, C))
+    out[:, :W] = input
+
+    # small box.
+    pt1 = pt
+    pt2 = (pt1[0] + size, pt1[1] + size)
+    cv2.rectangle(out, pt1, pt2, color, thickness)
+
+    # crop.
+    bbsize = H // 2
+    crop_img = input[pt[1]:pt[1] + size, pt[0]:pt[0] + size, :]
+    crop_img = cv2.resize(crop_img, (bbsize, bbsize))
+
+    # diff
+    diff = convert_color(np.abs(input-gt)[:,:,0], vmin=0, vmax=vmax)
+
+    crop_img_diff = diff[pt[1]:pt[1] + size, pt[0]:pt[0] + size, :]
+    crop_img_diff = cv2.resize(crop_img_diff, (bbsize, bbsize))
+
+    if len(crop_img.shape) == 2: crop_img = np.expand_dims(crop_img, axis=-1)
+    if len(crop_img_diff.shape) == 2: crop_img_diff = np.expand_dims(crop_img_diff, axis=-1)
+    
+    out[:H // 2, W+sep:W+sep + H // 2, :] = crop_img
+    out[H // 2:, W+sep:W+sep + H // 2, :] = crop_img_diff
+
+    return out
---
+++ packages/hsir/hsir-0.1-py3-none-any.whl/hsirun/test.py
@@ -0,0 +1,152 @@
+import argparse
+from collections import OrderedDict
+import os
+from os.path import join, exists
+
+import torch
+import torch.utils.data
+import imageio
+import numpy as np
+from tqdm import tqdm
+from tabulate import tabulate
+
+import hsir.model
+import hsir.data.utils
+from hsir.data import HSITestDataset
+
+import torchlight as tl
+
+tl.metrics.set_data_format('chw')
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def bchw2hwc(x):
+    return np.uint8(x.cpu().squeeze(0).permute(1, 2, 0).numpy() * 255)
+
+
+def eval(net, loader, name, logdir, clamp, bandwise):
+    print('Evaluating {}'.format(name))
+    os.makedirs(join(logdir, 'color'), exist_ok=True)
+    os.makedirs(join(logdir, 'gray'), exist_ok=True)
+
+    net.eval()
+    tracker = tl.trainer.util.MetricTracker()
+    detail_stat = {}
+
+    with torch.no_grad():
+        pbar = tqdm(total=len(loader), dynamic_ncols=True)
+        pbar.set_description(name)
+        for data in loader:
+            filename = data['filename'][0]
+            basename = tl.utils.filename(filename)
+            inputs, targets = data['input'].to(device), data['target'].to(device)
+
+            if clamp:
+                inputs = torch.clamp(inputs, 0., 1.)
+            tl.utils.timer.tic()
+            outputs = net(inputs)
+            torch.cuda.synchronize()
+            run_time = tl.utils.timer.toc() / 1000
+
+            outputs = outputs.clamp(0,1)
+            inputs = inputs.squeeze(1)
+            outputs = outputs.squeeze(1)
+            targets = targets.squeeze(1)
+
+            imageio.imwrite(
+                join(logdir, 'color', basename + '.png'),
+                bchw2hwc(hsir.data.utils.visualize_color(outputs))
+            )
+            imageio.imwrite(
+                join(logdir, 'gray', basename + '.png'),
+                bchw2hwc(hsir.data.utils.visualize_gray(outputs))
+            )
+
+            psnr = tl.metrics.mpsnr(targets, outputs).item()
+            ssim = tl.metrics.mssim(targets, outputs).item()
+            sam = tl.metrics.sam(targets, outputs).item()
+
+            tracker.update('MPSNR', psnr)
+            tracker.update('MSSIM', ssim)
+            tracker.update('SAM', sam)
+            tracker.update('Time', run_time)
+
+            pbar.set_postfix({k: f'{v:.4f}' for k, v in tracker.result().items()})
+            pbar.update()
+
+            detail_stat[basename] = {'MPSNR': psnr, 'MSSIM': ssim, 'SAM': sam, 'Time': run_time}
+
+        pbar.close()
+
+    avg_speed = tl.utils.format_time(tracker['Time'])
+    print(f'Average speed {avg_speed}')
+    print(f'Average results {tracker.summary()}')
+
+    # log structural results
+    avg_stat = {k: v for k, v in tracker.result().items()}
+    tl.utils.io.jsonwrite(join(logdir, 'log.json'), {'avg': avg_stat, 'detail': detail_stat})
+
+
+def pretty_summary(logdir):
+    stat = []
+    print('')
+    for folder in os.listdir(logdir):
+        if os.path.isdir(join(logdir, folder)):
+            path = join(logdir, folder, 'log.json')
+            if exists(path):
+                data = tl.utils.io.jsonload(path)
+                s = OrderedDict()
+                s['Name'] = folder
+                s.update(data['avg'])
+                stat.append(s)
+    print(tabulate(stat, headers='keys', tablefmt='github'))
+    print('')
+
+
+def main(args, logdir):
+    net = tl.utils.instantiate(args.arch)
+    net = net.to(device)
+
+    ckpt = tl.utils.dict_get(torch.load(args.resume), args.key_path)
+    if ckpt is None: print(f'key_path {args.key_path} might be wrong')
+    net.load_state_dict(ckpt)
+
+    for testset in args.testset:
+        testdir = join(args.basedir, testset)
+        dataset = HSITestDataset(testdir, use_chw=args.use_conv2d, return_name=True)
+        loader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=False, num_workers=1)
+        eval(net, loader, testset, join(logdir, testset), args.clamp, args.bandwise)
+
+    pretty_summary(logdir)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='HSIR Test script')
+    parser.add_argument('-a', '--arch', required=True, help='architecture name')
+    parser.add_argument('-n', '--name', default=None, help='save name')
+    parser.add_argument('-r', '--resume', required=True, help='checkpoint')
+    parser.add_argument('-t', '--testset', nargs='+', default=['icvl_512_50'], help='testset')
+    parser.add_argument('--basedir', default='data', help='basedir')
+    parser.add_argument('--logdir', default='results', help='logdir')
+    parser.add_argument('--save_img', action='store_true', help='whether to save image')
+    parser.add_argument('--clamp', action='store_true', help='whether clamp input into [0, 1]')
+    parser.add_argument('-kp', '--key_path', default='net', help='key path to access network state_dict in ckpt')
+    parser.add_argument('--bandwise', action='store_true')
+    parser.add_argument('--use-conv2d', action='store_true')
+    args = parser.parse_args()
+
+    save_name = args.arch if args.name is None else args.name
+    logdir = join(args.logdir, save_name)
+    if exists(logdir):
+        print(f'It seems that you have evaluated {args.arch} before.')
+        pretty_summary(logdir)
+        action = input('Are you sure you want to continue? (y) continue (n) exit\n')
+        if action != 'y': exit()
+
+    os.makedirs(logdir, exist_ok=True)
+    with open(join(logdir, 'meta.txt'), 'w') as f:
+        f.write(tl.utils.get_datetime() + '\n')
+        f.write(tl.utils.get_cmd() + '\n')
+        f.write(str(args) + '\n')
+
+    main(args, logdir)
