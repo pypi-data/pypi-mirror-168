d336c88d042640383598e1ea86f72ddd8b89366c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:19 +0000

    Add package python-mail-sender/0.1.1/python_mail_sender-0.1.1.tar.gz

---
+++ packages/python-mail-sender/python_mail_sender-0.1.1.tar.gz/python_mail_sender-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='python_mail_sender',
+    version='0.1.1',
+    py_modules=['mail_sender'],
+    license='MIT',
+    long_description='Mail Sender Module that helps you send your emails perfectly.',
+    python_requires='>=3.6',
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    readme = 'README.md',   
+)
