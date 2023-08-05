3134631a79e4e057995809376a98dacecfaafbcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:18 +0000

    Add package lampe/0.5.4/lampe-0.5.4.tar.gz

---
+++ packages/lampe/lampe-0.5.4.tar.gz/lampe-0.5.4/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import setuptools
+
+with open('README.md', 'r') as f:
+    readme = f.read()
+
+with open('requirements.txt', 'r') as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    name='lampe',
+    version='0.5.4',
+    packages=setuptools.find_packages(),
+    description='Likelihood-free AMortized Posterior Estimation with PyTorch',
+    keywords='parameter inference bayes posterior amortized likelihood ratio mcmc torch',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='François Rozet',
+    author_email='francois.rozet@outlook.com',
+    license='MIT license',
+    url='https://github.com/francois-rozet/lampe',
+    project_urls={
+        'Documentation': 'https://github.com/francois-rozet/lampe',
+        'Source': 'https://github.com/francois-rozet/lampe',
+        'Tracker': 'https://github.com/francois-rozet/lampe/issues',
+    },
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=required,
+    extras_require={
+        'docs': ['furo', 'sphinx'],
+        'test': ['pytest'],
+    },
+    python_requires='>=3.8',
+)
