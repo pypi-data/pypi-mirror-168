230c4766cf4bd07db5be4e532b661cbffefe62b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:40 +0000

    Add package d3blocks/1.0.3/d3blocks-1.0.3-py3-none-any.whl

---
+++ packages/d3blocks/d3blocks-1.0.3-py3-none-any.whl/d3blocks/__init__.py
@@ -0,0 +1,36 @@
+from d3blocks.d3blocks import D3Blocks
+
+# import chord.Chord as Chord
+# import imageslider.Imageslider as Imageslider
+# import movingbubbles.Movingbubbles as Movingbubbles
+# import sankey.Sankey as Sankey
+# import scatter.Scatter as Scatter
+# import timeseries.Timeseries as Timeseries
+# import violin.Violin as Violin
+# import particles.Particles as Particles
+
+__author__ = 'Erdogan Taskesen, Oliver Verver'
+__email__ = 'erdogant@gmail.com, mail@oliver3.nl'
+__version__ = '1.0.3'
+
+# module level doc-string
+__doc__ = """
+d3blocks
+=====================================================================
+
+Description
+-----------
+d3blocks is for the creation of exclusive stand alone and interactive graphs in d3 javascript.
+
+Example
+-------
+>>> from d3blocks import d3blocks
+>>> d3 = d3blocks()
+>>> df = d3.import_example(data='random_time')
+>>> d3.movingbubbles(df)
+
+References
+----------
+https://github.com/d3blocks/d3blocks
+
+"""
