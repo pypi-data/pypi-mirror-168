232baa8db72bf283ba1ac475cce13a9be2d942d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:20 +0000

    Add package zlado/1.0.20/zlado-1.0.20.tar.gz

---
+++ packages/zlado/zlado-1.0.20.tar.gz/zlado-1.0.20/setup.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='zlado',
+    version='1.0.20',
+    packages=find_packages(),  # 包含所有的py文件
+    package_data={
+        # @see https://www.cnblogs.com/babykick/archive/2012/01/18/2325702.html
+        '': ['*.ini']
+    },
+    # python_requires='>=3.0',
+    # 将数据文件也打包
+    include_package_data=True,
+    install_requires=[
+        'click',
+        'aliyun-python-sdk-ecs'
+    ],
+    entry_points={
+        'console_scripts': [
+            'ado = zlado.ado:cli',
+        ]
+    }
+)
---
+++ packages/zlado/zlado-1.0.20.tar.gz/zlado-1.0.20/zlado/ado.py
@@ -0,0 +1,41 @@
+# -*-coding=utf-8-*-
+import click
+import json
+import logging
+from zlado.aliyun import ali_cli
+from zlado import ado_ini
+import os
+
+
+def get_ecses():
+    cfg = ado_ini.read_cfg()
+    print('regions:',cfg.get('default', 'regionids').split(','))
+    cli = ali_cli.AliCli(cfg.get('default', 'accesskeyid'),
+                         cfg.get('default', 'accesskeysecret'),
+                         cfg.get('default', 'regionids').split(','))
+    return [ecs for ecs in cli.ecses_iterator()]
+
+
+@click.command()
+def cli():
+    ecses = get_ecses()
+    while True:
+        os.system('clear')
+        for i in range(len(ecses)):
+            print(''.join([str(i+1), ':', ecses[i].__str__()]))
+
+        indexstr=input('请选择ECS序号:')
+        if not isinstance(indexstr,int):
+            continue
+        if int(indexstr)>len(ecses):
+            continue
+
+        cur_ecs=ecses[int(indexstr)-1]
+        sship = cur_ecs.ip
+        # sship = '127.0.0.1'
+        print('ecs',':',cur_ecs.__str__())
+        os.system('ssh ' + sship)
+
+
+if __name__ == '__main__':
+    cli()
