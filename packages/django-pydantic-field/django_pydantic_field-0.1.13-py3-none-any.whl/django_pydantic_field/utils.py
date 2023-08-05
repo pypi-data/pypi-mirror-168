95e65f05e94153c776f5eb5fe9a2fda03e576c13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:59 +0000

    Add package django-pydantic-field/0.1.13/django_pydantic_field-0.1.13-py3-none-any.whl

---
+++ packages/django-pydantic-field/django_pydantic_field-0.1.13-py3-none-any.whl/django_pydantic_field/utils.py
@@ -0,0 +1,30 @@
+import sys
+import typing as t
+
+
+def get_annotated_type(cls, field, default=None) -> t.Any:
+    try:
+        annotations = cls.__annotations__
+        return annotations[field]
+    except (AttributeError, KeyError):
+        return default
+
+
+def get_local_namespace(cls) -> t.Dict[str, t.Any]:
+    module = cls.__module__
+    try:
+        return vars(sys.modules[module])
+    except KeyError:
+        return {}
+
+
+def is_forward_ref(type_):
+    if isinstance(type_, (str, t.ForwardRef)):
+        return True
+
+    origin = t.get_origin(type_)
+    if origin is None:
+        return False
+
+    type_args = t.get_args(type_)
+    return is_forward_ref(origin) or any(map(is_forward_ref, type_args))
