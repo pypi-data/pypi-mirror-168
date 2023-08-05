6f68a8cd68d9ea965d91d3bc1647c7b5328331a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:19 +0000

    Add package torch-adata/0.0.15/torch_adata-0.0.15-py3-none-any.whl

---
+++ packages/torch-adata/torch_adata-0.0.15-py3-none-any.whl/torch_adata/__init__.py
@@ -0,0 +1,17 @@
+
+__module_name__ = "__init__.py"
+__author__ = ", ".join(["Michael E. Vinyard"])
+__email__ = ", ".join(["vinyard@g.harvard.edu"])
+
+
+# -----------------------------------------------------------------------------
+__version__ = "0.0.15"
+
+
+# -----------------------------------------------------------------------------
+from ._AnnDataset import AnnDataset as AnnDataset
+from ._TimeResolvedAnnDataset import TimeResolvedAnnDataset
+
+
+# -----------------------------------------------------------------------------
+from . import _functions as funcs
