adcd724fbc0100c6be4e7b248d1fdc9aeae9f09c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:32 +0000

    Add package pyobjc-framework-WebKit/8.5.1/pyobjc-framework-WebKit-8.5.1.tar.gz

---
+++ packages/pyobjc-framework-WebKit/pyobjc-framework-WebKit-8.5.1.tar.gz/pyobjc-framework-WebKit-8.5.1/PyObjCTest/test_wkwebpagepreferences.py
@@ -0,0 +1,17 @@
+from PyObjCTools.TestSupport import TestCase, min_os_level
+import WebKit
+
+
+class TestWebPagePreferences(TestCase):
+    def test_enum_types(self):
+        self.assertIsEnumType(WebKit.WKContentMode)
+
+    def test_constants(self):
+        self.assertEqual(WebKit.WKContentModeRecommended, 0)
+        self.assertEqual(WebKit.WKContentModeMobile, 1)
+        self.assertEqual(WebKit.WKContentModeDesktop, 2)
+
+    @min_os_level("11.0")
+    def test_methods11_0(self):
+        self.assertResultIsBOOL(WebKit.WKWebpagePreferences.allowsContentJavaScript)
+        self.assertArgIsBOOL(WebKit.WKWebpagePreferences.setAllowsContentJavaScript_, 0)
