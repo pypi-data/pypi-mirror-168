ea2189a9cf4a38e6bd968a16dd56bcfa1b78168c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:19 +0000

    Add package climail/2.0.5/CLImail-2.0.5.tar.gz

---
+++ packages/climail/CLImail-2.0.5.tar.gz/CLImail-2.0.5/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['climail']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['colorama>=0.4.5,<0.5.0']
+
+setup_kwargs = {
+    'name': 'climail',
+    'version': '2.0.5',
+    'description': 'A Command Line Interface email client written in python.',
+    'long_description': 'None',
+    'author': 'HRLO77',
+    'author_email': 'None',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
