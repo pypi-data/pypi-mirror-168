bbdf85a07419986e079c7130c6d611659964c2dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:22 +0000

    Add package gotipy/0.2.1/gotipy-0.2.1.tar.gz

---
+++ packages/gotipy/gotipy-0.2.1.tar.gz/gotipy-0.2.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['gotipy']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.28.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'gotipy',
+    'version': '0.2.1',
+    'description': 'A simple Python wrapper for Gotify.',
+    'long_description': None,
+    'author': 'Alyetama',
+    'author_email': 'malyetama@pm.me',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
