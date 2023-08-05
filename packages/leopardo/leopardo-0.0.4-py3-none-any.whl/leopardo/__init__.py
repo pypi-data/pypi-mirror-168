47c2bdbfe848e381bde0f00d57ac88ef9c9debfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:54 +0000

    Add package leopardo/0.0.4/leopardo-0.0.4-py3-none-any.whl

---
+++ packages/leopardo/leopardo-0.0.4-py3-none-any.whl/leopardo/__init__.py
@@ -0,0 +1,6 @@
+# ------------------------------------------------------------------------------
+#  leopardo [Configurable fast-access toolset]
+#  (C) 2022. A. Shavykin <0.delameter@gmail.com>
+# ------------------------------------------------------------------------------
+
+from ._version import __version__
