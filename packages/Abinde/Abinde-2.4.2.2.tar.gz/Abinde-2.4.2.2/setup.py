a71e4b541275786631294f43b8eedd1a28801c84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:52 +0000

    Add package Abinde/2.4.2.2/Abinde-2.4.2.2.tar.gz

---
+++ packages/Abinde/Abinde-2.4.2.2.tar.gz/Abinde-2.4.2.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='Abinde',
+    version='2.4.2.2',
+    install_requires=[
+        'pygame>=2.1.2',
+        'importlib-metadata; python_version >= "3.8"',
+        'pillow',
+    ],
+    url="https://github.com/Abinde-Game-Dev",
+    author="_TheRealPenguin",
+    keywords="game python lightweight open-source raspberry-pi mac linux games image game-engine simple game-development game-2d pygame help-wanted easy-to-use easy open-source-project good-first-issue pygame-games pygame-game",
+    license="MIT",
+    description="Abinde is an easy-to-use python module for pygame.",
+    classifiers=[
+	    "Development Status :: 2 - Pre-Alpha",
+	    "License :: OSI Approved :: MIT License",
+	    "Natural Language :: English",
+	    "Programming Language :: Python :: 3 :: Only",
+	    "Topic :: Games/Entertainment",
+	    "Topic :: Utilities",
+	    "Intended Audience :: Developers",
+    ]
+)
