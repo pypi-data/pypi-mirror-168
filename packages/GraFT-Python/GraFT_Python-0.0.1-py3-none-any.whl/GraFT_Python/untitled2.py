eb351cc89c3bf8481dca7b8619e6c209c224c653
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:59 +0000

    Add package GraFT-Python/0.0.1/GraFT_Python-0.0.1-py3-none-any.whl

---
+++ packages/GraFT-Python/GraFT_Python-0.0.1-py3-none-any.whl/GraFT_Python/untitled2.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  9 23:33:11 2022
+
+@author: noga mudrik
+"""
+
+#%% Variables Explanation File:
+    
+"""
+Variables:
+"""    
+
+# structure: MATLAB -> Python
+# D -> \Phi
+#
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
