3c4716db5b438d3d7e66bdce2e3e7f529032b7ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:26 +0000

    Add package package-extras/0.1.0/package-extras-0.1.0.tar.gz

---
+++ packages/package-extras/package-extras-0.1.0.tar.gz/package-extras-0.1.0/README.md
@@ -0,0 +1,21 @@
+## extras
+
+For the detailed explanation read [this blog post]().
+
+### Usage
+
+```toml
+```
+
+Add this or similar block to your code
+
+```python
+```
+
+### Development
+
+```bash
+$ poetry install
+$ poetry build
+$ poetry publish
+```
---
+++ packages/package-extras/package-extras-0.1.0.tar.gz/package-extras-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['extras']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'package-extras',
+    'version': '0.1.0',
+    'description': 'Dummy package to mark extras installation',
+    'long_description': '## extras\n\nFor the detailed explanation read [this blog post]().\n\n### Usage\n\n```toml\n```\n\nAdd this or similar block to your code\n\n```python\n```\n\n### Development\n\n```bash\n$ poetry install\n$ poetry build\n$ poetry publish\n```',
+    'author': 'Misha Behersky',
+    'author_email': 'bmwant@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
