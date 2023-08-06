2f9d30973c9d8830029fc29ee82091b755d1410e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:20 +0000

    Add package audoma/0.4.10/audoma-0.4.10.tar.gz

---
+++ packages/audoma/audoma-0.4.10.tar.gz/audoma-0.4.10/audoma/drf/generics.py
@@ -0,0 +1,104 @@
+from typing import Type
+
+from rest_framework import generics
+from rest_framework.serializers import BaseSerializer
+
+
+class GenericAPIView(generics.GenericAPIView):
+
+    """
+    Extended GenericAPIView known from rest_framework.
+    This class extends `get_serializer` and `get_serializer_class` methods.
+    Also provides `get_result_serializer`, which is a shourtcut for `get_serializer` with proper param.
+    """
+
+    def get_serializer(self, *args, **kwargs) -> BaseSerializer:
+        """
+        Passes additional param to `get_serializer_class`.
+
+        kwargs:
+            serializer_type - defines if serializer is collect or result serializer.
+                result serializer will be used to produce response, collect to process incoming data.
+            serializer_class - it is possible to pass serializer_class to get_serializer, this will
+                ends with returning passed serializer_class object.
+
+        Returns:
+            Object of obtained serializer class.
+        """
+        many = kwargs.get("many", False)
+
+        serializer_type = kwargs.pop("serializer_type", "collect")
+        serializer_class = kwargs.pop(
+            "serializer_class",
+            self.get_serializer_class(type=serializer_type, many=many),
+        )
+        kwargs["context"] = self.get_serializer_context()
+
+        return serializer_class(*args, **kwargs)
+
+    # needed by AudomaSwaggerAutoSchema
+    def get_result_serializer(self, *args, **kwargs) -> BaseSerializer:
+        """
+        Shortuct for get_serializer.
+        Simply has serializer_type set to `result`
+        """
+        return self.get_serializer(*args, serializer_type="result", **kwargs)
+
+    def get_serializer_class(
+        self, type: str = "collect", many: bool = False
+    ) -> Type[BaseSerializer]:
+        """
+        Extends defuault `get_serializer_class` method.
+        This returns proper serializer_class for current request.
+
+        Args:
+            type - type of serializer to be returned, it may be collect or result serializer.
+
+        Returns:
+            This returns serializer_class
+
+        """
+        assert self.action not in [
+            "post",
+            "put",
+            "delete",
+            "patch",
+            "options",
+            "get",
+            "head",
+        ]
+        method = self.request.method.lower()
+        if self.action == "metadata":
+            action = self.action_map.get("post", "list")
+        else:
+            action = self.action
+        attr_names = [
+            "%s_%s_%s_serializer_class" % (method, action, type),
+            "%s_%s_serializer_class" % (action, type),
+            "%s_%s_serializer_class" % (method, action),
+            "%s_serializer_class" % (action),
+            "common_%s_serializer_class" % (type),
+            "serializer_class",
+        ]
+        for attr_name in attr_names:
+            try:
+                serializer_class = getattr(self, attr_name)
+            except AttributeError:
+                continue
+            else:
+                break
+
+        assert serializer_class is not None, (
+            "'%s' should either include a `serializer_class`  attribute, "
+            "or override the `get_serializer_class()` method." % self.__class__.__name__
+        )
+
+        if (
+            type == "result"
+            and self.action != "list"
+            and hasattr(serializer_class, "get_result_serializer_class")
+        ):
+            assert callable(serializer_class.get_result_serializer_class)
+            serializer_class = serializer_class.get_result_serializer_class()
+
+        return serializer_class
---
+++ packages/audoma/audoma-0.4.10.tar.gz/audoma-0.4.10/audoma/drf/viewsets.py
@@ -0,0 +1,92 @@
+import json
+import random
+from typing import (
+    Any,
+    List,
+)
+
+from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
+from audoma.drf.generics import GenericAPIView
+
+
+class AudomaPagination(PageNumberPagination):
+    """
+    A simple page number based style that supports page numbers as
+    query parameters.
+
+    Note:
+        If this won't be used it'll cause less explicit pagination documentation.
+        This class does not provide any additional functionality.
+
+    Args:
+        page_size (int) - number of items per page - by default this is set to 25
+        max_page_size (int) - maximum number of items per page - by default this is set to 2000
+    """
+
+    page_size = 25
+    max_page_size = 2000
+
+    def get_paginated_response_schema(self, schema: List[dict]) -> dict:
+        """
+        Simple method to add pagination information to the schema.
+
+        Args:
+            schema (List[dict]) - list of schema items
+        Returns:
+            Dictionary with pagination information including examples
+        """
+        return {
+            "type": "object",
+            "properties": {
+                "count": {"type": "integer", "example": random.randint(1, 100)},
+                "message": {
+                    "type": "string",
+                    "nullable": True,
+                },
+                "next": {
+                    "type": "string",
+                    "nullable": True,
+                    "format": "uri",
+                    "example": "http://api.example.org/accounts/?page=4",
+                },
+                "previous": {
+                    "type": "string",
+                    "nullable": True,
+                    "format": "uri",
+                    "example": "http://api.example.org/accounts/?page=2",
+                },
+                "results": schema,
+            },
+        }
+
+
+class GenericViewSet(viewsets.ViewSetMixin, GenericAPIView):
+    pagination_class = AudomaPagination
+
+    def _parse_response_data(self, response_data: List[Any]) -> List[str]:
+        parsed_data = []
+        for item in response_data:
+            if isinstance(item, str):
+                parsed_data.append(item)
+            else:
+                parsed_data.append(json.dumps(item))
+        return parsed_data
+
+    def handle_exception(self, exc: Exception) -> Response:
+        response = super().handle_exception(exc)
+        if isinstance(response.data, dict):
+            if response.status_code != 418:
+                for k in response.data:
+                    if isinstance(response.data[k], list):
+                        if not all(isinstance(item, str) for item in response.data[k]):
+                            response.data[k] = self._parse_response_data(
+                                response.data[k]
+                            )
+                        response.data[k] = " ".join(response.data[k])
+                response.data = {"errors": response.data}
+        elif isinstance(response.data, list):
+            response.data = {"errors": response.data}
+        return response
