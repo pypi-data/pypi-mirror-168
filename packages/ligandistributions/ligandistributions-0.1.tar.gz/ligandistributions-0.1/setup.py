11434db371a4a7c102633377c19c98f1aec18609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:08 +0000

    Add package ligandistributions/0.1/ligandistributions-0.1.tar.gz

---
+++ packages/ligandistributions/ligandistributions-0.1.tar.gz/ligandistributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ligandistributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['ligandistributions'],
+      zip_safe=False)
