1c77c671772fdf174a4e0cdbf16ebb811568963c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:31 +0000

    Add package tklinenums/1.3/tklinenums-1.3.tar.gz

---
+++ packages/tklinenums/tklinenums-1.3.tar.gz/tklinenums-1.3/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="tklinenums",
+    version="1.3",
+    description="A simple Tkinter widget for displaying line numbers",
+    readme="README.md",
+    author="Moosems",
+    author_email="moosems.j@gmail.com",
+    url="https://github.com/Moosems/TkLineNums",
+    packages=["tklinenums"],
+)
