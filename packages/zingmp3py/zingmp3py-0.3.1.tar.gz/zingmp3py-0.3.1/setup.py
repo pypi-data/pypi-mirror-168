8d3c63b3e48620e1689a06e19c74fd6dfea0eda4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:59:03 +0000

    Add package zingmp3py/0.3.1/zingmp3py-0.3.1.tar.gz

---
+++ packages/zingmp3py/zingmp3py-0.3.1.tar.gz/zingmp3py-0.3.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='zingmp3py',
+   version="0.3.1",
+   description='A light weight Python library for the ZingMp3 API',
+   author='The DT',
+   author_email='duongtuan30306@gmail.com',
+   packages=["zingmp3py"],
+   install_requires=["aiohttp", "requests"]
+)
