d5a3e567b7bbf5ec668e96426498c166d25967e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:01 +0000

    Add package armarx/0.21.0/armarx-0.21.0.tar.gz

---
+++ packages/armarx/armarx-0.21.0.tar.gz/armarx-0.21.0/armarx/foo.py
@@ -0,0 +1,9 @@
+def time
+
+ "timestamp": {
+                "clockType": ClockType.VIRTUAL,
+                "hostname": "unknown",
+                "timeSinceEpoch": {
+                    "microSeconds": np.int64(self.timestamp_usec),
+                },
+            },
---
+++ packages/armarx/armarx-0.21.0.tar.gz/armarx-0.21.0/armarx/slice_loader.py
@@ -0,0 +1,21 @@
+"""
+.. deprecated:: 0.10.6
+
+"""
+import sys
+import warnings
+
+from armarx_core.slice_loader import ArmarXProxyFinder
+
+
+def load_armarx_slice(armarx_package_name: str, filename: str):
+    """
+    ..deprecated:: add your slice file to a project's VariantInfo-*.xml instead
+    """
+    warnings.warn(
+        "Add the slice definition to VariantInfo-*.xml instead.", DeprecationWarning
+    )
+    for c in sys.meta_path:
+        if isinstance(c, ArmarXProxyFinder):
+            c._load_armarx_slice(armarx_package_name, filename)
+            c.update_loaded_modules()
