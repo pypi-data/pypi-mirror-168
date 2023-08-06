004f9c19c8f84788ab8735fc5b46534102abdff0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:23 +0000

    Add package console-creator/0.0.0a0/console_creator-0.0.0a0-py3-none-any.whl

---
+++ packages/console-creator/console_creator-0.0.0a0-py3-none-any.whl/consoles/abstract_console.py
@@ -0,0 +1,7 @@
+"""This is the base class for console.
+
+If you want create your own console classe, you muss inherits this class.
+"""
+
+class AbstractConsole() :
+    """Base class for console."""
