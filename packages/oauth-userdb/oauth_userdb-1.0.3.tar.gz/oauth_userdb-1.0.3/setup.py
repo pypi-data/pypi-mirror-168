ed03fb0cc4cca0affb1d630b719e02b6cf7d4525
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:34 +0000

    Add package oauth-userdb/1.0.3/oauth_userdb-1.0.3.tar.gz

---
+++ packages/oauth-userdb/oauth_userdb-1.0.3.tar.gz/oauth_userdb-1.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='oauth_userdb',
+    version='1.0.3',
+    description='OAuth User DB',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='',
+    author='Jimming Cheng',
+    author_email='jimming@gmail.com',
+    packages=['oauth_userdb'],
+    install_requires=[
+        'PyJWT',
+        'oauthlib',
+        'requests',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
