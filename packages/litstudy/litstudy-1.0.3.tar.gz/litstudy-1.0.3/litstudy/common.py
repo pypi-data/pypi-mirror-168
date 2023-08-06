7c807623ad6eed1976bbad1c471e06b1fd7da7bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:59 +0000

    Add package litstudy/1.0.3/litstudy-1.0.3.tar.gz

---
+++ packages/litstudy/litstudy-1.0.3.tar.gz/litstudy-1.0.3/litstudy/common.py
@@ -0,0 +1,87 @@
+import re
+from unidecode import unidecode
+
+try:
+    from tqdm import tqdm
+
+    def progress_bar(it):
+        return tqdm(it)
+except ImportError:
+    def progress_bar(it):
+        return it
+
+STOPWORDS = set([
+    '',
+    'and',
+    'at',
+    'for',
+    'in',
+    'into',
+    'of',
+    'on',
+    'onto',
+    'over',
+    'the',
+    'to',
+    'ltd',
+    'corporation',
+    'corp',
+])
+
+
+def canonical(key, aggresive=True, stopwords=None):
+    if stopwords is None:
+        stopwords = STOPWORDS
+
+    if aggresive:
+        key = unidecode(key).lower()
+
+    tokens = re.split(r'[\W]+', key)
+    new_tokens = []
+
+    for token in tokens:
+        if not token or token[0].isdigit():
+            continue
+
+        if aggresive and (token in stopwords or len(token) <= 1):
+            continue
+
+        new_tokens.append(token)
+
+    return ' '.join(new_tokens)
+
+
+def fuzzy_match(lhs, rhs):
+    if lhs is None or rhs is None:
+        return False
+
+    return canonical(lhs) == canonical(rhs)
+
+
+class FuzzyMatcher:
+    def __init__(self, initial=None):
+        mapping = dict()
+        unmapping = dict()
+
+        if initial is not None:
+            for src, dst in initial.items():
+                key = canonical(src)
+                dst_key = canonical(dst)
+
+                mapping[dst_key] = key
+                mapping[key] = key
+                unmapping[key] = dst
+
+        self.mapping = mapping
+        self.unmapping = unmapping
+
+    def get(self, name):
+        key = canonical(name)
+
+        if key in self.mapping:
+            return self.unmapping[self.mapping[key]]
+
+        nice_name = canonical(name, False)
+        self.mapping[key] = key
+        self.unmapping[key] = nice_name
+        return nice_name
---
+++ packages/litstudy/litstudy-1.0.3.tar.gz/litstudy-1.0.3/litstudy/sources/arxiv.py
@@ -0,0 +1,112 @@
+from litstudy.types import Document, DocumentSet, DocumentIdentifier, Author
+from typing import Optional, List
+import feedparser  # type: ignore
+from datetime import datetime
+from urllib.parse import urlencode
+import time
+
+
+class ArXivAuthor(Author):
+    def __init__(self, entry):
+        self.entry = entry
+
+    @property
+    def name(self):
+        return self.entry
+
+
+class ArXivDocument(Document):
+    def __init__(self, entry):
+        identifier = DocumentIdentifier(
+            entry.title,
+        )
+
+        super().__init__(identifier)
+        self.entry = entry
+
+    @property
+    def doi(self) -> Optional[str]:
+        return self.entry.get('arxiv_doi', None)
+
+    @property
+    def title(self) -> str:
+        return self.entry.get('title')
+
+    @property
+    def authors(self) -> List:
+        return [ArXivAuthor(name.get('name'))
+                for name
+                in self.entry.get('authors')]
+
+    @property
+    def journal_ref(self) -> Optional[str]:
+        return self.entry.get('arxiv_journal_ref', None)
+
+    @property
+    def publication_date(self):
+        publication_date = datetime.strptime(self.entry.get('published'),
+                                             "%Y-%m-%dT%H:%M:%SZ")
+        return publication_date.date()
+
+    @property
+    def abstract(self) -> Optional[str]:
+        return self.entry.get('summary', None)
+
+    @property
+    def language(self) -> Optional[str]:
+        return self.entry.get('language', None)
+
+    @property
+    def category(self) -> Optional[List[str]]:
+        '''returns arxiv category for article'''
+        return self.entry.get('tags', None)[0].get('term', None)
+
+
+# Base api query url
+ARXIV_SEARCH_URL = 'http://export.arxiv.org/api/query'
+
+
+def search_arxiv(search_query,
+                 start=0,
+                 total_results=100,
+                 results_per_iteration=100,
+                 sleep_time=3) -> DocumentSet:
+
+    '''
+    Search parameters:
+    ----------------------------------------------------------------
+    search_query = all:electron    #search for electron in all fields
+    start                          #start at the first result
+    total_results                  #total results
+    results_per_iteration          #results at a time
+    sleep_time                     #number of seconds to wait beetween calls
+    ----------------------------------------------------------------
+    Returns(DocumentSet with a folowing parameters)
+    ----------------------------------------------------------------
+    authors: list of authors separated by commas
+    title: title of the article
+    abstract: abstract of the article
+    published: publish_date in datetime format
+    arxiv_journal_ref: reference to the journal if existed
+    doi: digital document identifier
+    keywords: terms relevent to the document
+    language: language document is written in '''
+
+    docs = list()
+
+    for i in range(start, total_results, results_per_iteration):
+        query = urlencode(dict(
+            search_query=search_query,
+            start=i,
+            max_results=results_per_iteration
+        ))
+
+        url = f'{ARXIV_SEARCH_URL}?{query}'
+        data = feedparser.parse(url)
+
+        for entry in data.entries:
+            docs.append(ArXivDocument(entry))
+
+        time.sleep(sleep_time)
+
+    return DocumentSet(docs)
