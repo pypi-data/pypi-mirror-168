3a2e07ef52a6c76f0cdd5ac56783c5ab5c5e9ee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:59:50 +0000

    Add package Vlt-Comm/0.0.11/Vlt-Comm-0.0.11.tar.gz

---
+++ packages/Vlt-Comm/Vlt-Comm-0.0.11.tar.gz/Vlt-Comm-0.0.11/FcBus/Support.py
@@ -0,0 +1,18 @@
+
+
+class NoDataException(Exception):
+    def __init__(self, value):
+        self.value = value
+        
+class ErrorDataException(Exception):
+    def __init__(self, value):
+        self.value = value
+        
+class CommandError(Exception):
+    def __init__(self, value):
+        self.value = value
+        
+        
+class UnsupportPara(Exception):
+    def __init__(self, value):
+        self.value = value
---
+++ packages/Vlt-Comm/Vlt-Comm-0.0.11.tar.gz/Vlt-Comm-0.0.11/FcBus/boot.py
@@ -0,0 +1,26 @@
+
+class Product(object):
+    def __init__(self):
+        pass
+    
+    def laoc(self, startaddr, data):
+        pass
+    
+    def llang(self, startaddr, data):
+        pass
+    
+    def lmoc(self, startaddr, data):
+        pass
+    
+    def lee(self, addr, data):
+        pass
+    
+    def lpud(self, addr, data):
+        pass
+    
+    def lall(self):
+        pass
+
+class LoadBase(object):
+    def __init__(self):
+        pass
