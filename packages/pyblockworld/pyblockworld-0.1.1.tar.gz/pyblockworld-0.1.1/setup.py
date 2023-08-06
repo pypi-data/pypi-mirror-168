79a47d34aebbacb210bb5cd4a685b547f716aacf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:46 +0000

    Add package pyblockworld/0.1.1/pyblockworld-0.1.1.tar.gz

---
+++ packages/pyblockworld/pyblockworld-0.1.1.tar.gz/pyblockworld-0.1.1/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['pyblockworld']
+install_requires = \
+['pyglet>=1.5.26,<2.0.0']
+
+setup_kwargs = {
+    'name': 'pyblockworld',
+    'version': '0.1.1',
+    'description': 'Minecraft like Block world in Python',
+    'long_description': None,
+    'author': 'Marco Bakera',
+    'author_email': 'marco@bakera.de',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
