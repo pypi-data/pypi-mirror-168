09b9f0bfade1259e2b5063bb78f6a462494a7d8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:16 +0000

    Add package pn-calcy/0.0.1/pn_calcy-0.0.1.tar.gz

---
+++ packages/pn-calcy/pn_calcy-0.0.1.tar.gz/pn_calcy-0.0.1/README.txt
@@ -0,0 +1,1 @@
+This is a basic package for calculator
---
+++ packages/pn-calcy/pn_calcy-0.0.1.tar.gz/pn_calcy-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+classifiers = [
+   'Development Status:: 5 - Production/Stable',
+   'Intended Audience :: Education',
+   'Operating System :: Manjaro :: Linux :: Windows 10',
+   'Topic :: Utilities',
+   'License :: OSI Approved :: MIT License',
+   'Programming Language :: Python 3.0.9',
+]
+setup(
+    name='pn_calcy',
+    version='0.0.1',
+    description='this is a dumb package used for dumb calculation.',
+    url='',
+    author='Pranav Nair',
+    author_email='nairpranav541@gmail.com',
+    licence='MIT',
+    packages=['pn_calcy'],
+    install_requires=[]
+)
