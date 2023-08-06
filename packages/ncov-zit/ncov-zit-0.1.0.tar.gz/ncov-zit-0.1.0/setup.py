6c08981097986583358dcdf72c773dd919b66cc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:45:30 +0000

    Add package ncov-zit/0.1.0/ncov-zit-0.1.0.tar.gz

---
+++ packages/ncov-zit/ncov-zit-0.1.0.tar.gz/ncov-zit-0.1.0/setup.py
@@ -0,0 +1,42 @@
+'''
+Author: WildboarG
+version: 1.0
+Date: 2022-08-24 20:36:57
+LastEditors: WildboarG
+LastEditTime: 2022-09-23 17:39:41
+Descripttion: 
+'''
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+from numpy.distutils.command.install import install
+from setuptools import setup, find_packages
+from numpy.distutils.core import setup
+from numpy.distutils.misc_util import Configuration
+
+
+setup(
+    
+    name='ncov-zit',
+    version='0.1.0',
+    author='WildboarG',
+    author_email='mm62633482@gmail.com',
+    url='https://github.com/WildboarG/ncov-zit',
+    license="MIT",
+    description='A cli tool  to obtain the health reporting and submission information of Zzhou Institute of science and technology.',
+    long_description_content_type="text/markdown",
+    long_description = "test  Module",
+    install_requires=["requests","rich"],
+    keywords=["zit", "ncov"],
+    python_requires=">=3",
+
+    package_data={
+        '':['ncov_zit/info/*.py'],
+               },
+
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "oh_my-zit = ncov_zit.main:main",
+        ],
+    }
+)
