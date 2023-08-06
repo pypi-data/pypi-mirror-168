cd6969232f1fd5f8d116f9a6551dec8c671d14ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:20 +0000

    Add package damo-mysql-exec/1/damo_mysql_exec-1-py3-none-any.whl

---
+++ packages/damo-mysql-exec/damo_mysql_exec-1-py3-none-any.whl/build/lib/build/lib/damo_mysql_exec/__init__.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import pymysql
+def return_sql_info(host,user,passwd,port,sql):
+    conn = pymysql.connect(host=host, user=user, passwd=passwd, port=int(port))
+    # 获取游标
+    global result
+    cursor = conn.cursor()
+    try:
+        # 查询数据
+        # print sql
+        cursor.execute(sql)
+        result = cursor.fetchall()
+    except Exception as e:
+        conn.rollback()  # 事务回滚
+        print('事务处理失败{}'.format(e))
+    else:
+        conn.commit()  # 事务提交
+        # print '事务处理成功'
+    cursor.close()  # 先关闭游标
+    conn.close()  # 再关闭数据库连接
+    # print result
+    return result
+
+
+def return_sql_info_data(host,user,passwd,port,database,sql):
+    conn = pymysql.connect(host=host, user=user, passwd=passwd,database=database, port=int(port))
+    # 获取游标_test
+    global result
+    cursor = conn.cursor()
+    try:
+        # 查询数据
+        # print sql
+        cursor.execute(sql)
+        result = cursor.fetchall()
+    except Exception as e:
+        conn.rollback()  # 事务回滚
+        print(database)
+        print('事务处理失败{}'.format(e))
+    else:
+        conn.commit()  # 事务提交
+        # print '事务处理成功'
+    cursor.close()  # 先关闭游标
+    conn.close()  # 再关闭数据库连接
+    # print result
+    return result
+def test():
+    print("test succeed!!!")
