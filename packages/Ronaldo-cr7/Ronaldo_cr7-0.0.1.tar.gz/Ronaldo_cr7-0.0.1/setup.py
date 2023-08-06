12828f5c8d9fdf3783b920de280ba5097c929d13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:35 +0000

    Add package Ronaldo-cr7/0.0.1/Ronaldo_cr7-0.0.1.tar.gz

---
+++ packages/Ronaldo-cr7/Ronaldo_cr7-0.0.1.tar.gz/Ronaldo_cr7-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+classifiers = [
+   'Development Status:: 5 - Production/Stable',
+   'Intended Audience :: Education',
+   'Operating System :: Microsoft :: Windows :: Windows 10',
+   'Topic :: Utilities',
+   'License :: OSI Approved :: MIT License',
+   'Programming Language :: Python 3.0.9',
+]
+setup(
+    name='Ronaldo_cr7',
+    version='0.0.1',
+    description='this is a dumb package used for dumb calculation.',
+    url='',
+    author='RISHI CHOWDARY',
+    author_email='rishi1718.peram@gmail.com',
+    licence='MIT',
+    packages=['Ronaldo_cr7'],
+    install_requires=[]
+)
