a1d480e3105ca71fe2f1722892460b13b9672db5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:30 +0000

    Add package distributions-mini/0.1/distributions_mini-0.1.tar.gz

---
+++ packages/distributions-mini/distributions_mini-0.1.tar.gz/distributions_mini-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='distributions_mini',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_mini'],
+      author= 'Idris Aminat',
+      authors_email='idris.oaminat@gmail.com',
+      zip_safe=False
+      )
