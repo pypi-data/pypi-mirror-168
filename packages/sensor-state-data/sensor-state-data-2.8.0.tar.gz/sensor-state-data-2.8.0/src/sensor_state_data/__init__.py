576b574eb9479cfb5788c0788126331c222aa985
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:11 +0000

    Add package sensor-state-data/2.8.0/sensor-state-data-2.8.0.tar.gz

---
+++ packages/sensor-state-data/sensor-state-data-2.8.0.tar.gz/sensor-state-data-2.8.0/src/sensor_state_data/__init__.py
@@ -0,0 +1,30 @@
+__version__ = "2.8.0"
+
+from .base import BaseDeviceClass
+from .binary_sensor.device_class import BinarySensorDeviceClass
+from .data import SensorData, SensorDeviceInfo, SensorUpdate
+from .description import BinarySensorDescription, SensorDescription
+from .device import DeviceKey
+from .device_class import DeviceClass
+from .library import SensorLibrary
+from .sensor.device_class import SensorDeviceClass
+from .units import Units
+from .value import BaseValue, BinarySensorValue, SensorValue
+
+__all__ = [
+    "BaseDeviceClass",
+    "BaseValue",
+    "BinarySensorValue",
+    "BinarySensorDescription",
+    "BinarySensorDeviceClass",
+    "DeviceClass",
+    "DeviceKey",
+    "SensorData",
+    "SensorDescription",
+    "SensorDeviceClass",
+    "SensorDeviceInfo",
+    "SensorLibrary",
+    "SensorUpdate",
+    "SensorValue",
+    "Units",
+]
