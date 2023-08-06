916f85eb3c1cd1f66afe8f0794f74e7a251cfe4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:25 +0000

    Add package pre-commit-vauxoo/5.2.1/pre-commit-vauxoo-5.2.1.tar.gz

---
+++ packages/pre-commit-vauxoo/pre-commit-vauxoo-5.2.1.tar.gz/pre-commit-vauxoo-5.2.1/tests/test_pre_commit_vauxoo.py
@@ -0,0 +1,155 @@
+import logging
+import os
+import shutil
+import subprocess
+import sys
+import tempfile
+import unittest
+from contextlib import contextmanager
+
+from click.testing import CliRunner
+
+from pre_commit_vauxoo.cli import main
+
+
+class TestPreCommitVauxoo(unittest.TestCase):
+    def setUp(self):
+        super().setUp()
+        self.old_environ = os.environ.copy()
+        self.original_work_dir = os.getcwd()
+        self.tmp_dir = tempfile.mkdtemp(suffix="_pre_commit_vauxoo")
+        os.chdir(self.tmp_dir)
+        self.runner = CliRunner()
+        src_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "..", "resources")
+        self.create_dummy_repo(src_path, self.tmp_dir)
+        self.maxDiff = None
+        os.environ["EXCLUDE_AUTOFIX"] = "resources/module_autofix1"
+
+    def create_dummy_repo(self, src_path, dest_path):
+        subprocess.call(["git", "init", "--initial-branch=main", dest_path])
+        dest_subpath = os.path.join(dest_path, os.path.basename(src_path))
+        shutil.copytree(src_path, dest_subpath)
+        subprocess.call(["git", "add", dest_subpath])
+
+    def tearDown(self):
+        super().tearDown()
+        # change to original work dir
+        os.chdir(self.original_work_dir)
+        # Cleanup temporary files
+        if os.path.isdir(self.tmp_dir) and self.tmp_dir != "/":
+            shutil.rmtree(self.tmp_dir, ignore_errors=True)
+        # reset environment variables
+        os.environ.clear()
+        os.environ.update(self.old_environ)
+
+    @contextmanager
+    def custom_assert_logs(self, module, level, expected_logs):
+        if (sys.version_info.major, sys.version_info.minor) >= (3, 4):
+            with self.assertLogs(module, level=level) as logs:
+                yield
+            logger = logging.getLogger(module)
+            level_no = getattr(logging, level)
+            for log in logs.output:
+                logger.log(level_no, log)
+            diff = set(expected_logs) - set(logs.output)
+            self.assertFalse(diff, "Logs expected not raised %s" % diff)
+        else:
+            # bypassing for dual compatibility with py<3.4
+            yield
+
+    def test_basic(self):
+        os.environ["INCLUDE_LINT"] = "resources"
+        os.environ["PRECOMMIT_HOOKS_TYPE"] = "all"
+        result = self.runner.invoke(main, [])
+        self.assertEqual(result.exit_code, 0, "Exited with error %s" % result.output)
+        with open(os.path.join(self.tmp_dir, "pyproject.toml"), "r") as f_pyproject:
+            self.assertIn("skip-string-normalization=false", f_pyproject, "Skip string normalization not set")
+
+    def test_chdir(self):
+        self.runner = CliRunner()
+        os.environ["PRECOMMIT_HOOKS_TYPE"] = "all"
+        os.chdir("resources")
+        expected_logs = ["WARNING:pre-commit-vauxoo:Running in current directory 'resources'"]
+        with self.custom_assert_logs("pre-commit-vauxoo", level="WARNING", expected_logs=expected_logs):
+            result = self.runner.invoke(main, [])
+        self.assertEqual(result.exit_code, 0, "Exited with error %s" % result.output)
+
+    def test_exclude_lint_path(self):
+        self.runner = CliRunner()
+        os.chdir("resources")
+        os.environ["PRECOMMIT_HOOKS_TYPE"] = "all"
+        os.environ["BLACK_SKIP_STRING_NORMALIZATION"] = "false"
+        os.environ["EXCLUDE_LINT"] = "resources/module_example1/models"
+        result = self.runner.invoke(main, [])
+        self.assertEqual(result.exit_code, 0, "Exited with error %s" % result.output)
+        with open(os.path.join(self.tmp_dir, "pyproject.toml"), "r") as f_pyproject:
+            self.assertIn("skip-string-normalization=false", f_pyproject, "Skip string normalization not set")
+
+    def test_disable_lints(self):
+        self.runner = CliRunner()
+        os.environ["DISABLE_PYLINT_CHECKS"] = "import-error"
+        result = self.runner.invoke(main, [])
+        self.assertEqual(result.exit_code, 0, "Exited with error %s" % result.output)
+
+    def test_exclude_autofix(self):
+        self.runner = CliRunner()
+        os.chdir("resources")
+        os.environ["PRECOMMIT_HOOKS_TYPE"] = "all"
+        os.environ["EXCLUDE_AUTOFIX"] = "resources/module_example1/demo/"
+        os.environ["BLACK_SKIP_STRING_NORMALIZATION"] = "true"
+        result = self.runner.invoke(main, [])
+        self.assertEqual(result.exit_code, 0, "Exited with error %s" % result.output)
+        with open(os.path.join(self.tmp_dir, "pyproject.toml"), "r") as f_pyproject:
+            self.assertIn("skip-string-normalization=true", f_pyproject, "Skip string normalization not set")
+
+    def test_fail_warning(self):
+        os.environ["PRECOMMIT_FAIL_OPTIONAL"] = "1"
+        # Only optional
+        os.environ["PRECOMMIT_HOOKS_TYPE"] = "optional"
+        expected_logs = ["ERROR:pre-commit-vauxoo:Optional checks failed"]
+        with self.custom_assert_logs("pre-commit-vauxoo", level="ERROR", expected_logs=expected_logs):
+            result = self.runner.invoke(main, [])
+        self.assertEqual(result.exit_code, 1, "Exited without error")
+
+    def test_rm_options(self):
+        # Only mandatory
+        os.environ["PRECOMMIT_HOOKS_TYPE"] = "all,-optional,-fix,-experimental"
+        result = self.runner.invoke(main, [])
+        self.assertEqual(result.exit_code, 0, "Exited with error %s" % result.output)
+
+    def test_install_git_hook_pre_commit(self):
+        """Test .git/hooks/pre-commit script"""
+        git_hook_pre_commit = os.path.join(self.tmp_dir, ".git", "hooks", "pre-commit")
+        self.assertFalse(os.path.isfile(git_hook_pre_commit), "File created before to install it")
+        result = self.runner.invoke(main, ["--install"])
+        self.assertEqual(result.exit_code, 0, "Exited with error %s" % result.output)
+        self.assertTrue(os.path.isfile(git_hook_pre_commit), "File not created")
+        with open(git_hook_pre_commit, "r") as f_git_hook_pre_commit:
+            self.assertIn("pre-commit-vauxoo", f_git_hook_pre_commit.read(), "File pre-commit not generated correctly")
+        os.environ["NOLINT"] = "0"
+        exit_code = subprocess.call(
+            [
+                "git",
+                "-c",
+                "user.name='Test'",
+                "-c",
+                "user.email=test@vauxoo.com",
+                "commit",
+                "--allow-empty",
+                "-am",
+                "testing",
+            ]
+        )
+        self.assertEqual(exit_code, 0, "Exited with error_code %s" % exit_code)
+
+    def test_autofixes(self):
+        os.environ["PRECOMMIT_HOOKS_TYPE"] = "all"
+        os.environ["EXCLUDE_AUTOFIX"] = ""
+        expected_logs = ["ERROR:pre-commit-vauxoo:Autofix checks reformatted"]
+        with self.custom_assert_logs("pre-commit-vauxoo", level="ERROR", expected_logs=expected_logs):
+            result = self.runner.invoke(main, [])
+        self.assertEqual(result.exit_code, 1, "Exited with error %s" % result.output)
+
+
+if __name__ == "__main__":
+    unittest.main()
