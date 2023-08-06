ef9b9c269816d8d5400d3ae57a84838cf815980f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:54 +0000

    Add package observable-jupyter/0.1.14/observable_jupyter-0.1.14-py3-none-any.whl

---
+++ packages/observable-jupyter/observable_jupyter-0.1.14-py3-none-any.whl/observable_jupyter/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for observable-jupyter-embed."""
+
+__author__ = """Thomas Ballinger"""
+__email__ = "me@ballingt.com"
+__version__ = "0.1.14"
+
+from .jupyter_embed import embed
