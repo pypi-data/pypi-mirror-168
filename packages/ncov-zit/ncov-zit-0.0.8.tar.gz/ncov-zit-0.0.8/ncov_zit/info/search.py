1719f300062a894761e907ee3f47fdf78ba7fa9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:38 +0000

    Add package ncov-zit/0.0.8/ncov-zit-0.0.8.tar.gz

---
+++ packages/ncov-zit/ncov-zit-0.0.8.tar.gz/ncov-zit-0.0.8/ncov_zit/info/search.py
@@ -0,0 +1,183 @@
+'''
+Author: WildboarG
+version: 1.0
+Date: 2022-08-23 17:17:33
+LastEditors: WildboarG
+LastEditTime: 2022-08-30 22:49:26
+Descripttion: 
+'''
+
+from info import *
+from rich.table import Table
+from rich import print
+
+class Search:
+    def __init__(self, user : str,password  : str) -> None:
+        self.user = user
+        self.password = password
+        self.s = requests.session()
+        self.params = {
+            "m":"user",
+            "a":"userLogin",
+            "password":self.password,
+            "student_number":self.user,
+            "school":schoolcode,
+            "device_type":"android"
+        }
+    
+    def _require_verify(self):
+        return self.s.get(url=get_verify_url,params=self.params)
+    
+    def _handle_verify(self) -> json:
+        return json.loads(self._require_verify().text)
+    
+    def _parse_data(self) -> dict:
+        return self._handle_verify().get("data")
+    
+    def _get_verify(self) -> str:
+        return self._parse_data()["verify"]
+        
+    
+    def _get_path(self)->list:
+        my_info = self._parse_data()["my_info"]
+        path = my_info["path"]
+        boost = path.split(",")
+        return boost
+
+
+    ## school
+    def _get_school(self)->str:
+        return self._get_path()[1]
+
+    ## college
+    def _get_college(self) ->str:
+        return self._get_path()[2]
+
+    ## organization
+    def _get_organization(self) ->str:
+        return self._get_path()[3]
+    
+    ## my class
+    def _get_class(self) ->str:
+        return self._get_path()[4]
+    
+    ## get verify school college organization myclass
+    def set_verify(self):
+        try:
+            self.verify = self._get_verify()
+            self.school = self._get_school()
+            self.college = self._get_college()
+            self.organization = self._get_organization()
+            self.myclass = self._get_class()
+            #print(self.school,self.college,self.organization,self.myclass)
+            return 
+        except:
+            return "No verify"
+        
+    def _require_post(self,org_id):
+        header = {
+            'Content-Type':'application/json',
+            'Cookie':self.cookie,
+        }
+        postdata = {
+            "type":"org",
+            "identity":"student",
+            "para":{
+            "organization_id":org_id,
+            "organization_path_str":"0",
+            },
+            "date":self.date,
+            "activityid":activityid,
+            "flag":0,
+            "domain":schoolcode,
+            "stucode":self.user,
+        }
+        return requests.post(url=get_search_url,headers=header,json=postdata)
+
+    def _mode_choose(self,idd)->str:
+        if idd == "3":
+            return str(self.organization)
+        if idd == "2":
+            return str(self.college)
+        if idd == "1":
+            return str(self.school)
+        if idd == "4":
+            return str(self.myclass)
+        else:
+            return str(idd)  ## 自定义id14122-
+        
+    ## get the punching environment
+    def output_user(self,data):
+        try:
+            allorgUserCount = data["orgUserCount"] #总人数
+            allreportCount = data["reportCount"] # 已经打卡人数
+
+            user = data.get("users")
+            diagram = Table(show_header=True,header_style="Green")
+
+            diagram.add_column("Username",justify="center")
+            diagram.add_column("Userid",justify="center")
+            diagram.add_column("Gender",justify="center")
+            diagram.add_column("Punch",justify="center")
+
+            for mumber in user:
+                user_name = mumber.get("user_name")
+                user_id =mumber.get("user_id")
+                user_sex = mumber.get("user_sex")
+                isreport = mumber.get("is_report")
+                diagram.add_row(str(user_name),str(user_id),str(user_sex),str(isreport))
+
+            return diagram
+        except:
+            return 
+    def output_tree(self,data):
+        #print(data)
+        try:
+            allorgUserCount = data["orgUserCount"] #总人数
+            allreportCount = data["reportCount"] # 已经打卡人数
+
+            tree = data["tree"]
+            chart = Table(show_header=True,header_style="bold magenta")
+
+            chart.add_column("Tree_id",justify="center")
+            chart.add_column("Org_name",justify="center")
+            chart.add_column("Count",justify="center")
+            chart.add_column("Signed",justify="center")
+                
+            for mumber in tree:
+                tree_id = mumber["tree_id"]
+                org_name = mumber["tree_name"]
+                orgUserCount = mumber["orgUserCount"]
+                reportCount = mumber["reportCount"]
+                chart.add_row(str(tree_id),str(org_name),str(orgUserCount),str(reportCount))
+            return chart
+        except:
+            return "[E] :Faild to get chart"
+    
+    ## cookie 必选
+    ## orgid 可选参数 默认为class  可选 1 2 3 或者已经知道的班级或组织id
+    ## orgtype 可选参数 默认是user 如 组织类型不是班级将改为tree
+    ## dafaultdate 可选参数 默认是今天  格式 "2022-08-23"
+    
+    def get_feedback_user(self,cookie : str,classid:str,dafaultdate=time.strftime("%Y-%m-%d")):
+        self.set_verify()
+        self.date = dafaultdate
+        self.cookie = cookie
+        self.classid = self._mode_choose(classid)
+        res = json.loads(self._require_post(self.classid).text)
+        data = res.get("data")
+        graphic = self.output_user(data)
+        print(graphic)
+       
+
+    def get_feedback_tree(self,cookie : str, orgid :str,dafaultdate=time.strftime("%Y-%m-%d")):
+        self.set_verify()
+        self.date = dafaultdate
+        self.cookie = cookie
+        self.org_id = self._mode_choose(orgid)
+        res = json.loads(self._require_post(self.org_id).text)
+        data = res.get("data")
+        graphic = self.output_tree(data)
+        print(graphic)
+       
+
---
+++ packages/ncov-zit/ncov-zit-0.0.8.tar.gz/ncov-zit-0.0.8/ncov_zit/main.py
@@ -0,0 +1,292 @@
+'''
+Author: WildboarG
+version: 1.0
+Date: 2022-05-27 15:17:19
+LastEditors: WildboarG
+LastEditTime: 2022-09-23 17:03:36
+Descripttion: 
+'''
+import argparse
+import csv
+import json
+import time
+import queue
+import threading
+
+
+from rich import print
+from rich.table import Table
+
+from .info.login import GetContent
+from .info.post import sign as signn
+from .info.search import Search
+
+class User:
+    def __init__(self,schoolcode:str,username:str,password:str) -> None:
+        self.schoolcode = schoolcode
+        self.username = username
+        self.password = password
+        self.q = queue.Queue()
+
+
+    def _get_data(self,information:GetContent):
+        return information.reporter()
+
+    def __signed(self,username,password):
+        info = []
+        info.append(username)
+        sign_content = GetContent(
+            schoolcode = self.schoolcode,
+            username = username,
+            password = password,
+        )
+        cook = sign_content.rel_cookie()
+        data = sign_content.reporter(cook,mylocal='0')
+        ## return status
+        result = signn(data,cook)
+        info.append(result)
+        self.q.put(info)
+    
+      
+    
+    def _output_table(self,information :list)->Table:
+        table = Table(show_header=True,header_style="bold magenta")
+        for i in information:
+            table.add_column(i,justify="center")
+        return table
+    
+    def _print_table(self,table:Table)->str:
+        print(table)
+
+    def _json_crate_Threads(self,jsonfile:list):
+        threads=[]
+        for row in jsonfile:
+            user = row["username"]
+            password = row["password"]
+            print(user+password)
+            try:
+                threads.append(
+                        threading.Thread(
+                            target=self.__signed,
+                            args=(user,password
+                            )
+                        )
+                    )
+            except:
+                pass
+        for t in threads:
+            t.start()
+        
+        for t in threads:
+            t.join()
+
+
+    def _cvs_crate_Threads(self,rows:list):
+        threads=[]
+        for row in rows:
+            #print(row[0],row[1])
+            try:
+                threads.append(
+                        threading.Thread(
+                            target=self.__signed,
+                            args=(str(row[0]),str(row[1])
+                            )
+                        )
+                    )
+            except:
+                pass
+        for t in threads:
+            t.start()
+        
+        for t in threads:
+            t.join()
+    
+    def csv_sign(self,csvfire):
+        ## 创建队列，返回队列对象
+        with open(csvfire,"r") as f:
+            reader = csv.reader(f)
+            readlist = list(reader)
+        
+        res = []        
+        self._cvs_crate_Threads(readlist)
+
+        while not self.q.empty():
+            res.append(self.q.get())
+        
+
+        table = self._output_table(["ID","Status","Node"])
+        for i in res:
+            table.add_row(i[0],i[1],"123")
+
+        self._print_table(table)
+
+    ## json格式
+    def json_sign(self,fire):
+        ## 创建队列，返回队列对象
+        with open(fire,"r") as f:
+            reader = json.load(f)
+                
+        res = []        
+        self._json_crate_Threads(reader)
+
+        while not self.q.empty():
+            res.append(self.q.get())
+        
+
+        table = self._output_table(["ID","Status","Node"])
+        for i in res:
+            table.add_row(i[0],i[1],"123")
+        self._print_table(table)
+
+    ## 查询
+    def find_me(self, myclass="4",date=time.strftime("%Y-%m-%d")):
+        bin = GetContent(
+            schoolcode = self.schoolcode,
+            username = self.username,
+            password = self.password,
+        )
+
+        cook = bin.rel_cookie()
+        _search = Search(user=self.username,password=self.password)
+        _search.get_feedback_user(cookie=cook,classid=myclass,dafaultdate=date)
+        return 
+    def find_org(self,org="1",date=time.strftime("%Y-%m-%d")):
+        bin = GetContent(
+            schoolcode = self.schoolcode,
+            username = self.username,
+            password = self.password,
+        )
+
+        cook = bin.rel_cookie()
+        _search = Search(user=self.username,password=self.password)
+        _search.get_feedback_tree(cookie=cook,orgid=org,dafaultdate=date)
+        return 
+    
+    def test(self,mylocal="0"):
+        my = GetContent(
+            schoolcode = self.schoolcode,
+            username = self.username,
+            password = self.password,
+
+        )
+        cook = my.rel_cookie()
+        data = my.reporter(cook,mylocal)
+        print(signn(data,cook))
+        return        
+def main():
+    parser = argparse.ArgumentParser(
+            description=
+    "A cov cli tool for ZIT(zhengzhou Institute of Science and Technology)",
+            epilog=
+            "(c) 2021 mm62633482@gmail.com",
+    )
+    parser.add_argument("-u","--username",help="The username",required=True)
+    parser.add_argument("-p","--password",help="The password",required=True)
+    #parser.add_argument("-f","--file",help = "use json file",required=True) 
+    subparsers = parser.add_subparsers(dest="command")
+    sign = subparsers.add_parser("sign",help="Officially Signed in today")
+    sign.add_argument(
+            "-j",
+            "--json",
+            help="Load a file of type Json"
+            )
+    sign.add_argument(
+            "-c",
+            "--csv",
+            help="Load a file of type CSV"
+            )
+    sign.add_argument(
+            "-a",
+            "--address",   
+            help="Modify your address" 
+    )
+    # search report
+    query = subparsers.add_parser("query",help="query history of the user report")
+    query.add_argument(
+            '-m',
+            '--myclass',
+            help= "Query the specified class"
+            )
+    query.add_argument(
+            '-s',
+            '--start_time',
+            help= "the start time of report history(Year-Month-Day)"
+            )
+
+    org = subparsers.add_parser("org",help="query history of the the organization report")
+    org.add_argument(
+            '-g','--organization_id',
+            help ="""
+            search all organization report:\n
+                    "13096" :   "all", \n
+                    "14223" :   "体育",\n
+                    "14124" :   "电气",\n 
+                    "14125" :   "机械",\n
+                    "14126" :   "计科",\n
+                    "14127" :   "工商",\n
+                    "14129" :   "艺术",\n
+                    "14131" :   "中专",\n
+                    "14132" :   "土木",\n
+                    "14133" :   "交通",\n
+                    "14134" :   "食科",\n
+                    "14135" :   "舞蹈",\n
+                    "14136" :   "外语",\n
+                    "14137" :   "国标",\n
+                    "14139" :   "空乘",\n
+            """)
+    org.add_argument(
+            '-s','--start_time',
+            help = "the start time of report history(Year-Month-Day)"
+            )
+
+    args = parser.parse_args()
+
+    ## check command
+    if not args.command or args.command not in {"query","org","sign"}:
+        parser.print_help()
+        exit()
+    ## new a zit user obj
+    user = User("zit",args.username,args.password)
+    
+    if args.command == "sign":
+        if args.json:
+            user.json_sign(args.json)
+            exit()
+        if args.csv:
+            user.csv_sign(args.csv)
+            exit()
+        if args.address:
+            user.test(mylocal=args.address)
+            exit()
+        else:
+            user.test()
+            exit()
+
+    if args.command == "query":
+        if args.myclass and args.start_time:
+            user.find_me(myclass=args.myclass,date=args.start_time)
+        if args.myclass:
+            user.find_me(myclass=args.myclass)
+            exit()
+        if args.start_time:
+            user.find_me(date=args.start_time)
+            exit()
+        else:
+            user.find_me()
+            exit()
+    if args.command == "org":
+        if args.organization_id and args.start_time:
+            user.find_org(org=args.organization_id,date=args.start_time)
+            exit()
+        if args.organization_id:
+            user.find_org(org=args.organization_id)
+            exit()
+        if args.start_time:
+            user.find_org(date=args.start_time)
+            exit()
+        else:
+            user.find_org()
+        exit()
+
+if __name__ == "__main__":
+    main()
