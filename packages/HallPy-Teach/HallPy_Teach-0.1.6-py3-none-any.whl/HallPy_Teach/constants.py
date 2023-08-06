54e76cde66e71e2c8021ee34b297fbebbddf1fa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:59 +0000

    Add package HallPy-Teach/0.1.6/HallPy_Teach-0.1.6-py3-none-any.whl

---
+++ packages/HallPy-Teach/HallPy_Teach-0.1.6-py3-none-any.whl/HallPy_Teach/constants.py
@@ -0,0 +1,17 @@
+
+supportedInstruments = {
+    "Power Supply": ["TENMA 72-2710"],
+    "LCR Meter": ["B&K Precision ,891"],
+    "Multimeter": ["KEITHLEY INSTRUMENTS INC.,MODEL 2110", "GWInstek,GDM8342"]
+}
+"""List of supported instruments sorted by the type of instruments
+"""
+
+serialRegex = {
+    "TENMA 72-2710": r"SN:[0-9]{8}",
+    "GWInstek,GDM8342": r"[A-Z]{3}[0-9]{6}",
+    "B&K Precision ,891": r"[0-9A-Z]{9}",
+    "KEITHLEY INSTRUMENTS INC.,MODEL 2110": r"[0-9]{7}"
+}
+"""Regular expressions to look for serial numbers of specific devices
+"""
