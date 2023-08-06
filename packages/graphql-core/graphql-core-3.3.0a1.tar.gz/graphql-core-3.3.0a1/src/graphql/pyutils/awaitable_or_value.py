405e586cdfca24d0563e0646093a4747d909113d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:47 +0000

    Add package graphql-core/3.3.0a1/graphql-core-3.3.0a1.tar.gz

---
+++ packages/graphql-core/graphql-core-3.3.0a1.tar.gz/graphql-core-3.3.0a1/src/graphql/pyutils/awaitable_or_value.py
@@ -0,0 +1,9 @@
+from typing import Awaitable, TypeVar, Union
+
+
+__all__ = ["AwaitableOrValue"]
+
+
+T = TypeVar("T")
+
+AwaitableOrValue = Union[Awaitable[T], T]
---
+++ packages/graphql-core/graphql-core-3.3.0a1.tar.gz/graphql-core-3.3.0a1/tests/utils/__init__.py
@@ -0,0 +1,7 @@
+"""Test utilities"""
+
+from .dedent import dedent
+from .gen_fuzz_strings import gen_fuzz_strings
+
+
+__all__ = ["dedent", "gen_fuzz_strings"]
