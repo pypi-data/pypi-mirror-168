966598e3919f2641578a33f94fb56a12e850c827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:10 +0000

    Add package ezaws/0.23/ezaws-0.23.tar.gz

---
+++ packages/ezaws/ezaws-0.23.tar.gz/ezaws-0.23/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="ezaws",
+    version="0.23",
+    package_dir={"ezaws": "src/ezaws"},
+    packages=["ezaws"],
+    description="An easy to use interface to AWS, ezaws.",
+    url="https://github.com/saidvandeklundert/ezaws",
+    classifiers=["Programming Language :: Python :: 3"],
+    python_requires=">=3.10, <4",
+    project_urls={
+        "Bug Reports": "https://github.com/saidvandeklundert/ezaws/issues",
+        "Source": "https://github.com/saidvandeklundert/ezaws",
+        "Documentation": "https://github.com/saidvandeklundert/ezaws",
+    },
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
