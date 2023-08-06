be3f8dea11ff109e6c6bf0156cc6ffbd5b16379a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:42 +0000

    Add package superior6564/0.1.2/superior6564-0.1.2.tar.gz

---
+++ packages/superior6564/superior6564-0.1.2.tar.gz/superior6564-0.1.2/README.md
@@ -0,0 +1,29 @@
+Version of package is 0.1.2
+
+PyPi can have a newer version than GitHub.
+
+https://pypi.org/project/superior6564/#history
+
+## Installation
+
+Install the current version with PyPI:
+
+pip install superior6564
+
+Or from Github:
+
+pip install https://github.com/Superior-GitHub/Superior6564/archive/refs/heads/main.zip
+
+## Usage
+
+Before using you need to run packages.required()
+
+It will check for the necessary libraries and install them if needed.
+
+## Contributing
+
+Bug reports and/or pull requests are welcome
+
+## License 
+
+The module is available as open source under the terms of the [Apache License, Version 2.0](https://opensource.org/licenses/Apache-2.0)
---
+++ packages/superior6564/superior6564-0.1.2.tar.gz/superior6564-0.1.2/superior6564/packages.py
@@ -0,0 +1,34 @@
+"""
+:authors: Superior_6564
+:license: Apache License, Version 2.0, see LICENSE file
+:copyright: (c) 2022 Superior_6564
+"""
+
+import subprocess
+import sys
+import os
+import time
+
+
+def install(package: str, check=True):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", package])
+    if check:
+        if os.getcwd() == "/content":
+            package = "pip show " + package
+            get_ipython().system(package)
+
+
+def pip_upgrade():
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "pip"])
+
+
+def required():
+    install("python-time")
+    require = ["requests", "opencv-python", "progress", "dearpygui"]
+    print("Установка необходимых библиотек...")
+    for i in range(len(require)):
+        install(require[i], check=False)
+        print(f"Status: {i + 1} of {len(require)}")
+    print("Установка завершена.")
+    time.sleep(1)
