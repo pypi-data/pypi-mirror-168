3e6b572c8bab0149e8074b57c3a81e54b37b182a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:29 +0000

    Add package fast-sqlalchemy/0.11.5/fast_sqlalchemy-0.11.5-py3-none-any.whl

---
+++ packages/fast-sqlalchemy/fast_sqlalchemy-0.11.5-py3-none-any.whl/fast_sqlalchemy/event_bus/contexts.py
@@ -0,0 +1,23 @@
+import contextlib, logging
+from contextvars import ContextVar
+from typing import Set
+from fast_sqlalchemy.event_bus.bus import EventBus
+
+logger = logging.getLogger(__name__)
+
+
+_event_queue: ContextVar = ContextVar("event_queue", default=[])
+
+event_bus_store: Set[EventBus] = set()
+
+@contextlib.contextmanager
+def event_queue_ctx():
+    logger.debug("event queue set")
+    token = _event_queue.set([])
+    try:
+        yield
+    finally:
+        logger.debug("event queue reset")
+        _event_queue.reset(token)
+
+
