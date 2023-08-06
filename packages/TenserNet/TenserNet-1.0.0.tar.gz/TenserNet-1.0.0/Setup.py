dab73aa3ba4d1c17e4f1f6230ebf5a05e93e6438
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:23 +0000

    Add package TenserNet/1.0.0/TenserNet-1.0.0.tar.gz

---
+++ packages/TenserNet/TenserNet-1.0.0.tar.gz/TenserNet-1.0.0/Setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='TenserNet',
+    version='1.0.0',
+    description='This Module is Just for Holding Package Name For Now',
+    url='',
+    author='Parth Patel',
+    author_email='TenserNet@gmail.com',
+    license='MIT',
+    keywords='This Module is Just for Holding Package Name For Now',
+    packages=find_packages(),
+    install_requires=['']
+)
---
+++ packages/TenserNet/TenserNet-1.0.0.tar.gz/TenserNet-1.0.0/TenserNet/__init__.py
@@ -0,0 +1,3 @@
+def AboutTenserNet():
+    print("This Module is Just for Holding Package Name For Now")
+
