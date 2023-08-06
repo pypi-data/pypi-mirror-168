98a488586a5944ea783925a38dd1ea42cb1f6aff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:24 +0000

    Add package elementy/0.0.8/elementy-0.0.8-py3-none-any.whl

---
+++ packages/elementy/elementy-0.0.8-py3-none-any.whl/elementy/periodictable.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass, field
+from typing import Dict
+
+import elementy.elements as elements
+from .element import Element
+
+
+def make_periodic_table():
+    periodic_table_elements = {}
+    for element in elements.get_elements():
+        periodic_table_elements[element.symbol] = element
+    return periodic_table_elements
+
+
+@dataclass
+class PeriodicTable:
+    elements: Dict[str, Element] = field(default_factory=make_periodic_table)
