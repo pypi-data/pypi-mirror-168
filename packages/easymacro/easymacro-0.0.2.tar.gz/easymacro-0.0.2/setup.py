cc0a39bece75ad02ba9f452da5d55253417ce03a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:49 +0000

    Add package easymacro/0.0.2/easymacro-0.0.2.tar.gz

---
+++ packages/easymacro/easymacro-0.0.2.tar.gz/easymacro-0.0.2/setup.py
@@ -0,0 +1,24 @@
+import codecs, os
+from setuptools import setup, find_packages
+
+package_path = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(package_path, "README.md"), encoding="utf-8") as f:
+    detailed_desc = "\n" + f.read()
+
+version = "0.0.2"
+description = "Easily make macros in Python!"
+
+setup(
+    name="easymacro",
+    version=version,
+    author="Wurgo, Languste27",
+    author_email="<wurgoalt@gmail.com>",
+    description=description,
+    long_description_content_type="text/markdown",
+    long_description=detailed_desc,
+    packages=find_packages(),
+    install_requires=["pywin32"],
+    keywords=["python", "macro", "windows", "mouse", "keyboard", "input"],
+    classifiers=[]
+)
