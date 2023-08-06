a041994c04f8e34f51f78f1fc71e1748736b244f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:00:01 +0000

    Add package auto-editor/22.37.1/auto-editor-22.37.1.tar.gz

---
+++ packages/auto-editor/auto-editor-22.37.1.tar.gz/auto-editor-22.37.1/auto_editor/formats/premiere.py
@@ -0,0 +1,385 @@
+from __future__ import annotations
+
+import os.path
+from fractions import Fraction
+from os.path import abspath
+from platform import system
+from shutil import move
+from urllib.parse import quote
+
+from auto_editor.output import Ensure
+from auto_editor.timeline import Timeline
+
+from .utils import indent, safe_mkdir
+
+"""
+Premiere Pro uses the Final Cut Pro 7 XML Interchange Format
+
+See docs here:
+https://developer.apple.com/library/archive/documentation/AppleApplications/Reference/FinalCutPro_XML/Elements/Elements.html
+
+Also, Premiere itself will happily output subtlety incorrect XML files that don't
+come back the way they started.
+"""
+
+PIXEL_ASPECT_RATIO = "square"
+COLORDEPTH = "24"
+ANAMORPHIC = "FALSE"
+DEPTH = "16"
+
+
+def fix_url(path: str) -> str:
+    if system() == "Windows":
+        return "file://localhost/" + quote(abspath(path)).replace("%5C", "/")
+    return f"file://localhost{abspath(path)}"
+
+
+def speedup(speed: float) -> str:
+    return indent(
+        6,
+        "<filter>",
+        "\t<effect>",
+        "\t\t<name>Time Remap</name>",
+        "\t\t<effectid>timeremap</effectid>",
+        "\t\t<effectcategory>motion</effectcategory>",
+        "\t\t<effecttype>motion</effecttype>",
+        "\t\t<mediatype>video</mediatype>",
+        '\t\t<parameter authoringApp="PremierePro">',
+        "\t\t\t<parameterid>variablespeed</parameterid>",
+        "\t\t\t<name>variablespeed</name>",
+        "\t\t\t<valuemin>0</valuemin>",
+        "\t\t\t<valuemax>1</valuemax>",
+        "\t\t\t<value>0</value>",
+        "\t\t</parameter>",
+        '\t\t<parameter authoringApp="PremierePro">',
+        "\t\t\t<parameterid>speed</parameterid>",
+        "\t\t\t<name>speed</name>",
+        "\t\t\t<valuemin>-100000</valuemin>",
+        "\t\t\t<valuemax>100000</valuemax>",
+        f"\t\t\t<value>{speed}</value>",
+        "\t\t</parameter>",
+        '\t\t<parameter authoringApp="PremierePro">',
+        "\t\t\t<parameterid>reverse</parameterid>",
+        "\t\t\t<name>reverse</name>",
+        "\t\t\t<value>FALSE</value>",
+        "\t\t</parameter>",
+        '\t\t<parameter authoringApp="PremierePro">',
+        "\t\t\t<parameterid>frameblending</parameterid>",
+        "\t\t\t<name>frameblending</name>",
+        "\t\t\t<value>FALSE</value>",
+        "\t\t</parameter>",
+        "\t</effect>",
+        "</filter>",
+    )
+
+
+def premiere_xml(
+    ensure: Ensure,
+    output: str,
+    timeline: Timeline,
+) -> None:
+
+    src = timeline.sources[0]
+    chunks = timeline.chunks
+
+    if chunks is None:
+        raise ValueError("Timeline too complex")
+
+    fps = timeline.timebase
+    samplerate = timeline.samplerate
+
+    audio_file = len(src.videos) == 0 and len(src.audios) == 1
+
+    # See chart: https://developer.apple.com/library/archive/documentation/AppleApplications/Reference/FinalCutPro_XML/FrameRate/FrameRate.html#//apple_ref/doc/uid/TP30001158-TPXREF103
+
+    if fps == 23.98 or fps == 23.976 or fps == Fraction(24000, 1001):
+        timebase = 24
+        ntsc = "TRUE"
+    elif fps == 29.97 or fps == Fraction(30000, 1001):
+        timebase = 30
+        ntsc = "TRUE"
+    elif fps == 59.94 or fps == 59.94005994:
+        timebase = 60
+        ntsc = "TRUE"
+    else:
+        timebase = int(fps)
+        ntsc = "FALSE"
+
+    duration = chunks[-1][1]
+
+    clips = []
+    for chunk in chunks:
+        if chunk[2] != 99999:
+            clips.append(chunk)
+
+    pathurls = [fix_url(src.path)]
+
+    tracks = len(src.audios)
+
+    if tracks > 1:
+        name_without_extension = src.basename[: src.basename.rfind(".")]
+
+        fold = safe_mkdir(os.path.join(src.dirname, f"{name_without_extension}_tracks"))
+
+        for i in range(1, tracks):
+            newtrack = os.path.join(fold, f"{i}.wav")
+            move(ensure.audio(src.path, 0, i), newtrack)
+            pathurls.append(fix_url(newtrack))
+
+    width, height = timeline.res
+
+    group_name = f"Auto-Editor {'Audio' if audio_file else 'Video'} Group"
+
+    with open(output, "w", encoding="utf-8") as outfile:
+        outfile.write('<?xml version="1.0" encoding="UTF-8"?>\n<!DOCTYPE xmeml>\n')
+        outfile.write('<xmeml version="4">\n')
+        outfile.write("\t<sequence>\n")
+        outfile.write(f"\t\t<name>{group_name}</name>\n")
+        outfile.write(f"\t\t<duration>{duration}</duration>\n")
+        outfile.write("\t\t<rate>\n")
+        outfile.write(f"\t\t\t<timebase>{timebase}</timebase>\n")
+        outfile.write(f"\t\t\t<ntsc>{ntsc}</ntsc>\n")
+        outfile.write("\t\t</rate>\n")
+        outfile.write("\t\t<media>\n")
+        outfile.write(
+            indent(
+                3,
+                "<video>",
+                "\t<format>",
+                "\t\t<samplecharacteristics>",
+            )
+        )
+
+        if len(src.videos) > 0:
+            outfile.write(
+                indent(
+                    3,
+                    "\t\t\t<rate>",
+                    f"\t\t\t\t<timebase>{timebase}</timebase>",
+                    f"\t\t\t\t<ntsc>{ntsc}</ntsc>",
+                    "\t\t\t</rate>",
+                )
+            )
+
+        outfile.write(
+            indent(
+                3,
+                f"\t\t\t<width>{width}</width>",
+                f"\t\t\t<height>{height}</height>",
+                f"\t\t\t<pixelaspectratio>{PIXEL_ASPECT_RATIO}</pixelaspectratio>",
+            )
+        )
+
+        if len(src.videos) > 0:
+            outfile.write(
+                indent(
+                    3,
+                    "\t\t\t<fielddominance>none</fielddominance>",
+                    f"\t\t\t<colordepth>{COLORDEPTH}</colordepth>",
+                )
+            )
+
+        outfile.write(
+            indent(
+                3,
+                "\t\t</samplecharacteristics>",
+                "\t</format>",
+                "</video>" if len(src.videos) == 0 else "\t<track>",
+            )
+        )
+
+        if len(src.videos) > 0:
+            # Handle video clips
+
+            total = 0.0
+            for j, clip in enumerate(clips):
+
+                clip_duration = (clip[1] - clip[0] + 1) / clip[2]
+
+                _start = int(total)
+                _end = int(total) + int(clip_duration)
+                _in = int(clip[0] / clip[2])
+                _out = int(clip[1] / clip[2])
+
+                total += clip_duration
+
+                outfile.write(
+                    indent(
+                        5,
+                        f'<clipitem id="clipitem-{j+1}">',
+                        "\t<masterclipid>masterclip-2</masterclipid>",
+                        f"\t<name>{src.basename}</name>",
+                        f"\t<start>{_start}</start>",
+                        f"\t<end>{_end}</end>",
+                        f"\t<in>{_in}</in>",
+                        f"\t<out>{_out}</out>",
+                    )
+                )
+
+                if j == 0:
+                    outfile.write(
+                        indent(
+                            6,
+                            '<file id="file-1">',
+                            f"\t<name>{src.basename}</name>",
+                            f"\t<pathurl>{pathurls[0]}</pathurl>",
+                            "\t<rate>",
+                            f"\t\t<timebase>{timebase}</timebase>",
+                            f"\t\t<ntsc>{ntsc}</ntsc>",
+                            "\t</rate>",
+                            f"\t<duration>{duration}</duration>",
+                            "\t<media>",
+                            "\t\t<video>",
+                            "\t\t\t<samplecharacteristics>",
+                            "\t\t\t\t<rate>",
+                            f"\t\t\t\t\t<timebase>{timebase}</timebase>",
+                            f"\t\t\t\t\t<ntsc>{ntsc}</ntsc>",
+                            "\t\t\t\t</rate>",
+                            f"\t\t\t\t<width>{width}</width>",
+                            f"\t\t\t\t<height>{height}</height>",
+                            f"\t\t\t\t<anamorphic>{ANAMORPHIC}</anamorphic>",
+                            f"\t\t\t\t<pixelaspectratio>{PIXEL_ASPECT_RATIO}</pixelaspectratio>",
+                            "\t\t\t\t<fielddominance>none</fielddominance>",
+                            "\t\t\t</samplecharacteristics>",
+                            "\t\t</video>",
+                            "\t\t<audio>",
+                            "\t\t\t<samplecharacteristics>",
+                            f"\t\t\t\t<depth>{DEPTH}</depth>",
+                            f"\t\t\t\t<samplerate>{samplerate}</samplerate>",
+                            "\t\t\t</samplecharacteristics>",
+                            "\t\t\t<channelcount>2</channelcount>",
+                            "\t\t</audio>",
+                            "\t</media>",
+                            "</file>",
+                        )
+                    )
+                else:
+                    outfile.write('\t\t\t\t\t\t<file id="file-1"/>\n')
+
+                if clip[2] != 1:
+                    outfile.write(speedup(clip[2] * 100))
+
+                # Linking for video blocks
+                for i in range(max(6, tracks + 1)):
+                    outfile.write("\t\t\t\t\t\t<link>\n")
+                    outfile.write(
+                        f"\t\t\t\t\t\t\t<linkclipref>clipitem-{(i*(len(clips)))+j+1}</linkclipref>\n"
+                    )
+                    if i == 0:
+                        outfile.write("\t\t\t\t\t\t\t<mediatype>video</mediatype>\n")
+                    else:
+                        outfile.write("\t\t\t\t\t\t\t<mediatype>audio</mediatype>\n")
+                    if 1 <= i <= 6:
+                        outfile.write(f"\t\t\t\t\t\t\t<trackindex>{i}</trackindex>\n")
+                    else:
+                        outfile.write("\t\t\t\t\t\t\t<trackindex>1</trackindex>\n")
+                    outfile.write(f"\t\t\t\t\t\t\t<clipindex>{j+1}</clipindex>\n")
+                    if i > 0:
+                        outfile.write("\t\t\t\t\t\t\t<groupindex>1</groupindex>\n")
+                    outfile.write("\t\t\t\t\t\t</link>\n")
+
+                outfile.write("\t\t\t\t\t</clipitem>\n")
+            outfile.write(indent(3, "\t</track>", "</video>"))
+
+        # Audio Clips
+        outfile.write(
+            indent(
+                3,
+                "<audio>",
+                "\t<numOutputChannels>2</numOutputChannels>",
+                "\t<format>",
+                "\t\t<samplecharacteristics>",
+                f"\t\t\t<depth>{DEPTH}</depth>",
+                f"\t\t\t<samplerate>{samplerate}</samplerate>",
+                "\t\t</samplecharacteristics>",
+                "\t</format>",
+            )
+        )
+
+        for t in range(tracks):
+            outfile.write(
+                '\t\t\t\t<track currentExplodedTrackIndex="0" premiereTrackType="Stereo">\n'
+            )
+            total = 0
+            for j, clip in enumerate(clips):
+
+                clip_duration = (clip[1] - clip[0] + 1) / clip[2]
+
+                _start = int(total)
+                _end = int(total) + int(clip_duration)
+                _in = int(clip[0] / clip[2])
+                _out = int(clip[1] / clip[2])
+
+                total += clip_duration
+
+                if audio_file:
+                    clip_item_num = j + 1
+                    master_id = "1"
+                else:
+                    clip_item_num = len(clips) + 1 + j + (t * len(clips))
+                    master_id = "2"
+
+                outfile.write(
+                    indent(
+                        5,
+                        f'<clipitem id="clipitem-{clip_item_num}" premiereChannelType="stereo">',
+                        f"\t<masterclipid>masterclip-{master_id}</masterclipid>",
+                        f"\t<name>{src.basename}</name>",
+                        f"\t<start>{_start}</start>",
+                        f"\t<end>{_end}</end>",
+                        f"\t<in>{_in}</in>",
+                        f"\t<out>{_out}</out>",
+                    )
+                )
+
+                if j == 0 and (audio_file or t > 0):
+                    outfile.write(
+                        indent(
+                            6,
+                            f'<file id="file-{t+1}">',
+                            f"\t<name>{src.basename}</name>",
+                            f"\t<pathurl>{pathurls[t]}</pathurl>",
+                            "\t<rate>",
+                            f"\t\t<timebase>{timebase}</timebase>",
+                            f"\t\t<ntsc>{ntsc}</ntsc>",
+                            "\t</rate>",
+                            "\t<media>",
+                            "\t\t<audio>",
+                            "\t\t\t<samplecharacteristics>",
+                            f"\t\t\t\t<depth>{DEPTH}</depth>",
+                            f"\t\t\t\t<samplerate>{samplerate}</samplerate>",
+                            "\t\t\t</samplecharacteristics>",
+                            "\t\t\t<channelcount>2</channelcount>",
+                            "\t\t</audio>",
+                            "\t</media>",
+                            "</file>",
+                        )
+                    )
+                else:
+                    outfile.write(f'\t\t\t\t\t\t<file id="file-{t+1}"/>\n')
+
+                outfile.write(
+                    indent(
+                        6,
+                        "<sourcetrack>",
+                        "\t<mediatype>audio</mediatype>",
+                        "\t<trackindex>1</trackindex>",
+                        "</sourcetrack>",
+                        "<labels>",
+                        "\t<label2>Iris</label2>",
+                        "</labels>",
+                    )
+                )
+
+                if clip[2] != 1:
+                    outfile.write(speedup(clip[2] * 100))
+
+                outfile.write("\t\t\t\t\t</clipitem>\n")
+            if not audio_file:
+                outfile.write("\t\t\t\t\t<outputchannelindex>1</outputchannelindex>\n")
+            outfile.write("\t\t\t\t</track>\n")
+
+        outfile.write("\t\t\t</audio>\n")
+        outfile.write("\t\t</media>\n")
+        outfile.write("\t</sequence>\n")
+        outfile.write("</xmeml>\n")
---
+++ packages/auto-editor/auto-editor-22.37.1.tar.gz/auto-editor-22.37.1/auto_editor/formats/shotcut.py
@@ -0,0 +1,187 @@
+from __future__ import annotations
+
+from fractions import Fraction
+
+from auto_editor.timeline import Timeline
+from auto_editor.utils.func import aspect_ratio, to_timecode
+
+
+def timecode_to_frames(timecode: str, tb: Fraction) -> int:
+    _h, _m, _s = timecode.split(":")
+    h = int(_h)
+    m = int(_m)
+    s = float(_s)
+    return round((h * 3600 + m * 60 + s) * tb)
+
+
+def shotcut_xml(
+    output: str,
+    timeline: Timeline,
+) -> None:
+    width, height = timeline.res
+    num, den = aspect_ratio(width, height)
+
+    chunks = timeline.chunks
+    if chunks is None:
+        raise ValueError("Timeline too complex")
+    tb = timeline.timebase
+    src = timeline.sources[0]
+
+    global_out = to_timecode(timeline.out_len() / tb, "standard")
+    version = "21.05.18"
+
+    with open(output, "w", encoding="utf-8") as out:
+        out.write('<?xml version="1.0" standalone="no"?>\n')
+        out.write(
+            '<mlt LC_NUMERIC="C" version="7.1.0" '
+            f'title="Shotcut version {version}" producer="main_bin">\n'
+        )
+        out.write(
+            '\t<profile description="automatic" '
+            f'width="{width}" height="{height}" '
+            'progressive="1" sample_aspect_num="1" sample_aspect_den="1" '
+            f'display_aspect_num="{num}" display_aspect_den="{den}" '
+            f'frame_rate_num="{tb.numerator}" frame_rate_den="{tb.denominator}" colorspace="709"/>\n'
+        )
+        out.write('\t<playlist id="main_bin">\n')
+        out.write('\t\t<property name="xml_retain">1</property>\n')
+        out.write("\t</playlist>\n")
+
+        # out was the new video length in the original xml
+        out.write(f'\t<producer id="black" in="00:00:00.000" out="{global_out}">\n')
+        out.write(f'\t\t<property name="length">{global_out}</property>\n')
+        out.write('\t\t<property name="eof">pause</property>\n')
+        out.write('\t\t<property name="resource">0</property>\n')
+        out.write('\t\t<property name="aspect_ratio">1</property>\n')
+        out.write('\t\t<property name="mlt_service">color</property>\n')
+        out.write('\t\t<property name="mlt_image_format">rgba</property>\n')
+        out.write('\t\t<property name="set.test_audio">0</property>\n')
+        out.write("\t</producer>\n")
+
+        out.write('\t<playlist id="background">\n')  # same for this out too.
+        out.write(
+            f'\t\t<entry producer="black" in="00:00:00.000" out="{global_out}"/>\n'
+        )
+        out.write("\t</playlist>\n")
+
+        chains = 0
+        producers = 0
+
+        # Speeds like [1.5, 3] don't work because of duration issues, too bad!
+
+        for clip in chunks:
+            if clip[2] == 99999:
+                continue
+
+            speed = clip[2]
+
+            _out = to_timecode(clip[1] / speed / tb, "standard")
+            length = to_timecode((clip[1] / speed + 1) / tb, "standard")
+
+            if speed == 1:
+                resource = src.path
+                caption = src.basename
+                out.write(f'\t<chain id="chain{chains}" out="{_out}">\n')
+            else:
+                resource = f"{speed}:{src.path}"
+                caption = f"{src.basename} ({speed}x)"
+                out.write(
+                    f'\t<producer id="producer{producers}" in="00:00:00.000" out="{_out}">\n'
+                )
+                producers += 1
+            chains += 1
+
+            out.write(f'\t\t<property name="length">{length}</property>\n')
+            out.write('\t\t<property name="eof">pause</property>\n')
+            out.write(f'\t\t<property name="resource">{resource}</property>\n')
+
+            if speed == 1:
+                out.write(
+                    '\t\t<property name="mlt_service">avformat-novalidate</property>\n'
+                )
+                out.write('\t\t<property name="seekable">1</property>\n')
+                out.write('\t\t<property name="audio_index">1</property>\n')
+                out.write('\t\t<property name="video_index">0</property>\n')
+                out.write('\t\t<property name="mute_on_pause">0</property>\n')
+                out.write(
+                    f'\t\t<property name="shotcut:caption">{caption}</property>\n'
+                )
+                out.write('\t\t<property name="xml">was here</property>\n')
+            else:
+                out.write('\t\t<property name="aspect_ratio">1</property>\n')
+                out.write('\t\t<property name="seekable">1</property>\n')
+                out.write('\t\t<property name="audio_index">1</property>\n')
+                out.write('\t\t<property name="video_index">0</property>\n')
+                out.write('\t\t<property name="mute_on_pause">1</property>\n')
+                out.write(f'\t\t<property name="warp_speed">{speed}</property>\n')
+                out.write(f'\t\t<property name="warp_resource">{src.path}</property>\n')
+                out.write('\t\t<property name="mlt_service">timewarp</property>\n')
+                out.write('\t\t<property name="shotcut:producer">avformat</property>\n')
+                out.write('\t\t<property name="video_delay">0</property>\n')
+                out.write(
+                    f'\t\t<property name="shotcut:caption">{caption}</property>\n'
+                )
+                out.write('\t\t<property name="xml">was here</property>\n')
+                out.write('\t\t<property name="warp_pitch">1</property>\n')
+
+            out.write("\t</chain>\n" if speed == 1 else "\t</producer>\n")
+
+        out.write('\t<playlist id="playlist0">\n')
+        out.write('\t\t<property name="shotcut:video">1</property>\n')
+        out.write('\t\t<property name="shotcut:name">V1</property>\n')
+
+        producers = 0
+        i = 0
+        for clip in chunks:
+            if clip[2] == 99999:
+                continue
+
+            speed = clip[2]
+
+            if speed == 1:
+                in_len: float = clip[0] - 1
+            else:
+                in_len = max(clip[0] / speed, 0)
+
+            out_len = max((clip[1] - 2) / speed, 0)
+
+            _in = to_timecode(in_len / tb, "standard")
+            _out = to_timecode(out_len / tb, "standard")
+
+            tag_name = f"chain{i}"
+            if speed != 1:
+                tag_name = f"producer{producers}"
+                producers += 1
+
+            out.write(f'\t\t<entry producer="{tag_name}" in="{_in}" out="{_out}"/>\n')
+            i += 1
+
+        out.write("\t</playlist>\n")
+
+        out.write(
+            f'\t<tractor id="tractor0" title="Shotcut version {version}" '
+            f'in="00:00:00.000" out="{global_out}">\n'
+        )
+        out.write('\t\t<property name="shotcut">1</property>\n')
+        out.write('\t\t<property name="shotcut:projectAudioChannels">2</property>\n')
+        out.write('\t\t<property name="shotcut:projectFolder">0</property>\n')
+        out.write('\t\t<track producer="background"/>\n')
+        out.write('\t\t<track producer="playlist0"/>\n')
+        out.write('\t\t<transition id="transition0">\n')
+        out.write('\t\t\t<property name="a_track">0</property>\n')
+        out.write('\t\t\t<property name="b_track">1</property>\n')
+        out.write('\t\t\t<property name="mlt_service">mix</property>\n')
+        out.write('\t\t\t<property name="always_active">1</property>\n')
+        out.write('\t\t\t<property name="sum">1</property>\n')
+        out.write("\t\t</transition>\n")
+        out.write('\t\t<transition id="transition1">\n')
+        out.write('\t\t\t<property name="a_track">0</property>\n')
+        out.write('\t\t\t<property name="b_track">1</property>\n')
+        out.write('\t\t\t<property name="version">0.9</property>\n')
+        out.write('\t\t\t<property name="mlt_service">frei0r.cairoblend</property>\n')
+        out.write('\t\t\t<property name="threads">0</property>\n')
+        out.write('\t\t\t<property name="disable">1</property>\n')
+        out.write("\t\t</transition>\n")
+
+        out.write("\t</tractor>\n")
+        out.write("</mlt>\n")
