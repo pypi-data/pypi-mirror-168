a794477dc5104f838c2b08266563acc1fd0c99f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:19 +0000

    Add package pepperanalytics/0.2/pepperanalytics-0.2.tar.gz

---
+++ packages/pepperanalytics/pepperanalytics-0.2.tar.gz/pepperanalytics-0.2/pepperanalytics/__init__.py
@@ -0,0 +1,16 @@
+__version__="0.2"
+
+class Pepperanalytics:
+
+    def  hola(self):
+        """ function to return the string 'Hello World' """
+        return("Hello World!!")
+        
+    
+
+def adios():
+    return ("Adios!")
+
+
+
+
