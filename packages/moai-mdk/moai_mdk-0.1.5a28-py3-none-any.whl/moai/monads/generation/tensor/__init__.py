45c60a888066ff2d5d2ce592f4812e4eee104d15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:00 +0000

    Add package moai-mdk/0.1.5a28/moai_mdk-0.1.5a28-py3-none-any.whl

---
+++ packages/moai-mdk/moai_mdk-0.1.5a28-py3-none-any.whl/moai/monads/generation/tensor/__init__.py
@@ -0,0 +1,21 @@
+from moai.monads.generation.tensor.torch import (
+    Scalar,
+    Random,
+    Ones,
+    Zeros,
+    Clone,
+    Parameter,
+    Parameters,
+)
+from moai.monads.generation.tensor.npy import Npy
+
+__all__ = [
+    "Scalar",
+    "Random",
+    "Ones",
+    "Zeros",
+    "Clone",
+    "Parameter",
+    "Parameters",
+    "Npy",
+]
