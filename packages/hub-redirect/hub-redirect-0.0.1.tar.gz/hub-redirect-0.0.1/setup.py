74c2f8154faff15d460b6c1ec037b0f8a417cc67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:49 +0000

    Add package hub-redirect/0.0.1/hub-redirect-0.0.1.tar.gz

---
+++ packages/hub-redirect/hub-redirect-0.0.1.tar.gz/hub-redirect-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hub-redirect",
+    version="0.0.1",
+    description="hub-redirect",
+    packages=["hub-redirect"],
+    author="Activeloop",
+    author_email="support@activeloop.ai",
+    url="https://activeloop.ai",
+)
