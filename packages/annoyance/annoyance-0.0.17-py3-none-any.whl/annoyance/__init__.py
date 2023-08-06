3dc480973593701e46ccd1aea9086c427eb3bf4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:26 +0000

    Add package annoyance/0.0.17/annoyance-0.0.17-py3-none-any.whl

---
+++ packages/annoyance/annoyance-0.0.17-py3-none-any.whl/annoyance/__init__.py
@@ -0,0 +1,17 @@
+
+__module_name__ = "__init__.py"
+__author__ = ", ".join(["Michael E. Vinyard"])
+__email__ = ", ".join(["vinyard@g.harvard.edu",])
+
+
+# VERSION: --------------------------------------------------------------------
+__version__ = "v0.0.17"
+
+
+# Core module and functions: --------------------------------------------------
+from ._core._SpotifyAnnoy import SpotifyAnnoy as kNN
+from ._core._idx_funcs import *
+
+
+# Data formatting and other interactive modules: ------------------------------
+from ._interactive_funcs import *
