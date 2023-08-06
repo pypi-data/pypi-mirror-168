046944ce40a5b5dbed331f3ad8d43c75150aec14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:41 +0000

    Add package cloudshell-shell-load-balancer-standard/1.0.2/cloudshell-shell-load-balancer-standard-1.0.2.zip

---
+++ packages/cloudshell-shell-load-balancer-standard/cloudshell-shell-load-balancer-standard-1.0.2.zip/cloudshell-shell-load-balancer-standard-1.0.2/README.md
@@ -0,0 +1,8 @@
+# CloudShell Shell Load Balancer Standard
+
+[![Build status](https://travis-ci.org/QualiSystems/cloudshell-shell-load-balancer-standard.svg?branch=dev)](https://travis-ci.org/QualiSystems/cloudshell-shell-load-balancer-standard)
+[![codecov](https://codecov.io/gh/QualiSystems/cloudshell-shell-load-balancer-standard/branch/dev/graph/badge.svg)](https://codecov.io/gh/QualiSystems/cloudshell-shell-load-balancer-standard)
+[![PyPI version](https://badge.fury.io/py/cloudshell-shell-load-balancer-standard.svg)](https://badge.fury.io/py/cloudshell-shell-load-balancer-standard)
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/python/black)
+
+We use tox and pre-commit for testing. [Services description](https://github.com/QualiSystems/cloudshell-package-repo-template#description-of-services)
---
+++ packages/cloudshell-shell-load-balancer-standard/cloudshell-shell-load-balancer-standard-1.0.2.zip/cloudshell-shell-load-balancer-standard-1.0.2/setup.py
@@ -0,0 +1,31 @@
+import os
+
+from setuptools import find_packages, setup
+
+with open(os.path.join("version.txt")) as version_file:
+    version_from_file = version_file.read().strip()
+
+with open("requirements.txt") as f_required:
+    required = f_required.read().splitlines()
+
+with open("test_requirements.txt") as f_tests:
+    required_for_tests = f_tests.read().splitlines()
+
+setup(
+    name="cloudshell-shell-load-balancer-standard",
+    url="http://www.qualisystems.com/",
+    author="QualiSystems",
+    author_email="info@qualisystems.com",
+    packages=find_packages(),
+    install_requires=required,
+    tests_require=required_for_tests,
+    python_requires=(
+        ">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, !=3.6.*, <4"
+    ),
+    version=version_from_file,
+    package_data={"": ["*.txt"]},
+    description="Load Balancer Standard",
+    long_description="QualiSystems Shells Load Balancer Standard Package",
+    long_description_content_type="text/x-rst",
+    include_package_data=True,
+)
