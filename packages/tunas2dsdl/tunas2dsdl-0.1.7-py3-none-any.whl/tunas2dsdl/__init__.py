be9d70e43ed18ac7e42517ac9ed68d58150b0a6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:50 +0000

    Add package tunas2dsdl/0.1.7/tunas2dsdl-0.1.7-py3-none-any.whl

---
+++ packages/tunas2dsdl/tunas2dsdl-0.1.7-py3-none-any.whl/tunas2dsdl/__init__.py
@@ -0,0 +1,13 @@
+from tunas2dsdl.generator.generate import Generation
+from tunas2dsdl.parser import DetectionParse
+from .general.classdomain import ClassDomain, Label
+from .general.struct import Struct, Field
+
+__all__ = [
+    "Generation",
+    "DetectionParse",
+    "ClassDomain",
+    "Label",
+    "Struct",
+    "Field",
+]
