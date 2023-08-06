1cd5103f4ca30848399aa46e77f93d51c4f4efbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:36 +0000

    Add package basicco/7.1.0/basicco-7.1.0-py2.py3-none-any.whl

---
+++ packages/basicco/basicco-7.1.0-py2.py3-none-any.whl/basicco/explicit_hash.py
@@ -0,0 +1,14 @@
+"""Metaclass that forces `__hash__` to be declared when `__eq__` is declared."""
+
+__all__ = ["ExplicitHashMeta"]
+
+
+class ExplicitHashMeta(type):
+    """Metaclass that forces `__hash__` to be declared when `__eq__` is declared."""
+
+    @staticmethod
+    def __new__(mcs, name, bases, dct, **kwargs):
+        if "__eq__" in dct and "__hash__" not in dct:
+            error = "declared '__eq__' in {!r} but didn't declare '__hash__'".format(name)
+            raise TypeError(error)
+        return super(ExplicitHashMeta, mcs).__new__(mcs, name, bases, dct, **kwargs)
