3cd568e23ac2f825ffb6c03fbf7c5d941b5c84ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:55 +0000

    Add package qhub/0.4.4rc8/qhub-0.4.4rc8.tar.gz

---
+++ packages/qhub/qhub-0.4.4rc8.tar.gz/qhub-0.4.4rc8/tests_deployment/constants.py
@@ -0,0 +1,7 @@
+import os
+
+QHUB_HOSTNAME = os.environ.get("QHUB_HOSTNAME", "github-actions.qhub.dev")
+GATEWAY_ENDPOINT = "gateway"
+
+KEYCLOAK_USERNAME = os.environ["KEYCLOAK_USERNAME"]
+KEYCLOAK_PASSWORD = os.environ["KEYCLOAK_PASSWORD"]
