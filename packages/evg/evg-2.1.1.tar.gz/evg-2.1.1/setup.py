2e3a24c664d15bc69179f17a9dee596b5a7cfa05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:17 +0000

    Add package evg/2.1.1/evg-2.1.1.tar.gz

---
+++ packages/evg/evg-2.1.1.tar.gz/evg-2.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='evg',
+      version='2.1.1',
+      url='https://github.com/IceOne-i/evg',
+      license='MIT',
+      author='Nikita Belan',
+      author_email='nikitabelan9@gmail.com',
+      description='Простой и удобный инструмент для локализации Discord ботов',
+      zip_safe=False)
