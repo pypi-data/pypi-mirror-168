e3c231c112d1d7839f70592c22d676406506d208
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:41 +0000

    Add package easy-pack/1.0.11/easy-pack-1.0.11.tar.gz

---
+++ packages/easy-pack/easy-pack-1.0.11.tar.gz/easy-pack-1.0.11/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+
+setup(name='easy-pack',description='easy packing!',author='german espinosa',author_email='germanespinosa@gmail.com',packages=['easy_pack'],install_requires=['twine'],license='MIT',version='1.0.11',zip_safe=False)
