00da693d01bb5e82a56fa6f3fc3826c376f3e968
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:57:27 +0000

    Add package yafp/1.0.0/yafp-1.0.0.tar.gz

---
+++ packages/yafp/yafp-1.0.0.tar.gz/yafp-1.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='yafp',
+    version='1.0.0',
+    license='MIT',
+    author="Maxim Teplinsky",
+    author_email='email@example.com',
+    packages=['yet_another_fastapi_profiler'],
+    package_dir={'yet_another_fastapi_profiler': 'yet_another_fastapi_profiler'},
+    url='https://github.com/teplinsky-maxim/yet-another-fastapi-profiler',
+    keywords='fastapi yet_another_fastapi_profiler filter minimal time',
+    install_requires=[
+        'fastapi',
+    ],
+)
