becb051b1c6ac490b85028c59766fba4d3f14103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:44 +0000

    Add package streampq/0.0.46/streampq-0.0.46.tar.gz

---
+++ packages/streampq/streampq-0.0.46.tar.gz/streampq-0.0.46/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+
+def long_description():
+    with open('README.md', 'r') as file:
+        return file.read()
+
+
+setuptools.setup(
+    name='streampq',
+    version='0.0.46',
+    author='Department for International Trade',
+    author_email='sre@digital.trade.gov.uk',
+    description='PostgreSQL adapter to stream results of multi-statement queries without a server-side cursor',
+    long_description=long_description(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/uktrade/streampq',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Database',
+    ],
+    python_requires='>=3.8.0',
+    py_modules=[
+        'streampq',
+    ],
+)
