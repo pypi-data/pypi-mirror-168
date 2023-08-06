6e4340712c91960326d40972ba0bc0577c0a09b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:52 +0000

    Add package pycvm/0.3.0/pycvm-0.3.0-py3-none-any.whl

---
+++ packages/pycvm/pycvm-0.3.0-py3-none-any.whl/cvm/utils.py
@@ -0,0 +1,12 @@
+import datetime
+
+def date_from_string(date_string: str) -> datetime.date:
+    return datetime.datetime.strptime(date_string, '%Y-%m-%d').date()
+
+def date_to_string(date: datetime.time) -> str:
+    return date.strftime('%Y-%m-%d')
+
+def lzstrip(s: str) -> str:
+    """Strip leading zeroes."""
+
+    return s.lstrip('0')
