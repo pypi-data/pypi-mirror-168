4dbbbab2509ea459c5fd1e5416f8b6dc2035582d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:12 +0000

    Add package configur8/1.5/configur8-1.5-py2.py3-none-any.whl

---
+++ packages/configur8/configur8-1.5-py2.py3-none-any.whl/configur8/email.py
@@ -0,0 +1,18 @@
+from email_validator import EmailNotValidError
+from email_validator import validate_email
+
+from .core import InvalidConfig
+
+__all__ = ("parse",)
+
+
+def parse(email: str) -> str:
+    try:
+        result = validate_email(email, check_deliverability=False)
+    except EmailNotValidError as exc:
+        raise InvalidConfig(str(exc)) from exc
+
+    if not result.email:
+        raise InvalidConfig("Email is not valid??")
+
+    return result.email
