0911a57470505651565313cc93b3b94c57cb84f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:55 +0000

    Add package bodosdk/1.2.1/bodosdk-1.2.1-py3-none-any.whl

---
+++ packages/bodosdk/bodosdk-1.2.1-py3-none-any.whl/bodosdk/api/auth.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+
+import jwt
+
+from bodosdk.api.request_wrapper import RequestWrapper
+from bodosdk.exc import Unauthorized
+from bodosdk.models.base import APIKeys
+
+
+class AuthApi:
+    def __init__(
+        self,
+        auth: APIKeys,
+        auth_url="https://prod-auth.bodo.ai",
+        requests=RequestWrapper(),
+    ):
+        self._requests = requests
+        self._auth_url = auth_url
+        self._auth = auth
+        self._token = None
+
+    def _authenticate(self):
+        resp = self._requests.post(
+            f"{self._auth_url}/identity/resources/auth/v1/api-token",
+            json={
+                "clientId": self._auth.client_id,
+                "secret": self._auth.secret_key,
+            },
+        )
+        if resp.status_code == 200:
+            return resp.json()["accessToken"]
+        raise Unauthorized
+
+    def switch_workspace(self, auth: APIKeys):
+        self._auth = auth
+        self._token = self._authenticate()
+
+    @property
+    def auth_token(self):
+        try:
+            exp = datetime.fromtimestamp(
+                jwt.decode(self._token, options={"verify_signature": False})["exp"]
+            )
+            if exp < datetime.now():
+                self._token = self._authenticate()
+        except Exception as ex:  # noqa: F841
+            self._token = self._authenticate()
+        return self._token
