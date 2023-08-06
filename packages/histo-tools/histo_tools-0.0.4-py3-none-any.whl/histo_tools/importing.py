aa5f9bb69b740cb62e5978933cd795e4a622336c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:40 +0000

    Add package histo-tools/0.0.4/histo_tools-0.0.4-py3-none-any.whl

---
+++ packages/histo-tools/histo_tools-0.0.4-py3-none-any.whl/histo_tools/importing.py
@@ -0,0 +1,12 @@
+import tifffile
+import zarr
+
+
+def open_ndpi(path, level=0):
+    with tifffile.imread(path, aszarr=True) as store:
+        group = zarr.open(store, mode='r')
+        group_keys = [int(r) for r in group.keys()]
+        level = min(level, max(group_keys))
+        level = max(level, min(group_keys))
+        assert isinstance(group, zarr.Group)
+        return group[str(level)][..., -1]
