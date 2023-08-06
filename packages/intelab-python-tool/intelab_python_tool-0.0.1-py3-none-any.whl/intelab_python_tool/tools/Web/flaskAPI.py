e20972cab10f5a569d9c769eecdf3fba2d61dbdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:55 +0000

    Add package intelab-python-tool/0.0.1/intelab_python_tool-0.0.1-py3-none-any.whl

---
+++ packages/intelab-python-tool/intelab_python_tool-0.0.1-py3-none-any.whl/intelab_python_tool/tools/Web/flaskAPI.py
@@ -0,0 +1,17 @@
+
+from flask import Flask
+
+app = Flask(__name__)
+
+"""
+使用方法如下：
+    >> 该方法用来制作接口给后端数据
+
+    from intelab_python_tool.tools.Web.flaskAPI import app
+
+    @app.route('/index': 页面地址, method = ['post'])
+    def give_data():
+        ...
+
+    app.run(host=..., port=..., *args, **kwargs)
+"""
