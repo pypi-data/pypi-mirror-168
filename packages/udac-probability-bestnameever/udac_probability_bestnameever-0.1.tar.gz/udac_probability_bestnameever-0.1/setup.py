90ca16d870215b49944ff490376ed5802ebda64f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:58 +0000

    Add package udac-probability-bestnameever/0.1/udac_probability_bestnameever-0.1.tar.gz

---
+++ packages/udac-probability-bestnameever/udac_probability_bestnameever-0.1.tar.gz/udac_probability_bestnameever-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='udac_probability_bestnameever',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['udac_probability_bestnameever'],
+      author='Sarah Rosenberg',
+      email='ucsarebear@yahoo.com',
+      zip_safe=False),
