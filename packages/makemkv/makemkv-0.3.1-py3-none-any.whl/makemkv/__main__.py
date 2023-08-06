0adf7b4bc7f733b8814cc8c05c9e5e2904586919
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:21 +0000

    Add package makemkv/0.3.1/makemkv-0.3.1-py3-none-any.whl

---
+++ packages/makemkv/makemkv-0.3.1-py3-none-any.whl/makemkv/__main__.py
@@ -0,0 +1,244 @@
+from __future__ import annotations
+
+import json
+import logging
+from pathlib import Path
+from typing import Any, List, Mapping, TypedDict, Union, cast
+
+try:
+    import click
+    from rich import print
+    from rich.logging import RichHandler
+    from rich.tree import Tree
+except ImportError as exc:
+    raise ImportError(
+        "pymakemkv requires 'rich' and 'click' to be installed. You can "
+        "install them with 'pip install rich click' or 'pip install makemkv[cli].'"
+    ) from exc
+
+from ._cli_params import (
+    BACKUP_PARAMS,
+    INFO_PARAMS,
+    MKV_PARAMS,
+    BackupCliParams,
+    HelpfulGroup,
+    InfoCliParams,
+    MKVCliParams,
+    add_params,
+)
+from .makemkv import MakeMKV, MakeMKVError
+from .progress import ProgressParser
+from .types import MakeMKVOutput, ProgressUpdateHandlerType
+
+
+class MakeMKVArgs(TypedDict, total=False):
+    input: int | Path
+    progress_handler: ProgressUpdateHandlerType
+    cache: int
+    minlength: int
+
+
+rich_handler = RichHandler(level=logging.INFO)
+logging.basicConfig(
+    format="%(message)s",
+    handlers=[rich_handler],
+    level=logging.NOTSET,
+)
+logger = logging.getLogger()
+
+
+@click.group(cls=HelpfulGroup)
+def cli() -> None:
+    pass
+
+
+@cli.command()
+@add_params(INFO_PARAMS)
+def info(**_params: Any) -> None:
+    """Display information about a disc."""
+    params = cast(InfoCliParams, _params)
+
+    set_log_level(params)
+
+    with ProgressParser() as bar:
+        makemkv = MakeMKV(**extract_makemkv_args(params, bar))
+        try:
+            disc_info = makemkv.info()
+        except KeyboardInterrupt:
+            logger.warning("Received CTRL-C signal. Terminating makemkvcon.")
+            makemkv.kill()
+            raise
+        except MakeMKVError:
+            raise click.Abort from None
+        except FileNotFoundError as exc:
+            logger.critical(exc)
+            raise click.Abort from None
+
+    return_info(disc_info, params)
+
+
+@cli.command()
+@add_params(MKV_PARAMS)
+def mkv(**_params: Any) -> None:
+    """Copy titles from disc."""
+    params = cast(MKVCliParams, _params)
+
+    set_log_level(params)
+
+    with ProgressParser() as bar:
+        makemkv = MakeMKV(**extract_makemkv_args(params, bar))
+        try:
+            disc_info = makemkv.mkv(params["title"], params["output"])
+        except KeyboardInterrupt:
+            logger.warning("Received CTRL-C signal. Terminating makemkvcon.")
+            makemkv.kill()
+            raise
+        except MakeMKVError:
+            raise click.Abort from None
+        except FileNotFoundError as exc:
+            logger.critical(exc)
+            raise click.Abort from None
+
+    return_info(disc_info, params)
+
+
+@cli.command()
+@add_params(BACKUP_PARAMS)
+def backup(**_params: Any) -> None:
+    """Backup whole disc."""
+    params = cast(BackupCliParams, _params)
+
+    set_log_level(params)
+
+    with ProgressParser() as bar:
+        makemkv = MakeMKV(**extract_makemkv_args(params, bar))
+        try:
+            disc_info = makemkv.backup(params["output"], decrypt=params["decrypt"])
+        except KeyboardInterrupt:
+            logger.warning("Received CTRL-C signal. Terminating makemkvcon.")
+            makemkv.kill()
+            raise
+        except MakeMKVError:
+            raise click.Abort from None
+        except FileNotFoundError as exc:
+            logger.critical(exc)
+            raise click.Abort from None
+
+    return_info(disc_info, params)
+
+
+def set_log_level(params: InfoCliParams) -> None:
+    if params["verbose"]:
+        logger.setLevel(logging.DEBUG)
+    elif params["quiet"]:
+        logger.setLevel(logging.CRITICAL)
+
+
+def extract_makemkv_args(params: InfoCliParams, bar: ProgressParser) -> MakeMKVArgs:
+    makemkv_args = MakeMKVArgs()
+    input = makemkv_args["input"] = (
+        params["input"] if params["input"] else params["disc_nr"]
+    )
+    logger.debug(
+        f"input: {params['input']}, disc_nr: {params['disc_nr']} " f"-> {input}"
+    )
+    if not params["no_bar"] and not params["quiet"]:
+        makemkv_args["progress_handler"] = bar.parse_progress
+    if params["cache"]:
+        makemkv_args["cache"] = params["cache"]
+    if params["minlength"]:
+        makemkv_args["minlength"] = params["minlength"]
+    return makemkv_args
+
+
+def return_info(output: MakeMKVOutput, params: InfoCliParams) -> None:
+    if params["info_file"]:
+        with open(params["info_file"], "w") as f:
+            json.dump(output, f, indent=2, sort_keys=True)
+    elif params["json"]:
+        print(json.dumps(output, indent=2, sort_keys=True))
+    elif params["no_info"]:
+        pass
+    else:
+        print(MakeMKVOutputTree("Disc Info", output))
+        pass
+
+
+# see https://github.com/python/mypy/issues/731
+NestedDictItem = Union[str, int, "NestedDict", List["NestedDictItem"]]  # type: ignore [misc] # noqa: B950
+NestedDict = Mapping[str, NestedDictItem]  # type: ignore [misc]
+
+
+class MakeMKVOutputTree:
+    """A tree renderable generated from MakeMKVOutput."""
+
+    def __init__(
+        self,
+        label: str,
+        data: MakeMKVOutput,
+    ) -> None:
+        self.tree = Tree(
+            label,
+            style="tree",
+            guide_style="tree.line",
+            expanded=True,
+            highlight=True,
+        )
+        self.walk_dict(cast(NestedDict, data), self.tree)
+
+    def __rich__(self) -> Tree:
+        return self.tree
+
+    def add(
+        self,
+        label: str,
+        parent_tree: Tree,
+    ) -> Tree:
+        return parent_tree.add(
+            label,
+            style="tree",
+            guide_style="tree.line",
+            expanded=True,
+            highlight=True,
+        )
+
+    def walk_dict(
+        self,
+        data: NestedDict,
+        parent_tree: Tree,
+    ) -> None:
+        data = {key: value for key, value in sorted(data.items(), key=lambda x: x[0])}
+        for key, value in data.items():
+            key = key.replace("_", " ").capitalize()
+            if isinstance(value, str):
+                parent_tree.add(f"{key}: {value}")
+            elif isinstance(value, int):
+                parent_tree.add(f"{key}: {value}")
+            elif isinstance(value, list):
+                child_tree = self.add(key, parent_tree)
+                self.walk_list(value, child_tree, label=key[:-1])
+            elif isinstance(value, dict):
+                child_tree = self.add(key, parent_tree)
+                self.walk_dict(value, child_tree)
+
+    def walk_list(
+        self,
+        data: list[NestedDictItem],
+        parent_tree: Tree,
+        label: str = "",
+    ) -> None:
+        try:
+            data.sort()  # pyright: reportGeneralTypeIssues=false
+        except TypeError:
+            pass
+        for i, value in enumerate(data):
+            if isinstance(value, str):
+                parent_tree.add(f"{value.replace('_', ' ').capitalize()}")
+            elif isinstance(value, int):
+                parent_tree.add(f"{value}")
+            elif isinstance(value, list):
+                child_tree = self.add(f"{label} {str(i + 1)}", parent_tree)
+                self.walk_list(value, child_tree)
+            elif isinstance(value, dict):
+                child_tree = self.add(f"{label} {str(i + 1)}", parent_tree)
+                self.walk_dict(value, child_tree)
---
+++ packages/makemkv/makemkv-0.3.1-py3-none-any.whl/makemkv/makemkv.py
@@ -0,0 +1,517 @@
+"""Main module of python-makemkv that provides the `MakeMKV` class."""
+
+from __future__ import annotations
+
+import logging
+import platform
+import re
+import shutil
+from os import PathLike
+from pathlib import Path, WindowsPath
+from subprocess import PIPE, STDOUT, Popen
+from typing import Any, Iterable, Literal, Union
+
+from iso639 import Lang  # type: ignore [import]
+from typing_extensions import TypedDict, get_args, get_origin, get_type_hints
+
+from .output_codes import KEY_CODES, MESSAGE_CODES, SPECIAL_VALUES
+from .types import Disc, Drive, MakeMKVOutput, ProgressUpdateHandlerType, Stream, Title
+
+if platform.system() == "Windows":
+    MAKEMKVCON_BINARIES = [
+        "makemkvcon",
+        str(WindowsPath("C:/Program Files/MakeMKV/makemkvcon.exe")),
+        str(WindowsPath("C:/Program Files (x86)/MakeMKV/makemkvcon.exe")),
+    ]
+else:
+    MAKEMKVCON_BINARIES = ["makemkvcon"]
+
+logger = logging.getLogger(__package__)
+makemkvcon_logger = logger.getChild("makemkvcon")
+_split_msg_exp = re.compile(r'[A-Z]+(?=:)|(?<=,")[^"]*(?=")|(?!:)[^,"]+|(?<=,)(?=,)')
+
+
+def _do_nothing(*args: Any, **kwargs: Any) -> None:
+    return
+
+
+class MakeMKV:
+    """Wraps makemkvcon and exposes makemkvcon's commands as methods."""
+
+    def __init__(
+        self,
+        input: int | str | PathLike[str],
+        cache: int | str | None = None,
+        minlength: int | str | None = None,
+        progress_handler: ProgressUpdateHandlerType = _do_nothing,
+    ) -> None:
+        """Initialize MakeMKV with input.
+
+        Args:
+            input: Can be either a disc number starting with 0, a device,
+                a .IFO file or a VIDEO_TS folder.
+            cache: Size of read cache in megabytes.
+            minlength: Minimum title length in seconds.
+            progress_handler: A callback function to parse progress updates.
+                See :func:`makemkv.ProgressParser.parse_progress`
+                for an example.
+        """
+        self._input = self._parse_input(input)
+        self.cache = cache
+        self.minlength = minlength
+        self.progress_handler = progress_handler
+        self.process: Popen | None = None
+
+    def info(
+        self,
+        cache: int | str | None = None,
+        minlength: int | str | None = None,
+    ) -> MakeMKVOutput:
+        """Display information about a disc.
+
+        Args:
+            cache: Size of read cache in megabytes.
+            minlength: Minimum title length in seconds.
+
+        Returns:
+            MakeMKVOutput: A dict containing some information about drives,
+                discs, titles and streams.
+
+        Raises:
+            MakeMKVError: MakeMKV encountered a critical problem.
+            FileNotFoundError: Couldn't find `makemkvcon`.
+        """
+        cache = self.cache if cache is None else cache
+        minlength = self.minlength if minlength is None else minlength
+        cmd = [
+            _find_makemkvcon_binary(),
+            "info",
+            self._input,
+            "--robot",
+            "--progress=-same",
+            "--noscan",
+        ]
+        if cache:
+            cmd.extend(["--cache", str(cache)])
+        if minlength:
+            cmd.extend(["--minlength", str(minlength)])
+
+        return self._run(cmd)
+
+    def mkv(
+        self,
+        title: int | str,
+        output_dir: str | Path,
+        cache: int | str | None = None,
+        minlength: int | str | None = None,
+    ) -> MakeMKVOutput:
+        """Copy titles from disc.
+
+        Args:
+            title: Title to be ripped, can be either an integer starting
+                with 0 or the keyword "all".
+            output_dir: Output directory for created mkv files.
+            cache: Size of read cache in megabytes.
+            minlength: Minimum title length in seconds.
+
+        Returns:
+            MakeMKVOutput: A dict containing some information about drives,
+                discs, titles and streams.
+
+        Raises:
+            MakeMKVError: MakeMKV encountered a critical problem.
+            FileNotFoundError: Couldn't find `makemkvcon`.
+        """
+        cache = self.cache if cache is None else cache
+        minlength = self.minlength if minlength is None else minlength
+        cmd = [
+            _find_makemkvcon_binary(),
+            "mkv",
+            self._input,
+            str(title),
+            str(output_dir),
+            "--robot",
+            "--progress=-same",
+            "--noscan",
+        ]
+        if cache:
+            cmd.extend(["--cache", str(cache)])
+        if minlength:
+            cmd.extend(["--minlength", str(minlength)])
+
+        return self._run(cmd)
+
+    def backup(
+        self,
+        output_dir: str | Path,
+        cache: int | str | None = None,
+        minlength: int | str | None = None,
+        decrypt: bool = False,
+    ) -> MakeMKVOutput:
+        """Backup whole disc.
+
+        Args:
+            output_dir: Output directory for created backup files.
+            cache: Size of read cache in megabytes.
+            minlength: Minimum title length in seconds.
+            decrypt: Decrypt stream files during backup.
+
+        Returns:
+            MakeMKVOutput: A dict containing some information about drives,
+                discs, titles and streams.
+
+        Raises:
+            MakeMKVError: MakeMKV encountered a critical problem.
+            FileNotFoundError: Couldn't find `makemkvcon`.
+        """
+        cache = self.cache if cache is None else cache
+        minlength = self.minlength if minlength is None else minlength
+        cmd = [
+            _find_makemkvcon_binary(),
+            "backup",
+            self._input,
+            str(output_dir),
+            "--robot",
+            "--progress=-same",
+            "--noscan",
+        ]
+        if cache:
+            cmd.extend(["--cache", str(cache)])
+        if minlength:
+            cmd.extend(["--minlength", str(minlength)])
+        if decrypt:
+            cmd.append("--decrypt")
+
+        return self._run(cmd)
+
+    def kill(self) -> None:
+        """Terminate the `makemkvcon` progress."""
+        if self.process:
+            self.process.kill()
+
+    def _parse_input(self, input: int | str | PathLike[str]) -> str:
+        """Autodetect suitable input type and reformat it for makemkvcon."""
+        if isinstance(input, int):
+            return f"disc:{input}"
+
+        input_path = Path(input)
+
+        def is_video_ts_folder(path: Path) -> bool:
+            return (
+                path.is_dir()
+                and re.match(r"video[-_ ]?ts", path.name.lower()) is not None
+            )
+
+        if (
+            input_path.is_block_device()
+            or isinstance(input_path, WindowsPath)
+            and str(input_path) in (input_path.drive, input_path.anchor)
+        ):
+            return f"dev:{input_path}"
+        if input_path.suffix.lower() == ".iso":
+            return f"iso:{input_path}"
+        if input_path.suffix.lower() == ".ifo":
+            return f"file:{input_path.parent}"
+        if not is_video_ts_folder(input_path):
+            for path in input_path.iterdir():
+                if is_video_ts_folder(path):
+                    return f"file:{path}"
+        return f"file:{input_path}"
+
+    def _translate_codes(
+        self, flag: str, id: int, value: str, code: int
+    ) -> tuple[str, str | int | float]:
+        """Translate makemkvcon's codes into something useful.
+
+        Raises:
+            KeyError: if `id` is unknown or irrelevant
+        """
+        return_value: str | int | float
+        key = KEY_CODES[id]
+        if flag == "SINFO":
+            if id == 2:
+                # "downmix" seems to be more suitable
+                # for audiostreams than "name"
+                key = "downmix"
+
+        if code:
+            return_value = SPECIAL_VALUES.get(code, value)
+        elif key in ["information", "name", "volume_name"]:
+            return_value = value
+        elif key in ["metadata_langcode", "langcode"]:
+            # convert 3-letter language codes to 2-letter codes
+            return_value = Lang(value).pt1
+        elif key == "framerate":
+            # convert "##.### (#####/####)" to int string
+            if m := re.match(r"^(\d+(?:\.\d+)*)\s\(\d+/\d+\)$", value):
+                return_value = float(m[1])
+            else:
+                return_value = int(value)
+        elif key == "segments_map":
+            return_value = value
+        elif value.isdecimal():
+            return_value = int(value)
+        else:
+            return_value = value.strip()
+
+        return key, return_value
+
+    def _parse_makemkv_log(self, lines: Iterable[str]) -> MakeMKVOutput:
+        output = MakeMKVOutput(drives=[], titles=[])
+        progress_title = ""
+
+        for line in lines:
+            # flag, msg = line.strip().split(':', 1)
+            # msg_values = _split_values_exp.findall(msg)
+            # msg_values = [v.strip('"').strip() for v in msg_values]
+            if not line:
+                continue
+            flag: str
+            msg_values: list[str]
+            flag, *msg_values = _split_msg_exp.findall(line.strip())
+
+            if flag in "MSG":
+                # MSG:code,flags,count,message,format,param0,param1,...
+                #   code - unique message code, should be used to identify
+                #          particular string in language-neutral way.
+                #   flags - message flags, see AP_UIMSG_xxx flags in apdefs.h
+                #   count - number of parameters
+                #   message - raw message string suitable for output
+                #   format - format string used for message. This string is
+                #            localized and subject to change, unlike message
+                #            code.
+                #   paramX - parameter for message
+
+                try:
+                    code = int(msg_values[0])
+                    message = msg_values[3]
+                except (ValueError, IndexError):
+                    logger.exception(f"Error while parsing '{line.strip()}'")
+                    continue
+
+                loglevel = MESSAGE_CODES.get(code, 10)
+                makemkvcon_logger.log(loglevel, "%s (%s)", message, code)
+
+                if loglevel == logging.CRITICAL and self.process is not None:
+                    self.process.kill()
+                    raise MakeMKVError(message)
+
+            elif flag == "PRGT":
+                # PRGT:code,id,name
+                # code - unique message code
+                # id - operation sub-id
+                # name - name string
+
+                try:
+                    code = int(msg_values[0])
+                    message = msg_values[2]
+                except (ValueError, IndexError):
+                    logger.exception(f"Error while parsing '{line.strip()}'")
+                    continue
+
+                loglevel = MESSAGE_CODES.get(code, 10)
+                makemkvcon_logger.log(loglevel, "%s (%s)", message, code)
+
+            elif flag == "PRGC":
+                # PRGC:code,id,name
+                #   code - unique message code
+                #   id - operation sub-id
+                #   name - name string
+
+                try:
+                    progress_title = msg_values[2]
+                except IndexError:
+                    logger.exception(f"Error while parsing '{line.strip()}'")
+                    continue
+
+            elif flag == "PRGV":
+                # PRGV:current,total,max
+                #   current - current progress value
+                #   total - total progress value
+                #   max - maximum possible value for a progress bar, constant
+
+                try:
+                    current = int(msg_values[0])
+                    max = int(msg_values[2])
+                except (ValueError, IndexError):
+                    logger.exception(f"Error while parsing '{line.strip()}'")
+                    continue
+
+                self.progress_handler(progress_title, current, max)
+
+            elif flag == "DRV":
+                # DRV:index,visible,enabled,flags,drive name,disc name,
+                # device path (wrong documented)
+                #   index - drive index
+                #   visible - set to 1 if drive is present
+                #   enabled - set to 1 if drive is accessible
+                #   flags - media flags, see AP_DskFsFlagXXX in apdefs.h
+                #   drive name - drive name string
+                #   disc name - disc name string
+                #   device path - device path string (not documented)
+
+                try:
+                    _, _, _, _, drive_name, disc_name, device_path = msg_values
+                except ValueError:
+                    logger.exception(f"Error while parsing '{line.strip()}'")
+                    continue
+
+                if drive_name or disc_name or device_path:
+                    drive = Drive()
+                    if drive_name:
+                        drive["drive_name"] = drive_name
+                    if disc_name:
+                        drive["disc_name"] = disc_name
+                    if device_path:
+                        drive["device_path"] = device_path
+                    output["drives"].append(drive)
+
+            elif flag == "TCOUNT":
+                # TCOUNT:count
+                #   count - titles count
+                try:
+                    count = int(msg_values[0])
+                except (ValueError, IndexError):
+                    logger.exception(f"Error while parsing '{line.strip()}'")
+                    continue
+
+                output["title_count"] = count
+
+            elif flag == "CINFO":
+                # CINFO:id,code,value
+                #   id - attribute id, see AP_ItemAttributeId in apdefs.h
+                #   code - message code if attribute value is a constant string
+                #   value - attribute value
+                try:
+                    id = int(msg_values[0])
+                    code = int(msg_values[1])
+                    value = msg_values[2]
+                except (ValueError, IndexError):
+                    logger.exception(f"Error while parsing '{line.strip()}'")
+                    continue
+
+                if "disc" not in output:
+                    output["disc"] = Disc()
+                    assert "disc" in output
+
+                try:
+                    key, d_value = self._translate_codes(flag, id, value, code)
+                except KeyError:
+                    continue
+                if not _is_valid_typeddict_item(Disc, key, d_value):
+                    logger.error(f"Error while parsing '{line.strip()}'")
+                    continue
+
+                output["disc"][key] = d_value  # type: ignore[literal-required] # noqa: B950
+
+            elif flag == "TINFO":
+                # TINFO:disc_nr,title_nr,id,code,value (wrong documented)
+                #   title_nr - title number
+                #   id - attribute id, see AP_ItemAttributeId in apdefs.h
+                #   code - message code if attribute value is a constant string
+                #   value - attribute value
+
+                try:
+                    title_nr = int(msg_values[0])
+                    id = int(msg_values[1])
+                    code = int(msg_values[2])
+                    value = msg_values[3]
+                except (ValueError, IndexError):
+                    logger.exception(f"Error while parsing '{line.strip()}'")
+                    continue
+
+                while title_nr >= len(output["titles"]):
+                    output["titles"].append(Title(streams=[]))
+
+                try:
+                    key, d_value = self._translate_codes(flag, id, value, code)
+                except KeyError:
+                    continue
+
+                if not _is_valid_typeddict_item(Title, key, d_value):
+                    logger.error(f"Error while parsing '{line.strip()}'")
+                    continue
+
+                output["titles"][title_nr][key] = d_value  # type: ignore[literal-required] # noqa: B950
+
+            elif flag == "SINFO":
+                # SINFO:disc_nr,title_nr,stream_nr,id,code,value
+                # (wrong documented)
+                #   title_nr - title number
+                #   stream_nr - stream number
+                #   id - attribute id, see AP_ItemAttributeId in apdefs.h
+                #   code - message code if attribute value is a constant string
+                #   value - attribute value
+
+                try:
+                    title_nr = int(msg_values[0])
+                    stream_nr = int(msg_values[1])
+                    id = int(msg_values[2])
+                    code = int(msg_values[3])
+                    value = msg_values[4]
+                except (ValueError, IndexError):
+                    logger.exception(f"Error while parsing '{line.strip()}'")
+                    continue
+
+                while stream_nr >= len(output["titles"][title_nr]["streams"]):
+                    output["titles"][title_nr]["streams"].append(Stream())
+
+                try:
+                    key, d_value = self._translate_codes(flag, id, value, code)
+                except KeyError:
+                    continue
+
+                if not _is_valid_typeddict_item(Stream, key, d_value):
+                    logger.error(f"Error while parsing '{line.strip()}'")
+                    continue
+
+                output["titles"][title_nr]["streams"][stream_nr][key] = d_value  # type: ignore[literal-required] # noqa: B950
+            else:
+                logger.error(f"Error while parsing '{line.strip()}'")
+
+        return output
+
+    def _run(self, cmd: list[str]) -> MakeMKVOutput:
+        """Run makemkvcon and parse its output."""
+        p = self.process = Popen(cmd, stderr=STDOUT, stdout=PIPE, bufsize=1, text=True)
+        logger.info('Running "%s"', " ".join(cmd))
+        assert p.stdout is not None
+
+        output = self._parse_makemkv_log(p.stdout)
+        if (return_code := p.wait()) != 0:
+            raise MakeMKVError(
+                f"makemkvcon exited with non-zero return code {return_code}"
+            )
+        return output
+
+
+def _is_valid_typeddict_item(
+    td: type[TypedDict], key: str, value: Any  # type: ignore [valid-type]
+) -> bool:
+    """Check if `key` and `value` form a valid item for the TypedDict `td`."""
+    annotations = get_type_hints(td)
+    if key not in annotations:
+        return False
+    if get_origin(annotations[key]) is Literal:
+        return value in get_args(annotations[key])
+    if get_origin(annotations[key]) is Union:
+        return any(isinstance(value, tp) for tp in get_args(annotations[key]))
+    if get_origin(annotations[key]) is list:
+        return isinstance(value, list) and all(
+            isinstance(item, get_args(annotations[key])[0]) for item in value
+        )
+    return isinstance(value, annotations[key])
+
+
+def _find_makemkvcon_binary() -> str:
+    for bin_path in MAKEMKVCON_BINARIES:
+        if shutil.which(bin_path) is not None:
+            return bin_path
+    else:
+        raise FileNotFoundError(
+            "Couldn't find makemkvcon. Make sure it is installed and in your PATH."
+        )
+
+
+class MakeMKVError(Exception):
+    """Raised if MakeMKV encounters a critical problem."""
