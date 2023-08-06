7f67610b795794fe0fc424cf38ce42a623f9668d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:33 +0000

    Add package defgraficos/1.1/defgraficos-1.1.tar.gz

---
+++ packages/defgraficos/defgraficos-1.1.tar.gz/defgraficos-1.1/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from setuptools import setup
+setup(
+    name="defgraficos",
+    version="1.1",
+    description="Libreria usada para generacion de graficos customizados, mediante matplotlib",
+    author="Jorge Guerra Guerra",
+    author_email="jguerra91@gmail.com",
+    url="https://dina.concytec.gob.pe/appDirectorioCTI/VerDatosInvestigador.do?id_investigador=12581",
+    packages=['defgraficos'],
+    scripts=[]
+)
+
+
+# In[ ]:
+
+
+
+
