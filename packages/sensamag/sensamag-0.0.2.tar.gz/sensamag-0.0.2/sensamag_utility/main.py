6f0c2c6ad83e079f6910f86e4e162d60a0c7d61e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:19 +0000

    Add package sensamag/0.0.2/sensamag-0.0.2.tar.gz

---
+++ packages/sensamag/sensamag-0.0.2.tar.gz/sensamag-0.0.2/sensamag_utility/main.py
@@ -0,0 +1,13 @@
+from typer import Typer
+
+app = Typer()
+
+
+@app.command()
+def hello(name: str):
+    print(f"Hello {name}")
+
+
+@app.command()
+def goodbye(name: str):
+    print(f"Goodbye {name}")
