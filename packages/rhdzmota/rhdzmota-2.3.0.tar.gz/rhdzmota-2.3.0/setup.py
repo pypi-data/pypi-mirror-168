80ac2476bfe6c72ecd87293d347c5e89da83933d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:47 +0000

    Add package rhdzmota/2.3.0/rhdzmota-2.3.0.tar.gz

---
+++ packages/rhdzmota/rhdzmota-2.3.0.tar.gz/rhdzmota-2.3.0/setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import setup, find_packages
+
+with open("requirements.txt", "r") as file:
+    requirements = [line for line in file.read().splitlines() if line]
+
+with open(os.path.join("src", "rhdzmota", "VERSION"), "r") as file:
+    version = file.read().strip()
+
+with open("README.md") as file:
+    readme = file.read()
+
+setup(
+    name="rhdzmota",
+    version=version,
+    description="RHDZMOTA Package",
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    url="https://github.com/rhdzmota/rhdzmota-package",
+    author="Rodrigo H. Mota",
+    author_email="contact@rhdzmota.com",
+    packages=find_packages(where="src"),
+    package_dir={
+        "": "src"
+    },
+    package_data={
+        "": [
+            os.path.join("iso3166", "datafiles", "*.json"),
+        ]
+    },
+    scripts=[
+        "bin/rhdzmota"
+    ],
+    install_requires=requirements,
+    include_package_data=True,
+    python_requires=">=3.8.10"
+)
