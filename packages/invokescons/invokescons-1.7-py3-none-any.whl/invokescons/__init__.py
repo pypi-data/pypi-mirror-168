53138608966e10ce9a04f46be5a6e90c0454b7ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:32 +0000

    Add package invokescons/1.7/invokescons-1.7-py3-none-any.whl

---
+++ packages/invokescons/invokescons-1.7-py3-none-any.whl/invokescons/__init__.py
@@ -0,0 +1,7 @@
+from typing import List, Optional
+
+__version__ = "1.7.0"
+
+def main(args: Optional[List[str]] = None) -> int:
+    from invokescons.command_line import main as _main
+    return _main()
