98b631abab6cfb49531470a07f4e4858887729ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:54 +0000

    Add package opta-api/0.0.2/opta_api-0.0.2.tar.gz

---
+++ packages/opta-api/opta_api-0.0.2.tar.gz/opta_api-0.0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(name='opta_api',
+      version='0.0.2',
+      description='API wrapper for OPTA/StatsPerform',
+      packages=find_packages(),
+      )
