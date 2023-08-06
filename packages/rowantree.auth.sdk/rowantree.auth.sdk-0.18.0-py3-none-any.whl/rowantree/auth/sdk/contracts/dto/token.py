5c0d7e4ed5c38d1633601622310e9a0e5461f6d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:17 +0000

    Add package rowantree.auth.sdk/0.18.0/rowantree.auth.sdk-0.18.0-py3-none-any.whl

---
+++ packages/rowantree.auth.sdk/rowantree.auth.sdk-0.18.0-py3-none-any.whl/rowantree/auth/sdk/contracts/dto/token.py
@@ -0,0 +1,18 @@
+""" Token Definition """
+from rowantree.contracts import BaseModel
+
+
+class Token(BaseModel):
+    """
+    Token
+
+    Attributes
+    ----------
+    access_token: str
+        Minted access token
+    token_type: str
+        Type of token
+    """
+
+    access_token: str
+    token_type: str
