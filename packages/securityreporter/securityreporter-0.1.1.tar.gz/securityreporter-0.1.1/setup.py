05d1e97b4e8131727da4551649fb42c392c7e726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:57 +0000

    Add package securityreporter/0.1.1/securityreporter-0.1.1.tar.gz

---
+++ packages/securityreporter/securityreporter-0.1.1.tar.gz/securityreporter-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['reporter', 'reporter.objects']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.27.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'securityreporter',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Alexander Krigsman',
+    'author_email': 'alexander.krigsman@dongit.nl',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7.2,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
