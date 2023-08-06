322bbd388d5c9d666841c8ebdd88f520613c327a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:10 +0000

    Add package calculatorfirstversion/0.0.1/calculatorfirstversion-0.0.1-py3-none-any.whl

---
+++ packages/calculatorfirstversion/calculatorfirstversion-0.0.1-py3-none-any.whl/calculatorfirstversion/__init__.py
@@ -0,0 +1,20 @@
+def add_numbers(a,b):
+    return a + b
+
+def sub_numbers(a,b):
+    return a - b
+
+def mult_numbers(a,b):
+    return a * b  
+
+def div_numbers(a,b):
+    return a / b
+
+def mod_numbers(a,b):
+    return a % b    
+
+def squ_number(a, n):
+    return a ** n 
+
+def fdiv_numbers(a,b):
+    return a // b                 
