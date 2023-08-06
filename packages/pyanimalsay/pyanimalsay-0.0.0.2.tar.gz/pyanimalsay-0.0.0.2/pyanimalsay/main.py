dcb7041b1f76ef169043321b615436b183e639d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:16 +0000

    Add package pyanimalsay/0.0.0.2/pyanimalsay-0.0.0.2.tar.gz

---
+++ packages/pyanimalsay/pyanimalsay-0.0.0.2.tar.gz/pyanimalsay-0.0.0.2/pyanimalsay/main.py
@@ -0,0 +1,38 @@
+from typer import Typer
+from rich import print
+
+from app.animals import Cow, Cat, Dog
+
+
+# Creating app instance
+app = Typer()
+
+
+# Command for a cow
+@app.command()
+def cow(sound: str):
+    new_cow = Cow()
+    if new_cow.validate_sound(sound):
+        print(f"{new_cow.generate_image(sound)}")
+    else:
+        print(f"[bold red]{new_cow.species_name} does not say '{sound}'!")
+
+
+# Command for a cat
+@app.command()
+def cat(sound: str):
+    new_cat = Cat()
+    if new_cat.validate_sound(sound):
+        print(f"{new_cat.generate_image(sound)}")
+    else:
+        print(f"[bold red]{new_cat.species_name} does not say '{sound}'!")
+
+
+# Command for a dog
+@app.command()
+def dog(sound: str):
+    new_dog = Dog()
+    if new_dog.validate_sound(sound):
+        print(f"{new_dog.generate_image(sound)}")
+    else:
+        print(f"[bold red]{new_dog.species_name} does not say '{sound}'!")
---
+++ packages/pyanimalsay/pyanimalsay-0.0.0.2.tar.gz/pyanimalsay-0.0.0.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pyanimalsay', 'pyanimalsay.app']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['typer[all]>=0.6.1,<0.7.0']
+
+entry_points = \
+{'console_scripts': ['pyanimalsay = pyanimalsay.main:app']}
+
+setup_kwargs = {
+    'name': 'pyanimalsay',
+    'version': '0.0.0.2',
+    'description': '',
+    'long_description': "## Welcome to the FARM\n### (FAstAPI, React, MongoDB)\n###\n###\n\n#### Cow says 'mo'\n#### Cat says 'meow'\n#### Dog says 'woof'",
+    'author': 'sindbad_the_sailor',
+    'author_email': 'sindbad_the_sailor@example.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
