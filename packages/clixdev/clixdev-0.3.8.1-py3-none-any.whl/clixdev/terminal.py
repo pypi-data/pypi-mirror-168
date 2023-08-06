52607e8f04f85490e85f4a7f66b3a63e2eeb8b96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:07 +0000

    Add package clixdev/0.3.8.1/clixdev-0.3.8.1-py3-none-any.whl

---
+++ packages/clixdev/clixdev-0.3.8.1-py3-none-any.whl/clixdev/terminal.py
@@ -0,0 +1,24 @@
+def terminal():
+    try:
+        import argparse
+        parser = argparse.ArgumentParser(prog="clixdev", description="Clix.dev command line tool.")
+        parser.add_argument('action', choices=['generate', 'sync'])
+        parser.add_argument('terminal_token', type=str)
+        parser.add_argument('project_token', type=str)
+        parser.add_argument('-dir', type=str, required=False)
+        args = parser.parse_args()
+
+        if args.action == 'generate':
+            from .commands import generate
+            return generate(args.terminal_token, args.project_token, args.dir)
+
+        if args.action == 'sync':
+            from .commands import sync
+            return sync(args.terminal_token, args.project_token, args.dir)
+        
+        return False
+    except Exception as e:
+        print(e)
+        return False
+
+terminal()
