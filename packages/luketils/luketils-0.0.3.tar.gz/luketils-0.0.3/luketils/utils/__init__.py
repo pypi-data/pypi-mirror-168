be6715246880ac8c3f1c9a362cd2583e87ef0efe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:49 +0000

    Add package luketils/0.0.3/luketils-0.0.3.tar.gz

---
+++ packages/luketils/luketils-0.0.3.tar.gz/luketils-0.0.3/luketils/utils/__init__.py
@@ -0,0 +1,9 @@
+import os
+from datetime import datetime
+
+def ensure_exists(path):
+    os.makedirs(path, exist_ok=True)
+
+def experiment_name(base):
+    now = datetime.now()
+    return f"{base}-{now.strftime('%m/%d/%y-%H:%M%S')}"
