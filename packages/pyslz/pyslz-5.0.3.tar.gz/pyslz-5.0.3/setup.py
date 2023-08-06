92731e3f54338323ce10491fa6e1bb3d72dcc20b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:22 +0000

    Add package pyslz/5.0.3/pyslz-5.0.3.tar.gz

---
+++ packages/pyslz/pyslz-5.0.3.tar.gz/pyslz-5.0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+VERSION = '5.0.3'
+DESCRIPTION = 'Usefull functions'
+
+setup(name="pyslz",
+      version=VERSION,
+      author="Eduardo Maia",
+      author_email="<maia@pessoalize.com>",
+      description=DESCRIPTION,
+      long_description_content_type="text/markdown",
+      packages=find_packages(),
+      keywords=['pyslz'],
+      classifiers=["Development Status :: 1 - Planning",
+                   "Intended Audience :: Developers",
+                   "Programming Language :: Python :: 3",
+                   "Operating System :: Unix",
+                   "Operating System :: MacOS :: MacOS X",
+                   "Operating System :: Microsoft :: Windows"])
