9ffb0c52bad1124d4092ee31f1cec65c17c493fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:48 +0000

    Add package streamlit-text-rating-component/0.1.9/streamlit_text_rating_component-0.1.9.tar.gz

---
+++ packages/streamlit-text-rating-component/streamlit_text_rating_component-0.1.9.tar.gz/streamlit_text_rating_component-0.1.9/setup.py
@@ -0,0 +1,39 @@
+import pathlib
+import setuptools
+import os
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = pathlib.Path("../README.md").read_text()
+
+# Get the code version
+version = {}
+with open(os.path.join(HERE, "streamlit_text_rating/version.py")) as fp:
+    exec(fp.read(), version)
+__version__ = version["__version__"]
+
+setuptools.setup(
+    name="streamlit_text_rating_component",
+    version=__version__,
+    author="Ashish Rai, Naga Budigam, modified by Granit Luzhnica",
+    author_email="ashishraics512@gmail.com, nagaraju.iiit@gmail.com",
+    description="streamlit_text_ratings enables you to rate the text data",
+    long_description=README,
+    long_description_content_type="text/plain",
+    url="https://github.com/TVS-Motor-Company/streamlit-text-rating",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    license="MIT",
+    python_requires=">=3.6",
+    install_requires=[
+        "streamlit >= 1.0.0",
+    ],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ]
+)
---
+++ packages/streamlit-text-rating-component/streamlit_text_rating_component-0.1.9.tar.gz/streamlit_text_rating_component-0.1.9/streamlit_text_rating/frontend/build/precache-manifest.ff264b8d2a0ca9129428fd2dd8c04594.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a4ed7521d5b15ceedb7c270d3577b41",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ccb795514c71e2c98903",
+    "url": "./static/js/2.a69c7c9c.chunk.js"
+  },
+  {
+    "revision": "bc287589f3fefb198aaa0061e2ebdab0",
+    "url": "./static/js/2.a69c7c9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2492256427b89d5b5359",
+    "url": "./static/js/main.765c33cd.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
