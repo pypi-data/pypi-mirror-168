efdbfb75c8d870a52ce683e4396ff8b5d85ad9da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:43 +0000

    Add package web-poet/0.5.0/web_poet-0.5.0-py3-none-any.whl

---
+++ packages/web-poet/web_poet-0.5.0-py3-none-any.whl/tests/po_lib/a_module.py
@@ -0,0 +1,15 @@
+from url_matcher import Patterns
+
+from tests.po_lib import POBase
+from web_poet import ItemPage, handle_urls
+
+
+class POModuleOverriden(ItemPage):
+    ...
+
+
+@handle_urls("example.com", overrides=POModuleOverriden, extra_arg="foo")
+class POModule(POBase):
+    expected_overrides = POModuleOverriden
+    expected_patterns = Patterns(["example.com"])
+    expected_meta = {"extra_arg": "foo"}  # type: ignore
---
+++ packages/web-poet/web_poet-0.5.0-py3-none-any.whl/tests/po_lib/nested_package/__init__.py
@@ -0,0 +1,19 @@
+from url_matcher import Patterns
+
+from tests.po_lib import POBase
+from web_poet import ItemPage, handle_urls
+
+
+class PONestedPkgOverriden(ItemPage):
+    ...
+
+
+@handle_urls(
+    include=["example.com", "example.org"],
+    exclude=["/*.jpg|"],
+    overrides=PONestedPkgOverriden,
+)
+class PONestedPkg(POBase):
+    expected_overrides = PONestedPkgOverriden
+    expected_patterns = Patterns(["example.com", "example.org"], ["/*.jpg|"])
+    expected_meta = {}  # type: ignore
