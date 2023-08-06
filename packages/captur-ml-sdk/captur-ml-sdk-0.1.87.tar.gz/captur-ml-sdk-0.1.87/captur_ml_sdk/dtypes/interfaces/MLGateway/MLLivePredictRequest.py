df0fbc4d6c8c1632c174d857edc9587e2efcb12f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:58 +0000

    Add package captur-ml-sdk/0.1.87/captur-ml-sdk-0.1.87.tar.gz

---
+++ packages/captur-ml-sdk/captur-ml-sdk-0.1.87.tar.gz/captur-ml-sdk-0.1.87/captur_ml_sdk/dtypes/interfaces/MLGateway/MLLivePredictRequest.py
@@ -0,0 +1,22 @@
+from lib2to3.pgen2.token import OP
+from captur_ml_sdk.dtypes.generics import Image, Meta
+
+from pydantic import (
+    BaseModel
+)
+from typing import Optional
+
+
+class Model(BaseModel):
+    endpoint_id: str
+    location: Optional[str] = "us-central1"
+    type: str
+
+    class Config:
+        arbitrary_types_allowed = True
+
+
+class ModelLivePredictRequest(BaseModel):
+    meta: Optional[Meta] = None
+    model: Model
+    data: Image
