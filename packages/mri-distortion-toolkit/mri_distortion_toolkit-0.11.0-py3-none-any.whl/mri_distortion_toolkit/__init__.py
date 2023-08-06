e4bcc0c4d0061c6a601dbe0c6efa6357dd5bda10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:05 +0000

    Add package mri-distortion-toolkit/0.11.0/mri_distortion_toolkit-0.11.0-py3-none-any.whl

---
+++ packages/mri-distortion-toolkit/mri_distortion_toolkit-0.11.0-py3-none-any.whl/mri_distortion_toolkit/__init__.py
@@ -0,0 +1,8 @@
+"""
+This package is for the characterisation and reporting of geometric distortion in MRI
+Please see here for detailed documentation
+https://acrf-image-x-institute.github.io/MRI_DistortionQA/index.html
+"""
+
+__version__ = '0.11.0'
+from .calculate_harmonics import calculate_harmonics
