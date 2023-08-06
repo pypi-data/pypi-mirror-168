5367863eb5e25e889149d0db67da25286d32d169
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:13 +0000

    Add package wust/0.1.0/wust-0.1.0.tar.gz

---
+++ packages/wust/wust-0.1.0.tar.gz/wust-0.1.0/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['wust']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Bottleneck>=1.3.5,<2.0.0',
+ 'dask>=2022.9.1,<2023.0.0',
+ 'intake>=0.6.6,<0.7.0',
+ 'ipykernel>=6.15.3,<7.0.0',
+ 'matplotlib>=3.6.0,<4.0.0',
+ 'netCDF4>=1.6.1,<2.0.0',
+ 'pandas>=1.5.0,<2.0.0',
+ 'xarray>=2022.6.0,<2023.0.0']
+
+setup_kwargs = {
+    'name': 'wust',
+    'version': '0.1.0',
+    'description': 'WEkEO User Support Toolbox contains a set of tools to ease work in WEkEO.',
+    'long_description': '',
+    'author': 'David Bazin',
+    'author_email': 'dbazin@wekeo.eu',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
