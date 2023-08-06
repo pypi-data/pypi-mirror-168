fe85e63de72f426a150b091cc572decac5f77c46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:33 +0000

    Add package tikzify/0.15.1/tikzify-0.15.1.tar.gz

---
+++ packages/tikzify/tikzify-0.15.1.tar.gz/tikzify-0.15.1/tikzify/function_graph/curve_source/curve_source.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+from collections.abc import Iterable
+from typing import Any
+
+import numpy as np
+
+__all__ = ['CurveSource']
+
+
+class CurveSource:
+
+    def __init__(self, start_time: float, end_time: float):
+        """
+        Args:
+            start_time: The start time, which is inclusive.
+            end_time: The end time, which is inclusive.
+        """
+        super().__init__()
+        self.start_time = start_time
+        self.end_time = end_time
+
+    # Abstract methods -----------------------------------------------------------------------------
+    def times_and_values(self, resolution: int) -> Iterable[np.ndarray[Any, Any]]:
+        raise NotImplementedError
