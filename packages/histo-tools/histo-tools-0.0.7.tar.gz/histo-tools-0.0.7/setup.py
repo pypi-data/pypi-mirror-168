efddc0874dd7320d48a8082b063c9cb4028cb860
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:52 +0000

    Add package histo-tools/0.0.7/histo-tools-0.0.7.tar.gz

---
+++ packages/histo-tools/histo-tools-0.0.7.tar.gz/histo-tools-0.0.7/setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+setup(
+    name='histo-tools',
+    version='0.0.7',
+    description='General histology tools.',
+    author='Austin E. Y. T. Lefebvre',
+    license='MIT',
+    author_email='austin.e.lefebvre@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/aelefebv/histo-tools',
+    keywords='histology',
+    install_requires=[
+        'numpy',
+        'zarr',
+        'imagecodecs',
+        'tifffile',
+      ],
+)
