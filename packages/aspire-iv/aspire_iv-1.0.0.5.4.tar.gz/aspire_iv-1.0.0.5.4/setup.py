24be72f57b331a25ae4e20a7320194a72b7f2e87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:21 +0000

    Add package aspire-iv/1.0.0.5.4/aspire_iv-1.0.0.5.4.tar.gz

---
+++ packages/aspire-iv/aspire_iv-1.0.0.5.4.tar.gz/aspire_iv-1.0.0.5.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='aspire_iv',
+    version='1.0.0.5.4',
+    license='',
+    author="Girish Kumar R M",
+    author_email='',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='',
+    keywords='iv_and_prediction',
+    install_requires=[
+          'scikit-learn',
+          'pymysql'
+      ],
+
+)
