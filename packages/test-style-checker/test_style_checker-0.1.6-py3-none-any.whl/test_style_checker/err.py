f9bfd9cd58e3514f319482527f7356433d26289a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:33 +0000

    Add package test-style-checker/0.1.6/test_style_checker-0.1.6-py3-none-any.whl

---
+++ packages/test-style-checker/test_style_checker-0.1.6-py3-none-any.whl/test_style_checker/err.py
@@ -0,0 +1,10 @@
+"""Text and error codes"""
+
+ERROR: dict[str, str] = {
+    "MC100": "MC100: Invalid file name",
+    "MC101": "MC101: Dublicate files names:\n{}\n{}",
+    "MC102": "MC102: Bad test function name {}",
+    "MC103": "MC103: No '@qasecase' or '@case' decorator or invalid value case id",
+    "MC104": "MC104: Dublicate case id: {}",
+    "MC105": "MC105: Invalid function"
+}
