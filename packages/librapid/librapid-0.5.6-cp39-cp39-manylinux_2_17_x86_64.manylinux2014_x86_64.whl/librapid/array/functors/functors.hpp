20e8410ab152cb97100a92478719e2614ffb78b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:43 +0000

    Add package librapid/0.5.6/librapid-0.5.6-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/librapid/librapid-0.5.6-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/librapid/array/functors/functors.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "binary/bitwise.hpp"
+#include "binary/scalarArithmetic.hpp"
+#include "unary/negate.hpp"
+#include "unary/generic.hpp"
+#include "matrix/transpose.hpp"
+#include "misc/fill.hpp"
+#include "assignOp.hpp"
