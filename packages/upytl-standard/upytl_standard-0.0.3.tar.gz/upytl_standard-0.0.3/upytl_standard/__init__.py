87e0accdd82457856606970829d1cd208ade52a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:51 +0000

    Add package upytl-standard/0.0.3/upytl_standard-0.0.3.tar.gz

---
+++ packages/upytl-standard/upytl_standard-0.0.3.tar.gz/upytl_standard-0.0.3/upytl_standard/__init__.py
@@ -0,0 +1,23 @@
+from .s_components import *
+
+__all__ = (
+    'HTMLPage',
+    'NavBarItem',
+    'StandardNavBar',
+    'TextAreaField',
+    'TreeView',
+    'StringField',
+    'PasswordField',
+    'SelectField',
+    'FileField',
+    'CheckBoxField',
+    'RadioField',
+    'StandardField',
+    'StandardForm',
+    'DTGrid',
+    'HTMLGrid'
+)
+
+__author__ = "John Bannister <eudorajab1@gmail.com>"
+__license__ = "MIT"
+__version__ = "0.0.3"
