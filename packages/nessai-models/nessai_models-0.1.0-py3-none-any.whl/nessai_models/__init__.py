e61ebf48af7f6557fd600d15cb59f0087ec5ab9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:33 +0000

    Add package nessai-models/0.1.0/nessai_models-0.1.0-py3-none-any.whl

---
+++ packages/nessai-models/nessai_models-0.1.0-py3-none-any.whl/nessai_models/__init__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Models for use with the nested sampler \
+    `nessai <https://github.com/mj-will/nessai-models>`_.
+"""
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:  # for Python < 3.8
+    from importlib_metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    pass
+
+from .eggbox import EggBox
+from .gaussian import Gaussian
+from .gaussianmixture import GaussianMixture, GaussianMixtureWithData
+from .halfgaussian import HalfGaussian
+from .pyramid import Pyramid
+from .rosenbrock import Rosenbrock
+
+__all__ = [
+    "EggBox",
+    "Gaussian",
+    "GaussianMixture",
+    "GaussianMixtureWithData",
+    "HalfGaussian",
+    "Pyramid",
+    "Rosenbrock",
+]
