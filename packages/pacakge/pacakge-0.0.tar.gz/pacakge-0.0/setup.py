a9a6598eda1e8d370b1d54ca6b419e582a39a9c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:33 +0000

    Add package pacakge/0.0/pacakge-0.0.tar.gz

---
+++ packages/pacakge/pacakge-0.0.tar.gz/pacakge-0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pacakge',
+      version='0.0',
+      description='Gaussian and Binomial distributions',
+      packages=['pacakge'],
+      author='PPPP',
+      author_email='purvaa1425@gmail.com',
+      zip_safe=False)
