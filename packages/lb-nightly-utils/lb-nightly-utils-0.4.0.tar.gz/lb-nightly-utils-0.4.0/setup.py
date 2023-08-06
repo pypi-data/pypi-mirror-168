50f1cca0f8fb846498dd9afc567232e7a816acd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:57 +0000

    Add package lb-nightly-utils/0.4.0/lb-nightly-utils-0.4.0.tar.gz

---
+++ packages/lb-nightly-utils/lb-nightly-utils-0.4.0.tar.gz/lb-nightly-utils-0.4.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['lb', 'lb.nightly.utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['lb-nightly-configuration>=0.3,<0.4', 'requests>=2.26.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'lb-nightly-utils',
+    'version': '0.4.0',
+    'description': 'Common utilities for LHCb Nightly and Continuous Integration Build System',
+    'long_description': 'None',
+    'author': 'Marco Clemencic',
+    'author_email': 'marco.clemencic@cern.ch',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
