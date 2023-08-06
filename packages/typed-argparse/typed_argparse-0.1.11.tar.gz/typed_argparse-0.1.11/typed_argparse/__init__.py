3daae3f06da772b833eb3a23d18049e532789a96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:39 +0000

    Add package typed-argparse/0.1.11/typed_argparse-0.1.11.tar.gz

---
+++ packages/typed-argparse/typed_argparse-0.1.11.tar.gz/typed_argparse-0.1.11/typed_argparse/__init__.py
@@ -0,0 +1,19 @@
+from .choices import Choices
+from .typed_argparse import (
+    TypedArgs,
+    WithUnionType,
+    get_choices_from,
+    get_choices_from_class,
+    validate_type_union,
+)
+
+VERSION = "0.1.11"
+
+__all__ = [
+    "Choices",
+    "TypedArgs",
+    "WithUnionType",
+    "get_choices_from",
+    "get_choices_from_class",
+    "validate_type_union",
+]
