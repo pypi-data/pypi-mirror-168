a9c906567d334ee9dbdb2edb46c2f637e43ca6d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:56 +0000

    Add package gmzyeahye426/1.0.4/gmzyeahye426-1.0.4-py3-none-any.whl

---
+++ packages/gmzyeahye426/gmzyeahye426-1.0.4-py3-none-any.whl/tools.py
@@ -0,0 +1,12 @@
+# from sztestlib import werwq
+#
+# cast=['nihao','ggg','jji']
+# werwq.print_lol(cast)   #建议还是用这个
+
+# import sztestlib
+#
+# cast=['nihao','ggg','jji']
+# sztestlib.werwq.print_lol(cast)
+def ff():
+    print("nihao")
+
