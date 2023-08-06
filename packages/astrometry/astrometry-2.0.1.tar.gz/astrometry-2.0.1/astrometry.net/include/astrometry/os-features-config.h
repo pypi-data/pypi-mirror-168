2c02e54233355a837072d757784ab671ad9a53e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:58 +0000

    Add package astrometry/2.0.1/astrometry-2.0.1.tar.gz

---
+++ packages/astrometry/astrometry-2.0.1.tar.gz/astrometry-2.0.1/astrometry.net/include/astrometry/os-features-config.h
@@ -0,0 +1,4 @@
+#define HAVE_NETPBM 0
+#define AN_GIT_REVISION "0.89-7-g47849f04"
+#define AN_GIT_DATE "Fri Jan 28 16:25:01 2022 -0500"
+#define AN_GIT_URL "https://github.com/dstndstn/astrometry.net"
