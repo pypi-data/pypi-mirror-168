edad3a344aa9ce9b95c54c3e7bb6e17a390ebfd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:15 +0000

    Add package pyblockworld/0.1.6/pyblockworld-0.1.6.tar.gz

---
+++ packages/pyblockworld/pyblockworld-0.1.6.tar.gz/pyblockworld-0.1.6/README.md
@@ -0,0 +1,14 @@
+Eine an Minecraft angelehnte Welt aus Blöcken.
+
+
+```python
+    def b_key_pressed(world:World):
+        print("b pressed. player at", world.player_position())
+        x,y,z = world.player_position()
+        world.setBlock(x,y,z, "default:brick")
+        
+    world = World("DEMO")
+    print("Block types", World.MATERIALS.keys())
+    world.build_key_function = b_key_pressed
+    world.run()
+```
---
+++ packages/pyblockworld/pyblockworld-0.1.6.tar.gz/pyblockworld-0.1.6/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['pyblockworld']
+install_requires = \
+['pyglet>=1.5.26,<2.0.0']
+
+setup_kwargs = {
+    'name': 'pyblockworld',
+    'version': '0.1.6',
+    'description': 'Minecraft like Block world in Python',
+    'long_description': 'Eine an Minecraft angelehnte Welt aus Blöcken.\n\n\n```python\n    def b_key_pressed(world:World):\n        print("b pressed. player at", world.player_position())\n        x,y,z = world.player_position()\n        world.setBlock(x,y,z, "default:brick")\n        \n    world = World("DEMO")\n    print("Block types", World.MATERIALS.keys())\n    world.build_key_function = b_key_pressed\n    world.run()\n```\n',
+    'author': 'Marco Bakera',
+    'author_email': 'marco@bakera.de',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
