704106fa7e3a00e53d71342f7b323aa762d08322
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:13 +0000

    Add package cryptomus/1.0.1/cryptomus-1.0.1.tar.gz

---
+++ packages/cryptomus/cryptomus-1.0.1.tar.gz/cryptomus-1.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='cryptomus',
+      version='1.0.1',
+      description='Python SDK for Cryptomus.com API',
+      packages=['cryptomus'],
+      author_email='9or9or9@mail.ru',
+      zip_safe=False
+      )
