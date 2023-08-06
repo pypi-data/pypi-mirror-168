b0998c5cc238db66b7f5055df98356fb6c6d6dc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:49 +0000

    Add package gradio/3.4b2/gradio-3.4b2-py3-none-any.whl

---
+++ packages/gradio/gradio-3.4b2-py3-none-any.whl/gradio/templates/frontend/assets/index.60d34eec.js
@@ -0,0 +1,5 @@
+export { Q as Component } from './index.3d8797ed.js';
+
+const modes = ["static"];
+
+export { modes };
---
+++ packages/gradio/gradio-3.4b2-py3-none-any.whl/gradio/templates/frontend/assets/index.865315ab.js
@@ -0,0 +1,7 @@
+export { C as Component } from './Column.dd028c3e.js';
+import './index.3d8797ed.js';
+import './styles.ed3b21b5.js';
+
+const modes = ["static"];
+
+export { modes };
