85d0f73b6c598dc8b94061ac5c722c200da0e4e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:59 +0000

    Add package PyWindows/0.2/PyWindows-0.2.tar.gz

---
+++ packages/PyWindows/PyWindows-0.2.tar.gz/PyWindows-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="PyWindows",
+    version=0.2,
+    description="Windows for Python",
+    long_description="AAA",
+    packages=['src'],
+    author="Phoneguytech"
+)
