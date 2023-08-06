2784a0db13a1310aa32b712ce83f1e57cc9f8174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:18 +0000

    Add package unimport/0.12.1/unimport-0.12.1-py3-none-any.whl

---
+++ packages/unimport/unimport-0.12.1-py3-none-any.whl/unimport/commands/diff.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+from unimport import utils
+from unimport.color import difference
+
+__all__ = ("diff",)
+
+
+def diff(path: Path, source: str, refactor_result: str) -> bool:
+    diff_ = utils.diff(source=source, refactor_result=refactor_result, fromfile=path)
+    exists_diff = bool(diff_)
+    if exists_diff:
+        print(difference(diff_))
+
+    return exists_diff
