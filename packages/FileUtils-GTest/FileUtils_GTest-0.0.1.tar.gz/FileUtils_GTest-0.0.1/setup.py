5f60110125818dc7cafbfe681348cf508396bc90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:58 +0000

    Add package FileUtils-GTest/0.0.1/FileUtils_GTest-0.0.1.tar.gz

---
+++ packages/FileUtils-GTest/FileUtils_GTest-0.0.1.tar.gz/FileUtils_GTest-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='FileUtils_GTest',
+      version='0.0.1',
+      description='FileUtils_GTest',
+      url='https://github.com/BigDataFounder/FileUtils_GTest',
+      author='gengch',
+      author_email='gengchuanhua@gmail.com',
+      license='MIT',
+      packages=['FileUtils_GTest'])
