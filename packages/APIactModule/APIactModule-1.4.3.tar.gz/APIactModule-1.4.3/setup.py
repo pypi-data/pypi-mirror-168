5d76a5d1ef7638ffc1759c558bc37d2aff626906
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:34 +0000

    Add package APIactModule/1.4.3/APIactModule-1.4.3.tar.gz

---
+++ packages/APIactModule/APIactModule-1.4.3.tar.gz/APIactModule-1.4.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="APIactModule",
+    version="1.4.3",
+    author="MasatoSiraisi",
+    url="https://github.com/siraisi368/APIActionModule",
+    download_url="https://github.com/siraisi368/APIActionModule",
+    license="Apache License2.0",
+    install_requires=["requests","discord","ntplib","xmltodict"]
+    )
