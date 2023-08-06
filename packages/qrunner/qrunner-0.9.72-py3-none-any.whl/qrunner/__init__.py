babde4b75e2bd4d2a916b2903aa62f8f3064d329
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:54 +0000

    Add package qrunner/0.9.72/qrunner-0.9.72-py3-none-any.whl

---
+++ packages/qrunner/qrunner-0.9.72-py3-none-any.whl/qrunner/__init__.py
@@ -0,0 +1,8 @@
+from qrunner.running.runner import main
+from qrunner.utils.log import logger
+from qrunner.utils.decorate import *
+from qrunner.utils.mock import mock_data
+from qrunner.utils.config import conf
+
+__version__ = "0.9.71"
+__description__ = "全栈自动化测试框架"
