aab93fe09c6592c4fb75dc9000ee55478ade3e1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:57:12 +0000

    Add package browserist/1.1.2/browserist-1.1.2-py3-none-any.whl

---
+++ packages/browserist/browserist-1.1.2-py3-none-any.whl/browserist/browser/get/texts.py
@@ -0,0 +1,13 @@
+from selenium.webdriver.common.by import By
+
+from ...model.browser.base.driver import BrowserDriver
+from ...model.type.xpath import XPath
+from ..wait.for_element import wait_for_element
+
+
+def get_texts(browser_driver: BrowserDriver, xpath: str, timeout: float) -> list[str]:
+    xpath = XPath(xpath)
+    wait_for_element(browser_driver, xpath, timeout)
+    driver = browser_driver.get_webdriver()
+    elements: list[object] = driver.find_elements(By.XPATH, xpath)  # type: ignore
+    return [element.text for element in elements]  # type: ignore
