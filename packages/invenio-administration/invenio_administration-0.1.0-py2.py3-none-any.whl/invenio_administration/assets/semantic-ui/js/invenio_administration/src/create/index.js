d05ff6aee5085c6f5186b9436be67b1ed0bea51e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:46 +0000

    Add package invenio-administration/0.1.0/invenio_administration-0.1.0-py2.py3-none-any.whl

---
+++ packages/invenio-administration/invenio_administration-0.1.0-py2.py3-none-any.whl/invenio_administration/assets/semantic-ui/js/invenio_administration/src/create/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { CreatePage } from "./CreatePage";
+import _get from "lodash/get";
+import { NotificationController } from "../ui_messages/context";
+
+const domContainer = document.getElementById("invenio-administration-create-root");
+const resourceSchema = JSON.parse(domContainer.dataset.resourceSchema);
+const apiEndpoint = _get(domContainer.dataset, "apiEndpoint");
+const formFields = JSON.parse(domContainer.dataset.formFields);
+const listUIEndpoint = domContainer.dataset.listEndpoint;
+
+ReactDOM.render(
+  <NotificationController>
+    <CreatePage
+      resourceSchema={resourceSchema}
+      apiEndpoint={apiEndpoint}
+      formFields={formFields}
+      listUIEndpoint={listUIEndpoint}
+    />
+  </NotificationController>,
+  domContainer
+);
