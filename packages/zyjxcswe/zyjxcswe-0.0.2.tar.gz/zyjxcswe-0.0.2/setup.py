abcd3bc9f542fc34c149ab8ab2d999e75dfc1a85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:01 +0000

    Add package zyjxcswe/0.0.2/zyjxcswe-0.0.2.tar.gz

---
+++ packages/zyjxcswe/zyjxcswe-0.0.2.tar.gz/zyjxcswe-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(name='zyjxcswe',
+      version='0.0.2',
+      description='gk first demo',
+      author='laomaoQAQ',
+      author_email='17863923011@163.com',
+      requires=[],  # 定义依赖哪些模块
+      packages=['zyjxcswe'],  # 系统自动从当前目录开始找包
+      # packages=['代码1','代码2','__init__'],  #指定目录中需要打包的py文件，注意不要.py后缀
+      license="apache 3.0"
+      )
