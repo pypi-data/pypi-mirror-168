35184bfa34e91a8852d49ac3751b63e59864bf3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:23 +0000

    Add package data-operations/0.0.2/data_operations-0.0.2.tar.gz

---
+++ packages/data-operations/data_operations-0.0.2.tar.gz/data_operations-0.0.2/README.md
@@ -0,0 +1,14 @@
+Data Operations
+
+#### Pacote construído para a manipulação de um banco de dados e retorno de resultados, baseado nas condições definidas pelo usuário.
+
+
+#### Exemplos de Aplicações:
+
+
+#### Instalação: 
+
+    pip install data_operations
+
+
+#### Uso:
---
+++ packages/data-operations/data_operations-0.0.2.tar.gz/data_operations-0.0.2/src/data_operations/data_operations.py
@@ -0,0 +1,48 @@
+class data_operations():
+
+    '''
+    Pacote construído para a manipulação de um banco de dados e 
+    retorno de resultados, baseado nas condições definidas pelo usuário.
+    '''
+
+    def __init__(self):
+        pass
+
+
+    def desvio_padrao():
+        pass
+
+
+    def diferenca():
+        pass
+
+
+    def media():
+        pass
+
+
+    def mediana():
+        pass
+
+
+    def moda():
+        pass
+
+
+    def porcentagem():
+        pass
+
+
+    def selecao():
+        pass
+
+
+    def somatorio():
+        pass
+
+
+    def variancia():
+        pass
+
+
+    pass
