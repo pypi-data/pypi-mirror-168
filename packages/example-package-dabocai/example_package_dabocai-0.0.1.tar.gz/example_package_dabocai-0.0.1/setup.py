8d8cbbf512c1daae45986267ee58c07de2da1064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:39 +0000

    Add package example-package-dabocai/0.0.1/example_package_dabocai-0.0.1.tar.gz

---
+++ packages/example-package-dabocai/example_package_dabocai-0.0.1.tar.gz/example_package_dabocai-0.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+
+
+setuptools.setup(
+    name="example_package_dabocai",
+    version="0.0.1",
+    author="Example Author dabocai",
+    author_email="author@example.com",
+
+    description="A small example package",
+    long_description="long_description=",
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=['dabocai'],
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',
+)
