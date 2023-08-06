d99a22c77855418cfaf1e56e3cc273c2bffbf3ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:46 +0000

    Add package pip-model/0.6/pip_model-0.6.tar.gz

---
+++ packages/pip-model/pip_model-0.6.tar.gz/pip_model-0.6/setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup
+
+setup(name='pip_model',
+      version='0.6',
+      description='string to date',
+      url="https://github.com/18832823200/pip_model",
+      aauthor="sbdsyf",
+      author_email="zkw8119@163.com",
+      license='MIT',
+      packages=['pip_model'],
+      include_package_data=True,
+      platforms="any",
+      install_requires=[]
+      )
