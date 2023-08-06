f5d5b33b25099dd8b81b1a499c564731063f3cad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:07 +0000

    Add package datasetstore/1.0.0.10/datasetstore-1.0.0.10.tar.gz

---
+++ packages/datasetstore/datasetstore-1.0.0.10.tar.gz/datasetstore-1.0.0.10/datasetstore/eda/__init__.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+from pandas_profiling import ProfileReport
+
+
+def quick_analysis(dataset, title="数据集快速分析报告", minimal=False):
+    # config_file=Path(__file__).parent / "profiling.yml",
+    profile = ProfileReport(
+        dataset.to_pandas(), title=title, explorative=True, dark_mode=True, minimal=minimal,
+    )
+    return profile
