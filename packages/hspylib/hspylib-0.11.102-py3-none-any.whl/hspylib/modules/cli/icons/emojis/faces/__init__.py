08d7d27bd1dc45d69ae9aa4bd127c1f8fa5ad316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:42 +0000

    Add package hspylib/0.11.102/hspylib-0.11.102-py3-none-any.whl

---
+++ packages/hspylib/hspylib-0.11.102-py3-none-any.whl/hspylib/modules/cli/icons/emojis/faces/__init__.py
@@ -0,0 +1,11 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib v0.11.102
+#
+# Package: main.hspylib.modules.cli.icons.emojis.faces
+"""Package initialization."""
+
+__all__ = [
+  'face_smiling'
+]
+__version__ = '0.11.102'
