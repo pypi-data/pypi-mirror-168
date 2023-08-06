7d74da451cef707004b1dcb412b526ad0db19a87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:02 +0000

    Add package virtool-core/2.32.0/virtool-core-2.32.0.tar.gz

---
+++ packages/virtool-core/virtool-core-2.32.0.tar.gz/virtool-core-2.32.0/virtool_core/models/index.py
@@ -0,0 +1,56 @@
+from datetime import datetime
+from typing import List, Dict
+
+from virtool_core.models.basemodel import BaseModel
+from virtool_core.models.job import JobNested
+from virtool_core.models.reference import ReferenceNested
+from virtool_core.models.searchresult import SearchResult
+from virtool_core.models.user import UserNested
+
+
+class IndexNested(BaseModel):
+    id: str
+    version: int
+
+
+class IndexMinimal(IndexNested):
+    change_count: int
+    created_at: datetime
+    has_files: bool
+    job: JobNested
+    modified_otu_count: int
+    reference: ReferenceNested
+    user: UserNested
+
+
+class IndexContributor(UserNested):
+    count: int
+
+
+class IndexOTU(BaseModel):
+    change_count: int
+    id: str
+    name: str
+
+
+class IndexFile(BaseModel):
+    download_url: str
+    id: int
+    index: str
+    name: str
+    size: int
+    type: str
+
+
+class Index(IndexMinimal):
+    contributors: List[IndexContributor]
+    files: List[IndexFile]
+    manifest: Dict[str, int]
+    otus: List[IndexOTU]
+    ready: bool
+
+
+class IndexSearchResult(SearchResult):
+    documents: List[IndexMinimal]
+    modified_otu_count: int
+    total_otu_count: int
