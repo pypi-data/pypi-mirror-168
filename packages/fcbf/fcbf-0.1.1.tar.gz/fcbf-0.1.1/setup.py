13ab5ead5b910ee9846d9f8c1d5a69ae8ae1dc61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:19 +0000

    Add package fcbf/0.1.1/fcbf-0.1.1.tar.gz

---
+++ packages/fcbf/fcbf-0.1.1.tar.gz/fcbf-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['fcbf', 'fcbf.data']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy', 'pandas', 'scipy']
+
+setup_kwargs = {
+    'name': 'fcbf',
+    'version': '0.1.1',
+    'description': 'Categorical feature selection based on information theoretical considerations',
+    'long_description': 'Implementation of the fast correlation-based filter (FCBF) proposed by Yu and Liu:\n\n@inproceedings{inproceedings,\nauthor = {Yu, Lei and Liu, Huan},\nyear = {2003},\nmonth = {01},\npages = {856-863},\ntitle = {Feature Selection for High-Dimensional Data: A Fast Correlation-Based Filter Solution},\nvolume = {2},\njournal = {Proceedings, Twentieth International Conference on Machine Learning}\n}\n',
+    'author': 'Martin Trat',
+    'author_email': 'martin.trat@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/m-martin-j/fcbf',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
