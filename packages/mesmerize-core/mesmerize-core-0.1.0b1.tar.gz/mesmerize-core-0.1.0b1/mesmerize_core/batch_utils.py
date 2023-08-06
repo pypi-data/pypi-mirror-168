a3c118add5f885f6ee0798c328e7a4fe99cd8831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:47 +0000

    Add package mesmerize-core/0.1.0b1/mesmerize-core-0.1.0b1.tar.gz

---
+++ packages/mesmerize-core/mesmerize-core-0.1.0b1.tar.gz/mesmerize-core-0.1.0b1/mesmerize_core/batch_utils.py
@@ -0,0 +1,237 @@
+import os
+from pathlib import Path
+from typing import Union
+
+import pandas as pd
+
+from .algorithms import cnmf, mcorr
+from .algorithms import cnmfe
+from .utils import validate_path
+
+CURRENT_BATCH_PATH: Path = None  # only one batch at a time
+PARENT_DATA_PATH: Path = None
+
+ALGO_MODULES = {
+    "cnmf": cnmf,
+    "mcorr": mcorr,
+    "cnmfe": cnmfe,
+}
+
+COMPUTE_BACKEND_SUBPROCESS = "subprocess"  #: subprocess backend
+COMPUTE_BACKEND_SLURM = "slurm"  #: SLURM backend, not yet implemented
+
+COMPUTE_BACKENDS = [COMPUTE_BACKEND_SUBPROCESS, COMPUTE_BACKEND_SLURM]
+
+DATAFRAME_COLUMNS = ["algo", "item_name", "input_movie_path", "params", "outputs", "comments", "uuid"]
+
+
+def set_parent_raw_data_path(path: Union[Path, str]) -> Path:
+    """
+    Set the global `PARENT_DATA_PATH`
+
+    Parameters
+    ----------
+    path: Union[Path, str]
+        Full parent data path
+    """
+    global PARENT_DATA_PATH
+    path = validate_path(path)
+    PARENT_DATA_PATH = Path(path)
+
+    return PARENT_DATA_PATH
+
+
+def get_parent_raw_data_path() -> Path:
+    """
+    Get the global `PARENT_DATA_PATH`
+    Returns
+    -------
+    Path
+        global `PARENT_DATA_PATH`
+
+    """
+    global PARENT_DATA_PATH
+    return PARENT_DATA_PATH
+
+
+class _BasePathExtensions:
+    def __init__(self, data: Union[pd.DataFrame, pd.Series]):
+        self._data = data
+
+    def set_batch_path(self, path: Union[str, Path]):
+        self._data.attrs["batch_path"] = Path(path)
+
+    def get_batch_path(self) -> Path:
+        if "batch_path" in self._data.attrs.keys():
+            if self._data.attrs["batch_path"] is not None:
+                return self._data.attrs["batch_path"]
+        else:
+            raise ValueError("Batch path is not set")
+
+    def resolve(self, path: Union[str, Path]) -> Path:
+        """
+        Resolve the full path of ``path`` if possible, first tries batch_dir then raw_data_dir
+        Last resort plain resolve.
+
+        Parameters
+        ----------
+        path: Union[str, Path]
+            The relative path to resolve
+
+        Returns
+        -------
+        Full path with the batch path or raw data path appended
+
+        """
+        path = Path(path)
+        # check if input movie is within batch dir
+        if self.get_batch_path().parent.joinpath(path).exists():
+            return self.get_batch_path().parent.joinpath(path)
+
+        # else check if in parent raw data dir
+        elif get_parent_raw_data_path() is not None:
+            if get_parent_raw_data_path().joinpath(path).exists():
+                return get_parent_raw_data_path().joinpath(path)
+
+        raise FileNotFoundError(f"Could not resolve full path of:\n{path}")
+
+    def split(self, path: Union[str, Path]):
+        """
+        Split a full path into (batch_dir, relative_path) or (raw_data_dir, relative_path)
+        Parameters
+        ----------
+        path: Union[str, Path]
+            Full path to split with respect to batch_dir or raw_data_dir
+
+        Returns
+        -------
+        Tuple[Path, Path]
+            (<batch_dir or raw_data_dir>, <relative_path>)
+
+        """
+        path = Path(path)
+        # check if input movie is within batch dir
+        if self.get_batch_path().parent in path.parents:
+            return self.get_batch_path().parent, path.relative_to(
+                self.get_batch_path().parent
+            )
+
+        # else check if in parent raw data dir
+        elif get_parent_raw_data_path() is not None:
+            if get_parent_raw_data_path() in path.parents:
+                return get_parent_raw_data_path(), path.relative_to(
+                    get_parent_raw_data_path()
+                )
+
+        raise NotADirectoryError(
+                f"Could not split `path`:\n{path}"
+                f"\nnot relative to either batch path:\n{self.get_batch_path()}"
+                f"\nor parent raw data path:\n{get_parent_raw_data_path()}"
+            )
+
+
+@pd.api.extensions.register_dataframe_accessor("paths")
+class PathsDataFrameExtension(_BasePathExtensions):
+    pass
+
+
+@pd.api.extensions.register_series_accessor("paths")
+class PathsSeriesExtension(_BasePathExtensions):
+    pass
+
+
+def load_batch(path: Union[str, Path]) -> pd.DataFrame:
+    """
+    Load the batch pickle file, also sets the global `CURRENT_BATCH_PATH`
+
+    Parameters
+    ----------
+    path: Union[str, Path])
+
+    Returns
+    -------
+    pd.DataFrame
+        batch dataframe loaded from the specified path
+
+    Examples
+    --------
+
+    .. code-block:: python
+
+        from mesmerize_core import *
+
+        df = load_batch("/path/to/batch.pickle")
+
+        # view dataframe
+        df.head()
+
+    """
+    # global CURRENT_BATCH_PATH
+    path = validate_path(path)
+
+    df = pd.read_pickle(Path(path))
+
+    # CURRENT_BATCH_PATH = pathlib.Path(batch_file)
+
+    df.paths.set_batch_path(path)
+
+    return df
+
+
+def create_batch(path: Union[str, Path], remove_existing: bool = False) -> pd.DataFrame:
+    """
+    Create a new batch DataFrame
+
+    Parameters
+    ----------
+    path: Union[str. Path]
+        path to save the new batch DataFrame
+
+    remove_existing: bool
+        If `True`, remove an existing batch DataFrame file if it exists at the given `path`
+
+    Returns
+    -------
+    pd.DataFrame
+        New empty batch DataFrame
+
+    Examples
+    --------
+
+    .. code-block:: python
+
+        from mesmerize_core import *
+
+        df = create_batch("/path/to/new_batch.pickle")
+
+    """
+    path = validate_path(path)
+
+    if Path(path).is_file():
+        if remove_existing:
+            os.remove(path)
+        else:
+            raise FileExistsError(
+                f"Batch file already exists at specified location: {path}"
+            )
+
+    if not Path(path).parent.is_dir():
+        os.makedirs(Path(path).parent)
+
+    df = pd.DataFrame(columns=DATAFRAME_COLUMNS)
+    df.paths.set_batch_path(path)
+
+    df.to_pickle(path)
+
+    # global CURRENT_BATCH_PATH
+    # CURRENT_BATCH_PATH = path
+
+    return df
+
+
+def get_full_raw_data_path(path: Union[Path, str]) -> Path:
+    path = Path(path)
+    if PARENT_DATA_PATH is not None:
+        return PARENT_DATA_PATH.joinpath(path)
+
+    return path
---
+++ packages/mesmerize-core/mesmerize-core-0.1.0b1.tar.gz/mesmerize-core-0.1.0b1/mesmerize_core/utils.py
@@ -0,0 +1,277 @@
+"""
+Useful functions adapted from old mesmerize
+
+GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
+"""
+
+
+import numpy as np
+from matplotlib import cm as matplotlib_color_map
+from functools import wraps
+import os
+from stat import S_IEXEC
+from typing import *
+import re as regex
+from pathlib import Path
+from warnings import warn
+import sys
+from tempfile import NamedTemporaryFile
+from subprocess import check_call
+
+if os.name == "nt":
+    IS_WINDOWS = True
+    HOME = "USERPROFILE"
+else:
+    IS_WINDOWS = False
+    HOME = "HOME"
+
+if "MESMERIZE_LRU_CACHE" in os.environ.keys():
+    MESMERIZE_LRU_CACHE = os.environ["MESMERIZE_LRU_CACHE"]
+else:
+    MESMERIZE_LRU_CACHE = 10
+
+
+qualitative_colormaps = [
+    "Pastel1",
+    "Pastel2",
+    "Paired",
+    "Accent",
+    "Dark2",
+    "Set1",
+    "Set2",
+    "Set3",
+    "tab10",
+    "tab20",
+    "tab20b",
+    "tab20c",
+]
+
+
+def warning_experimental(more_info: str = ""):
+    """
+    decorator to warn the user that the function is experimental
+    """
+    def catcher(func):
+        @wraps(func)
+        def fn(self, *args, **kwargs):
+            warn(
+                f"You are trying to use the following experimental feature, "
+                f"this may change in the future without warning:\n"
+                f"{func.__qualname__}\n"
+                f"{more_info}\n",
+                FutureWarning,
+                stacklevel=2
+            )
+            return func(self, *args, **kwargs)
+        return fn
+    return catcher
+
+
+def validate_path(path: Union[str, Path]):
+    if not regex.match("^[A-Za-z0-9@\/\\\:._-]*$", str(path)):
+        raise ValueError(
+            "Paths must only contain alphanumeric characters, "
+            "hyphens ( - ), underscores ( _ ) or periods ( . )"
+        )
+    return path
+
+
+def auto_colormap(
+    n_colors: int,
+    cmap: str = "hsv",
+    output: Union[str, type] = "float",
+    spacing: str = "uniform",
+    alpha: float = 1.0,
+) -> List[Union[np.ndarray, str]]:
+    """
+    If non-qualitative map: returns list of colors evenly spread through the chosen colormap.
+    If qualitative map: returns subsequent colors from the chosen colormap
+
+    Parameters
+    ----------
+    n_colors: int
+        Numbers of colors to return
+
+    cmap: str
+        item_name of colormap
+
+    output: Union[str, type]
+        option: "float" or ``float`` returns RGBA values between 0-1: [R, G, B, A],
+        option: "hex" returns hex strings that correspond to the RGBA values
+
+    spacing: str
+        option: "uniform"'" returns evenly spaced colors across the entire cmap range
+        option: "subsequent" returns subsequent colors from the cmap
+
+    alpha: float
+        alpha level, 0.0 - 1.0
+
+    Returns
+    -------
+    List[Union[np.ndarray, str]]
+        List of colors as either ``numpy.ndarray``, or hex ``str`` with length ``n_colors``
+    """
+
+    valid = ["float", float, "hex"]
+    if output not in valid:
+        raise ValueError(f"output must be one {valid}")
+
+    valid = ["uniform", "subsequent"]
+    if spacing not in valid:
+        raise ValueError(f"spacing must be one of either {valid}")
+
+    if alpha < 0.0 or alpha > 1.0:
+        raise ValueError("alpha must be within 0.0 and 1.0")
+
+    cm = matplotlib_color_map.get_cmap(cmap)
+    cm._init()
+
+    lut = (cm._lut).view(np.ndarray)
+
+    lut[:, 3] *= alpha
+
+    if spacing == "uniform":
+        if not cmap in qualitative_colormaps:
+            if cmap == "hsv":
+                cm_ixs = np.linspace(30, 210, n_colors, dtype=int)
+            else:
+                cm_ixs = np.linspace(0, 210, n_colors, dtype=int)
+        else:
+            if n_colors > len(lut):
+                raise ValueError("Too many colors requested for the chosen cmap")
+            cm_ixs = np.arange(0, len(lut), dtype=int)
+    else:
+        cm_ixs = range(n_colors)
+
+    colors = []
+    for ix in range(n_colors):
+        c = lut[cm_ixs[ix]]
+
+        if output == "hex":
+            c = tuple(c[:3] * 255)
+            hc = "#%02x%02x%02x" % tuple(map(int, c))
+            colors.append(hc)
+
+        else:  # floats
+            colors.append(c)
+
+    return colors
+
+
+def make_runfile(
+    module_path: str, args_str: Optional[str] = None, filename: Optional[str] = None
+) -> str:
+    """
+    Make an executable bash script.
+    Used for running python scripts in external processes within the same python environment as the main/parent process.
+
+    Parameters
+    ----------
+    module_path: str
+        absolute path to the python module/script that should be run externally
+
+    args_str: Optional[str]
+        optinal str of args that is directly passed to the script specified by ``module_path``
+
+    filename: Optional[str]
+        optional, filename of the executable bash script
+
+    Returns
+    -------
+    str
+        path to the shell script that can be executed
+    """
+
+    if filename is None:
+        if IS_WINDOWS:
+            sh_file = os.path.join(os.environ[HOME], "run.ps1")
+        else:
+            sh_file = os.path.join(os.environ[HOME], "run.sh")
+    else:
+        if IS_WINDOWS:
+            if not filename.endswith(".ps1"):
+                filename = filename + ".ps1"
+
+    sh_file = filename
+
+    if args_str is None:
+        args_str = ""
+
+    if not IS_WINDOWS:
+        with open(sh_file, "w") as f:
+
+            f.write(f"#!/bin/bash\n")
+
+            if "CONDA_PREFIX" in os.environ.keys():
+                f.write(
+                    f'export CONDA_PREFIX={os.environ["CONDA_PREFIX"]}\n'
+                    f'export CONDA_PYTHON_EXE={os.environ["CONDA_PYTHON_EXE"]}\n'
+                    f'export CONDA_PREFIX_1={os.environ["CONDA_PREFIX_1"]}\n'
+                )
+
+            elif "VIRTUAL_ENV" in os.environ.keys():
+                f.write(
+                    f'export PATH={os.environ["PATH"]}\n'
+                    f'export VIRTUAL_ENV={os.environ["VIRTUAL_ENV"]}\n'
+                    f'export LD_LIBRARY_PATH={os.environ["LD_LIBRARY_PATH"]}\n'
+                )
+
+            if "PYTHONPATH" in os.environ.keys():
+                f.write(f'export PYTHONPATH={os.environ["PYTHONPATH"]}\n')
+
+            # for k, v in os.environ.items():  # copy the current environment
+            #     if '\n' in v:
+            #         continue
+            #
+            # f.write(f'export {k}="{v}"\n')
+
+            # User-setable n-processes
+            if "MESMERIZE_N_PROCESSES" in os.environ.keys():
+                f.write(
+                    f'export MESMERIZE_N_PROCESSES={os.environ["MESMERIZE_N_PROCESSES"]}\n'
+                )
+
+            f.write(f"export OPENBLAS_NUM_THREADS=1\n" f"export MKL_NUM_THREADS=1\n")
+
+            f.write(f"python {module_path} {args_str}")  # call the script to run
+
+    else:
+        with open(sh_file, "w") as f:
+            for k, v in os.environ.items():  # copy the current environment
+                if regex.match("^.*[\(\)]", str(k)) or regex.match("^.*[\(\)]", str(v)):
+                    continue
+                with NamedTemporaryFile(suffix=".ps1", delete=False) as tmp:
+                    try:  # windows powershell is stupid so make sure all the env var names work
+                        tmp.write(f'$env:{k}="{v}";\n')
+                        tmp.close()
+                        check_call(f"powershell {tmp.name}")
+                        os.unlink(tmp.name)
+                    except:
+                        continue
+                f.write(f'$env:{k}="{v}";\n')  # write only env vars that powershell likes
+            f.write(f"{sys.executable} {module_path} {args_str}")
+
+    st = os.stat(sh_file)
+    os.chmod(sh_file, st.st_mode | S_IEXEC)
+
+    return sh_file
+
+
+def quick_min_max(data: np.ndarray) -> Tuple[float, float]:
+    # from pyqtgraph.ImageView
+    # Estimate the min/max values of *data* by subsampling.
+    # Returns [(min, max), ...] with one item per channel
+    while data.size > 1e6:
+        ax = np.argmax(data.shape)
+        sl = [slice(None)] * data.ndim
+        sl[ax] = slice(None, None, 2)
+        data = data[tuple(sl)]
+
+    return float(np.nanmin(data)), float(np.nanmax(data))
+
+
+def _organize_coordinates(contour: dict):
+    coors = contour["coordinates"]
+    coors = coors[~np.isnan(coors).any(axis=1)]
+
+    return coors
