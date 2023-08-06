9a0ac39cc1d4b1e0035509381a518de27a23a059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:17 +0000

    Add package MuMailer/0.4.2/MuMailer-0.4.2.tar.gz

---
+++ packages/MuMailer/MuMailer-0.4.2.tar.gz/MuMailer-0.4.2/mumailer/encryption.py
@@ -0,0 +1,30 @@
+##
+#     Project: MuMailer
+# Description: Simple mailer agent using SMTP
+#      Author: Fabio Castelli (Muflone) <muflone@muflone.com>
+#   Copyright: 2021-2022 Fabio Castelli
+#     License: GPL-3+
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+##
+
+import ssl
+
+ENCRYPTION_PROTOCOLS = {
+    'SSLv23': ssl.PROTOCOL_SSLv23,
+    'TLS_CLIENT': ssl.PROTOCOL_TLS_CLIENT,
+    'TLS_SERVER': ssl.PROTOCOL_TLS_SERVER,
+    'TLSv1': ssl.PROTOCOL_TLSv1,
+    'TLSv1_1': ssl.PROTOCOL_TLSv1_1,
+    'TLSv1_2': ssl.PROTOCOL_TLSv1_2,
+}
---
+++ packages/MuMailer/MuMailer-0.4.2.tar.gz/MuMailer-0.4.2/mumailer/profile_smtp.py
@@ -0,0 +1,50 @@
+##
+#     Project: MuMailer
+# Description: Simple mailer agent using SMTP
+#      Author: Fabio Castelli (Muflone) <muflone@muflone.com>
+#   Copyright: 2021-2022 Fabio Castelli
+#     License: GPL-3+
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+##
+
+from .yaml_profile import YamlProfile
+
+
+class ProfileSmtp(YamlProfile):
+    SECTION = 'SMTP'
+    OPTION_SERVER = 'SERVER'
+    OPTION_PORT = 'PORT'
+    OPTION_USERNAME = 'USERNAME'
+    OPTION_PASSWORD = 'PASSWORD'
+    OPTION_USE_TLS = 'TLS'
+    OPTION_USE_SSL = 'SSL'
+    OPTION_TIMEOUT = 'TIMEOUT'
+    OPTION_ENCRYPTION = 'ENCRYPTION'
+    OPTION_CIPHERS = 'CIPHERS'
+
+    def __init__(self, filename: str):
+        super().__init__(filename)
+        self.section_name = self.SECTION
+        # Get options from profile file
+        self.server = self.get_option(option=self.OPTION_SERVER)
+        self.port = self.get_option(option=self.OPTION_PORT,
+                                    default=25)
+        self.username = self.get_option(option=self.OPTION_USERNAME)
+        self.password = self.get_option(option=self.OPTION_PASSWORD)
+        self.use_tls = self.get_option(option=self.OPTION_USE_TLS,
+                                       default=False)
+        self.use_ssl = self.get_option(option=self.OPTION_USE_SSL,
+                                       default=False)
+        self.encryption = self.get_option(option=self.OPTION_ENCRYPTION)
+        self.ciphers = self.get_option(option=self.OPTION_CIPHERS)
