f190e66152edae711a6bfd3150134e5386a422dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:48 +0000

    Add package digi/0.2.4/digi-0.2.4.tar.gz

---
+++ packages/digi/digi-0.2.4.tar.gz/digi-0.2.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+URLS = {
+}
+
+setup(
+    name="digi",
+    version="0.2.4",
+    description="dSpace driver library",
+    url=URLS,
+    author="Silvery Fu",
+    author_email="silveryfu@gmail.com",
+    license="BSD-3-Clause License",
+    packages=find_packages(exclude=("tests",)),
+    python_requires='>=3.7',
+    include_package_data=True,
+    install_requires=[
+        "pyyaml",
+        "inflection",
+        "python-box",
+        "kubernetes",
+        'kopf',
+        "zed",
+        "dash",
+        "pandas",
+    ],
+)
