19922465f0823a096b4ad97bd57dfb5904b1588e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:38 +0000

    Add package atr/1.0.1/atr-1.0.1.tar.gz

---
+++ packages/atr/atr-1.0.1.tar.gz/atr-1.0.1/atr/__init__.py
@@ -0,0 +1,7 @@
+__author__ = 'Pol Llagostera Blasco'
+from . import relmodules
+from .main import calculate_reliability
+from . import graphtools
+from . import selector
+
+
---
+++ packages/atr/atr-1.0.1.tar.gz/atr-1.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='atr',
+    version='1.0.1',
+    author='Pol Llagostera Blasco',
+    author_email='llagosterapol.work@gmail.com',
+    packages=['atr', 'atr.relmodules'],
+    url='https://github.com/LlagosteraPol/ATR',
+    license='LICENSE.txt',
+    description='This library provides a tool to calculate the all-terminal reliability polynomial of any undirected graph.',
+    long_description='README.rst',
+    long_description_content_type='text/markdown',
+    install_requires=[
+           "networkx >= 2.5.1",
+           "sympy >= 1.4.0",
+           "numpy >= 1.21.6"
+       ],
+)
