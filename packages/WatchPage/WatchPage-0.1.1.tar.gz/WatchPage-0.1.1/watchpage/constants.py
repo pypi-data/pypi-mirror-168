43cfb0983eaf9317f62f2e98c74a084a667076db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:00 +0000

    Add package WatchPage/0.1.1/WatchPage-0.1.1.tar.gz

---
+++ packages/WatchPage/WatchPage-0.1.1.tar.gz/WatchPage-0.1.1/watchpage/constants.py
@@ -0,0 +1,28 @@
+##
+#     Project: WatchPage
+# Description: Watch webpages for changes
+#      Author: Fabio Castelli (Muflone) <muflone@muflone.com>
+#   Copyright: 2022 Fabio Castelli
+#     License: GPL-3+
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+##
+
+APP_NAME = 'WatchPage'
+APP_VERSION = '0.1.1'
+APP_DESCRIPTION = 'Watch webpages for changes'
+APP_ID = 'watchpage.muflone.com'
+APP_URL = 'http://www.muflone.com/watchpage/'
+APP_AUTHOR = 'Fabio Castelli'
+APP_AUTHOR_EMAIL = 'muflone@muflone.com'
+APP_COPYRIGHT = 'Copyright 2022 %s' % APP_AUTHOR
