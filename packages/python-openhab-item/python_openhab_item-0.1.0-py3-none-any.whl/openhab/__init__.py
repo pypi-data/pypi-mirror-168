f0afb2e575073adf7dfffd6dc48db047451d89c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:42 +0000

    Add package python-openhab-item/0.1.0/python_openhab_item-0.1.0-py3-none-any.whl

---
+++ packages/python-openhab-item/python_openhab_item-0.1.0-py3-none-any.whl/openhab/__init__.py
@@ -0,0 +1,8 @@
+"""Module entry point."""
+
+from .item import Item, ColorItem, ContactItem, DateTimeItem, DimmerItem, GroupItem, ImageItem, LocationItem, NumberItem, PlayerItem, RollershutterItem, StringItem, SwitchItem
+
+__all__ = ['Item', 'ColorItem', 'ContactItem', 'DateTimeItem', 'DimmerItem', 'GroupItem', 'ImageItem', 'LocationItem', 'NumberItem', 'PlayerItem', 'RollershutterItem', 'StringItem', 'SwitchItem']
+__version__ = "0.1.0"
+__author__ = 'Michael Dörflinger'
+__credits__ = 'Furtwangen University'
