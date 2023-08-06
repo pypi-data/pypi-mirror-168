eeaaf56c0a9114b6e05e304f5c9988eebb20a469
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:26 +0000

    Add package ipcompacter/0.2/ipcompacter-0.2.tar.gz

---
+++ packages/ipcompacter/ipcompacter-0.2.tar.gz/ipcompacter-0.2/src/ipcompacter/ipmonitor.py
@@ -0,0 +1,15 @@
+class Test():
+
+    slots = ['_nome','_idade']
+
+    def __init__(self, nome, idade):
+        self._nome = nome
+        self._idade = idade
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @property
+    def idade(self):
+        return self._idade
