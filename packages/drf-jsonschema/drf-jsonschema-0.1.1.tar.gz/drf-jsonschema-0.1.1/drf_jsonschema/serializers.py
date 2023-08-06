3735ffb81cf47c8b5eefb4b772a01e335c31c4fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:15 +0000

    Add package drf-jsonschema/0.1.1/drf-jsonschema-0.1.1.tar.gz

---
+++ packages/drf-jsonschema/drf-jsonschema-0.1.1.tar.gz/drf-jsonschema-0.1.1/drf_jsonschema/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from drf_jsonschema.fields import JSONSchemaField
+from django.db import models
+
+class JsonSchemaModelSerializer(serializers.ModelSerializer):
+    serializer_field_mapping = serializers.ModelSerializer \
+        .serializer_field_mapping \
+        .copy()[models.JSONField] = JSONSchemaField
