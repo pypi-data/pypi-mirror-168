dfb4ebaf275869a0c3b609b8dafc7123da5bda10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:38 +0000

    Add package operacionjgg/1.0/operacionjgg-1.0-py3-none-any.whl

---
+++ packages/operacionjgg/operacionjgg-1.0-py3-none-any.whl/operacionjgg/MathOperations.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#creating MathOperation module
+
+
+# #the module provides addition, subtraction, multiplication, and division functions
+
+# #all the functions take two numbers as argument
+
+# In[2]:
+
+
+#creating addition function
+
+def addition(num1,num2):
+
+    return num1+num2
+
+ 
+#creating subtraction function
+
+def subtraction(num1,num2):
+
+    return num1-num2
+
+
+#creating multiplication function
+
+def multiplication(num1,num2):
+
+    return num1*num2
+
+
+#creating division function
+
+def division(num1,num2):
+
+    return num1/num2
+
+
+# In[ ]:
+
+
+
+
