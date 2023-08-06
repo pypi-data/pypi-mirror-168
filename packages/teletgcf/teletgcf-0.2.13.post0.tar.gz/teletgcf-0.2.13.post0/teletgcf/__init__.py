bc2bd0b76c084a88a09437aa9fbd238970ad4736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:23 +0000

    Add package teletgcf/0.2.13.post0/teletgcf-0.2.13.post0.tar.gz

---
+++ packages/teletgcf/teletgcf-0.2.13.post0.tar.gz/teletgcf-0.2.13.post0/teletgcf/__init__.py
@@ -0,0 +1,9 @@
+"""Package teletgcf.
+
+The ultimate tool to automate custom telegram message forwarding.
+https://github.com/jabrapatel800/teletgcf
+"""
+
+from importlib.metadata import version
+
+__version__ = version(__package__)
