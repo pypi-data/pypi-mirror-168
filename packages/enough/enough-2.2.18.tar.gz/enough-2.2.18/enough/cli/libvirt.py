1e99dcac54bb9a85b59dd5436fe071c7a3f247f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:15 +0000

    Add package enough/2.2.18/enough-2.2.18.tar.gz

---
+++ packages/enough/enough-2.2.18.tar.gz/enough-2.2.18/enough/cli/libvirt.py
@@ -0,0 +1,21 @@
+from cliff.command import Command
+
+from enough import settings
+from enough.common import Enough
+
+
+class Install(Command):
+    "Install"
+
+    def get_parser(self, prog_name):
+        parser = super().get_parser(prog_name)
+        parser.add_argument('--vpn', help='relative path to VPN credentials')
+        parser.add_argument('host')
+        return parser
+
+    def take_action(self, parsed_args):
+        args = vars(self.app.options)
+        args.update(vars(parsed_args))
+        args['driver'] = 'libvirt'
+        e = Enough(settings.CONFIG_DIR, settings.SHARE_DIR, **args)
+        e.libvirt_install()
