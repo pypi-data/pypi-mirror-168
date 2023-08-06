9276a3b9cba0ffe83aedb205512af2d25d74b39c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:56 +0000

    Add package parzen-estimator/0.2.7/parzen_estimator-0.2.7-py3-none-any.whl

---
+++ packages/parzen-estimator/parzen_estimator-0.2.7-py3-none-any.whl/parzen_estimator/loglikelihoods.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+from parzen_estimator.utils import logsumexp
+
+
+def compute_config_loglikelihoods(basis_loglikelihoods: np.ndarray, weight: float) -> np.ndarray:
+    """
+    Calculate the loglikelihood of configurations based on parzen estimator.
+
+    Args:
+        basis_loglikelihoods (np.ndarray):
+            The array of basis loglikelihood with the shape of (dim, n_basis, n_ei_candidates)
+
+    Returns:
+        config_loglikelihoods:
+            The loglikelihoods of configs (n_ei_candidates, )
+    """
+    # Product of kernels with respect to dimension
+    config_basis_loglikelihoods = basis_loglikelihoods.sum(axis=0)
+    # Compute config loglikelihoods using logsumexp to avoid overflow
+    config_loglikelihoods = logsumexp(config_basis_loglikelihoods, weight=weight, axis=0)
+
+    return config_loglikelihoods
