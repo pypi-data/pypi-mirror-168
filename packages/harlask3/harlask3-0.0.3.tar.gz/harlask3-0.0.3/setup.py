4650992b4de9c46d69f0088384a20a4ef366bbfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:26 +0000

    Add package harlask3/0.0.3/harlask3-0.0.3.tar.gz

---
+++ packages/harlask3/harlask3-0.0.3.tar.gz/harlask3-0.0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup 
+setup(
+    name='harlask3',
+    version = '0.0.3',
+    description='functions for the harley desktop assistant',
+    package_dir = {'': 'src'},
+    py_modules = ["harlask2"],
+    author =  "Ayush Dutta",
+    license = "MIT",
+    url = "https://github.com/bravosickz/harlask"
+)
