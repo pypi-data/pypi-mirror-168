3481d9de91e0900b717b6e3255bb8f1a3c2facbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:30 +0000

    Add package pydantic-core/0.2.1/pydantic_core-0.2.1-cp39-none-win_amd64.whl

---
+++ packages/pydantic-core/pydantic_core-0.2.1-cp39-none-win_amd64.whl/pydantic_core/_pydantic_core.pyi
@@ -0,0 +1,50 @@
+import sys
+from typing import Any, TypedDict
+
+from pydantic_core.schema_types import Config, Schema
+
+if sys.version_info < (3, 11):
+    from typing_extensions import NotRequired
+else:
+    from typing import NotRequired
+
+__all__ = '__version__', 'SchemaValidator', 'SchemaError', 'ValidationError', 'PydanticValueError'
+__version__: str
+
+class SchemaValidator:
+    def __init__(self, schema: Schema, config: 'Config | None' = None) -> None: ...
+    def validate_python(self, input: Any, strict: 'bool | None' = None, context: Any = None) -> Any: ...
+    def isinstance_python(self, input: Any, strict: 'bool | None' = None, context: Any = None) -> bool: ...
+    def validate_json(
+        self, input: 'str | bytes | bytearray', strict: 'bool | None' = None, context: Any = None
+    ) -> Any: ...
+    def isinstance_json(
+        self, input: 'str | bytes | bytearray', strict: 'bool | None' = None, context: Any = None
+    ) -> bool: ...
+    def validate_assignment(
+        self, field: str, input: Any, data: 'dict[str, Any]', strict: 'bool | None' = None, context: Any = None
+    ) -> 'dict[str, Any]': ...
+
+class SchemaError(Exception):
+    pass
+
+class ErrorDetails(TypedDict):
+    kind: str
+    loc: 'list[int | str]'
+    message: str
+    input_value: Any
+    context: NotRequired['dict[str, Any]']
+
+class ValidationError(ValueError):
+    title: str
+
+    def error_count(self) -> int: ...
+    def errors(self) -> 'list[ErrorDetails]': ...
+
+class PydanticValueError(ValueError):
+    kind: str
+    message_template: str
+    context: 'dict[str, str | int] | None'
+
+    def __init__(self, kind: str, message_template: str, context: 'dict[str, str | int] | None' = None) -> None: ...
+    def message(self) -> str: ...
