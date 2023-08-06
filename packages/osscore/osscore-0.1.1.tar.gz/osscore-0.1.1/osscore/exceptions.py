00dd353db195be8d613c0a71bb9c4732fa850491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:31 +0000

    Add package osscore/0.1.1/osscore-0.1.1.tar.gz

---
+++ packages/osscore/osscore-0.1.1.tar.gz/osscore-0.1.1/osscore/exceptions.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @author: similraface
+# @contact: similarface@gmail.com
+# @software: PyCharm
+# @file: exceptions.py
+# @time: 2022/9/22 2:59 下午
+# @desc:
