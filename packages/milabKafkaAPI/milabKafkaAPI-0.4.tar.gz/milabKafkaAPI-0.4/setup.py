e65adfee532030d619450f205dc22e37ee58974d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:10 +0000

    Add package milabKafkaAPI/0.4/milabKafkaAPI-0.4.tar.gz

---
+++ packages/milabKafkaAPI/milabKafkaAPI-0.4.tar.gz/milabKafkaAPI-0.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='milabKafkaAPI',
+    version='0.4',
+    license='MIT',
+    author="milab",
+    author_email='omer.sadeh@milab.idc.ac.il',
+    packages=['milabKafkaAPI'],
+    url='https://github.com/idc-milab/Kafka-API',
+    keywords='kafka api milab butter',
+    install_requires=[
+        'kafka-python',
+        'lxml',
+        'butter.mas-api',
+      ],
+)
