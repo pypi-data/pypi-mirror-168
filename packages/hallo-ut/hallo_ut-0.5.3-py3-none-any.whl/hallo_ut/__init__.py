717a337bd155ef8ed2d45b32149c6492156aeaa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:51 +0000

    Add package hallo-ut/0.5.3/hallo_ut-0.5.3-py3-none-any.whl

---
+++ packages/hallo-ut/hallo_ut-0.5.3-py3-none-any.whl/hallo_ut/__init__.py
@@ -0,0 +1,10 @@
+from .faq import Faq
+from .tiket import Tiket
+from .hallo_ut import HalloUt
+
+
+__author__ = "hexatester <habibrohman@protonmail.com>"
+__version__ = "0.5.3"
+
+
+__all__ = ["Faq", "Tiket", "HalloUt"]
