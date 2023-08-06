4ce2cf97ec4f7e3198bf7c610ef64b1f4cabb1b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:38 +0000

    Add package rowantree.auth.sdk/0.14.0/rowantree.auth.sdk-0.14.0-py3-none-any.whl

---
+++ packages/rowantree.auth.sdk/rowantree.auth.sdk-0.14.0-py3-none-any.whl/rowantree/auth/sdk/contracts/requests/register_user_request.py
@@ -0,0 +1,21 @@
+""" Auth Service Register User Base Definition """
+from rowantree.contracts import BaseModel
+
+
+class RegisterUserRequest(BaseModel):
+    """
+    Auth Service Register User Base
+
+    Attributes
+    ----------
+    username: str
+        The username of the user.
+    email: Union[str, None] = None
+        The user email address.
+    password: str
+        user password.
+    """
+
+    username: str
+    email: str
+    password: str
