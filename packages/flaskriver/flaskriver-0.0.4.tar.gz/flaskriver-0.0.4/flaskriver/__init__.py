d011a70e0f3bc5949822afea232c62c316c71b09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:36 +0000

    Add package flaskriver/0.0.4/flaskriver-0.0.4.tar.gz

---
+++ packages/flaskriver/flaskriver-0.0.4.tar.gz/flaskriver-0.0.4/flaskriver/__init__.py
@@ -0,0 +1,8 @@
+"""This is a package which aims to combine the lighweight web-framework flask
+with the online-ML library river in an easy way.
+"""
+
+__version__ = "0.0.4"
+
+from .ClassificationInterface import ClassificationInterface
+from .RegressionInterface import RegressionInterface
