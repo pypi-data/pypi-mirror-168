8f8d7840ec8adb4238cbd3b4ef8e5fb50fd7540c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:33 +0000

    Add package svscrape/2022.4/svscrape-2022.4-py3-none-any.whl

---
+++ packages/svscrape/svscrape-2022.4-py3-none-any.whl/svwebscraper/exporter.py
@@ -0,0 +1,34 @@
+HEADER = ['NAME', 'ADRESSE', 'PLZ', 'ORT', 'TEL_NR']
+
+
+class SchoolCSVExporter:
+    def __init__(self, schools, delimiter=';', newline='\n'):
+        self.schools = schools
+        self.delimiter = delimiter
+        self.newline = newline
+
+    def export_to_file(self, filename, append=False):
+        print(f'Export {len(self.schools)} schools to csv file "{filename}"')
+
+        if append:
+            mode = 'w+'
+        else:
+            mode = 'w'
+
+        with open(filename, mode) as file:
+            self._append_header_to_file(file)
+            for school in self.schools:
+                self._append_school_to_file(file, school)
+
+    def _append_header_to_file(self, file):
+        file.write(self.delimiter.join(HEADER) + self.newline)
+
+    def _append_school_to_file(self, file, school):
+        school_fields = [
+            school.get_name(),
+            school.get_address(),
+            school.get_postal_code(),
+            school.get_city(),
+            school.get_phone_number()
+        ]
+        file.write(self.delimiter.join(school_fields) + self.newline)
