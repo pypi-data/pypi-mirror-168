63847a9eea88386a715f943586977ab9c5fab421
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:57:16 +0000

    Add package pystage/0.0.16/pystage-0.0.16-py3-none-any.whl

---
+++ packages/pystage/pystage-0.0.16-py3-none-any.whl/pystage/__init__.py
@@ -0,0 +1,7 @@
+from os import environ
+
+# Prevent PyGame support prompt
+environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
+# Do not mess with the compositor
+environ['SDL_VIDEO_X11_NET_WM_BYPASS_COMPOSITOR'] = '0'
+
