6b52e3fc74946a67154dc4f7a0a86d53d2f5c063
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:20 +0000

    Add package TSM-opt-test/0.0.1/tsm_opt_test-0.0.1-py3-none-any.whl

---
+++ packages/TSM-opt-test/tsm_opt_test-0.0.1-py3-none-any.whl/src/TravelingSalesman/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='TravelingSalesman',
+    version='v0.1',
+    packages=find_packages(),
+    url='https://github.com/Tommer-R/Traveling-salesman',
+    license='MIT',
+    author='Tommer Rissin',
+    author_email='tommerrissin@gmail.com',
+    description='Traveling salesman problem'
+)
