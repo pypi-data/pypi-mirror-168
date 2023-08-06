c55beeb9cb7150f54c3a7f52cd7ce96989ec8162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:15 +0000

    Add package titiler.mosaic/0.7.1/titiler.mosaic-0.7.1.tar.gz

---
+++ packages/titiler.mosaic/titiler.mosaic-0.7.1.tar.gz/titiler.mosaic-0.7.1/setup.py
@@ -0,0 +1,38 @@
+"""Setup titiler.mosaic."""
+
+from setuptools import find_namespace_packages, setup
+
+with open("README.md") as f:
+    long_description = f.read()
+
+inst_reqs = ["titiler.core>=0.7,<0.8", "cogeo-mosaic>=4.1,<4.2"]
+extra_reqs = {
+    "test": ["pytest", "pytest-cov", "pytest-asyncio", "requests"],
+}
+
+
+setup(
+    name="titiler.mosaic",
+    description="MosaicJSON plugin for TiTiler.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.7",
+    classifiers=[
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    keywords="MosaicJSON",
+    author="Vincent Sarago",
+    author_email="vincent@developmentseed.org",
+    url="https://github.com/developmentseed/titiler",
+    license="MIT",
+    packages=find_namespace_packages(exclude=["tests*"]),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=inst_reqs,
+    extras_require=extra_reqs,
+)
