e41c0a8b307d4ba5f424f82c9eeaea10732c9bcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:15 +0000

    Add package divintseg/0.2.0/divintseg-0.2.0.tar.gz

---
+++ packages/divintseg/divintseg-0.2.0.tar.gz/divintseg-0.2.0/divintseg/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022 Darren Erik Vengroff
+
+"""
+Tools for computing diversity, integration, and segregation
+metrics.
+"""
+
+from ._core import di, diversity, integration, segregation  # noqa: F401
