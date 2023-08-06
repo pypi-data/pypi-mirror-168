f5bc26bb2b651610025fe8699099ea3dc6c3c4ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:17 +0000

    Add package ddgui/0.2/ddgui-0.2-py3-none-any.whl

---
+++ packages/ddgui/ddgui-0.2-py3-none-any.whl/ddgui/__init__.py
@@ -0,0 +1,39 @@
+import sys
+import time
+import itertools # for the zip()
+
+from PyQt5.QtWidgets import QDialog, QApplication
+from PyQt5.uic import loadUi
+from PyQt5.QtCore import Qt
+from PyQt5.QtCore import QThreadPool, QRunnable, QThread
+
+def tambah_angka(a,b):
+    return a+b
+
+def kali_angka(a,b):
+    return a*b
+
+def bagi_angka(a,b):
+    return a/b
+
+def kurang_angka(a,b):
+    return a-b
+
+input("Press Enter to continue...")
+
+class MyForm(QDialog):
+
+    def __init__(self):
+        super().__init__()
+        self.ui = loadUi("ddgui.ui", self)
+        self.ui.pushButtonExit.clicked.connect(self.exitMethod)
+        self.show()
+
+    def exitMethod(self):
+        QApplication.instance().quit()
+
+if __name__ == "__main__":
+    a = QApplication(sys.argv)
+    form = MyForm()
+    form.show()
+    a.exec_()
