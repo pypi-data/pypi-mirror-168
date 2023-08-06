3cd55afea481b0cfd7e1c2ca1c28d420581b2eb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:22 +0000

    Add package pyekfmm/0.0.7/pyekfmm-0.0.7.tar.gz

---
+++ packages/pyekfmm/pyekfmm-0.0.7.tar.gz/pyekfmm-0.0.7/pyekfmm/__init__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+:copyright:
+    Yangkang Chen (chenyk2016@gmail.com), 2022-Present 
+:license:
+    GNU General Public License, Version 3
+    (http://www.gnu.org/copyleft/gpl.html)
+"""
+
+__version__ = "0.0.6"
+
+from .fmm import eikonal
+from .fmm import eikonal_surf
+
+from calculator import * #This is a template for writing python-C APIs
+from eikonalc import *
+
+
+
+
+
+
+
+
+
+
+
+
