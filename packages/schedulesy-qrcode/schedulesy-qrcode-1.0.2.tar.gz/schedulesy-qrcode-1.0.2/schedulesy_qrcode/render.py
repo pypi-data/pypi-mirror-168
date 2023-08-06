ce986a542440eaf1e93f4a0390085bc88d6dc13c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:35 +0000

    Add package schedulesy-qrcode/1.0.2/schedulesy-qrcode-1.0.2.tar.gz

---
+++ packages/schedulesy-qrcode/schedulesy-qrcode-1.0.2.tar.gz/schedulesy-qrcode-1.0.2/schedulesy_qrcode/render.py
@@ -0,0 +1,25 @@
+import json
+
+from jinja2 import Environment, FileSystemLoader, PackageLoader, select_autoescape
+
+from schedulesy_qrcode.config import S3_CONF
+
+
+def render(data):
+    env = Environment(
+        loader=FileSystemLoader("templates"), autoescape=select_autoescape()
+    )
+    template = env.get_template("tree.html")
+
+    return template.render(
+        data=data,
+        s3={
+            "endpoint": S3_CONF['endpoint'],
+            "bucket": S3_CONF['bucket'],
+            "tenant": S3_CONF['tenant'],
+        },
+    )
+
+
+# data = json.load(open('out.json'))
+# print(render(data))
