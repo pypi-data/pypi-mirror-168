984ce3e8511701af2b6cd7346330cd385f52f68d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:26 +0000

    Add package EnglishResumeParser/0.12/EnglishResumeParser-0.12.tar.gz

---
+++ packages/EnglishResumeParser/EnglishResumeParser-0.12.tar.gz/EnglishResumeParser-0.12/setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="EnglishResumeParser",
+    version ="0.12",
+    packages = find_packages(),
+    py_modules=["EnglishResumeParser"]
+)
