a09eb874943c09e9e5e2844889ce8f36388a40a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:43 +0000

    Add package perun/0.1.0b14/perun-0.1.0b14.tar.gz

---
+++ packages/perun/perun-0.1.0b14.tar.gz/perun-0.1.0b14/perun/__init__.py
@@ -0,0 +1,28 @@
+"""perun module."""
+# flake8: noqa
+__version__ = "0.1.0-beta.14"
+from perun.configuration import config
+from perun.logging import init_logging
+
+log = init_logging(config.get("perun", "log_lvl"))
+
+import os
+
+os.environ["IBV_FORK_SAFE"] = "1"
+os.environ["RDMAV_FORK_SAFE"] = "1"
+
+from perun.perun import getDeviceConfiguration, monitor, perunSubprocess, postprocessing
+from perun.report import report
+from perun.storage import ExperimentStorage, LocalStorage
+
+__all__ = [
+    "perunSubprocess",
+    "getDeviceConfiguration",
+    "LocalStorage",
+    "ExperimentStorage",
+    "log",
+    "monitor",
+    "report",
+    "postprocessing",
+    "config",
+]
