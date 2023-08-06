7a727588be866679a591ab251dfd4c71a21c1bd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:51 +0000

    Add package gp-tools-torch/0.1.0/gp-tools-torch-0.1.0.tar.gz

---
+++ packages/gp-tools-torch/gp-tools-torch-0.1.0.tar.gz/gp-tools-torch-0.1.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_namespace_packages, setup
+
+with open("README.rst") as fp:
+    long_description = fp.read()
+
+setup(
+    name="gp-tools-torch",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    packages=find_namespace_packages(),
+    version="0.1.0",
+    install_requires=[
+        "gp-tools-util",
+        "numpy",
+        "torch",
+    ],
+    extras_require={
+        "tests": [
+            "doit-interface",
+            "flake8",
+            "jupyter",
+            "matplotlib",
+            "networkx",
+            "pytest",
+            "pytest-cov",
+            "scipy",
+            "tabulate",
+            "tqdm",
+            "twine",
+        ],
+    }
+)
