d830dc65b5f66b417e2d74386d209703bc2b24ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:44 +0000

    Add package deephaven-plugin-plotly-js/0.1.0.dev1/deephaven_plugin_plotly_js-0.1.0.dev1-py3-none-any.whl

---
+++ packages/deephaven-plugin-plotly-js/deephaven_plugin_plotly_js-0.1.0.dev1-py3-none-any.whl/deephaven_plugin_plotly_js/__init__.py
@@ -0,0 +1,30 @@
+import json
+import pathlib
+from struct import pack
+
+from deephaven.plugin import Registration
+from deephaven.plugin.js import JsType
+
+from importlib.resources import files, as_file
+from contextlib import contextmanager
+from typing import Generator
+
+
+@contextmanager
+def _path() -> Generator[pathlib.Path, None, None]:
+    with as_file(files(__package__)) as package:
+        yield package / 'data' / 'package' / 'package.json'
+
+
+class PlotlyJs(JsType):
+    @contextmanager
+    def path(self) -> Generator[pathlib.Path, None, None]:
+        # is this the same as return _path()?
+        with _path() as path:
+            yield path
+
+
+class PlotlyJsRegistration(Registration):
+    @classmethod
+    def register_into(cls, callback: Registration.Callback) -> None:
+        callback.register(PlotlyJs)
