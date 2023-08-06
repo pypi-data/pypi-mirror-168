4d25295cbbeab316f2af911e7751e820e1aea3a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:47 +0000

    Add package python-for-math/1.0.0/python_for_math-1.0.0-py3-none-any.whl

---
+++ packages/python-for-math/python_for_math-1.0.0-py3-none-any.whl/example_pkg/app.py
@@ -0,0 +1,12 @@
+def sum(a, b):
+    return a + b
+
+def difference(a, b):
+    return a - b;
+
+def member_team():
+    print("Hoang Thi Hai Yen")
+    print("\tBui Thi Ha Tien")
+    print("\tDuong Truong Vu")
+    print("\tTran Ngoc Tin")
+    print("\tTran Anh Vu")
