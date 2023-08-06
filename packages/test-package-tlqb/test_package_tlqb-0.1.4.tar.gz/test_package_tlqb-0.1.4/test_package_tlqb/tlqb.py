f51e1504438ee447e7e1b14aa62af683a05a313f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:21 +0000

    Add package test-package-tlqb/0.1.4/test_package_tlqb-0.1.4.tar.gz

---
+++ packages/test-package-tlqb/test_package_tlqb-0.1.4.tar.gz/test_package_tlqb-0.1.4/test_package_tlqb/tlqb.py
@@ -0,0 +1,25 @@
+def test(a,b):
+	return a+b  
+# import numpy as np
+# from matplotlib import pyplot as plt
+# from mpl_toolkits.mplot3d import Axes3D
+
+# x = np.arange(0, 2*np.pi, 0.1)
+# y = np.arange(0, 5, 0.1)
+
+# X, Y = np.meshgrid(x, y)
+# z = np.sin(X)*Y
+
+# fig = plt.figure()
+# ax = Axes3D(fig)
+# ax.plot_surface(X, Y, z)
+
+# # x = np.arange(-10, 10, 0.05)
+# # y = x**2 + 5*np.sin(x)
+# # plt.plot(X, Y, z)
+
+# plt.xlabel("Truc x")
+# plt.ylabel("Truc y")
+# plt.title('Toan cao cap')
+
+# plt.show() 
