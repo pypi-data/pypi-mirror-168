aa5bf8a8283bd6adc0caebf4c351592cc31a63e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:48 +0000

    Add package botstrap/0.1.2/botstrap-0.1.2.tar.gz

---
+++ packages/botstrap/botstrap-0.1.2.tar.gz/botstrap-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    url="https://botstrap.readthedocs.io",
+    project_urls={
+        "Documentation": "https://botstrap.readthedocs.io/en/latest/api/",
+        "Issue Tracker": "https://github.com/nuztalgia/botstrap/issues",
+        "Source Code": "https://github.com/nuztalgia/botstrap",
+    },
+    install_requires=[
+        "colorama >=0.4.5",
+        "cryptography >=37.0.0",
+    ],
+)
