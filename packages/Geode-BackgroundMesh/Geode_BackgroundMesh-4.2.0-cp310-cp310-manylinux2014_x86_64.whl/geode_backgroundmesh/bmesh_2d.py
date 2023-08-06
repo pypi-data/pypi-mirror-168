315772ba5f88d990744479f0bc3e9eba4e184b31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:15 +0000

    Add package Geode-BackgroundMesh/4.2.0/Geode_BackgroundMesh-4.2.0-cp310-cp310-manylinux2014_x86_64.whl

---
+++ packages/Geode-BackgroundMesh/Geode_BackgroundMesh-4.2.0-cp310-cp310-manylinux2014_x86_64.whl/geode_backgroundmesh/bmesh_2d.py
@@ -0,0 +1,10 @@
+#
+# Copyright (c) 2019 - 2022 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+from geode_common import core, modifier_surface, cutter_surface, helpers
+from geode_conversion import model
+
+from .geode_backgroundmesh_py_bmesh_2d import *
+BackgroundMesh2d.initialize()
