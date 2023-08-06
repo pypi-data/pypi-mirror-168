a665997d5f582ed92e556a270134a4448a2e3a4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:51 +0000

    Add package cmsworkflow/0.0.10/cmsworkflow-0.0.10.tar.gz

---
+++ packages/cmsworkflow/cmsworkflow-0.0.10.tar.gz/cmsworkflow-0.0.10/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='cmsworkflow',
+      author="Hasan Ozturk, Luca Lavezzo, Hamed Bakhshiansohi",
+      version='0.0.10',
+      description="A high level package which provides tools to get useful information about CMS workflows",
+      url="https://github.com/pypa/sampleproject",
+      python_requires=">=3.6",
+      packages=['workflow'],
+
+)
