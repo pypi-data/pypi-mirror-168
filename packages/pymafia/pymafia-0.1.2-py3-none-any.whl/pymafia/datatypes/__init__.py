145caf099859e90a1ce51156013f5e2cbb66d965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:28 +0000

    Add package pymafia/0.1.2/pymafia-0.1.2-py3-none-any.whl

---
+++ packages/pymafia/pymafia-0.1.2-py3-none-any.whl/pymafia/datatypes/__init__.py
@@ -0,0 +1,16 @@
+from ._bounty import Bounty
+from ._class import Class
+from ._coinmaster import Coinmaster
+from ._effect import Effect, EffectQuality
+from ._element import Element
+from ._familiar import Familiar
+from ._item import CandyType, Item, ItemQuality
+from ._location import Location
+from ._monster import Monster
+from ._phylum import Phylum
+from ._servant import Servant
+from ._skill import Skill
+from ._slot import Slot
+from ._stat import Stat
+from ._thrall import Thrall
+from ._vykea import Vykea
