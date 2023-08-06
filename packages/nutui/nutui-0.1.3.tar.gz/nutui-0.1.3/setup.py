a925abf7b6b41aa8a4ac881f90c8455d1b2f6dde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:31 +0000

    Add package nutui/0.1.3/nutui-0.1.3.tar.gz

---
+++ packages/nutui/nutui-0.1.3.tar.gz/nutui-0.1.3/setup.py
@@ -0,0 +1,59 @@
+from pathlib import Path
+from setuptools import find_packages, setup
+dependencies = [
+                "nut2",
+                "flask",
+                "flask_sqlalchemy",
+                "pytz",
+                "PyYAML",
+                "requests"
+                ]
+# read the contents of README file
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+setup(
+    name='nutui',
+    packages=find_packages(),
+    version='0.1.3',
+    description="Nut Web UI using Flask",
+    author="The Bearded Tek",
+    author_email="kenny@beardedtek.com",
+    url="https://github.com/beardedtek-com/nutui",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='AGPLv3',
+    project_urls={
+        "Bug Tracker": "https://github.com/beardedtek-com/nutui/issues",
+        "Funding": "https://github.com/sponsors/BeardedTek-com",
+        "Source": "https://github.com/beardedtek-com/nutui/",
+    },
+    python_requires=">=3.6, <4",
+    keywords=[
+        "ups",
+        "nut-server",
+        "nutd",
+        "nut",
+        "battery-backup",
+        "apc",
+        "cyberpower",
+        "tripp-lite"
+    ],
+    classifiers=[
+        "Environment :: Web Environment",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Topic :: System :: Power (UPS)",
+        "Topic :: Utilities",
+        "Development Status :: 3 - Alpha",
+        "Framework :: Flask",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators"
+    ],
+    install_requires=dependencies,
+    py_modules=['nutui','app'],
+    entry_points={
+        'console_scripts': [
+            "nutui=nutui:main"
+        ],
+    },
+)
