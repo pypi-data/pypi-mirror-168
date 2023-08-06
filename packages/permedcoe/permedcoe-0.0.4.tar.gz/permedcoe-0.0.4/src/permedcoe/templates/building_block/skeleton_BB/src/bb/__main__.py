976bca37e7489919db6fe135377022f828dd33f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:27 +0000

    Add package permedcoe/0.0.4/permedcoe-0.0.4.tar.gz

---
+++ packages/permedcoe/permedcoe-0.0.4.tar.gz/permedcoe-0.0.4/src/permedcoe/templates/building_block/skeleton_BB/src/bb/__main__.py
@@ -0,0 +1,16 @@
+"""
+NOTE: Do not modify this file unless you want to change the name
+      of the main.py file. Then it is necessary to adapt the import.
+"""
+
+from permedcoe import invoker
+from NEW_NAME.main import invoke
+from NEW_NAME.main import arguments_info
+
+
+def main():
+    invoker(invoke, arguments_info)
+
+
+if __name__ == "__main__":
+    main()
