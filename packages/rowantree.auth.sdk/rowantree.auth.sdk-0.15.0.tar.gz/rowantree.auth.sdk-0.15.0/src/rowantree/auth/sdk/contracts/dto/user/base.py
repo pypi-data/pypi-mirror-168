ece579a3e03a945855ec2294c0f6c070f9f02545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:43 +0000

    Add package rowantree.auth.sdk/0.15.0/rowantree.auth.sdk-0.15.0.tar.gz

---
+++ packages/rowantree.auth.sdk/rowantree.auth.sdk-0.15.0.tar.gz/rowantree.auth.sdk-0.15.0/src/rowantree/auth/sdk/contracts/dto/user/base.py
@@ -0,0 +1,30 @@
+""" Auth Service User Base Definition """
+
+from typing import Optional
+
+from rowantree.contracts import BaseModel
+
+
+class UserBase(BaseModel):
+    """
+    Auth Service User Base
+
+    Attributes
+    ----------
+    username: str
+        The username of the user.
+    guid: str
+        The user guid.
+    email: Union[str, None] = None
+        The user email address.
+    disabled: Union[bool, None] = None
+        Whether the account is enabled or not.
+    admin: Union[bool, None] = None
+        Whether the account is an admin or not.
+    """
+
+    username: str
+    guid: Optional[str] = None
+    email: Optional[str] = None
+    disabled: Optional[bool] = None
+    admin: Optional[bool] = None
