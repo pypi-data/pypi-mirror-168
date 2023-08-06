36b48a67c2af9a67fae9de640ddcce1da83fe532
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:47 +0000

    Add package python-for-math/1.0.0/python%20for%20math-1.0.0.tar.gz

---
+++ packages/python-for-math/python%20for%20math-1.0.0.tar.gz/python for math-1.0.0/README.md
@@ -0,0 +1,12 @@
+def sum(a, b):       Tinh tong 2 so
+    return a + b
+
+def difference():    Tinh hieu 2 so
+    return a - b;
+
+def member_team():
+    print("Hoang Thi Hai Yen")
+    print("\tBui Thi Ha Tien")
+    print("\tDuong Truong Vu")
+    print("\tTran Ngoc Tin")
+    print("\tTran Anh Vu")
---
+++ packages/python-for-math/python%20for%20math-1.0.0.tar.gz/python for math-1.0.0/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="python for math",
+    version="1.0.0",
+    author="TeamPython",
+    author_email="teampython@gmail.com",
+    description="Sort description",
+    long_description="Full description",
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
