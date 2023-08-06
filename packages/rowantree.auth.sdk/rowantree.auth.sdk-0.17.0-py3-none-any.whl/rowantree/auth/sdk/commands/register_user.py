fca38251f0ad135b2e614302498992e9bcfdf2ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:50 +0000

    Add package rowantree.auth.sdk/0.17.0/rowantree.auth.sdk-0.17.0-py3-none-any.whl

---
+++ packages/rowantree.auth.sdk/rowantree.auth.sdk-0.17.0-py3-none-any.whl/rowantree/auth/sdk/commands/register_user.py
@@ -0,0 +1,39 @@
+""" Register User Command Definition """
+
+from starlette import status
+
+from ..contracts.dto.request_status_codes import RequestStatusCodes
+from ..contracts.dto.user.base import UserBase
+from ..contracts.dto.wrapped_request import WrappedRequest
+from ..contracts.request_verb import RequestVerb
+from ..contracts.requests.register_user_request import RegisterUserRequest
+from .abstract_command import AbstractCommand
+
+
+class RegisterUserCommand(AbstractCommand):
+    """
+    Register User Command
+
+    Methods
+    -------
+    execute(self, request: User) -> None
+        Register User
+    """
+
+    def execute(self, request: RegisterUserRequest) -> UserBase:
+        """
+        Register User
+
+        Parameters
+        ----------
+        request: RegisterUserRequest
+        """
+
+        request: WrappedRequest = WrappedRequest(
+            verb=RequestVerb.POST,
+            url=f"https://api.{self.options.tld}/auth/v1/register",
+            statuses=RequestStatusCodes(allow=[status.HTTP_200_OK], reauth=[], retry=[]),
+            data=request.dict(by_alias=True),
+        )
+        response: dict = self.wrapped_request(request=request)
+        return UserBase.parse_obj(response)
