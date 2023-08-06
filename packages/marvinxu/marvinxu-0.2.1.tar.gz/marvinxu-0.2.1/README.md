0ccd59979cac49b259016cf7667a8ed5fb4e927f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:09 +0000

    Add package marvinxu/0.2.1/marvinxu-0.2.1.tar.gz

---
+++ packages/marvinxu/marvinxu-0.2.1.tar.gz/marvinxu-0.2.1/README.md
@@ -0,0 +1,3 @@
+# marvinxu
+
+Marvin's toolkit
---
+++ packages/marvinxu/marvinxu-0.2.1.tar.gz/marvinxu-0.2.1/tests/test_common.py
@@ -0,0 +1,16 @@
+import unittest
+
+from marvinxu import filter_none, is_valid_idcard
+
+
+class TestCommon(unittest.TestCase):
+    def test_is_valid_idcard(self):
+        self.assertTrue(is_valid_idcard("441223197807234511"), "valid idcard")
+        self.assertFalse(is_valid_idcard("441223197807234512"), "invalid idcard")
+
+    def test_filter_none(self):
+        assert filter_none({"foo": 1, "bar": None}) == {"foo": 1}
+
+
+if __name__ == "__main__":
+    unittest.main()
