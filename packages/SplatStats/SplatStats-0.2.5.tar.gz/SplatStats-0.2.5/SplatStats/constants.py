15b35cc7b2ca156f29588df0c9922233ead61193
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:18 +0000

    Add package SplatStats/0.2.5/SplatStats-0.2.5.tar.gz

---
+++ packages/SplatStats/SplatStats-0.2.5.tar.gz/SplatStats-0.2.5/SplatStats/constants.py
@@ -0,0 +1,13 @@
+
+BATTLE_DTYPES = {
+    'self': bool,
+    'ko': bool,
+    'kill': int,
+    'death': int,
+    'assist': int,
+    'special': int
+}
+
+MATCH_TYPES = (
+    'Turf War', 'Tower Control', 'Rainmaker', 'Splat Zones', 'Clam Blitz'
+)
