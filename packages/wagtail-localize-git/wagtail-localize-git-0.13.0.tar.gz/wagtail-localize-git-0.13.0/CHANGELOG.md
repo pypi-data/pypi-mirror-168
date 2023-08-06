9e536aa627605bda52e4483cb3d7836368d069f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:58 +0000

    Add package wagtail-localize-git/0.13.0/wagtail-localize-git-0.13.0.tar.gz

---
+++ packages/wagtail-localize-git/wagtail-localize-git-0.13.0.tar.gz/wagtail-localize-git-0.13.0/CHANGELOG.md
@@ -0,0 +1,50 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+## [0.13] - 2022-09-23
+
+### Added
+
+- Support for Wagtail 4, wagtail-localize 1.2+ ([#26](https://github.com/wagtail/wagtail-localize-git/pull/26))
+
+### Changed
+
+- Dropped support for Wagtail < 2.15
+- Relaxed the wagtail-localize version constraints
+
+## [0.12] - 2022-02-03
+
+### Breaking changes
+This release will use `main` as the default branch. To change it anything else, use `WAGTAILLOCALIZE_GIT_DEFAULT_BRANCH = "my-branch"` in your settings file
+
+### Added
+- Support for configurable mainline branch, with a default of `main` ([#21](https://github.com/wagtail/wagtail-localize-git/pull/21))
+- Ability to install with Wagtail 2.16 ([#23](https://github.com/wagtail/wagtail-localize-git/pull/23))
+  Note: can be installed on Wagtail 2.16 with wagtail-localize >= 1.1
+- A sync retry mechanism on push failures ([#24](https://github.com/wagtail/wagtail-localize-git/pull/24))
+  Sync will retry up to 3 times before throwing a `SyncPushError` exception
+
+## [0.11] - 2021-12-02
+
+### Added
+
+- [Support for Wagtail 2.15](https://github.com/wagtail/wagtail-localize-git/pull/17)
+- Testing with Python 3.9 and 3.10 ([#17](https://github.com/wagtail/wagtail-localize-git/pull/17), [#18](https://github.com/wagtail/wagtail-localize-git/pull/18))
+
+## [0.10] - 2021-09-28
+
+### Added
+
+ - [Support for Wagtail 2.14](https://github.com/wagtail/wagtail-localize-git/pull/15)
+
+[unreleased]: https://github.com/wagtail/wagtail-localize-git/compare/v0.14.0...HEAD
+[0.13]: https://github.com/wagtail/wagtail-localize-git/compare/v0.12.0...v0.13.0
+[0.12]: https://github.com/wagtail/wagtail-localize-git/compare/v0.11.0...v0.12.0
+[0.11]: https://github.com/wagtail/wagtail-localize-git/compare/v0.10.0...v0.11.0
+[0.10]: https://github.com/wagtail/wagtail-localize-git/compare/v0.9.3...v0.10.0
---
+++ packages/wagtail-localize-git/wagtail-localize-git-0.13.0.tar.gz/wagtail-localize-git-0.13.0/setup.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+from os import path
+
+from setuptools import find_packages, setup
+
+from wagtail_localize_git import __version__
+
+
+# Hack to prevent "TypeError: 'NoneType' object is not callable" error
+# in multiprocessing/util.py _exit_function when setup.py exits
+# (see http://www.eby-sarna.com/pipermail/peak/2010-May/003357.html)
+try:
+    import multiprocessing  # noqa
+except ImportError:
+    pass
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="wagtail-localize-git",
+    version=__version__,
+    description="Wagtail Localize integration for Git-based translation services",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Karl Hobley",
+    author_email="karl@torchbox.com",
+    url="https://github.com/wagtail/wagtail-localize-git",
+    project_urls={
+        "Changelog": "https://github.com/wagtail/wagtail-localize-git/blob/main/CHANGELOG.md",  # noqa: E501
+    },
+    packages=find_packages(),
+    include_package_data=True,
+    license="BSD",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Framework :: Django",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
+        "Framework :: Django :: 3.1",
+        "Framework :: Django :: 3.2",
+        "Framework :: Wagtail",
+        "Framework :: Wagtail :: 2",
+        "Framework :: Wagtail :: 3",
+        "Framework :: Wagtail :: 4",
+    ],
+    install_requires=[
+        "Django>=3.2,<4.2",
+        "Wagtail>=2.15,<5.0",
+        "wagtail-localize>=1.0",
+        "pygit2>=1.0,<2.0",
+        "gitpython>=3.0,<4.0",
+        "toml>=0.10,<0.11",
+    ],
+    extras_require={
+        "testing": ["dj-database-url==0.5.0", "freezegun==1.2.2"],
+    },
+    zip_safe=False,
+)
