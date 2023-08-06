cd55af24d77d99f54ceb14b3d506cec181f3d064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:05 +0000

    Add package brawling/0.3.3/brawling-0.3.3.tar.gz

---
+++ packages/brawling/brawling-0.3.3.tar.gz/brawling-0.3.3/README.md
@@ -0,0 +1,18 @@
+# Brawling
+
+Synchronous API wrapper for Brawl Stars made by Supercell
+
+## Installation
+
+```
+pip install brawling
+```
+
+To install with caching support (for performance)
+
+```
+pip install brawling[cache]
+```
+
+
+This content is not affiliated with, endorsed, sponsored, or specifically approved by Supercell and Supercell is not responsible for it. For more information see Supercell’s Fan Content Policy: www.supercell.com/fan-content-policy.
---
+++ packages/brawling/brawling-0.3.3.tar.gz/brawling-0.3.3/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['brawling', 'brawling.models']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['python-dateutil>=2.8.2,<3.0.0', 'requests>=2.28.1,<3.0.0']
+
+extras_require = \
+{'cache': ['requests-cache>=0.9.6,<0.10.0']}
+
+setup_kwargs = {
+    'name': 'brawling',
+    'version': '0.3.3',
+    'description': 'Brawl stars API wrapper (synchronous)',
+    'long_description': '# Brawling\n\nSynchronous API wrapper for Brawl Stars made by Supercell\n\n## Installation\n\n```\npip install brawling\n```\n\nTo install with caching support (for performance)\n\n```\npip install brawling[cache]\n```\n\n\nThis content is not affiliated with, endorsed, sponsored, or specifically approved by Supercell and Supercell is not responsible for it. For more information see Supercell’s Fan Content Policy: www.supercell.com/fan-content-policy.',
+    'author': 'dankmeme01',
+    'author_email': 'kirill.babikov28@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
