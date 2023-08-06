965681ba2c0ae6e16bbccb0ca752ed9bf078d702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:46 +0000

    Add package sysnet-dict/1.0.0/sysnet-dict-1.0.0.tar.gz

---
+++ packages/sysnet-dict/sysnet-dict-1.0.0.tar.gz/sysnet-dict-1.0.0/dict/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from dict.api.admins_api import AdminsApi
+from dict.api.developers_api import DevelopersApi
+from dict.api.public_api import PublicApi
