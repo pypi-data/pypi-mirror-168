9978014c5f847f97161df4087a7643d571bf2897
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:50 +0000

    Add package mycustomtest/0.1.0/mycustomtest-0.1.0.tar.gz

---
+++ packages/mycustomtest/mycustomtest-0.1.0.tar.gz/mycustomtest-0.1.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['mycustomtest']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['typer[all]>=0.6.1,<0.7.0']
+
+entry_points = \
+{'console_scripts': ['mycustomtest = mycustomtest.main:app']}
+
+setup_kwargs = {
+    'name': 'mycustomtest',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': '',
+    'author': 'mycustomtest',
+    'author_email': 'mycustomtest@example.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
