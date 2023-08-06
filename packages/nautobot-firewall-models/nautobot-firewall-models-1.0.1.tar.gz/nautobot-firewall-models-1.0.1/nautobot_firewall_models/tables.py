0bb410ba5728793f7cad847cedf1a9b86e7a1226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:57:27 +0000

    Add package nautobot-firewall-models/1.0.1/nautobot-firewall-models-1.0.1.tar.gz

---
+++ packages/nautobot-firewall-models/nautobot-firewall-models-1.0.1.tar.gz/nautobot-firewall-models-1.0.1/nautobot_firewall_models/tables.py
@@ -0,0 +1,199 @@
+"""Table Views for Firewall Models."""
+
+import django_tables2 as tables
+from nautobot.extras.tables import StatusTableMixin
+from nautobot.utilities.tables import BaseTable, ButtonsColumn, ToggleColumn
+
+from nautobot_firewall_models import models
+
+
+class IPRangeTable(StatusTableMixin, BaseTable):
+    """Table for list view."""
+
+    pk = ToggleColumn()
+    start_address = tables.Column(linkify=True)
+    vrf = tables.LinkColumn()
+    actions = ButtonsColumn(models.IPRange, buttons=("edit", "delete"))
+
+    class Meta(BaseTable.Meta):
+        """Meta attributes."""
+
+        model = models.IPRange
+        fields = ("pk", "start_address", "end_address", "vrf", "size", "description", "status")
+
+
+class FQDNTable(StatusTableMixin, BaseTable):
+    """Table for list view."""
+
+    pk = ToggleColumn()
+    name = tables.Column(linkify=True)
+    actions = ButtonsColumn(models.FQDN, buttons=("edit", "delete"))
+
+    class Meta(BaseTable.Meta):
+        """Meta attributes."""
+
+        model = models.FQDN
+        fields = ("pk", "name", "description", "ip_addresses", "status")
+
+
+class AddressObjectTable(StatusTableMixin, BaseTable):
+    """Table for list view."""
+
+    pk = ToggleColumn()
+    name = tables.Column(linkify=True)
+    actions = ButtonsColumn(models.AddressObject, buttons=("edit", "delete"))
+
+    class Meta(BaseTable.Meta):
+        """Meta attributes."""
+
+        model = models.AddressObject
+        fields = ("pk", "name", "description", "ip_address", "ip_range", "prefix", "fqdn", "status")
+
+
+class AddressObjectGroupTable(StatusTableMixin, BaseTable):
+    """Table for list view."""
+
+    pk = ToggleColumn()
+    name = tables.Column(linkify=True)
+    actions = ButtonsColumn(models.AddressObjectGroup, buttons=("edit", "delete"))
+
+    class Meta(BaseTable.Meta):
+        """Meta attributes."""
+
+        model = models.AddressObjectGroup
+        fields = ("pk", "name", "description", "address_objects", "status")
+
+
+class ServiceObjectTable(StatusTableMixin, BaseTable):
+    """Table for list view."""
+
+    pk = ToggleColumn()
+    name = tables.Column(linkify=True)
+    actions = ButtonsColumn(models.ServiceObject, buttons=("edit", "delete"))
+
+    class Meta(BaseTable.Meta):
+        """Meta attributes."""
+
+        model = models.ServiceObject
+        fields = ("pk", "name", "port", "ip_protocol", "description", "status")
+
+
+class ServiceObjectGroupTable(StatusTableMixin, BaseTable):
+    """Table for list view."""
+
+    pk = ToggleColumn()
+    name = tables.Column(linkify=True)
+    actions = ButtonsColumn(models.ServiceObjectGroup, buttons=("edit", "delete"))
+
+    class Meta(BaseTable.Meta):
+        """Meta attributes."""
+
+        model = models.ServiceObjectGroup
+        fields = ("pk", "name", "description", "service_objects", "status")
+
+
+class UserObjectTable(StatusTableMixin, BaseTable):
+    """Table for list view."""
+
+    pk = ToggleColumn()
+    username = tables.Column(linkify=True)
+    actions = ButtonsColumn(models.UserObject, buttons=("edit", "delete"))
+
+    class Meta(BaseTable.Meta):
+        """Meta attributes."""
+
+        model = models.UserObject
+        fields = ("pk", "username", "name", "status")
+
+
+class UserObjectGroupTable(StatusTableMixin, BaseTable):
+    """Table for list view."""
+
+    pk = ToggleColumn()
+    name = tables.Column(linkify=True)
+    actions = ButtonsColumn(models.UserObjectGroup, buttons=("edit", "delete"))
+
+    class Meta(BaseTable.Meta):
+        """Meta attributes."""
+
+        model = models.UserObjectGroup
+        fields = ("pk", "name", "description", "user_objects", "status")
+
+
+class ZoneTable(StatusTableMixin, BaseTable):
+    """Table for list view."""
+
+    pk = ToggleColumn()
+    name = tables.Column(linkify=True)
+    actions = ButtonsColumn(models.Zone, buttons=("edit", "delete"))
+
+    class Meta(BaseTable.Meta):
+        """Meta attributes."""
+
+        model = models.Zone
+        fields = ("pk", "name", "vrfs", "interfaces", "description", "status")
+
+
+# TODO: refactor
+class PolicyRuleTable(StatusTableMixin, BaseTable):
+    """Table for list view."""
+
+    pk = ToggleColumn()
+    name = tables.LinkColumn()
+    actions = ButtonsColumn(models.PolicyRule, buttons=("edit", "delete"))
+
+    class Meta(BaseTable.Meta):
+        """Meta attributes."""
+
+        model = models.PolicyRule
+        fields = (
+            # pylint: disable=duplicate-code
+            "pk",
+            "name",
+            "source_users",
+            "source_user_groups",
+            "source_addresses",
+            "source_address_groups",
+            "source_zone",
+            "source_services",
+            "source_service_groups",
+            "destination_addresses",
+            "destination_address_groups",
+            "destination_zone",
+            "destination_services",
+            "destination_service_groups",
+            "action",
+            "log",
+            "status",
+        )
+
+
+class PolicyTable(StatusTableMixin, BaseTable):
+    """Table for list view."""
+
+    pk = ToggleColumn()
+    name = tables.Column(linkify=True)
+    actions = ButtonsColumn(models.Policy, buttons=("edit", "delete"))
+    assigned_devices = tables.ManyToManyColumn(linkify_item=True)
+    assigned_dynamic_groups = tables.ManyToManyColumn(linkify_item=True)
+
+    class Meta(BaseTable.Meta):
+        """Meta attributes."""
+
+        model = models.Policy
+        fields = ("pk", "name", "description", "policy_rules", "assigned_devices", "assigned_dynamic_groups", "status")
+
+
+class CapircaPolicyTable(BaseTable):
+    """Table for list view."""
+
+    pk = ToggleColumn()
+    device = tables.TemplateColumn(
+        template_code="""<a href="{% url 'plugins:nautobot_firewall_models:capircapolicy' pk=record.pk  %}" <strong>{{ record.device }}</strong></a> """
+    )
+
+    class Meta(BaseTable.Meta):
+        """Meta attributes."""
+
+        model = models.CapircaPolicy
+        fields = ("pk", "device")
