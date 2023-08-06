6e13238b3a0dd730401330b1faf7527cc7ba4e45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:53 +0000

    Add package Mensajes-dcampinoa/5.0/Mensajes-dcampinoa-5.0.tar.gz

---
+++ packages/Mensajes-dcampinoa/Mensajes-dcampinoa-5.0.tar.gz/Mensajes-dcampinoa-5.0/mensajes/hola/saludos.py
@@ -0,0 +1,21 @@
+import numpy as np #para utilizar numpy con el nombre np
+
+def saludar():
+    print("Hola, te saludo desde saludos.saludar()")
+
+def prueba():
+    print("Esto es una prueba de la nueva versión")
+
+def generar_array(numeros):
+    #llamar a numpy para generar array
+    return np.arange(numeros)
+
+
+class Saludo:
+    def __init__(self):
+        print("Hola, te saludo desde Saludos.__init__")
+
+#comprobación para que esto no se llame al importar el archivo en otro 
+# print(__name__)
+if __name__ == "__main__": #esto devuelve el nombre en clave del fichero
+    print(generar_array(5))
