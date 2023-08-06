8354b074de60fe8468f0f6edbe47c13a61bbb6b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:48 +0000

    Add package python-openhab-logsaver/0.1.0/python_openhab_logsaver-0.1.0-py3-none-any.whl

---
+++ packages/python-openhab-logsaver/python_openhab_logsaver-0.1.0-py3-none-any.whl/openHABLogs/__init__.py
@@ -0,0 +1,8 @@
+"""Module entry point."""
+
+from .logsaver import OpenHABLogReader
+
+__all__ = ['OpenHABLogReader']
+__version__ = "0.1.1"
+__author__ = 'Michael Dörflinger'
+__credits__ = 'Furtwangen University'
