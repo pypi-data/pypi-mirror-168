51cc8706127cd78783f4f17ccc7a7635015ea326
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:06 +0000

    Add package pip-model/0.8/pip_model-0.8.tar.gz

---
+++ packages/pip-model/pip_model-0.8.tar.gz/pip_model-0.8/setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup,find_packages
+
+setup(name='pip_model',
+      version='0.8',
+      description='string to date',
+      # url="https://github.com/18832823200/pip_model",
+      aauthor="sbdsyf",
+      author_email="zkw8119@163.com",
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      platforms="any",
+      install_requires=[]
+      )
