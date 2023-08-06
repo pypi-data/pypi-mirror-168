5ba5b32ba4f8f192715233b955e7e0b81572489f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:25 +0000

    Add package third-factor/0.0.2/third_factor-0.0.2.tar.gz

---
+++ packages/third-factor/third_factor-0.0.2.tar.gz/third_factor-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+version = '0.0.2'
+
+with open("README.md", "rb") as f:
+    long_descr = f.read().decode("utf-8")
+
+setup(
+    name="third_factor",
+    packages=[],
+    entry_points={
+        "console_scripts": ['thirdfactor = third_factor.third_factor:main']
+    },
+    version=version,
+    description="Sometimes security is protection from yourself.",
+    long_description_content_type="text/markdown",
+    long_description=long_descr,
+    author="Steamship",
+    author_email="dave@steamship.com",
+    url="https://github.com/dkolas/third-factor",
+)
