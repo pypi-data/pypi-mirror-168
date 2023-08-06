e421ff2893765442b885003ade43bd5915218ab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:15 +0000

    Add package Geode-BackgroundMesh/4.2.0/Geode_BackgroundMesh-4.2.0-cp39-cp39-manylinux2014_x86_64.whl

---
+++ packages/Geode-BackgroundMesh/Geode_BackgroundMesh-4.2.0-cp39-cp39-manylinux2014_x86_64.whl/geode_backgroundmesh/bmesh_3d.py
@@ -0,0 +1,10 @@
+#
+# Copyright (c) 2019 - 2022 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+from geode_common import core, modifier_surface, modifier_solid, cutter_surface, cutter_solid, helpers
+from geode_conversion import model
+
+from .geode_backgroundmesh_py_bmesh_3d import *
+BackgroundMesh3d.initialize()
