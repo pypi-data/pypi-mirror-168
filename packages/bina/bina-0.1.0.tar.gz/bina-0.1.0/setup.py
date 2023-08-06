474756b8d1d53eb9b2a4d166eb5cc44f5e95b666
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:24 +0000

    Add package bina/0.1.0/bina-0.1.0.tar.gz

---
+++ packages/bina/bina-0.1.0.tar.gz/bina-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['bina']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'bina',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': '',
+    'author': 'Ahmad Amirivojdan',
+    'author_email': 'aamirivo@vols.utk.edu',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
