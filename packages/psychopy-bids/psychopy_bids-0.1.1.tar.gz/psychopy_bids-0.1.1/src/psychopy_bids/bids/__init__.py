71ba71a8732b6238e5897edcaea3ac88195558df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:37 +0000

    Add package psychopy-bids/0.1.1/psychopy_bids-0.1.1.tar.gz

---
+++ packages/psychopy-bids/psychopy_bids-0.1.1.tar.gz/psychopy_bids-0.1.1/src/psychopy_bids/bids/__init__.py
@@ -0,0 +1,7 @@
+"""
+    Package to support the creation of valid bids-datasets.
+"""
+from .bidstaskevent import BIDSTaskEvent
+from .bidshandler import BIDSHandler
+
+__all__ = ['BIDSTaskEvent', 'BIDSHandler']
