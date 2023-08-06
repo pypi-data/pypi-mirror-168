8d5bb193dd47a8b232d1d329d3d90a94d84168ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:02 +0000

    Add package sqlalchemycollector/1.3.14/sqlalchemycollector-1.3.14.tar.gz

---
+++ packages/sqlalchemycollector/sqlalchemycollector-1.3.14.tar.gz/sqlalchemycollector-1.3.14/common/consts.py
@@ -0,0 +1,15 @@
+from opentelemetry.semconv.trace import SpanAttributes
+
+METIS_DO_NOT_TRACK_COMMENT = "/*METIS_DO_NOT_TRACK*/"
+
+METIS_REQUEST_SPAN_ATTRIBUTE_IDENTIFIER = "track.by.metis"
+METIS_STATEMENT_SPAN_ATTRIBUTE = f"{SpanAttributes.DB_STATEMENT}.metis"
+METIS_PLAN_SPAN_ATTRIBUTE = f"{METIS_STATEMENT_SPAN_ATTRIBUTE}.plan"
+
+METIS_QUERY_SPAN_NAME = "metis-query"
+EXPLAIN_SUPPORTED_STATEMENTS = (
+    "SELECT",
+    "INSERT",
+    "UPDATE",
+    "DELETE",
+)
