565447cad748423ac7189c40bd9ed820fd2b7210
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:25 +0000

    Add package friendly-pandas/0.1.0/friendly_pandas-0.1.0.tar.gz

---
+++ packages/friendly-pandas/friendly_pandas-0.1.0.tar.gz/friendly_pandas-0.1.0/friendly_pandas/__init__.py
@@ -0,0 +1,26 @@
+__version__ = "0.1.0"
+
+import pathlib
+import warnings
+
+import pandas
+from friendly_traceback import exclude_directory_from_traceback, config
+
+# The following import will automatically add relevant parsers to
+# those known by friendly_traceback
+from . import key_error
+from . import pandas_warnings
+warnings.simplefilter("always")
+
+print(f"friendly_pandas version {__version__}")
+
+# We want to focus on the code entered by the user.
+# We remove anything that occurs inside pandas' library from the traceback
+_pandas_init = pathlib.Path(pandas.__file__)
+_pandas_dir = _pandas_init.parents[0]
+exclude_directory_from_traceback(_pandas_dir)
+
+# Disabling showing chained exceptions in normal "friendly" tracebacks
+# as these likely come from code all inside pandas library.
+# This will likely become the default in a future version.
+config.session.include_chained_exception = False
