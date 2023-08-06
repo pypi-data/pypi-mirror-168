190584764e2b65ea5c11e803071d154233a2abad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:43 +0000

    Add package isar/1.10.0/isar-1.10.0.tar.gz

---
+++ packages/isar/isar-1.10.0.tar.gz/isar-1.10.0/src/isar/config/log.py
@@ -0,0 +1,29 @@
+import logging
+import logging.config
+
+import yaml
+from uvicorn.logging import ColourizedFormatter
+
+from isar.config.settings import settings
+
+
+def setup_logger():
+    log_levels: dict = settings.LOG_LEVELS
+    log_config = yaml.safe_load(open("./src/isar/config/logging.conf"))
+    log_handler = logging.StreamHandler()
+
+    log_handler.setLevel(log_config["root"]["level"])
+    log_handler.setFormatter(
+        ColourizedFormatter(
+            log_config["formatters"]["colourized"]["format"],
+            style="{",
+            use_colors=True,
+        )
+    )
+
+    logging.config.dictConfig(log_config)
+
+    for loggers in log_config["loggers"].keys():
+        logging.getLogger(loggers).addHandler(log_handler)
+        logging.getLogger(loggers).setLevel(log_levels[loggers])
+    logging.getLogger().addHandler(log_handler)
