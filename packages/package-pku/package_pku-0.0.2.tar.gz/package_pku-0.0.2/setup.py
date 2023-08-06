ef47f7c8097d769c839ef5ea4fc580e594ff1211
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:06 +0000

    Add package package-pku/0.0.2/package_pku-0.0.2.tar.gz

---
+++ packages/package-pku/package_pku-0.0.2.tar.gz/package_pku-0.0.2/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+with open("README.md", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Demonstrating how to use PyPI'
+
+
+
+setup(  
+    # name des paketes auf dem Package index
+    name="package_pku",
+    version=VERSION,
+    author="ExperTeach GmbH",
+    author_email="et_pku@web.de",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'numpy',
+        'pandas'
+        ],
+    keywords=[
+        'python',
+        'code',
+        ' Expertttt'  
+        ],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
