0ea16f8ea04adbbb67687032c2167c1983a64ddc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:30 +0000

    Add package leeger/1.4.1/leeger-1.4.1.tar.gz

---
+++ packages/leeger/leeger-1.4.1.tar.gz/leeger-1.4.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+pkg_vars = dict()
+with open("leeger/_version.py") as f:
+    exec(f.read(), pkg_vars)
+
+with open("README.md") as f:
+    readme = f.read()
+
+setuptools.setup(
+    name="leeger",
+    version=pkg_vars["__version__"],
+    author="Joey Greco",
+    author_email="joeyagreco@gmail.com",
+    description="Instant stats for your fantasy football league.",
+    long_description_content_type="text/markdown",
+    long_description=readme,
+    license="MIT",
+    packages=setuptools.find_packages(exclude=("test", "docs")),
+    install_requires=["numpy",
+                      "setuptools",
+                      "openpyxl",
+                      "espn-api",
+                      "yahoofantasy",
+                      "sleeper",
+                      "pymfl"]
+)
