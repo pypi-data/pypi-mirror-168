5d970be93a8a7c355a73b6613de31bb4754be5a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:59 +0000

    Add package instruqt-xblock/0.2.0/instruqt-xblock-0.2.0.tar.gz

---
+++ packages/instruqt-xblock/instruqt-xblock-0.2.0.tar.gz/instruqt-xblock-0.2.0/instruqtxblock/settings/test.py
@@ -0,0 +1,15 @@
+"""
+Stub Settings
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+    },
+}
+INSTALLED_APPS = (
+    'instruqtxblock',
+)
+LOCALE_PATHS = [
+    'instruqtxblock/locale',
+]
+SECRET_KEY = 'SECRET_KEY'
