e847872184eae5b3dcac229bbe542936874660ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:38 +0000

    Add package superior6564/0.1.4/superior6564-0.1.4.tar.gz

---
+++ packages/superior6564/superior6564-0.1.4.tar.gz/superior6564-0.1.4/superior6564/info.py
@@ -0,0 +1,28 @@
+"""
+:authors: Superior_6564
+:license: Apache License, Version 2.0, see LICENSE file
+:copyright: (c) 2022 Superior_6564
+"""
+import os
+import requests
+
+
+readme_md = requests.get('https://raw.githubusercontent.com/Superior-GitHub/superior6564/main/README.md')
+out_1 = open("readme.md", "wb")
+out_1.write(readme_md.content)
+out_1.close()
+
+
+def get_info_superior6564():
+    with open("readme.md") as f:
+        version = f.readline()[22:].strip()
+    if os.getcwd() == "/content":
+        path = "/usr/local/lib/python3.7/dist-packages/superior6564-" + version + ".dist-info/METADATA"
+        with open(path) as f:
+            f.readline()
+            dictionary = {"Name": f.readline(), "Version": f.readline(), "Summary": f.readline(),
+                          "Home-Page": f.readline(), "Author": f.readline(), "Author-email": f.readline(),
+                          "License": f.readline(), "Download-URL": f.readline(), }
+            print(dictionary["Name"] + dictionary["Version"] + dictionary["Home-Page"] +
+                  dictionary["Author"] + dictionary["Author-email"] + dictionary["License"] +
+                  dictionary["Download-URL"] + dictionary["Summary"][9:])
---
+++ packages/superior6564/superior6564-0.1.4.tar.gz/superior6564-0.1.4/superior6564/packages.py
@@ -0,0 +1,29 @@
+"""
+:authors: Superior_6564
+:license: Apache License, Version 2.0, see LICENSE file
+:copyright: (c) 2022 Superior_6564
+"""
+import subprocess
+import sys
+import time
+
+
+def install(package: str):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", package])
+    print(f"Библиотека {package} установлена.")
+
+
+def pip_upgrade():
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "pip"])
+
+
+def required():
+    install("python-time")
+    require = ["requests", "opencv-python", "progress", "dearpygui"]
+    print("Установка необходимых библиотек...")
+    for i in range(len(require)):
+        install(require[i])
+        print(f"Status: {i + 1} of {len(require)}")
+    print("Установка завершена.")
+    time.sleep(1)
