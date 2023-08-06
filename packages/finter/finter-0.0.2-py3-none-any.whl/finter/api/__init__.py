b55f83f98c2baac1a30171e14479da468143ae0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:38 +0000

    Add package finter/0.0.2/finter-0.0.2-py3-none-any.whl

---
+++ packages/finter/finter-0.0.2-py3-none-any.whl/finter/api/__init__.py
@@ -0,0 +1,23 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from finter.api.alpha_cli_api import AlphaCLIApi
+from finter.api.alpha_dashboard_api import AlphaDashboardApi
+from finter.api.content_cli_api import ContentCLIApi
+from finter.api.content_dashboard_api import ContentDashboardApi
+from finter.api.flexible_fund_cli_api import FlexibleFundCLIApi
+from finter.api.flexible_fund_dashboard_api import FlexibleFundDashboardApi
+from finter.api.fund_cli_api import FundCLIApi
+from finter.api.fund_dashboard_api import FundDashboardApi
+from finter.api.metafund_cli_api import MetafundCLIApi
+from finter.api.portfolio_cli_api import PortfolioCLIApi
+from finter.api.portfolio_dashboard_api import PortfolioDashboardApi
+from finter.api.test_api import TestApi
+from finter.api.wm_ap_api import WMAPApi
+from finter.api.wm_portfolio_api import WMPortfolioApi
+from finter.api.default_api import DefaultApi
+from finter.api.performance_api import PerformanceApi
+from finter.api.theme_api import ThemeApi
+from finter.api.theme_vaiv_api import ThemeVaivApi
