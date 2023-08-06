0107b28ac3463e1694a7c0531dfcc85a20f98a56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:39 +0000

    Add package operacionjgg/1.0/operacionjgg-1.0.tar.gz

---
+++ packages/operacionjgg/operacionjgg-1.0.tar.gz/operacionjgg-1.0/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#get_ipython().system('pip install setuptools')
+
+
+# In[2]:
+
+
+#get_ipython().system('pip install twine')
+
+
+# In[3]:
+
+
+from setuptools import setup
+
+
+# In[4]:
+
+
+#from setuptools import find_packages
+
+
+# In[7]:
+
+setup(
+    name="operacionjgg",
+    version="1.0",
+    description="Operaciones Matematicas",
+    author="Jorge Guerra Guerra",
+    author_email="jguerra91@gmail.com",
+    url="https://dina.concytec.gob.pe/appDirectorioCTI/VerDatosInvestigador.do?id_investigador=12581",
+    packages=['operacionjgg'],
+    scripts=[]
+)
+
+
+# In[ ]:
+
+
+
+
