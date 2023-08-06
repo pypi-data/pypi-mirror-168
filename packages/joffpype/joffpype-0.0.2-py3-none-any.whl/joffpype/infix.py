b863721ab809f107a86a49821976ed46ebce2da1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:15 +0000

    Add package joffpype/0.0.2/joffpype-0.0.2-py3-none-any.whl

---
+++ packages/joffpype/joffpype-0.0.2-py3-none-any.whl/joffpype/infix.py
@@ -0,0 +1,24 @@
+"""Extension: add a %_% and |_| pipe that can do everything but handling funcs() with parantheses while not requiring @ pipe function blocks"""
+
+from functools import partial
+from .superpipe import pipes
+
+class Infix(object):
+    def __init__(self, func):
+        self.func = func
+    def __or__(self, other):
+        return self.func(other)
+    def __ror__(self, other):
+        return Infix(partial(self.func, other))
+    def __mod__(self, other):
+        return self.func(other)
+    def __rmod__(self, other):
+        return Infix(partial(self.func, other))
+    def __call__(self, v1, v2):
+        return self.func(v1, v2)
+
+
+@Infix
+@pipes
+def _(x, f):
+    return x >> f
