9fccc20d077394aa988592bd086d0fce51580c92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:37 +0000

    Add package flask-user-utils/1.2.0/flask_user_utils-1.2.0-py3-none-any.whl

---
+++ packages/flask-user-utils/flask_user_utils-1.2.0-py3-none-any.whl/flask_user/__init__.py
@@ -0,0 +1,35 @@
+from flask import Flask
+
+from . import blueprint
+from .extensions import login
+from .views import (LoginView, LogoutView, MakeLoginView,
+                    MakeSuperUserLoginView, RegisterView, RegistrationView,
+                    SuperUserLoginView)
+
+
+class FlaskUser:
+
+    def __init__(self):
+        self.bp = blueprint.bp
+
+    def init_app(self, app: Flask):
+        login.init_app(app)
+        blueprint.init_app(app)
+
+    def add_login(self, success_endpoint: str) -> None:
+        self.bp.add_url_rule('/login', view_func=LoginView.as_view('login'))
+        self.bp.add_url_rule('/logout', view_func=LogoutView.as_view('logout'))
+        MakeLoginView.success_endpoint = success_endpoint
+        self.bp.add_url_rule('/make-login', view_func=MakeLoginView.as_view('make_login'))
+
+    def add_superuser_login(self, success_endpoint: str) -> None:
+        self.bp.add_url_rule('/superuser-login',
+                             view_func=SuperUserLoginView.as_view('superuser_login'))
+        MakeSuperUserLoginView.success_endpoint = success_endpoint
+        self.bp.add_url_rule('/make-superuser-login',
+                             view_func=MakeSuperUserLoginView.as_view('make_superuser_login'))
+
+    def add_registration(self, success_endpoint: str) -> None:
+        self.bp.add_url_rule('/registration', view_func=RegistrationView.as_view('registration'))
+        RegisterView.success_endpoint = success_endpoint
+        self.bp.add_url_rule('/register', view_func=RegisterView.as_view('register'))
