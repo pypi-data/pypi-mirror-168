89f2ff7ba9687e7a89732872fa763ef682edfa53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:53 +0000

    Add package pycronjob/1.2.0/pycronjob-1.2.0-py3-none-any.whl

---
+++ packages/pycronjob/pycronjob-1.2.0-py3-none-any.whl/pycronjob/__init__.py
@@ -0,0 +1,9 @@
+'''
+Author: Hexu
+Date: 2022-09-20 17:40:46
+LastEditors: Hexu
+LastEditTime: 2022-09-20 18:03:50
+FilePath: /pycronjob/pycronjob/__init__.py
+Description: 
+'''
+__VERSION__ = "1.2.0"
