a1acf581435644d9ac5aa3cf1c68a53099184ebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:37 +0000

    Add package incqueryserver-api-python-client/0.24.1/incqueryserver-api-python-client-0.24.1.tar.gz

---
+++ packages/incqueryserver-api-python-client/incqueryserver-api-python-client-0.24.1.tar.gz/incqueryserver-api-python-client-0.24.1/model_analyzer_client/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from model_analyzer_client.api.api_preview_api import APIPreviewApi
+from model_analyzer_client.api.handle_jobs_api import HandleJobsApi
+from model_analyzer_client.api.resources_api import ResourcesApi
