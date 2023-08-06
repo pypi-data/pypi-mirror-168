60e80cb43cf5e18aea804c6cd3bf560312df5205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:10 +0000

    Add package contentmonster/0.0.5/contentmonster-0.0.5-py3-none-any.whl

---
+++ packages/contentmonster/contentmonster-0.0.5-py3-none-any.whl/contentmonster/classes/config.py
@@ -0,0 +1,58 @@
+import configparser
+
+from pathlib import Path
+from typing import Union
+
+from .vessel import Vessel
+from .directory import Directory
+from .database import Database
+
+
+class MonsterConfig:
+    def readFile(self, path: Union[str, Path], dbclass: type = Database) -> None:
+        """Read .ini file into MonsterConfig object
+
+        Args:
+            path (str, pathlib.Path): Location of the .ini file to read
+              (absolute or relative to the working directory)
+            dbclass (type): Class to use for database connections. Defaults to
+              built-in Database using sqlite3.
+
+        Raises:
+            ValueError: Raised if the passed file is not a ContentMonster .ini
+            IOError: Raised if the file cannot be read from the provided path
+        """
+        parser = configparser.ConfigParser()
+        parser.read(str(path))
+
+        if not "MONSTER" in parser.sections():
+            raise ValueError("Config file does not contain a MONSTER section!")
+
+        try:
+            self.database = parser["MONSTER"]["Database"]
+        except KeyError:
+            pass
+
+        try:
+            self.chunksize = int(parser["MONSTER"]["ChunkSize"])
+        except KeyError:
+            pass
+
+        for section in parser.sections():
+            # Read Directories from the config file
+            if section.startswith("Directory"):
+                self.directories.append(
+                    Directory.fromConfig(parser[section]))
+
+            # Read Vessels from the config file
+            elif section.startswith("Vessel"):
+                self.vessels.append(
+                    Vessel.fromConfig(parser[section], dbclass))
+
+    def __init__(self) -> None:
+        """Initialize a new (empty) MonsterConfig object
+        """
+        self.directories = []
+        self.vessels = []
+        self.chunksize = 10485760  # Default: 10 MiB
+        self.database = None  # Default: "database.sqlite3" in base directory
