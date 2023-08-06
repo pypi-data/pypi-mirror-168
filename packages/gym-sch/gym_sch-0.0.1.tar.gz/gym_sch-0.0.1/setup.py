4aae027169f4d4bf853ad6d3b6961a8533ae77c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:07 +0000

    Add package gym-sch/0.0.1/gym_sch-0.0.1.tar.gz

---
+++ packages/gym-sch/gym_sch-0.0.1.tar.gz/gym_sch-0.0.1/setup.py
@@ -0,0 +1,14 @@
+#!E:\Pycharm Projects\Waytous
+# -*- coding: utf-8 -*-
+# @Time : 2022/9/24 14:46
+# @Author : Opfer
+# @Site :
+# @File : setup.py    
+# @Software: PyCharm
+
+from setuptools import setup
+
+setup(name='gym_sch',
+		  version='0.0.1',
+		  install_requires=['gym']
+)
