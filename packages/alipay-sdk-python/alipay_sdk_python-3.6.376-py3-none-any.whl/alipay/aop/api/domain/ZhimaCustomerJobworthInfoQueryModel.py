153e9afeccc6c890ff0d1ab8d2d7caf5bdff766b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:47 +0000

    Add package alipay-sdk-python/3.6.376/alipay_sdk_python-3.6.376-py3-none-any.whl

---
+++ packages/alipay-sdk-python/alipay_sdk_python-3.6.376-py3-none-any.whl/alipay/aop/api/domain/ZhimaCustomerJobworthInfoQueryModel.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import json
+
+from alipay.aop.api.constant.ParamConstants import *
+
+
+class ZhimaCustomerJobworthInfoQueryModel(object):
+
+    def __init__(self):
+        self._back_url = None
+        self._cert_no = None
+        self._cert_type = None
+        self._conn_key = None
+        self._industry_id = None
+        self._job_id = None
+        self._user_id = None
+        self._user_name = None
+
+    @property
+    def back_url(self):
+        return self._back_url
+
+    @back_url.setter
+    def back_url(self, value):
+        self._back_url = value
+    @property
+    def cert_no(self):
+        return self._cert_no
+
+    @cert_no.setter
+    def cert_no(self, value):
+        self._cert_no = value
+    @property
+    def cert_type(self):
+        return self._cert_type
+
+    @cert_type.setter
+    def cert_type(self, value):
+        self._cert_type = value
+    @property
+    def conn_key(self):
+        return self._conn_key
+
+    @conn_key.setter
+    def conn_key(self, value):
+        self._conn_key = value
+    @property
+    def industry_id(self):
+        return self._industry_id
+
+    @industry_id.setter
+    def industry_id(self, value):
+        self._industry_id = value
+    @property
+    def job_id(self):
+        return self._job_id
+
+    @job_id.setter
+    def job_id(self, value):
+        self._job_id = value
+    @property
+    def user_id(self):
+        return self._user_id
+
+    @user_id.setter
+    def user_id(self, value):
+        self._user_id = value
+    @property
+    def user_name(self):
+        return self._user_name
+
+    @user_name.setter
+    def user_name(self, value):
+        self._user_name = value
+
+
+    def to_alipay_dict(self):
+        params = dict()
+        if self.back_url:
+            if hasattr(self.back_url, 'to_alipay_dict'):
+                params['back_url'] = self.back_url.to_alipay_dict()
+            else:
+                params['back_url'] = self.back_url
+        if self.cert_no:
+            if hasattr(self.cert_no, 'to_alipay_dict'):
+                params['cert_no'] = self.cert_no.to_alipay_dict()
+            else:
+                params['cert_no'] = self.cert_no
+        if self.cert_type:
+            if hasattr(self.cert_type, 'to_alipay_dict'):
+                params['cert_type'] = self.cert_type.to_alipay_dict()
+            else:
+                params['cert_type'] = self.cert_type
+        if self.conn_key:
+            if hasattr(self.conn_key, 'to_alipay_dict'):
+                params['conn_key'] = self.conn_key.to_alipay_dict()
+            else:
+                params['conn_key'] = self.conn_key
+        if self.industry_id:
+            if hasattr(self.industry_id, 'to_alipay_dict'):
+                params['industry_id'] = self.industry_id.to_alipay_dict()
+            else:
+                params['industry_id'] = self.industry_id
+        if self.job_id:
+            if hasattr(self.job_id, 'to_alipay_dict'):
+                params['job_id'] = self.job_id.to_alipay_dict()
+            else:
+                params['job_id'] = self.job_id
+        if self.user_id:
+            if hasattr(self.user_id, 'to_alipay_dict'):
+                params['user_id'] = self.user_id.to_alipay_dict()
+            else:
+                params['user_id'] = self.user_id
+        if self.user_name:
+            if hasattr(self.user_name, 'to_alipay_dict'):
+                params['user_name'] = self.user_name.to_alipay_dict()
+            else:
+                params['user_name'] = self.user_name
+        return params
+
+    @staticmethod
+    def from_alipay_dict(d):
+        if not d:
+            return None
+        o = ZhimaCustomerJobworthInfoQueryModel()
+        if 'back_url' in d:
+            o.back_url = d['back_url']
+        if 'cert_no' in d:
+            o.cert_no = d['cert_no']
+        if 'cert_type' in d:
+            o.cert_type = d['cert_type']
+        if 'conn_key' in d:
+            o.conn_key = d['conn_key']
+        if 'industry_id' in d:
+            o.industry_id = d['industry_id']
+        if 'job_id' in d:
+            o.job_id = d['job_id']
+        if 'user_id' in d:
+            o.user_id = d['user_id']
+        if 'user_name' in d:
+            o.user_name = d['user_name']
+        return o
+
+
