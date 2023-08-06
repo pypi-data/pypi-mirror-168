e36070ae010355f489981c6a7fd9b04680eed2a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:25 +0000

    Add package hdx-python-scraper/1.9.3/hdx-python-scraper-1.9.3.tar.gz

---
+++ packages/hdx-python-scraper/hdx-python-scraper-1.9.3.tar.gz/hdx-python-scraper-1.9.3/src/hdx/scraper/base_scraper.py
@@ -0,0 +1,257 @@
+from abc import ABC, abstractmethod
+from copy import deepcopy
+from datetime import datetime
+from typing import Dict, List, Optional, Set, Tuple
+
+from .utilities.reader import Read
+
+
+class BaseScraper(ABC):
+    """Base scraper class for scrapers to inherit
+
+    Args:
+        name (str): Name of scraper
+        datasetinfo (Dict): Information about dataset
+        headers: Dict[str, Tuple]: Headers to be oytput at each level_name
+    """
+
+    population_lookup = dict()
+
+    def __init__(
+        self, name: str, datasetinfo: Dict, headers: Dict[str, Tuple]
+    ) -> None:
+        self.setup(name, headers)
+        self.datasetinfo = deepcopy(datasetinfo)
+        self.errors_on_exit = None
+        self.can_fallback = True
+
+    def setup(self, name: str, headers: Dict[str, Tuple]) -> None:
+        """Initialise member variables including name and headers which is of form:
+        {"national": (("School Closure",), ("#impact+type",)), ...},
+
+        Args:
+            name (str): Name of scraper
+            headers (Dict[str, Tuple]): Headers to be output at each level_name
+
+        Returns:
+             None
+        """
+        self.name = name
+        self.headers = headers
+        self.initialise_values_sources()
+        self.has_run = False
+        self.fallbacks_used = False
+        self.source_urls = set()
+        self.population_key = None
+
+    def initialise_values_sources(self) -> None:
+        """
+        Create values and sources member variables for inheriting scrapers to populate.
+        values will be of form:
+        {"national": ({"AFG": 1.2, "PSE": 1.4}, {"AFG": 123, "PSE": 241}, ...})}
+        sources will be of form:
+        {"national": [("#food-prices", "2022-07-15", "WFP", "https://data.humdata.org/dataset/global-wfp-food-prices"), ...]
+
+        Returns:
+             None
+        """
+        self.values: Dict[str, Tuple] = {
+            level: tuple(dict() for _ in value[0])
+            for level, value in self.headers.items()
+        }
+        self.sources: Dict[str, List] = {
+            level: list() for level in self.headers
+        }
+
+    def get_reader(
+        self, name: Optional[str] = None, prefix: Optional[str] = None
+    ):
+        """Get reader given name if provided or using name member variable if not.
+        Set reader prefix to given prefix or name if not provided.
+
+        Args:
+            name (str): Name of scraper
+            prefix (Optional[str]): Prefix to use. Defaults to None (use scraper name).
+
+        Returns:
+             None
+        """
+        if not name:
+            name = self.name
+        reader = Read.get_reader(name)
+        if not prefix:
+            prefix = name
+        reader.prefix = prefix
+        return reader
+
+    def get_headers(self, level: str) -> Optional[Tuple[Tuple]]:
+        """
+        Get headers for a particular level_name like national or subnational. Will be
+        of form: (("School Closure",), ("#impact+type",))
+
+        Args:
+            level (str): Level to get like national, subnational or single
+
+        Returns:
+            Optional[Tuple[Tuple]]: Scraper headers or None
+        """
+        return self.headers.get(level)
+
+    def get_values(self, level: str) -> Optional[Tuple]:
+        """
+        Get values for a particular level_name like national or subnational. Will be of
+        form: ({"AFG": 1.2, "PSE": 1.4}, {"AFG": 123, "PSE": 241}, ...})}
+
+        Args:
+            level (str): Level for which to get headers
+
+        Returns:
+            Optional[Tuple]: Scraper values or None
+        """
+        return self.values.get(level)
+
+    def add_sources(self) -> None:
+        """
+        Adds sources for a particular level_name
+
+        Returns:
+            None
+        """
+        source = self.datasetinfo["source"]
+        if isinstance(source, str):
+            source = {"default_source": source}
+        source_url = self.datasetinfo["source_url"]
+        if isinstance(source_url, str):
+            source_url = {"default_url": source_url}
+        date = self.datasetinfo["source_date"]
+        if isinstance(date, datetime):
+            date = {"default_date": date}
+        for level in self.headers:
+            self.sources[level] = [
+                (
+                    hxltag,
+                    date.get(hxltag, date["default_date"]).strftime(
+                        "%Y-%m-%d"
+                    ),
+                    source.get(hxltag, source["default_source"]),
+                    source_url.get(hxltag, source_url["default_url"]),
+                )
+                for hxltag in self.headers[level][1]
+            ]
+
+    def add_hxltag_source(self, key: str, indicator: str) -> None:
+        """
+        Adds source under a particular key with a particular indicator expressed as a
+        HXL hashtag.
+
+        Args:
+            key (str): Key under which to add source
+            indicator (str): HXL hashtag to use for source
+
+        Returns:
+            None
+        """
+        self.sources[key] = [
+            (
+                indicator,
+                self.datasetinfo["source_date"].strftime("%Y-%m-%d"),
+                self.datasetinfo["source"],
+                self.datasetinfo["source_url"],
+            )
+        ]
+
+    def get_sources(self, level: str) -> Optional[List[Tuple]]:
+        """
+        Get values for a particular level_name like national or subnational. Will be of
+        form:
+        [("#food-prices", "2022-07-15", "WFP", "https://data.humdata.org/dataset/global-wfp-food-prices"), ...]
+
+        Args:
+            level (str): Level to get like national, subnational or single
+
+        Returns:
+            Optional[List[Tuple]]: Scraper sources or None
+        """
+        return self.sources.get(level)
+
+    def add_source_urls(self) -> None:
+        """
+        Add source urls from the datasetinfo member variable
+
+        Returns:
+            None
+        """
+        source_url = self.datasetinfo.get("source_url")
+        if source_url:
+            if isinstance(source_url, str):
+                self.source_urls.add(source_url)
+            else:
+                for url in source_url.values():
+                    self.source_urls.add(url)
+
+    def get_source_urls(self) -> Set[str]:
+        """
+        Get source urls
+
+        Returns:
+            Set[str]: Source urls
+        """
+        return self.source_urls
+
+    def add_population(self) -> None:
+        """
+        Add population data by looking for the #population HXL hashtag among the
+        headers and pulling out the associated values
+
+        Returns:
+            None
+        """
+        for level in self.headers:
+            try:
+                population_index = self.headers[level][1].index("#population")
+            except ValueError:
+                population_index = None
+            if population_index is not None:
+                values = self.values[level][population_index]
+                if len(values) == 1 and "value" in values:
+                    values = (
+                        (
+                            self.datasetinfo.get("population_key") or level,
+                            values["value"],
+                        ),
+                    )
+                else:
+                    values = values.items()
+                for key, value in values:
+                    try:
+                        valint = int(value)
+                        self.population_lookup[key] = valint
+                    except (TypeError, ValueError):
+                        pass
+
+    @abstractmethod
+    def run(self) -> None:
+        """
+        Run scraper. Must be overridden.
+
+        Returns:
+            None
+        """
+
+    def run_after_fallbacks(self) -> None:
+        """
+        Executed after fallbacks are used. Can be overridden if needed.
+
+        Returns:
+            None
+        """
+        pass
+
+    def post_run(self) -> None:
+        """
+        Executed after running. Can be overridden if needed.
+
+        Returns:
+            None
+        """
+        pass
---
+++ packages/hdx-python-scraper/hdx-python-scraper-1.9.3.tar.gz/hdx-python-scraper-1.9.3/tests/hdx/scraper/test_scrapers_custom.py
@@ -0,0 +1,253 @@
+from copy import deepcopy
+
+from hdx.utilities.dateparse import parse_date
+
+from hdx.scraper.base_scraper import BaseScraper
+from hdx.scraper.runner import Runner
+
+from .conftest import check_scraper, check_scrapers
+from .education_closures import EducationClosures
+from .education_enrolment import EducationEnrolment
+
+
+class TestScrapersCustom:
+    def test_get_custom(self, configuration, fallbacks):
+        BaseScraper.population_lookup = dict()
+        source_date = "2022-04-30"
+        today = parse_date("2020-10-01")
+        level = "national"
+        countries = ("AFG",)
+
+        class Region:
+            iso3_to_region_and_hrp = {"AFG": ("ROAP",)}
+
+        region = Region()
+        runner = Runner(("AFG",), today)
+        datasetinfo = configuration["education_closures"]
+        education_closures = EducationClosures(
+            datasetinfo, today, countries, region
+        )
+        runner.add_custom(education_closures)
+        runner.run()
+        name = education_closures.name
+        headers = (["School Closure"], ["#impact+type"])
+        values = [{"AFG": "Closed due to COVID-19"}]
+        sources = [
+            (
+                "#impact+type",
+                source_date,
+                "UNESCO",
+                "https://data.humdata.org/dataset/global-school-closures-covid19",
+            )
+        ]
+        check_scraper(name, runner, "national", headers, values, sources)
+        headers = (["No. closed countries"], ["#status+country+closed"])
+        values = [{"ROAP": 1}]
+        sources = [
+            (
+                "#status+country+closed",
+                source_date,
+                "UNESCO",
+                "https://data.humdata.org/dataset/global-school-closures-covid19",
+            )
+        ]
+        check_scraper(name, runner, "regional", headers, values, sources)
+
+        scraper_configuration = configuration[f"scraper_{level}"]
+        runner.add_configurables(scraper_configuration, level)
+        runner.run_one("population")
+        check_scraper(name, runner, "regional", headers, values, sources)
+        names = ("population", name)
+        assert (
+            runner.get_headers(
+                names=names,
+                levels=("regional",),
+                hxltags=("#population", "#status+country+closed"),
+            )["regional"]
+            == headers
+        )
+        headers = (
+            ["Population", "School Closure"],
+            ["#population", "#impact+type"],
+        )
+        values = [{"AFG": 38041754}, {"AFG": "Closed due to COVID-19"}]
+        sources = [
+            (
+                "#population",
+                "2020-10-01",
+                "World Bank",
+                "https://data.humdata.org/organization/world-bank-group",
+            ),
+            (
+                "#impact+type",
+                source_date,
+                "UNESCO",
+                "https://data.humdata.org/dataset/global-school-closures-covid19",
+            ),
+        ]
+        assert (
+            runner.get_headers(
+                names=names,
+                levels=("national",),
+                headers=("Population", "School Closure"),
+            )["national"]
+            == headers
+        )
+        check_scrapers(
+            names,
+            runner,
+            "national",
+            headers,
+            values,
+            sources,
+        )
+        assert runner.get_source_urls() == [
+            "https://data.humdata.org/dataset/global-school-closures-covid19",
+            "https://data.humdata.org/organization/world-bank-group",
+        ]
+
+        runner = Runner(("AFG",), today)
+        datasetinfo = deepcopy(datasetinfo)
+        datasetinfo["url"] = "NOTEXIST.csv"
+        education_closures = EducationClosures(
+            datasetinfo, today, countries, Region()
+        )
+        runner.add_custom(education_closures)
+        runner.run()
+        name = education_closures.name
+        headers = (["School Closure"], ["#impact+type"])
+        values = [{"AFG": "Closed due to COVID-19"}]
+        sources = [
+            (
+                "#impact+type",
+                "2020-09-01",
+                "UNESCO",
+                "tests/fixtures/fallbacks.json",
+            )
+        ]
+        check_scraper(
+            name,
+            runner,
+            "national",
+            headers,
+            values,
+            sources,
+            fallbacks_used=True,
+        )
+        headers = (["No. closed countries"], ["#status+country+closed"])
+        values = [{"ROAP": 3}]
+        sources = [
+            (
+                "#status+country+closed",
+                "2020-09-01",
+                "UNESCO",
+                "tests/fixtures/fallbacks.json",
+            )
+        ]
+        check_scraper(
+            name,
+            runner,
+            "regional",
+            headers,
+            values,
+            sources,
+            fallbacks_used=True,
+        )
+
+        datasetinfo = configuration["education_enrolment"]
+        education_enrolment = EducationEnrolment(
+            datasetinfo, education_closures, countries, region
+        )
+        runner.add_custom(education_enrolment)
+        runner.run()
+        name = education_enrolment.name
+        headers = (
+            [
+                "No. pre-primary to upper-secondary learners",
+                "No. tertiary learners",
+                "No. affected learners",
+            ],
+            [
+                "#population+learners+pre_primary_to_secondary",
+                "#population+learners+tertiary",
+                "#affected+learners",
+            ],
+        )
+        values = [{"AFG": 9865894}, {"AFG": 430980}, {"AFG": 10296874}]
+        sources = [
+            (
+                "#population+learners+pre_primary_to_secondary",
+                "2022-04-30",
+                "UNESCO",
+                "https://data.humdata.org/dataset/global-school-closures-covid19",
+            ),
+            (
+                "#population+learners+tertiary",
+                "2022-04-30",
+                "UNESCO",
+                "https://data.humdata.org/dataset/global-school-closures-covid19",
+            ),
+            (
+                "#affected+learners",
+                "2022-04-30",
+                "UNESCO",
+                "https://data.humdata.org/dataset/global-school-closures-covid19",
+            ),
+        ]
+        check_scraper(name, runner, "national", headers, values, sources)
+        headers = (
+            ["No. affected learners", "Percentage affected learners"],
+            ["#affected+learners", "#affected+learners+pct"],
+        )
+        values = [{"ROAP": 10296874}, {"ROAP": "1.0000"}]
+        sources = [
+            (
+                "#affected+learners",
+                "2022-04-30",
+                "UNESCO",
+                "https://data.humdata.org/dataset/global-school-closures-covid19",
+            ),
+            (
+                "#affected+learners+pct",
+                "2022-04-30",
+                "UNESCO",
+                "https://data.humdata.org/dataset/global-school-closures-covid19",
+            ),
+        ]
+        check_scraper(name, runner, "regional", headers, values, sources)
+
+        # closures used fallbacks so there is no no url from that scraper
+        assert runner.get_source_urls() == [
+            "https://data.humdata.org/dataset/global-school-closures-covid19"
+        ]
+
+        datasetinfo = configuration["education_closures"]
+        education_closures = EducationClosures(
+            datasetinfo, today, countries, region
+        )
+        runner.add_custom(education_closures)
+        runner.run()
+        assert runner.get_source_urls() == [
+            "https://data.humdata.org/dataset/global-school-closures-covid19",
+        ]
+
+        assert runner.get_scraper_names() == [
+            "education_closures",
+            "education_enrolment",
+        ]
+        runner.prioritise_scrapers(("education_enrolment",))
+        assert runner.get_scraper_names() == [
+            "education_enrolment",
+            "education_closures",
+        ]
+
+        education_closures.add_hxltag_source("test", "#lala")
+        sources = education_closures.get_sources("test")
+        assert sources == [
+            (
+                "#lala",
+                "2022-04-30",
+                "UNESCO",
+                "https://data.humdata.org/dataset/global-school-closures-covid19",
+            )
+        ]
