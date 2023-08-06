193b8c48a8bbc57e8765374d1376b5ff502041da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:00 +0000

    Add package pipda/0.7.2/pipda-0.7.2.tar.gz

---
+++ packages/pipda/pipda-0.7.2.tar.gz/pipda-0.7.2/pipda/__init__.py
@@ -0,0 +1,10 @@
+from .context import Context, ContextBase
+from .expression import Expression
+from .function import Function, FunctionCall, register_func
+from .operator import Operator, OperatorCall, register_operator
+from .reference import ReferenceAttr, ReferenceItem
+from .symbolic import Symbolic
+from .utils import evaluate_expr
+from .verb import Verb, VerbCall, register_verb, register_piping
+
+__version__ = "0.7.2"
