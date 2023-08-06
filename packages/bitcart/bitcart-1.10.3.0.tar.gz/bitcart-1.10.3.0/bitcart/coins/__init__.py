95821d5e6b68a419122f4d09999bc08e98c3ef68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:22 +0000

    Add package bitcart/1.10.3.0/bitcart-1.10.3.0.tar.gz

---
+++ packages/bitcart/bitcart-1.10.3.0.tar.gz/bitcart-1.10.3.0/bitcart/coins/__init__.py
@@ -0,0 +1,25 @@
+from .bch import BCH
+from .bnb import BNB
+from .bsty import BSTY
+from .btc import BTC
+from .eth import ETH
+from .ltc import LTC
+from .matic import MATIC
+from .sbch import SBCH
+from .trx import TRX
+from .xrg import XRG
+
+COINS = {
+    "BTC": BTC,
+    "BCH": BCH,
+    "ETH": ETH,
+    "BNB": BNB,
+    "SBCH": SBCH,
+    "LTC": LTC,
+    "MATIC": MATIC,
+    "BSTY": BSTY,
+    "TRX": TRX,
+    "XRG": XRG,
+}
+
+__all__ = list(COINS.keys()) + ["COINS"]
