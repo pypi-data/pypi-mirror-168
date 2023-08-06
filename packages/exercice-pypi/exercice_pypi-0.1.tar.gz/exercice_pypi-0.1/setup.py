2854c3e5e34a9473b9723cbcda64a5c6c9462ebb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:40 +0000

    Add package exercice-pypi/0.1/exercice_pypi-0.1.tar.gz

---
+++ packages/exercice-pypi/exercice_pypi-0.1.tar.gz/exercice_pypi-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='exercice_pypi',
+      version='0.1',
+      description='Gaussian and Binominal distributions',
+	  author= 'Karima Bouricha',
+      packages=['exercice_pypi'],
+      zip_safe=False)
