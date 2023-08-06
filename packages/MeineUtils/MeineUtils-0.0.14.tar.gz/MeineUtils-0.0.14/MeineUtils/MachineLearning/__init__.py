ecbfb48770139951c12d215c8d06659c956114a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:18 +0000

    Add package MeineUtils/0.0.14/MeineUtils-0.0.14.tar.gz

---
+++ packages/MeineUtils/MeineUtils-0.0.14.tar.gz/MeineUtils-0.0.14/MeineUtils/MachineLearning/__init__.py
@@ -0,0 +1,12 @@
+from .linear_regression import (
+    LinearRegression
+)
+
+# from .normalization import (
+#     MinMaxScaler,
+#     StandardScaler
+# )
+
+from .bounding_box import (
+    BoundingBoxConverter
+)
