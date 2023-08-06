210a8fd7a1f2365d334abbc173c0802ad8029b3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:43 +0000

    Add package arclet-alconna-avilla/0.2.0/arclet-alconna-avilla-0.2.0.tar.gz

---
+++ packages/arclet-alconna-avilla/arclet-alconna-avilla-0.2.0.tar.gz/arclet-alconna-avilla-0.2.0/arclet/alconna/avilla/__init__.py
@@ -0,0 +1,23 @@
+from arclet.alconna.graia import (
+    Alc as Alc,
+    GraiaCommandAnalyser as GraiaCommandAnalyser,
+    Match as Match,
+    Query as Query,
+    success_record as success_record,
+    AlconnaProperty as AlconnaProperty,
+    AlconnaSchema as AlconnaSchema,
+    AlconnaBehaviour as AlconnaBehaviour,
+    shortcuts as shortcuts,
+    match_path as match_path,
+    match_value as match_value,
+    from_command as from_command,
+    assign as assign,
+    MatchPrefix as MatchPrefix,
+    MatchSuffix as MatchSuffix,
+    startswith as startswith,
+    endswith as endswith
+)
+from .dispatcher import AvillaAlconnaOutputMessage
+from .utils import NoticeID, fetch_name, alcommand
+
+GraiaCommandAnalyser.filter_out = []
