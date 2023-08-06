f29f8bc85670810fcbd1085220e1ec2dc902138f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:24 +0000

    Add package imm-apps/1.3.4/imm-apps-1.3.4.tar.gz

---
+++ packages/imm-apps/imm-apps-1.3.4.tar.gz/imm-apps-1.3.4/api/schemas.py
@@ -0,0 +1,72 @@
+from pydantic import BaseModel, EmailStr
+from basemodels.user import User
+from typing import List
+from basemodels.experience.employmentcert import ExperienceModel, ExperienceModelE
+
+def user_schema(user):
+    return {
+        "_id": str(user["_id"]),
+        "username": user["username"],
+        "email": user["email"],
+        "phone": user["phone"],
+        # "password": user["password"],
+        "role": user.get("role"),
+        "permission": user.get("permission"),
+    }
+
+
+def users_schema(users):
+    return [user_schema(user) for user in users]
+
+
+class ShowUser(BaseModel):
+    _id: str
+    username: str
+    email: EmailStr
+    phone: str
+    password: str
+    role: str
+    permission: list
+
+
+class Login(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    # email: str | None = None
+    email: str
+
+
+class Permission(BaseModel):
+    user_email: EmailStr
+    permissions: List[str]
+    remove: bool
+
+
+class Role(BaseModel):
+    user_email: EmailStr
+    role: str
+
+class EmploymentCertificate(BaseModel):
+    source:dict
+    company:str
+    language:str
+    
+    @property
+    def data(self):
+        return ExperienceModel(**self.source)
+    
+class EmploymentCompany(BaseModel):
+    source:dict
+    language:str
+    
+    @property
+    def data(self):
+        return ExperienceModel(**self.source)
