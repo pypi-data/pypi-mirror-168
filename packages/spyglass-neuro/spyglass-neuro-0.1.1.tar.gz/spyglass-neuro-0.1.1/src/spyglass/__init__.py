894cfcfddc184a55fdeec4f4ef83dab4f81047c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:49 +0000

    Add package spyglass-neuro/0.1.1/spyglass-neuro-0.1.1.tar.gz

---
+++ packages/spyglass-neuro/spyglass-neuro-0.1.1.tar.gz/spyglass-neuro-0.1.1/src/spyglass/__init__.py
@@ -0,0 +1,9 @@
+# Configure datajoint
+# You can use:
+# export DJ_HOST=...
+# export DJ_USER=...
+# export DJ_PASS=...
+
+# Important to do this so that we add the franklab namespace for pynwb
+# Note: This is franklab-specific
+import ndx_franklab_novela
