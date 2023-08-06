e4916d139de32bf30b3be53abb3f4e61ac30df45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:28 +0000

    Add package donald/0.20.1/donald-0.20.1.tar.gz

---
+++ packages/donald/donald-0.20.1.tar.gz/donald-0.20.1/donald/types.py
@@ -0,0 +1,40 @@
+from numbers import Number
+from typing import Any, Awaitable, Callable, Coroutine, Dict, Literal, Optional, Tuple, TypedDict
+
+TBackendType = Literal["memory", "redis", "amqp"]
+
+TTaskParams = TypedDict(
+    "TTaskParams",
+    {
+        "bind": Optional[bool],
+        "delay": Optional[Number],
+        "timeout": Optional[Number],
+    },
+)
+
+TRunArgs = Tuple[Callable, Tuple, Dict[str, Any], TTaskParams]
+
+TWorkerParams = TypedDict(
+    "TWorkerParams",
+    {
+        "max_tasks": Optional[int],
+        "task_defaults": Optional[TTaskParams],
+        "on_start": Optional[Callable[[], Awaitable]],
+        "on_stop": Optional[Callable[[], Awaitable]],
+        "on_error": Optional[Callable[[BaseException], Coroutine]],
+        # "timeout": Number,
+        # "task_retry": int,
+        # "task_retry_delay": Number,
+    },
+)
+
+TManagerParams = TypedDict(
+    "TManagerParams",
+    {
+        "backend": TBackendType,
+        "backend_params": Dict,
+        "log_config": Optional[Dict],
+        "log_level": str,
+        "worker_params": TWorkerParams,
+    },
+)
