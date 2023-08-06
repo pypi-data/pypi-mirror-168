1cd07fd2bd6035cedd0c3619f3d27316277aa97a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:36 +0000

    Add package tipey/0.0.2/tipey-0.0.2-py3-none-any.whl

---
+++ packages/tipey/tipey-0.0.2-py3-none-any.whl/tipey/unit/currency.py
@@ -0,0 +1,176 @@
+from ..quantity import Currency
+from .base_units import u
+
+class CurrencyUnit(u):
+
+    def quantity():
+        return Currency
+
+
+class MYR(CurrencyUnit):
+    def d():
+        return 'MYR'
+
+class CYP(CurrencyUnit):
+    def d():
+        return 'CYP'
+
+class EEK(CurrencyUnit):
+    def d():
+        return 'EEK'
+
+class MTL(CurrencyUnit):
+    def d():
+        return 'MTL'
+
+class JPY(CurrencyUnit):
+    def d():
+        return 'JPY'
+
+class DKK(CurrencyUnit):
+    def d():
+        return 'DKK'
+
+class BRL(CurrencyUnit):
+    def d():
+        return 'BRL'
+
+class KRW(CurrencyUnit):
+    def d():
+        return 'KRW'
+
+class MXN(CurrencyUnit):
+    def d():
+        return 'MXN'
+
+class CAD(CurrencyUnit):
+    def d():
+        return 'CAD'
+
+class AUD(CurrencyUnit):
+    def d():
+        return 'AUD'
+
+class HRK(CurrencyUnit):
+    def d():
+        return 'HRK'
+
+class HKD(CurrencyUnit):
+    def d():
+        return 'HKD'
+
+class TRL(CurrencyUnit):
+    def d():
+        return 'TRL'
+
+class ISK(CurrencyUnit):
+    def d():
+        return 'ISK'
+
+class NZD(CurrencyUnit):
+    def d():
+        return 'NZD'
+
+class USD(CurrencyUnit):
+    def d():
+        return 'USD'
+
+class HUF(CurrencyUnit):
+    def d():
+        return 'HUF'
+
+class LVL(CurrencyUnit):
+    def d():
+        return 'LVL'
+
+class GBP(CurrencyUnit):
+    def d():
+        return 'GBP'
+
+class INR(CurrencyUnit):
+    def d():
+        return 'INR'
+
+class TRY(CurrencyUnit):
+    def d():
+        return 'TRY'
+
+class CNY(CurrencyUnit):
+    def d():
+        return 'CNY'
+
+class RUB(CurrencyUnit):
+    def d():
+        return 'RUB'
+
+class LTL(CurrencyUnit):
+    def d():
+        return 'LTL'
+
+class BGN(CurrencyUnit):
+    def d():
+        return 'BGN'
+
+class PLN(CurrencyUnit):
+    def d():
+        return 'PLN'
+
+class NOK(CurrencyUnit):
+    def d():
+        return 'NOK'
+
+class EUR(CurrencyUnit):
+    def d():
+        return 'EUR'
+
+class ZAR(CurrencyUnit):
+    def d():
+        return 'ZAR'
+
+class SIT(CurrencyUnit):
+    def d():
+        return 'SIT'
+
+class ILS(CurrencyUnit):
+    def d():
+        return 'ILS'
+
+class SGD(CurrencyUnit):
+    def d():
+        return 'SGD'
+
+class THB(CurrencyUnit):
+    def d():
+        return 'THB'
+
+class ROL(CurrencyUnit):
+    def d():
+        return 'ROL'
+
+class RON(CurrencyUnit):
+    def d():
+        return 'RON'
+
+class SEK(CurrencyUnit):
+    def d():
+        return 'SEK'
+
+class CZK(CurrencyUnit):
+    def d():
+        return 'CZK'
+
+class PHP(CurrencyUnit):
+    def d():
+        return 'PHP'
+
+class IDR(CurrencyUnit):
+    def d():
+        return 'IDR'
+
+class SKK(CurrencyUnit):
+    def d():
+        return 'SKK'
+
+class CHF(CurrencyUnit):
+    def d():
+        return 'CHF'
