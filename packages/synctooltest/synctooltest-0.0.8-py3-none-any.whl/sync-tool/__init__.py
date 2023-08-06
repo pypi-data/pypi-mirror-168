ab5a924c32ea1455cb387311ec3a4c5c7586d703
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:35 +0000

    Add package synctooltest/0.0.8/synctooltest-0.0.8-py3-none-any.whl

---
+++ packages/synctooltest/synctooltest-0.0.8-py3-none-any.whl/sync-tool/__init__.py
@@ -0,0 +1,7 @@
+from sync import getDataFromServer
+from sync import downloadFile
+from sync import handleOneDownload
+from sync import downloadPage
+from sync import initiateDownload
+from sync import getAllPages
+from sync import main
