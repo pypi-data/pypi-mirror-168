131ff02ff5dcbcf64f49c7ded8a70f7d4640bbdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:49 +0000

    Add package trafalgar-log/1.1.0/trafalgar_log-1.1.0-py3-none-any.whl

---
+++ packages/trafalgar-log/trafalgar_log-1.1.0-py3-none-any.whl/trafalgar_log/app/__init__.py
@@ -0,0 +1,23 @@
+from dynaconf import Dynaconf, Validator, ValidationError
+
+from trafalgar_log.core.exceptions import ConfigurationError
+
+DEFAULT_FIELDS_TO_SHAMBLE: list = ["password", "senha", "contraseña"]
+SETTINGS = Dynaconf(
+    envvar_prefix="TRA_LOG",
+    load_dotenv=True,
+    validators=[
+        Validator(
+            "APP_NAME",
+            "DOMAIN",
+            must_exist=True,
+        ),
+        Validator("HAKI", default="INFO"),
+        Validator("SHAMBLES", default=[]),
+    ],
+)
+
+try:
+    SETTINGS.validators.validate_all()
+except ValidationError as e:
+    raise ConfigurationError()
