7597fa0a16a46ed832d5370685d5d20d1f398618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:11 +0000

    Add package nmesh/0.1.2/nmesh-0.1.2-py3-none-any.whl

---
+++ packages/nmesh/nmesh-0.1.2-py3-none-any.whl/nmesh/tests/test_nmesh.py
@@ -0,0 +1,24 @@
+import unittest
+from nmesh import NMesh, cfg
+import numpy as np
+
+
+class TestNMeshMethods(unittest.TestCase):
+    def test_download_gdrive(self):
+        NMesh(cfg.gdrive.bull)
+
+    def test_ranges(self):
+        m = NMesh(cfg.gdrive.bull)
+        bbox = [[-0.5, -0.5, -0.5], [0.5, 0.5, 0.5]]
+        r = m.crop_bounding_box(bbox).ranges()
+        self.assertTrue(np.min(r[0]) > np.min(bbox))
+        self.assertTrue(np.max(r[1]) < np.max(bbox))
+
+    def test_components(self):
+        m = NMesh(cfg.gdrive.bull)
+        cmpts = m.components()
+        self.assertTrue(len(cmpts) >= 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
