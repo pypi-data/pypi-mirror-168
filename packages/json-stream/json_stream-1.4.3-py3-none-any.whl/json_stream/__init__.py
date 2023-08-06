3760e0f3323aefa17baffcb0dcaa1c2d80367978
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:38 +0000

    Add package json-stream/1.4.3/json_stream-1.4.3-py3-none-any.whl

---
+++ packages/json-stream/json_stream-1.4.3-py3-none-any.whl/json_stream/__init__.py
@@ -0,0 +1,3 @@
+from json_stream.loader import load
+from json_stream.visitor import visit
+from json_stream.writer import StreamableList, StreamableDict
---
+++ packages/json-stream/json_stream-1.4.3-py3-none-any.whl/json_stream/writer.py
@@ -0,0 +1,48 @@
+from collections import deque
+
+
+class Streamable:
+    def __init__(self, iterable):
+        super().__init__()
+        self._it = iter(iterable)
+        self._cache = deque()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._cache:
+            return self._cache.popleft()
+        return next(self._it)
+
+    def _peek(self):
+        try:
+            peek = next(self._it)
+        except StopIteration:
+            pass
+        else:
+            self._cache.append(peek)
+
+    def __bool__(self):
+        self._peek()
+        return bool(self._cache)
+
+
+class StreamableList(Streamable, list):
+    """
+        Class specifically designed to pass isinstance(o, list)
+        and conform to the implementation of json.dump(o)
+        for lists, except items are provided by passed in
+        generator
+    """
+
+
+class StreamableDict(Streamable, dict):
+    """
+        Class specifically designed to pass isinstance(o, dict)
+        and conform to the implementation of json.dump(o)
+        for lists, except items are provided by passed in
+        generator. Generator must produce pairs of key/value
+    """
+    def items(self):
+        return self
