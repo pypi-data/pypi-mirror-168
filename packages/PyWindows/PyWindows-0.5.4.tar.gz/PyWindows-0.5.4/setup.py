b8a26fd23e6cb066de0514126a670e81397ef8c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:17 +0000

    Add package PyWindows/0.5.4/PyWindows-0.5.4.tar.gz

---
+++ packages/PyWindows/PyWindows-0.5.4.tar.gz/PyWindows-0.5.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="PyWindows",
+    version="0.5.4",
+    description="Windows for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    author="Phoneguytech",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
