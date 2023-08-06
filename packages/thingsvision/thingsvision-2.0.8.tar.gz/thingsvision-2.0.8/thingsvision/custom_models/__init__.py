2f5e177b1d1e2ccdb32bef57a80aafef2fee9906
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:12 +0000

    Add package thingsvision/2.0.8/thingsvision-2.0.8.tar.gz

---
+++ packages/thingsvision/thingsvision-2.0.8.tar.gz/thingsvision-2.0.8/thingsvision/custom_models/__init__.py
@@ -0,0 +1,7 @@
+from .alexnet_ecoset import Alexnet_ecoset
+from .barlowtwins import BarlowTwins
+from .resnet50_ecoset import Resnet50_ecoset
+from .swav import Swav
+from .vgg16bn_ecoset import VGG16bn_ecoset
+from .openclip import OpenCLIP
+from .clip.custom_model import clip
