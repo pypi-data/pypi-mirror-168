60d0400538d37279b3d3d94913e339a06e984023
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:35 +0000

    Add package roUtilities/0.1.2/roUtilities-0.1.2-py3-none-any.whl

---
+++ packages/roUtilities/roUtilities-0.1.2-py3-none-any.whl/roUtilities/__init__.py
@@ -0,0 +1,9 @@
+from . import roUtils
+import colorama
+from colorama import Fore, Back, Style
+colorama.init()
+NAME = "roUtilities"
+AUTHOR = "rorosin"
+# print(f"{Style.BRIGHT}This code uses the {Back.BLACK + Fore.LIGHTGREEN_EX}{AUTHOR}`s"
+#       f"{Style.RESET_ALL + Style.BRIGHT} module"
+#       f"{Back.BLACK + Fore.LIGHTGREEN_EX}'{NAME}'{Style.RESET_ALL}")
