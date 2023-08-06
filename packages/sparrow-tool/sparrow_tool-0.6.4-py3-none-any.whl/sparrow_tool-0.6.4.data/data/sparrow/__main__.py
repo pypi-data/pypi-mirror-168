16a968ba3bff9c244242f2d76df20177a00a07ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:08 +0000

    Add package sparrow-tool/0.6.4/sparrow_tool-0.6.4-py3-none-any.whl

---
+++ packages/sparrow-tool/sparrow_tool-0.6.4-py3-none-any.whl/sparrow_tool-0.6.4.data/data/sparrow/__main__.py
@@ -0,0 +1,19 @@
+import fire
+from .widgets import timer
+from .multiprocess import start_server
+from .docker import save_all_images as save_docker_images
+from .docker import load_dir_images as load_docker_images
+
+func_list = [
+    timer,
+    start_server,
+    save_docker_images,
+    load_docker_images,
+]
+func_dict = {}
+for func in func_list:
+    func_dict[func.__name__] = func
+
+
+def main():
+    fire.Fire(func_dict)
