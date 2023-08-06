ea7bf47b0cfdbd8bfd9d70c96a3f30dc8c3b7cfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:36 +0000

    Add package simple-hello-new/0.0.3/simple_hello_new-0.0.3.tar.gz

---
+++ packages/simple-hello-new/simple_hello_new-0.0.3.tar.gz/simple_hello_new-0.0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='simple_hello_new',
+    version='0.0.3',
+    license='MIT',
+    author="Kek Shmek",
+    author_email='email@example.com',
+    keywords='example project',
+
+)
