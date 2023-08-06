1429e8d0373cebad9f112e3455bf61489f405a13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:01 +0000

    Add package redframes/1.0rc1/redframes-1.0rc1-py3-none-any.whl

---
+++ packages/redframes/redframes-1.0rc1-py3-none-any.whl/redframes/types.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+from typing import Any, Callable, Literal, Union
+
+import numpy as np
+import pandas as pd
+import pandas.core.groupby.generic as pg
+
+Any = Any
+Value = Any
+Values = list[Value]
+OldValue = Value
+NewValue = Value
+Column = str
+Columns = list[Column]
+LazyColumns = Union[Column, Columns]
+OldColumn = Column
+NewColumn = Column
+Direction = Literal["up", "down"]
+Func = Callable[..., Any]
+Join = Literal["left", "right", "inner", "full"]
+NumpyArray = np.ndarray
+PandasDataFrame = pd.DataFrame
+PandasGroupedFrame = pg.DataFrameGroupBy
+PandasIndex = pd.Index
+PandasRangeIndex = pd.RangeIndex
