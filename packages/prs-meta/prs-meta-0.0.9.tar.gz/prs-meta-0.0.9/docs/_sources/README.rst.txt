4ff10e7da7b86348ecf1d0ae054e6291389c6cd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:57 +0000

    Add package prs-meta/0.0.9/prs-meta-0.0.9.tar.gz

---
+++ packages/prs-meta/prs-meta-0.0.9.tar.gz/prs-meta-0.0.9/docs/_sources/README.rst.txt
@@ -0,0 +1,8 @@
+prs meta
+========
+
+**prs meta** a wrapper for pyreadstat's metadata objects
+
+.. note::
+
+   This project is under active development.
---
+++ packages/prs-meta/prs-meta-0.0.9.tar.gz/prs-meta-0.0.9/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.9'
+DESCRIPTION = 'wrapper for pyreadstat'
+LONG_DESCRIPTION = 'wrapper for pyreadstat to easily read, create, and alter .sav files'
+
+# Setting up
+setup(
+    name="prs-meta",
+    version=VERSION,
+    author="DvGils",
+    author_email="demian_vg@hotmail.nl",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['pyreadstat', 'pandas'],
+    keywords=['python', 'pyreadstat', 'SPSS', '.sav'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
