e548d7a9b87add71fe34ffbf0357d7772c8a7f47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:32 +0000

    Add package returnn/1.20220921.122326/returnn-1.20220921.122326.tar.gz

---
+++ packages/returnn/returnn-1.20220921.122326.tar.gz/returnn-1.20220921.122326/returnn/torch/engine.py
@@ -0,0 +1,86 @@
+"""
+Main engine for PyTorch
+"""
+
+from torch.utils.data import DataLoader
+
+from returnn.log import log
+from returnn.engine.base import EngineBase
+from returnn.datasets.basic import init_dataset
+from . import data_pipeline
+
+
+class Engine(EngineBase):
+  """
+  PyTorch engine
+  """
+
+  def __init__(self, config):
+    """
+    :param returnn.config.Config config:
+    """
+    super(Engine, self).__init__()
+    self.config = config
+    self.train_dataset = None
+    self.eval_datasets = {}
+    self.learning_rate = config.float("learning_rate", 1.)  # TODO LR control...
+
+  def init_train_from_config(self, config=None, train_data=None, dev_data=None, eval_data=None):
+    """
+    :param returnn.config.Config|None config:
+    :param returnn.datasets.basic.Dataset|None train_data:
+    :param returnn.datasets.basic.Dataset|None dev_data:
+    :param returnn.datasets.basic.Dataset|None eval_data:
+    """
+    assert config is self.config
+    self.train_dataset = train_data
+    self.eval_datasets.clear()
+    if dev_data:
+      self.eval_datasets["dev"] = dev_data
+    if eval_data:
+      self.eval_datasets["eval"] = eval_data
+    if config.has("eval_datasets"):
+      for dataset_name, dataset_opts in config.typed_value("eval_datasets", {}).items():
+        self.eval_datasets[dataset_name] = init_dataset(dataset_opts, default_kwargs={"name": dataset_name})
+
+  def train(self):
+    """
+    Main training loop.
+    """
+    start_epoch, _ = self.get_train_start_epoch_batch(self.config)
+    final_epoch = self.config_get_final_epoch(self.config)
+
+    print("Starting training at epoch {}.".format(start_epoch), file=log.v3)
+
+    self.epoch = start_epoch
+    while self.epoch <= final_epoch:
+      self.init_train_epoch()
+      self.train_epoch()
+
+      self.epoch += 1
+
+    print("Finished training at epoch {}.".format(self.epoch), file=log.v3)
+
+  def init_train_epoch(self):
+    """
+    init train (sub)epoch. LR etc
+    """
+    pass
+
+  def train_epoch(self):
+    """
+    train one (sub)epoch
+    """
+    print("start", self.get_epoch_str(), "with learning rate", self.learning_rate, "...", file=log.v4)
+
+    train_data = data_pipeline.DatasetWrapper(self.train_dataset, epoch=self.epoch)
+
+    batch_max_seqs = self.config.int('max_seqs', 1)  # TODO wrong default, actually -1, no limit (limit via batch_size)
+    data_loader = DataLoader(
+      train_data,
+      batch_size=batch_max_seqs,
+      collate_fn=data_pipeline.collate_batch,
+    )
+
+    for data in data_loader:
+      assert data  # TODO: only iterates through dataset so far
