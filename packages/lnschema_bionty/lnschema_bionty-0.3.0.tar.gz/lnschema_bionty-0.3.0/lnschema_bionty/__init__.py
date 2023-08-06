52aec4c5f299eda3eeb4fc4bdf08674f31d90f03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:29 +0000

    Add package lnschema_bionty/0.3.0/lnschema_bionty-0.3.0.tar.gz

---
+++ packages/lnschema_bionty/lnschema_bionty-0.3.0.tar.gz/lnschema_bionty-0.3.0/lnschema_bionty/__init__.py
@@ -0,0 +1,58 @@
+"""Schema module for universal biological entities (`zdno`).
+
+Import the package::
+
+   import lnschema_bionty
+
+Biological entities, all initialized via `Bionty <https://lamin.ai/docs/bionty>`__:
+
+.. autosummary::
+   :toctree: .
+
+   gene
+   protein
+   cell_marker
+   cell_type
+   species
+   tissue
+   disease
+   featureset
+
+Link tables:
+
+.. autosummary::
+   :toctree: .
+
+   featureset_gene
+   featureset_protein
+   featureset_cell_marker
+
+Tracking versions & migrations:
+
+.. autosummary::
+   :toctree: .
+
+   version_zdno
+   migration_zdno
+
+"""
+# This is lnschema-module zdno.
+_schema = "zdno"
+_migration = None
+__version__ = "0.3.0"
+
+from ._core import (  # noqa
+    cell_marker,
+    cell_type,
+    disease,
+    featureset,
+    featureset_cell_marker,
+    featureset_gene,
+    featureset_protein,
+    gene,
+    migration_zdno,
+    protein,
+    species,
+    tissue,
+    version_zdno,
+)
---
+++ packages/lnschema_bionty/lnschema_bionty-0.3.0.tar.gz/lnschema_bionty-0.3.0/lnschema_bionty/_core.py
@@ -0,0 +1,147 @@
+from datetime import datetime as datetime
+from typing import Optional  # noqa
+
+from sqlmodel import Field, SQLModel
+
+
+def utcnow():
+    return datetime.utcnow().replace(microsecond=0)
+
+
+class species(SQLModel, table=True):  # type: ignore
+    """Species."""
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    common_name: str = Field(default=None, index=True, unique=True)
+    taxon_id: str = Field(default=None, index=True, unique=True)
+    scientific_name: str = Field(default=None, index=True, unique=True)
+    short_name: Optional[str] = None
+
+
+class featureset_gene(SQLModel, table=True):  # type: ignore
+    """Link table between geneset and gene."""
+
+    featureset_id: Optional[int] = Field(
+        default=None, foreign_key="featureset.id", primary_key=True
+    )
+    gene_id: Optional[int] = Field(
+        default=None, foreign_key="gene.id", primary_key=True
+    )
+
+
+class featureset_protein(SQLModel, table=True):  # type: ignore
+    """Link table between proteinset and protein."""
+
+    featureset_id: Optional[int] = Field(
+        default=None, foreign_key="featureset.id", primary_key=True
+    )
+    protein_id: Optional[int] = Field(
+        default=None, foreign_key="protein.id", primary_key=True
+    )
+
+
+class featureset_cell_marker(SQLModel, table=True):  # type: ignore
+    """Link table between proteinset and protein."""
+
+    featureset_id: Optional[int] = Field(
+        default=None, foreign_key="featureset.id", primary_key=True
+    )
+    cell_marker_id: Optional[int] = Field(
+        default=None, foreign_key="cell_marker.id", primary_key=True
+    )
+
+
+class featureset(SQLModel, table=True):  # type: ignore
+    """Sets of biological features.
+
+    See the corresponding link tables.
+    """
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    feature_entity: str
+    name: str = Field(default=None, unique=True)
+
+
+class gene(SQLModel, table=True):  # type: ignore
+    """Genes."""
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    ensembl_gene_id: str = Field(default=None, index=True)
+    symbol: str = Field(default=None, index=True)
+    gene_type: Optional[str] = None
+    ncbi_gene_id: int = Field(default=None, index=True)
+    hgnc_id: Optional[str] = None
+    mgi_id: Optional[str] = None
+    omim_id: Optional[int] = None
+    synonyms: Optional[str] = None
+    species_id: int = Field(default=None, foreign_key="species.id")
+
+
+class protein(SQLModel, table=True):  # type: ignore
+    """Proteins."""
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field(default=None, index=True)
+    uniprotkb_id: str = Field(default=None, index=True)
+    uniprotkb_name: str = Field(default=None, index=True)
+    protein_names: Optional[str] = None
+    length: Optional[int] = None
+    species_id: int = Field(default=None, foreign_key="species.id")
+    gene_symbols: Optional[str] = None
+    gene_synonyms: Optional[str] = None
+    ensembl_transcript_ids: Optional[str] = None
+    ncbi_gene_ids: Optional[str] = None
+
+
+class tissue(SQLModel, table=True):  # type: ignore
+    """Tissues."""
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    ontology_id: str = Field(default=None, index=True, unique=True)
+    name: str = Field(default=None, index=True)
+
+
+class cell_type(SQLModel, table=True):  # type: ignore
+    """Cell types."""
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    ontology_id: str = Field(default=None, index=True, unique=True)
+    name: str = Field(default=None, index=True)
+
+
+class disease(SQLModel, table=True):  # type: ignore
+    """Diseases."""
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    ontology_id: str = Field(default=None, index=True, unique=True)
+    name: str = Field(default=None, index=True)
+
+
+class cell_marker(SQLModel, table=True):  # type: ignore
+    """Cell markers: protein complexes."""
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field(default=None, index=True, unique=True)
+    gene_symbols: Optional[str] = None  # TODO: link table
+    ncbi_gene_ids: Optional[str] = None  # TODO: link table
+    protein_names: Optional[str] = None  # TODO: link table
+    uniprotkb_ids: Optional[str] = None  # TODO: link table
+    species_id: int = Field(default=None, foreign_key="species.id")
+
+
+class version_zdno(SQLModel, table=True):  # type: ignore
+    """Schema versions."""
+
+    v: Optional[str] = Field(primary_key=True)
+    migration: Optional[str] = None
+    user_id: str = Field(foreign_key="user.id")
+    time_created: datetime = Field(default_factory=utcnow, nullable=False)
+
+
+class migration_zdno(SQLModel, table=True):  # type: ignore
+    """Latest migration.
+
+    This stores the reference to the latest migration script deployed.
+    """
+
+    version_num: Optional[str] = Field(primary_key=True)
