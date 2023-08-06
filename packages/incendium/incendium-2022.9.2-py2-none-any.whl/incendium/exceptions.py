0ec84e3bba955576d5e403a7d22e1d924d112b60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:01 +0000

    Add package incendium/2022.9.2/incendium-2022.9.2-py2-none-any.whl

---
+++ packages/incendium/incendium-2022.9.2-py2-none-any.whl/incendium/exceptions.py
@@ -0,0 +1,75 @@
+"""Exceptions module."""
+
+from __future__ import unicode_literals
+
+__all__ = ["ApplicationError", "TagError"]
+
+from typing import Optional
+
+from java.lang import Throwable
+
+from incendium.helper.types import InnerException, String
+
+
+class ApplicationError(Exception):
+    """Application Error class."""
+
+    cause = None  # type: Optional[Throwable]
+    inner_exception = None  # type: InnerException
+    message = None  # type: String
+
+    def __init__(
+        self,
+        message,  # type: String
+        inner_exception=None,  # type: InnerException
+        cause=None,  # type: Optional[Throwable]
+    ):
+        # type: (...) -> None
+        """Application Error initializer.
+
+        Args:
+            message: The error message.
+            inner_exception: The inner Exception. Optional.
+            cause: The cause of the Exception. Optional.
+        """
+        self.message = message
+        self.inner_exception = inner_exception
+        self.cause = cause
+        super(ApplicationError, self).__init__(message)
+
+    def __repr__(self):
+        """Compute the "official" string representation."""
+        return "{}(message={!r}, inner_exception={!r}, cause={!r})".format(
+            self.__class__.__name__,
+            self.message,
+            self.inner_exception,
+            self.cause,
+        )
+
+    def __str__(self):
+        """Compute the "informal" string representation."""
+        return "{!r}, {!r}, {!r}".format(self.message, self.inner_exception, self.cause)
+
+
+class TagError(Exception):
+    """Tag Error class."""
+
+    message = None  # type: String
+
+    def __init__(self, message):
+        # type: (String) -> None
+        """Tag Error initializer.
+
+        Args:
+            message: The error message.
+        """
+        self.message = message
+        super(TagError, self).__init__(message)
+
+    def __repr__(self):
+        """Compute the "official" string representation."""
+        return "{}(message={!r})".format(self.__class__.__name__, self.message)
+
+    def __str__(self):
+        """Compute the "informal" string representation."""
+        return repr(self.message)
---
+++ packages/incendium/incendium-2022.9.2-py2-none-any.whl/incendium/version.py
@@ -0,0 +1,3 @@
+"""Version module."""
+
+__version__ = "2022.9.2"
