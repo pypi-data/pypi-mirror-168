b14088007783012568bebded21f2ff54d340718e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:27 +0000

    Add package distributions2022/0.1/distributions2022-0.1.tar.gz

---
+++ packages/distributions2022/distributions2022-0.1.tar.gz/distributions2022-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions2022',
+      version='0.1',
+      description='Gaussian distributions and Binomial distributions',
+      packages=['distributions2022'],
+      author='deepa',
+      zip_safe=False)
