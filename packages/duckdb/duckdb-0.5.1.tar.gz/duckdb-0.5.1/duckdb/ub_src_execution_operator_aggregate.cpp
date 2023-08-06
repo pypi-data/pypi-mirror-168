1dc3184b940fc30e555a476ecee96e79b55a85de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:24 +0000

    Add package duckdb/0.5.1/duckdb-0.5.1.tar.gz

---
+++ packages/duckdb/duckdb-0.5.1.tar.gz/duckdb-0.5.1/duckdb/ub_src_execution_operator_aggregate.cpp
@@ -0,0 +1,14 @@
+#include "src/execution/operator/aggregate/aggregate_object.cpp"
+
+#include "src/execution/operator/aggregate/distinct_aggregate_data.cpp"
+
+#include "src/execution/operator/aggregate/physical_hash_aggregate.cpp"
+
+#include "src/execution/operator/aggregate/physical_perfecthash_aggregate.cpp"
+
+#include "src/execution/operator/aggregate/physical_ungrouped_aggregate.cpp"
+
+#include "src/execution/operator/aggregate/physical_window.cpp"
+
+#include "src/execution/operator/aggregate/physical_streaming_window.cpp"
+
