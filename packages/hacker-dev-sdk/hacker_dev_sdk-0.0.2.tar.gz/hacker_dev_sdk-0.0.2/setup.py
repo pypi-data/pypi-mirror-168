d3bad17689940d6baaf6015ec32cb861cc9f20f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:12 +0000

    Add package hacker-dev-sdk/0.0.2/hacker_dev_sdk-0.0.2.tar.gz

---
+++ packages/hacker-dev-sdk/hacker_dev_sdk-0.0.2.tar.gz/hacker_dev_sdk-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hacker_dev_sdk",
+    version="0.0.2",
+    author="9eek",
+    author_email="voodoos_hearty.0e@icloud.com",
+    description="A useful, general SDK for hackers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shellfeel/HackerDevSdk.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
