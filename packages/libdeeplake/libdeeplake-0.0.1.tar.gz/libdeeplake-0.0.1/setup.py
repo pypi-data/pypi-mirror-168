fe30e2cb829b0863fe217d430ca28aa109d8b69e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:36 +0000

    Add package libdeeplake/0.0.1/libdeeplake-0.0.1.tar.gz

---
+++ packages/libdeeplake/libdeeplake-0.0.1.tar.gz/libdeeplake-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="libdeeplake",
+    version="0.0.1",
+    description="libdeeplake",
+    packages=["libdeeplake"],
+    author="Activeloop",
+    author_email="support@activeloop.ai",
+    url="https://activeloop.ai",
+)
