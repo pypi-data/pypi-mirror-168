c73c291e9b07995ced8023b607d40331199f1b0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:44 +0000

    Add package vk-sdk/1.8.2/vk_sdk-1.8.2.tar.gz

---
+++ packages/vk-sdk/vk_sdk-1.8.2.tar.gz/vk_sdk-1.8.2/vk_sdk/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+:authors: SPRAVEDLIVO
+:license: GNU AFFERO GENERAL PUBLIC LICENSE Version 3, see LICENSE file
+
+:copyright: (c) 2022 SPRAVEDLIVO
+"""
+from .lp import BotLongPoll, UserLongPoll
+from .user import User
+from .keyboard import Keyboard
+from .cmd import menu, command, after_text_matcher, after_func
+
+__author__ = 'SPRAVEDLIVO'
+__version__ = '1.8.2'
+__email__ = 'admin@spravedlivo.dev'
