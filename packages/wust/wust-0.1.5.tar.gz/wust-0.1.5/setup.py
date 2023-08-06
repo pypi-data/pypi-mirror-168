38bed6adaad956a172f2050b73353f67ae328962
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:43 +0000

    Add package wust/0.1.5/wust-0.1.5.tar.gz

---
+++ packages/wust/wust-0.1.5.tar.gz/wust-0.1.5/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['wust']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['intake-thredds>=2021.6.16', 'matplotlib>=3.5.2', 'netCDF4>=1.6.0']
+
+setup_kwargs = {
+    'name': 'wust',
+    'version': '0.1.5',
+    'description': 'WEkEO User Support Toolbox contains a set of tools to ease work in WEkEO.',
+    'long_description': '',
+    'author': 'David Bazin',
+    'author_email': 'dbazin@wekeo.eu',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/wust/wust-0.1.5.tar.gz/wust-0.1.5/wust/__init__.py
@@ -0,0 +1,2 @@
+__version__ = '0.1.5'
+from .wekeo_user_support_tools import *
