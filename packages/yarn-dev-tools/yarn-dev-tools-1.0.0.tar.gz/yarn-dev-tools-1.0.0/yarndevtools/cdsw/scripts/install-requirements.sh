e055b3a9898834346c831ed35aa826bbb04cb06a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:44 +0000

    Add package yarn-dev-tools/1.0.0/yarn-dev-tools-1.0.0.tar.gz

---
+++ packages/yarn-dev-tools/yarn-dev-tools-1.0.0.tar.gz/yarn-dev-tools-1.0.0/yarndevtools/cdsw/scripts/install-requirements.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -x
+
+#echo "Uninstalling package: 'yarn-dev-tools'"
+set +e
+pip3 -V
+pip3 show yarn-dev-tools
+pip3 uninstall -y yarn-dev-tools
+pip3 uninstall -y python-commons
+pip3 uninstall -y google-api-wrapper
+
+set -e
+echo $@
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <execution mode>"
+    echo "Example: $0 cloudera --> Uses execution mode: 'cloudera'"
+    echo "Example: $0 upstream --> Uses execution mode: 'upstream'"
+    exit 1
+fi
+
+EXEC_MODE="$1"
+
+if [[ "$EXEC_MODE" == "cloudera" ]]; then
+  curl -o /tmp/requirements-cdsw.txt https://raw.githubusercontent.com/szilard-nemeth/yarn-dev-tools/master/yarndevtools/cdsw/requirements.txt
+else
+  curl -o /tmp/requirements-cdsw.txt https://raw.githubusercontent.com/szilard-nemeth/yarn-dev-tools/master/yarndevtools/cdsw/requirements-github.txt
+fi
+echo "Installing python requirements..."
+pip3 install -r /tmp/requirements-cdsw.txt --upgrade --force-reinstall
