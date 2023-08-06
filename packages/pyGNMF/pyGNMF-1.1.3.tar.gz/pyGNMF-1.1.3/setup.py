985a10d9b05d6c75f397111ad1b31a2392aa0e97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:23 +0000

    Add package pyGNMF/1.1.3/pyGNMF-1.1.3.tar.gz

---
+++ packages/pyGNMF/pyGNMF-1.1.3.tar.gz/pyGNMF-1.1.3/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyGNMF",
+    version="1.1.3",
+    author=["Nirav Lekinwala","Mani Bhushan"],
+    author_email="nirav.lekinwala@gmail.com",
+    description="Python implementation of Generalised Non-negative Matrix Factorisation with Multiplicative and Projected Gradient Approaches.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/niravl/pyGNMF",
+    project_urls={
+        "Bug Tracker": "https://github.com/niravl/pyGNMF/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires = ['numpy',
+                        'rich',
+                        'scipy'],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    py_modules = ['pyGNMF'],
+    include_package_data = True,
+    python_requires=">=3.8",
+)
