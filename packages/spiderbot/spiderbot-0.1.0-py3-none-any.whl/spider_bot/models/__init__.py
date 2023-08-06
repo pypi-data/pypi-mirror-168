01a1ca3192d681ebc419e8e621aa259e6b19d96f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:40 +0000

    Add package spiderbot/0.1.0/spiderbot-0.1.0-py3-none-any.whl

---
+++ packages/spiderbot/spiderbot-0.1.0-py3-none-any.whl/spider_bot/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""spider_bot.models"""
+from spider_bot.models.api import DBAPI
+from spider_bot.models.base import Base, BaseDB
+from spider_bot.models.posts import Post
+from spider_bot.models.posturls import PostURL
+from spider_bot.models.users import User
