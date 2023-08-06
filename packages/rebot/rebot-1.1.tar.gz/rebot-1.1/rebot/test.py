c638ed0aadb1f3033f6ee1b237fd26d17519c4c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:03 +0000

    Add package rebot/1.1/rebot-1.1.tar.gz

---
+++ packages/rebot/rebot-1.1.tar.gz/rebot-1.1/rebot/test.py
@@ -0,0 +1,9 @@
+import rebot
+from rebot import bot
+print(bot.exportLink(token="",username=""))
+print(bot.admins(token="",username=""))
+print(bot.bot(token=""))
+print(bot.count(token="",username=""))
+print(bot.leaveChat(token="",username=""))
+print(bot.user_channel(token="",channel="",id=""))
+print(bot.commands(token=""))
