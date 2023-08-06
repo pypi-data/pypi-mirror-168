78f1c88865fa41567766bd1258dfe51e91ae5077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:51 +0000

    Add package qEngineApi/0.0.3/qEngineApi-0.0.3.tar.gz

---
+++ packages/qEngineApi/qEngineApi-0.0.3.tar.gz/qEngineApi-0.0.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='qEngineApi',
+      version='0.0.3',
+      description='Connect and do something with qlik engine apu',
+      packages=['qEngineApi'],
+      author_email='daniil.semenov@internet.ru',
+      zip_safe=False)
