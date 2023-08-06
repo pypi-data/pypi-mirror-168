22f4270ec680cdbdc5238cae9f5c225302f0a829
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:31 +0000

    Add package grsdk/1.0.6/grsdk-1.0.6.tar.gz

---
+++ packages/grsdk/grsdk-1.0.6.tar.gz/grsdk-1.0.6/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+# with open("requirements.txt", "r", encoding="utf-8") as f:
+#     install_requires = [x.strip() for x in f]
+
+setup(
+    name="grsdk",
+    version="1.0.6",  
+    # packages=find_packages(where="grsdk"),
+    package_dir={'': 'grsdk'},
+    py_modules = ['grsdk'],
+    # packages=find_packages(include=['grsdk']),
+    python_requires=">=3.8, <4",
+    # install_requires=["boto3","pandas","gql[aiohttp]"],
+    # extras_require={
+    #     "dev": ["wheel","setuptools","pipdeptree"]
+    # }
+)
