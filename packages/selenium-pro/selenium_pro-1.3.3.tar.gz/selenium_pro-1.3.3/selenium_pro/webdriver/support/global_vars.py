46e52ab987639a339c4f3dae24dc87619a7fa7d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:21 +0000

    Add package selenium-pro/1.3.3/selenium_pro-1.3.3.tar.gz

---
+++ packages/selenium-pro/selenium_pro-1.3.3.tar.gz/selenium_pro-1.3.3/selenium_pro/webdriver/support/global_vars.py
@@ -0,0 +1,8 @@
+global global_driver
+global_driver=""
+def set_global_driver(dr):
+    global global_driver
+    global_driver=dr
+def get_global_driver():
+    global global_driver
+    return global_driver
