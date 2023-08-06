594176112a8f6f318d4ec0e545c39e1cb3afc407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:15 +0000

    Add package tensorflow-rocm/2.10.0.520/tensorflow_rocm-2.10.0.520-cp38-cp38-manylinux2014_x86_64.whl

---
+++ packages/tensorflow-rocm/tensorflow_rocm-2.10.0.520-cp38-cp38-manylinux2014_x86_64.whl/tensorflow/include/external/local_config_rocm/rocm/rocm/include/hipblas/hipblas-version.h
@@ -0,0 +1,18 @@
+/* ************************************************************************
+ * Copyright 2016-2021 Advanced Micro Devices, Inc.
+ * ************************************************************************ */
+
+#ifndef HIPBLAS_VERSION_H_
+#define HIPBLAS_VERSION_H_
+
+/* the configured version and settings
+ */
+// clang-format off
+#define hipblasVersionMajor 0
+#define hipblaseVersionMinor 51 // typo preserved for compatibility
+#define hipblasVersionMinor 51
+#define hipblasVersionPatch 0
+#define hipblasVersionTweak f2dc496
+// clang-format on
+
+#endif
---
+++ packages/tensorflow-rocm/tensorflow_rocm-2.10.0.520-cp38-cp38-manylinux2014_x86_64.whl/tensorflow/include/external/local_config_rocm/rocm/rocm/include/hipsolver/internal/hipsolver-version.h
@@ -0,0 +1,17 @@
+/* ************************************************************************
+ * Copyright 2020-2021 Advanced Micro Devices, Inc.
+ * ************************************************************************ */
+
+#ifndef HIPSOLVER_VERSION_H_
+#define HIPSOLVER_VERSION_H_
+
+/* the configured version and settings
+ */
+// clang-format off
+#define hipsolverVersionMajor 1
+#define hipsolverVersionMinor 4
+#define hipsolverVersionPatch 0
+#define hipsolverVersionTweak 74dafd0
+// clang-format on
+
+#endif
