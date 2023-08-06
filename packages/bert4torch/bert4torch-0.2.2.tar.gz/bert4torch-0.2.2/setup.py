48cf7de5da0bdf4147c863d067b59eee0645d6f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:51 +0000

    Add package bert4torch/0.2.2/bert4torch-0.2.2.tar.gz

---
+++ packages/bert4torch/bert4torch-0.2.2.tar.gz/bert4torch-0.2.2/setup.py
@@ -0,0 +1,15 @@
+#! -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='bert4torch',
+    version='0.2.2',
+    description='an elegant bert4torch',
+    long_description='bert4torch: https://github.com/Tongjilibo/bert4torch',
+    license='MIT Licence',
+    url='https://github.com/Tongjilibo/bert4torch',
+    author='Tongjilibo',
+    install_requires=['torch>1.6'],
+    packages=find_packages()
+)
