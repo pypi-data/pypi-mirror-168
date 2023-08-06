8359c26600ae94ca044ad6413444acd267ed9aac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:24 +0000

    Add package KoiLang/0.1.0b5/KoiLang-0.1.0b5-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/KoiLang/KoiLang-0.1.0b5-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/kola/exception.py
@@ -0,0 +1,22 @@
+from traceback import format_tb
+
+
+class KoiLangError(Exception):
+    """
+    Base exception for marktext compiler
+    """
+    __slots__ = []
+
+
+class KoiLangSyntaxError(KoiLangError):
+    """
+    Raised when failing to parse text
+    """
+    __slots__ = []
+
+
+class KoiLangCommandError(KoiLangError):
+    """
+    Raised when errors occure during executing commands
+    """
+    __slots__ = []
