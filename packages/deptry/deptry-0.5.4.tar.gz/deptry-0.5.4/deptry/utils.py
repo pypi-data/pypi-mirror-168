3d109cc7f3d481ca8110c9bd610193b1224fef27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:47 +0000

    Add package deptry/0.5.4/deptry-0.5.4.tar.gz

---
+++ packages/deptry/deptry-0.5.4.tar.gz/deptry-0.5.4/deptry/utils.py
@@ -0,0 +1,54 @@
+import os
+import sys
+import types
+from contextlib import contextmanager
+from pathlib import Path
+from typing import Dict, Tuple
+
+import toml
+
+
+@contextmanager
+def run_within_dir(path: Path) -> None:
+    """
+    Utility function to run some code within a directory, and change back to the current directory afterwards.
+
+    Example usage:
+
+    ```
+    with run_within_dir(directory):
+        some_code()
+    ```
+
+    """
+    oldpwd = os.getcwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(oldpwd)
+
+
+def import_importlib_metadata() -> Tuple[types.ModuleType, Exception]:
+    """
+    importlib.metadata is in the standard library since Python version 3.8
+    """
+    if sys.version_info[1] == 7:
+        import importlib_metadata as metadata
+        from importlib_metadata import PackageNotFoundError
+
+        return metadata, PackageNotFoundError
+    else:
+        import importlib.metadata as metadata
+        from importlib.metadata import PackageNotFoundError
+
+        return metadata, PackageNotFoundError
+
+
+def load_pyproject_toml() -> Dict:
+    try:
+        pyproject_text = Path("./pyproject.toml").read_text()
+        pyproject_data = toml.loads(pyproject_text)
+        return pyproject_data
+    except FileNotFoundError:
+        raise FileNotFoundError(f"No file `pyproject.toml` found in directory {os.getcwd()}")
