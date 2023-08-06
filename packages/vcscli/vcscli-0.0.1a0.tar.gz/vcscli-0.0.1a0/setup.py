338077b7e95e1bd50045f295bd2e1995c277d278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:22 +0000

    Add package vcscli/0.0.1a0/vcscli-0.0.1a0.tar.gz

---
+++ packages/vcscli/vcscli-0.0.1a0.tar.gz/vcscli-0.0.1a0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['vcscli']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'vcscli',
+    'version': '0.0.1a0',
+    'description': '',
+    'long_description': '',
+    'author': 'Tony Narlock',
+    'author_email': 'tony@git-pull.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
