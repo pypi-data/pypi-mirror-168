a4fb5649bce62f022b35769fa7e8179125b46738
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:17 +0000

    Add package moat-mqtt/0.38.2/moat-mqtt-0.38.2.tar.gz

---
+++ packages/moat-mqtt/moat-mqtt-0.38.2.tar.gz/moat-mqtt-0.38.2/tests/test_tester.py
@@ -0,0 +1,22 @@
+import pytest
+import trio  # noqa: F401
+from moat.util import P
+
+from moat.mqtt.test import test_client, test_server
+
+
+@pytest.mark.trio
+async def test_basic(autojump_clock):  # pylint: disable=unused-argument
+    async with test_client() as c:
+        await c.set(P("test.a.b.c"), value=123)
+        res = await c.get(P("test.a.b.c"))
+        assert res.value == 123
+
+
+@pytest.mark.trio
+async def test_split(autojump_clock):  # pylint: disable=unused-argument
+    async with test_server() as s:
+        async with s.test_client() as c1, s.test_client() as c2:
+            await c1.set(P("test.a.b.c"), value=123)
+            res = await c2.get(P("test.a.b.c"))
+            assert res.value == 123
