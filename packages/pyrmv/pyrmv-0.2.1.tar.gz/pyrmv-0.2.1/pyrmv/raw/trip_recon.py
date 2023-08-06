244888952e270b50c756f6624d011bf828f1a6cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:33 +0000

    Add package pyrmv/0.2.1/pyrmv-0.2.1.tar.gz

---
+++ packages/pyrmv/pyrmv-0.2.1.tar.gz/pyrmv-0.2.1/pyrmv/raw/trip_recon.py
@@ -0,0 +1,36 @@
+from requests import get
+from typing import List, Union
+from xmltodict import parse as xmlparse
+
+from pyrmv.utility.find_exception import find_exception
+
+try:
+    from typing import Literal
+except ImportError:
+    from typing_extensions import Literal
+
+# 2.17. Reconstruction (recon)
+def trip_recon(accessId: str,
+        json: bool = True
+    ) -> dict:
+
+    if json:
+        headers = {"Accept": "application/json"}
+    else:
+        headers = {"Accept": "application/xml"}
+
+    payload = {}
+
+    for var, val in locals().items():
+        if str(var) not in ["json", "headers", "payload"]:
+            if val != None:
+                payload[str(var)] = val
+
+    output = get("https://www.rmv.de/hapi/recon", params=payload, headers=headers)
+
+    find_exception(output.json())
+
+    if json:
+        return output.json()
+    else:
+        return xmlparse(output.content)
