f74bbb79d00bd6a8bcd9873c37e2616dbd4d3a79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:18 +0000

    Add package shellodoro/1.0.0/shellodoro-1.0.0.tar.gz

---
+++ packages/shellodoro/shellodoro-1.0.0.tar.gz/shellodoro-1.0.0/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="UTF-8") as f:
+    long_description = f.read()
+
+with open("requirements.txt", "r", encoding="UTF-8") as f:
+    requirements = f.read().splitlines()
+
+
+setuptools.setup(
+    name="shellodoro",
+    version="1.0.0",
+    author="StepanZubkov",
+    author_email="zubkovbackend@gmail.com",
+    description="Pomodoro timer right in your favorite terminal!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/StepanZubkov/shellodoro",
+    keywords="pomodoro, timer, tool, cli",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.6",
+    entry_points={"console_scripts": ["shdr=shellodoro.shellodoro:main"]},
+    install_requires=requirements,
+)
