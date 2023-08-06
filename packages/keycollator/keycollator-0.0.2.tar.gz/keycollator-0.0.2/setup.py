20f0d2e7e99bb769c9c69d6f425b63a5c316687b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:33 +0000

    Add package keycollator/0.0.2/keycollator-0.0.2.tar.gz

---
+++ packages/keycollator/keycollator-0.0.2.tar.gz/keycollator-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='keycollator',
+    version='0.0.2',
+    long_description=long_description,
+    py_modules=[
+        'cli',
+        'extractonator'
+    ],
+    install_requires=[
+        'click',
+        'progressbar',
+        'verboselogs',
+        'nltk',
+        'fuzzywuzzy',
+        'python-Levenshtein',
+        'halo'
+    ],
+    entry_points='''
+        [console_scripts]
+        keycollator=keycollator:cli
+    '''
+)
