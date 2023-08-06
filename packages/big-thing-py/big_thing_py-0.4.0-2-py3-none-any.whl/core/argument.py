a3d0528ee96d3b9e2a8cdf628caff93d1c12cd88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:40 +0000

    Add package big-thing-py/0.4.0/big_thing_py-0.4.0-2-py3-none-any.whl

---
+++ packages/big-thing-py/big_thing_py-0.4.0-2-py3-none-any.whl/core/argument.py
@@ -0,0 +1,24 @@
+from utils import *
+from common import *
+
+
+class SoPArgument:
+
+    def __init__(self, name: str = None, type: SoPType = None, bound: Tuple[float, float] = (None, None)):
+        self._name: str = name
+        self._type: SoPType = type
+        self._min: float = bound[0]
+        self._max: float = bound[1]
+
+    def __eq__(self, o: object) -> bool:
+        return isinstance(o, SoPArgument) and o._name == self._name
+
+    def dump(self) -> Dict:
+        return {
+            "type": self._type.value,
+            "name": self._name,
+            "bound": {
+                "min_value": self._min,
+                "max_value": self._max
+            }
+        }
