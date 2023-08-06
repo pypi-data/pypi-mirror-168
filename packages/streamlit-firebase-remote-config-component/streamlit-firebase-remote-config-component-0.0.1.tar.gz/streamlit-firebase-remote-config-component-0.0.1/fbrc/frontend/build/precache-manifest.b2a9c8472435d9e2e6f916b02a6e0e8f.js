af455f60b2801f6238a4594f232ad4c1c6f596a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:34 +0000

    Add package streamlit-firebase-remote-config-component/0.0.1/streamlit-firebase-remote-config-component-0.0.1.tar.gz

---
+++ packages/streamlit-firebase-remote-config-component/streamlit-firebase-remote-config-component-0.0.1.tar.gz/streamlit-firebase-remote-config-component-0.0.1/fbrc/frontend/build/precache-manifest.b2a9c8472435d9e2e6f916b02a6e0e8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca1de84849797f8694b1f01e44406387",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7e43bdc7e30bc55dcfcd",
+    "url": "./static/js/2.8a7be6ee.chunk.js"
+  },
+  {
+    "revision": "2d41bb3b31cddceca83553974cf4dbb9",
+    "url": "./static/js/2.8a7be6ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47a12b2be2ce1cdc2d2e",
+    "url": "./static/js/main.0e51aef3.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
