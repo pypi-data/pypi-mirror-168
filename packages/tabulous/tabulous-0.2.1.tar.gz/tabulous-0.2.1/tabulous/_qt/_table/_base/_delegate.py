d6345d61d488ea69e84820d81172b5cb56abc3bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:09 +0000

    Add package tabulous/0.2.1/tabulous-0.2.1.tar.gz

---
+++ packages/tabulous/tabulous-0.2.1.tar.gz/tabulous-0.2.1/tabulous/_qt/_table/_base/_delegate.py
@@ -0,0 +1,128 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING, cast
+from qtpy import QtWidgets as QtW, QtCore, QtGui
+from qtpy.QtCore import Qt
+
+from ._table_base import QBaseTable
+from ._line_edit import QCellLineEdit
+
+if TYPE_CHECKING:
+    import numpy as np
+    import pandas as pd
+    from pandas.core.dtypes.dtypes import CategoricalDtype
+    from ._enhanced_table import _QTableViewEnhanced
+    from ._item_model import AbstractDataFrameModel
+
+
+class TableItemDelegate(QtW.QStyledItemDelegate):
+    """Displays table widget items with properly formatted numbers."""
+
+    def createEditor(
+        self, parent: QtW.QWidget, option, index: QtCore.QModelIndex
+    ) -> QtW.QWidget:
+        """Create different type of editors for different dtypes."""
+        qtable_view: _QTableViewEnhanced = parent.parent()
+        table: QBaseTable = qtable_view.parentTable()
+        if qtable_view.model()._editable:
+            model = qtable_view.model()
+            df = model.df
+            row = index.row()
+            col = index.column()
+            font = QtGui.QFont(
+                qtable_view._font, int(qtable_view._font_size * qtable_view.zoom())
+            )
+            if row >= df.shape[0] or col >= df.shape[1]:
+                # out-of-bounds
+                line = QCellLineEdit(parent, table, (row, col))
+                line.setFont(font)
+                return line
+
+            dtype: np.dtype = df.dtypes.values[col]
+            value = df.iat[row, col]
+            if dtype == "category":
+                # use combobox for categorical data
+                dtype: CategoricalDtype
+                cbox = QtW.QComboBox(parent)
+                cbox.setFont(font)
+                choices = list(map(str, dtype.categories))
+                cbox.addItems(choices)
+                cbox.setCurrentIndex(choices.index(value))
+                cbox.currentIndexChanged.connect(qtable_view.setFocus)
+                return cbox
+            elif dtype == "bool":
+                # use checkbox for boolean data
+                cbox = QtW.QComboBox(parent)
+                cbox.setFont(font)
+                choices = ["True", "False"]
+                cbox.addItems(choices)
+                cbox.setCurrentIndex(0 if value else 1)
+                cbox.currentIndexChanged.connect(qtable_view.setFocus)
+                return cbox
+            elif dtype.kind == "M":
+                dt = QtW.QDateTimeEdit(parent)
+                dt.setFont(font)
+                value: pd.Timestamp
+                dt.setDateTime(value.to_pydatetime())
+                return dt
+            else:
+                line = QCellLineEdit(parent, table, (row, col))
+                line.setText(str(value))
+                line.setFont(font)
+                return line
+
+    def setEditorData(self, editor: QtW.QWidget, index: QtCore.QModelIndex) -> None:
+        super().setEditorData(editor, index)
+        if isinstance(editor, QtW.QComboBox):
+            editor = cast(QtW.QComboBox, editor)
+            editor.showPopup()
+        return None
+
+    def setModelData(
+        self,
+        editor: QtW.QWidget,
+        model: AbstractDataFrameModel,
+        index: QtCore.QModelIndex,
+    ) -> None:
+        if isinstance(editor, QtW.QDateTimeEdit):
+            editor = cast(QtW.QDateTimeEdit, editor)
+            dt = editor.dateTime().toPyDateTime()
+            model.setData(index, dt, Qt.ItemDataRole.EditRole)
+        else:
+            return super().setModelData(editor, model, index)
+
+    def paint(
+        self,
+        painter: QtGui.QPainter,
+        option: QtW.QStyleOptionViewItem,
+        index: QtCore.QModelIndex,
+    ):
+        option.textElideMode = Qt.TextElideMode.ElideNone
+        return super().paint(painter, option, index)
+
+    def initStyleOption(
+        self, option: QtW.QStyleOptionViewItem, index: QtCore.QModelIndex
+    ):
+        super().initStyleOption(option, index)
+        if option.state & QtW.QStyle.StateFlag.State_HasFocus:
+            option.state = option.state & ~QtW.QStyle.StateFlag.State_HasFocus
+
+
+# TODO: add timedelta-specific editor
+
+# class QTimeRangeEdit(QtW.QAbstractSpinBox):
+#     def __init__(self, val: pd.Timedelta, parent: QtW.QWidget | None = None) -> None:
+#         super().__init__(parent)
+#         self._value = val
+#         self.setLineEdit
+
+#     def text(self):
+#         return str(self._value)
+
+#     def setValue(self, val: pd.Timedelta):
+#         self._value = str(val)
+
+# class QTimerangeLineEdit(QtW.QLineEdit):
+#     def __init__(self, val: pd.Timedelta, parent: QtW.QWidget | None = None) -> None:
+#         super().__init__(parent)
+#         self._value = val
+#         self.setText(str(val))
---
+++ packages/tabulous/tabulous-0.2.1.tar.gz/tabulous-0.2.1/tabulous/_qt/_toolbar/_dialogs.py
@@ -0,0 +1,172 @@
+from typing import List
+from typing_extensions import Annotated
+import numpy as np
+import pandas as pd
+from ..._magicgui import dialog_factory, Axes
+from ...types import TableData
+
+
+@dialog_factory
+def summarize_table(df: TableData, methods: List[str], new_table: bool = False):
+    return df.agg(methods), new_table
+
+
+@dialog_factory
+def groupby(df: TableData, by: List[str]):
+    return df.groupby(by)
+
+
+@dialog_factory
+def concat(
+    viewer, names: List[str], axis: int, ignore_index: bool = False
+) -> TableData:
+    dfs = [viewer.tables[name].data for name in names]
+    return pd.concat(dfs, axis=axis, ignore_index=ignore_index)
+
+
+@dialog_factory
+def pivot(df: TableData, index: str, columns: str, values: str) -> TableData:
+    return df.pivot(index=index, columns=columns, values=values)
+
+
+@dialog_factory
+def melt(df: TableData, id_vars: List[str]) -> TableData:
+    return pd.melt(df, id_vars)
+
+
+@dialog_factory
+def sort(df: TableData, by: List[str], ascending: bool = True) -> TableData:
+    return df.sort_values(by=by, ascending=ascending)
+
+
+@dialog_factory
+def plot(ax: Axes, x: str, y: List[str], data, alpha: float = 1.0):
+    for ylabel in y:
+        ydata: pd.Series = data[ylabel]
+        if x is None:
+            xdata = np.arange(len(ydata))
+        else:
+            xdata = data[x]
+        ax.plot(xdata, ydata, alpha=alpha, label=ylabel, picker=True)
+    return True
+
+
+@dialog_factory
+def scatter(ax: Axes, x: str, y: List[str], data, alpha: float = 1.0):
+    for ylabel in y:
+        ydata: pd.Series = data[ylabel]
+        if x is None:
+            xdata = np.arange(len(ydata))
+        else:
+            xdata = data[x]
+        ax.scatter(xdata, ydata, alpha=alpha, label=ylabel, picker=True)
+    return True
+
+
+@dialog_factory
+def errorbar(ax: Axes, x: str, y: str, yerr: str, data, alpha: float = 1.0):
+    if x is None:
+        xdata = np.arange(len(y))
+    else:
+        xdata = data[x]
+    ax.errorbar(
+        xdata, data[y], yerr=data[yerr], alpha=alpha, fmt="o", label=y, picker=True
+    )
+    return True
+
+
+@dialog_factory
+def hist(
+    ax: Axes,
+    y,
+    data,
+    bins: int = 10,
+    alpha: float = 1.0,
+    density: bool = False,
+    histtype: str = "bar",
+):
+    for _y in y:
+        ydata = data[_y]
+        ax.hist(
+            ydata,
+            bins=bins,
+            alpha=alpha,
+            density=density,
+            label=_y,
+            histtype=histtype,
+            picker=True,
+        )
+    ax.axhline(0, color="gray", lw=0.5, alpha=0.5, zorder=-1)
+    return True
+
+
+@dialog_factory
+def swarmplot(
+    ax: Axes,
+    x: str,
+    y: str,
+    data,
+    hue: str = None,
+    dodge: bool = False,
+    alpha: Annotated[float, {"min": 0.0, "max": 1.0}] = 1.0,
+):
+    import seaborn as sns
+
+    sns.swarmplot(
+        x=x, y=y, data=data, hue=hue, dodge=dodge, alpha=alpha, ax=ax, picker=True
+    )
+    return True
+
+
+@dialog_factory
+def barplot(
+    ax: Axes,
+    x: str,
+    y: str,
+    data,
+    hue: str = None,
+    dodge: bool = False,
+    alpha: Annotated[float, {"min": 0.0, "max": 1.0}] = 1.0,
+):
+    import seaborn as sns
+
+    sns.barplot(
+        x=x, y=y, data=data, hue=hue, dodge=dodge, alpha=alpha, ax=ax, picker=True
+    )
+    ax.axhline(0, color="gray", lw=0.5, alpha=0.5, zorder=-1)
+    return True
+
+
+@dialog_factory
+def boxplot(
+    ax: Axes,
+    x: str,
+    y: str,
+    data,
+    hue: str = None,
+    dodge: bool = False,
+):
+    import seaborn as sns
+
+    sns.boxplot(x=x, y=y, data=data, hue=hue, dodge=dodge, ax=ax)
+    return True
+
+
+@dialog_factory
+def boxenplot(
+    ax: Axes,
+    x: str,
+    y: str,
+    data,
+    hue: str = None,
+    dodge: bool = False,
+):
+    import seaborn as sns
+
+    sns.boxenplot(x=x, y=y, data=data, hue=hue, dodge=dodge, ax=ax, picker=True)
+    return True
+
+
+@dialog_factory
+def choose_one(choice: str):
+    return choice
