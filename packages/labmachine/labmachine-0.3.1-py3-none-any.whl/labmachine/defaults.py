6f49733f5db600d434954670f9f1744842a22eca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:13 +0000

    Add package labmachine/0.3.1/labmachine-0.3.1-py3-none-any.whl

---
+++ packages/labmachine/labmachine-0.3.1-py3-none-any.whl/labmachine/defaults.py
@@ -0,0 +1,4 @@
+NANO_ID_ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz-"
+NANO_URLSAFE_ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyz-_"
+NANO_MACHINE_ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyz"
+JUPCTL_CONF = "jupctl.conf"
