e8cb0474501fa0b493e2dd05d8e275629f76bb66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:31 +0000

    Add package django-google-keys/0.0.3/django-google-keys-0.0.3.tar.gz

---
+++ packages/django-google-keys/django-google-keys-0.0.3.tar.gz/django-google-keys-0.0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "r") as arq:
+    readme = arq.read()
+
+setup(name='django-google-keys',
+    version='0.0.3',
+    license='MIT License',
+    author='issei momonge',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='mggyggf@gmail.com',
+    keywords='django key',
+    description=u'Não é oficial do google',
+    packages=['django_key'],
+    install_requires=['yake','bs4'],)
