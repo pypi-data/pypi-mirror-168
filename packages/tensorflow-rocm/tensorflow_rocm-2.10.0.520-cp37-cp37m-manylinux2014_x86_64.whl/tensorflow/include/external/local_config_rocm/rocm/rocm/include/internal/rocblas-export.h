d5059c41339ba7d184da7920d7b4bc510f7e8c2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:55 +0000

    Add package tensorflow-rocm/2.10.0.520/tensorflow_rocm-2.10.0.520-cp37-cp37m-manylinux2014_x86_64.whl

---
+++ packages/tensorflow-rocm/tensorflow_rocm-2.10.0.520-cp37-cp37m-manylinux2014_x86_64.whl/tensorflow/include/external/local_config_rocm/rocm/rocm/include/internal/rocblas-export.h
@@ -0,0 +1,50 @@
+/*
+    Copyright (c) 2022 Advanced Micro Devices, Inc. All rights reserved.
+*/
+
+#ifndef ROCM_SYMLINK_INTERNAL_ROCBLAS_EXPORT_H
+#define ROCM_SYMLINK_INTERNAL_ROCBLAS_EXPORT_H
+
+#if defined(ROCM_NO_WRAPPER_HEADER_WARNING) || defined(ROCM_SYMLINK_GAVE_WARNING)
+/* include file */
+#include "../rocblas/internal/rocblas-export.h"
+#else
+/* give warning */
+#if defined(_MSC_VER)
+#pragma message(": warning:This file is deprecated. Use the header file from /opt/rocm-5.2.0/include/rocblas/internal/rocblas-export.h by using #include <rocblas/internal/rocblas-export.h>")
+#elif defined(__GNUC__)
+#pragma message(": warning : This file is deprecated. Use the header file from /opt/rocm-5.2.0/include/rocblas/internal/rocblas-export.h by using #include <rocblas/internal/rocblas-export.h>")
+#endif
+/* include file */
+#define ROCM_SYMLINK_GAVE_WARNING
+#include "../rocblas/internal/rocblas-export.h"
+#undef ROCM_SYMLINK_GAVE_WARNING
+#endif /* defined(ROCM_NO_WRAPPER_HEADER_WARNING) || defined(ROCM_SYMLINK_GAVE_WARNING) */
+
+#endif /* ROCM_SYMLINK_INTERNAL_ROCBLAS_EXPORT_H */
+
+#if 0
+
+/* The following is a copy of the original file for the benefit of build systems which grep for values
+ * in this file rather than preprocess it. This is just for backward compatibility */
+
+/* ************************************************************************
+ * Copyright 2016-2022 Advanced Micro Devices, Inc.
+ * ************************************************************************ */
+
+/* the configured version and settings
+ */
+#ifndef ROCBLAS_VERSION_H_
+#define ROCBLAS_VERSION_H_
+
+// clang-format off
+#define ROCBLAS_VERSION_MAJOR       2
+#define ROCBLAS_VERSION_MINOR       44
+#define ROCBLAS_VERSION_PATCH       0
+#define ROCBLAS_VERSION_TWEAK       4a92c6f1-dirty
+#define ROCBLAS_VERSION_COMMIT_ID   
+// clang-format on
+
+#endif
+
+#endif
