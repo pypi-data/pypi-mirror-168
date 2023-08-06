20a7f6cb213651f4addec0c866d28d078eecd59e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:50 +0000

    Add package cs-sender/1.0.0/cs_sender-1.0.0.tar.gz

---
+++ packages/cs-sender/cs_sender-1.0.0.tar.gz/cs_sender-1.0.0/setup.py
@@ -0,0 +1,45 @@
+"""
+@Description: 
+@Usage: 
+@Author: liuxianglong
+@Date: 2021/8/21 下午6:44
+"""
+# !/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='cs_sender',
+    version='V1.0.0',
+    description=(
+        'crawler studio api'
+    ),
+    long_description=open('README.md').read(),
+    author='liuxianglong',
+    author_email='liu_xianglong@live.com',
+    maintainer='liuxianglong',
+    maintainer_email='liu_xianglong@live.com',
+    license='BSD License',
+    packages=['cs_sender'],
+    py_modules=["cs_sender.__init__"],
+    platforms=["all"],
+    # url='https://github.com/xlomg/scrapy_box.git',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries'
+    ],
+    install_requires=[
+        'scrapy', 'requests'
+    ]
+)
