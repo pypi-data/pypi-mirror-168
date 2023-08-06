f37700b071b5f806acf8df062c873aded83f73ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:16 +0000

    Add package blades/0.1.1/blades-0.1.1-py3-none-any.whl

---
+++ packages/blades/blades-0.1.1-py3-none-any.whl/blades/aggregators/__init__.py
@@ -0,0 +1,18 @@
+from .autogm import Autogm
+from .clippedclustering import Clippedclustering
+from .clustering import Clustering
+from .geomed import Geomed
+from .multikrum import Multikrum
+from .mean import Mean
+from .median import Median
+from .trimmedmean import Trimmedmean
+
+__all__ = ['Multikrum',
+           'Median',
+           'Geomed',
+           'Autogm',
+           'Mean',
+           'Clustering',
+           'Trimmedmean',
+           'Clippedclustering',
+           ]
