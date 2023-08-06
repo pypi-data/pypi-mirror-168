1aa60a8207b57b9be592f0bdcc7818b27b9bea16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:39 +0000

    Add package upbge-stubs/0.3.1.27.dev3087248845/upbge-stubs-0.3.1.27.dev3087248845.tar.gz

---
+++ packages/upbge-stubs/upbge-stubs-0.3.1.27.dev3087248845.tar.gz/upbge-stubs-0.3.1.27.dev3087248845/property_flag_items/__init__.pyi
@@ -0,0 +1,29 @@
+"""
+
+
+Property Flag Items
+^^^^^^^^^^^^^^^^^^^
+
+:HIDDEN:          
+  Hidden.
+
+:SKIP_SAVE:       
+  Skip Save.
+
+:ANIMATABLE:      
+  Animatable.
+
+:LIBRARY_EDITABLE:
+  Library Editable.
+
+:PROPORTIONAL:    
+  Adjust values proportionally to each other.
+
+:TEXTEDIT_UPDATE: 
+  Update on every keystroke in textedit 'mode'.
+
+.. _rna-enum-property-flag-items:
+
+"""
+
+import typing
