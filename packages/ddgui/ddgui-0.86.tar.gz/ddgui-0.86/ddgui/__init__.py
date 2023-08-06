59ef3801c9d94d8728c2954565d65bb7e36be0ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:46 +0000

    Add package ddgui/0.86/ddgui-0.86.tar.gz

---
+++ packages/ddgui/ddgui-0.86.tar.gz/ddgui-0.86/ddgui/__init__.py
@@ -0,0 +1,82 @@
+import sys
+import time
+import itertools # for the zip()
+
+from PyQt5.QtWidgets import QDialog, QApplication, QWidget
+from PyQt5.uic import loadUi
+from PyQt5.QtCore import Qt
+from PyQt5.QtCore import QThreadPool, QRunnable, QThread
+
+from PyQt5 import QtWidgets, uic
+import sys
+
+def tambah_angka(a,b):
+    return a+b
+
+def kali_angka(a,b):
+    return a*b
+
+def bagi_angka(a,b):
+    return a/b
+
+def kurang_angka(a,b):
+    return a-b
+
+def ddprint(a):
+    return a
+
+#input("DD GUI Library has been installed.")
+
+"""
+# First Method
+
+class Ui(QDialog):
+    def __init__(self):
+        super(Ui, self).__init__()
+        uic.loadUi('ddgui.ui', self)
+
+        #self.button = self.findChild(QtWidgets.QPushButton, 'printButton') # Find the button
+        #self.button.clicked.connect(self.printButtonPressed) # Remember to pass the definition/method, not the return value!
+
+        #self.input = self.findChild(QtWidgets.QLineEdit, 'input')
+
+        self.show()
+
+    #def printButtonPressed(self):
+    #    # This is executed when the button is pressed
+    #    print('Input text:' + self.input.text())
+
+app = QtWidgets.QApplication(sys.argv)
+window = Ui()
+app.exec_()
+
+"""
+
+
+#Second Method
+#app = QApplication(sys.argv)
+#window = QWidget()
+#window.show()
+#sys.exit(app.exec())
+
+
+# Third Method
+
+class MyForm(QDialog):
+
+    def __init__(self):
+        super().__init__()
+        self.ui = loadUi("ddgui.ui", self)
+        self.ui.pushButtonExit.clicked.connect(self.exitMethod)
+        self.show()
+
+    def exitMethod(self):
+        QApplication.instance().quit()
+
+if __name__ == "__main__":
+    a = QApplication(sys.argv)
+    form = MyForm()
+    form.show()
+    a.exec_()
+
+
