7e16bcb02711b19386dd4ed020fe232ce3b186e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:28 +0000

    Add package predictions/1.0.2/predictions-1.0.2-py3-none-any.whl

---
+++ packages/predictions/predictions-1.0.2-py3-none-any.whl/predictions/__init__.py
@@ -0,0 +1,4 @@
+from predictions.crash import crashpredictor
+from predictions.towers import towerspredictor
+from predictions.mines import minespredictor
+from predictions.roulette import roulettepredictor
---
+++ packages/predictions/predictions-1.0.2-py3-none-any.whl/predictions/roulette.py
@@ -0,0 +1,9 @@
+import random
+
+
+
+
+def roulettepredictor():
+  mylist = [":red_square:", ":purple_square:"]
+  amo = random.choice(mylist)
+  return amo
