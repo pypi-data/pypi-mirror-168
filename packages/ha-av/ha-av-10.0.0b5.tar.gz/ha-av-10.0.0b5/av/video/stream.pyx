4dfc5e306b99b4ee3ca2622f18ef95195c63d529
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:50 +0000

    Add package ha-av/10.0.0b5/ha-av-10.0.0b5.tar.gz

---
+++ packages/ha-av/ha-av-10.0.0b5.tar.gz/ha-av-10.0.0b5/av/video/stream.pyx
@@ -0,0 +1,32 @@
+from av.utils cimport avrational_to_fraction
+
+
+cdef class VideoStream(Stream):
+
+    def __repr__(self):
+        return '<av.%s #%d %s, %s %dx%d at 0x%x>' % (
+            self.__class__.__name__,
+            self.index,
+            self.name,
+            self.format.name if self.format else None,
+            self.codec_context.width,
+            self.codec_context.height,
+            id(self),
+        )
+
+    property rate:
+        """
+        Returns the average_frame_rate of the stream.
+        :type: :class:`~fractions.Fraction` or ``None``
+        """
+        def __get__(self):
+            return avrational_to_fraction(&self.ptr.avg_frame_rate)
+
+    property framerate:
+        """
+        Returns the average_frame_rate of the stream.
+        :type: :class:`~fractions.Fraction` or ``None``
+        """
+        def __get__(self):
+            return avrational_to_fraction(&self.ptr.avg_frame_rate)
+    
