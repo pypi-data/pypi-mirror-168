59af2efbcb900e12abfbc23805d84d71f7935770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:23 +0000

    Add package aga/0.10.0/aga-0.10.0-py3-none-any.whl

---
+++ packages/aga/aga-0.10.0-py3-none-any.whl/aga/__init__.py
@@ -0,0 +1,12 @@
+"""Aga grades assignments: a library for easily producing autograders for code.
+
+Anything not explicitly documented here should not be used directly by clients and is
+only exposed for testing, the CLI, and type hinting.
+"""
+
+from importlib.metadata import version
+
+from .core import group, problem, test_case, test_cases
+
+__all__ = ("problem", "test_case", "test_cases", "group")
+__version__ = version(__name__)  # type: ignore
