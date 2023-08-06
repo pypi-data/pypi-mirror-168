9bf22e906e04e809cb07e86256fd9c718bda3847
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:50 +0000

    Add package nameCreator/0.8.0/nameCreator-0.8.0-py3-none-any.whl

---
+++ packages/nameCreator/nameCreator-0.8.0-py3-none-any.whl/nameCreator/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+Generates male or female names, from a specified country. Languages are "american", "russian", "spanish", "italian", "german","french","swedish".
+
+"""
+
+__version__ = "0.8.0"
+__author__ = 'Justin Huddleston'
+
+from nameCreator.F import firstF
+from nameCreator.M import firstM
+from nameCreator.L import last
