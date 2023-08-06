205c58a81c435b0b4acb97f3561511e54f5a6749
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:43 +0000

    Add package shadowlegion/0.0.3/shadowlegion-0.0.3-py3-none-any.whl

---
+++ packages/shadowlegion/shadowlegion-0.0.3-py3-none-any.whl/shadowlegion/utils.py
@@ -0,0 +1,15 @@
+from unencryptedsocket import SC
+
+
+class Client:
+    def __init__(self, **sc_kwargs):
+        self.sc = lambda: SC(**sc_kwargs)
+
+    def sws_info(self):
+        return self.sc().request(command="sws_info")
+
+    def sws_restart(self, i):
+        return self.sc().request(command="sws_restart", data=((i,), {}))
+
+
+
