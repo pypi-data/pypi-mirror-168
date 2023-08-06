b4cb373d44e15d289a1163b07175339a8754e4e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:45 +0000

    Add package metpx-sr3/3.0.17/metpx_sr3-3.0.17-py3-none-any.whl

---
+++ packages/metpx-sr3/metpx_sr3-3.0.17-py3-none-any.whl/sarracenia/integrity/random.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import random
+
+from sarracenia.integrity import Integrity
+
+
+class Random(Integrity):
+    """
+      Trivial minimalist checksumming algorithm, returns random number for any file.
+      """
+    @property
+    def value(self):
+        return '%.4d' % random.randint(0, 9999)
+
+    @staticmethod
+    def registered_as():
+        return '0'
