182c5cf8cbcfc0000bb915f82e637bdec209d2c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:43 +0000

    Add package ai2-tango/0.14.0/ai2_tango-0.14.0-py3-none-any.whl

---
+++ packages/ai2-tango/ai2_tango-0.14.0-py3-none-any.whl/tango/version.py
@@ -0,0 +1,8 @@
+_MAJOR = "0"
+_MINOR = "14"
+_PATCH = "0"
+# This is mainly for pre-releases which have the suffix "rc[0-9]+".
+_SUFFIX = ""
+
+VERSION_SHORT = "{0}.{1}".format(_MAJOR, _MINOR)
+VERSION = "{0}.{1}.{2}{3}".format(_MAJOR, _MINOR, _PATCH, _SUFFIX)
