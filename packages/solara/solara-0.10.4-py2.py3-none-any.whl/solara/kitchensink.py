05db659e705c94438b856b438d407d65f45209db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:41 +0000

    Add package solara/0.10.4/solara-0.10.4-py2.py3-none-any.whl

---
+++ packages/solara/solara-0.10.4-py2.py3-none-any.whl/solara/kitchensink.py
@@ -0,0 +1,8 @@
+import react_ipywidgets as react  # noqa: F401
+from react_ipywidgets import ipyvue as vue  # noqa: F401
+from react_ipywidgets import ipyvuetify as v  # noqa: F401
+from react_ipywidgets import ipywidgets as w  # noqa: F401
+
+import solara as sol  # noqa: F401
+
+from . import layout  # noqa: F401
