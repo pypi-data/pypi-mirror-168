c28871aae6090433a4f2f1dfcdbcd8223191a249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:54 +0000

    Add package spatialmath-rospy/0.2.0/spatialmath-rospy-0.2.0.tar.gz

---
+++ packages/spatialmath-rospy/spatialmath-rospy-0.2.0.tar.gz/spatialmath-rospy-0.2.0/spatialmath_rospy/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.2.0"
+
+from spatialmath_rospy.convert import to_spatialmath, to_ros
+from spatialmath_rospy.monkey_patch_spatialmath import monkey_patch_spatialmath
+from spatialmath_rospy.ros_compat_mixin import ROSCompatMixin, SE3, SO3, UnitQuaternion
+
+__all__ = ["to_spatialmath", "to_ros", "ROSCompatMixin", "monkey_patch_spatialmath", "SE3", "SO3", "UnitQuaternion"]
