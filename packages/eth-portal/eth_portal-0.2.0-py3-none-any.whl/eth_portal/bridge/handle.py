cdc0537bc09a6703b5589bc9c7cef089f2d158d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:06 +0000

    Add package eth-portal/0.2.0/eth_portal-0.2.0-py3-none-any.whl

---
+++ packages/eth-portal/eth_portal-0.2.0-py3-none-any.whl/eth_portal/bridge/handle.py
@@ -0,0 +1,24 @@
+from .history import propagate_block
+from .insert import PortalInserter
+
+
+def handle_new_header(
+    w3, portal_inserter: PortalInserter, header_hash: bytes, chain_id=1
+):
+    """
+    Handle header hash notifications by posting all new data to Portal History Network.
+
+    This data to be propagated will at least include header, block bodies
+    (uncles & transactions), and receipts. At documentation time, only headers
+    are propagated.
+
+    :param w3: web3 access to core Ethereum content
+    :param portal_inserter: a class responsible for pushing content keys and
+        values into the network via a group of running portal clients
+    :param header_hash: the new header hash that we were notified exists on the network
+    :param chain_id: Ethereum network Chain ID that this header exists on
+    """
+    # Retrieve data to post to network
+    block_fields = w3.eth.get_block(header_hash, full_transactions=True)
+
+    propagate_block(w3, portal_inserter, block_fields, chain_id)
