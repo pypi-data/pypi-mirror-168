cbbb9f339b294011111776ab68dc0e03da9dc54f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:27 +0000

    Add package oc-python-sdk/1.0.0/oc_python_sdk-1.0.0.tar.gz

---
+++ packages/oc-python-sdk/oc_python_sdk-1.0.0.tar.gz/oc_python_sdk-1.0.0/python_sdk/utils/logger.py
@@ -0,0 +1,13 @@
+# Logger configuration
+import logging
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
+LOG_FORMAT = os.environ.get('LOG_FORMAT', '%(asctime)s %(name)-32s %(levelname)-8s %(message)s')
+logging.basicConfig(format=LOG_FORMAT, level=LOG_LEVEL)
+
+payment_logger = logging.getLogger('python-sdk.payment')
