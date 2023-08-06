4e184ffcdd21966747c5075646a43ea32f4041ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:14 +0000

    Add package ddgui/0.4/ddgui-0.4.tar.gz

---
+++ packages/ddgui/ddgui-0.4.tar.gz/ddgui-0.4/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+ name='ddgui',
+ version='0.4',
+ author="Limadijaya",
+ author_email="limasuhendra@Gmail.com",
+ description="This is DD Graphic User Interface",
+ packages=setuptools.find_packages(),
+ classifiers=[
+ "Programming Language :: Python :: 3",
+ "License :: OSI Approved :: MIT License",
+ "Operating System :: OS Independent",
+ ],
+)
