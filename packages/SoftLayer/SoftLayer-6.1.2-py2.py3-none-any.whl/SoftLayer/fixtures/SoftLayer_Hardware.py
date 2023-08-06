1b213c8c10fe5bc5cb3e300317c06e8a6a767c3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:37 +0000

    Add package SoftLayer/6.1.2/SoftLayer-6.1.2-py2.py3-none-any.whl

---
+++ packages/SoftLayer/SoftLayer-6.1.2-py2.py3-none-any.whl/SoftLayer/fixtures/SoftLayer_Hardware.py
@@ -0,0 +1,106 @@
+getObject = {
+    'id': 1234,
+    'globalIdentifier': 'xxxxc-asd',
+    'datacenter': {'id': 12, 'name': 'DALLAS21',
+                   'description': 'Dallas 21'},
+    'billingItem': {
+        'id': 6327,
+        'recurringFee': 1.54,
+        'nextInvoiceTotalRecurringAmount': 16.08,
+        'children': [
+            {'description': 'test', 'nextInvoiceTotalRecurringAmount': 1},
+        ],
+        'orderItem': {
+            'order': {
+                'userRecord': {
+                    'username': 'bob',
+                }
+            }
+        }
+    },
+    'primaryIpAddress': '4.4.4.4',
+    'hostname': 'testtest1',
+    'domain': 'test.sftlyr.ws',
+    'bareMetalInstanceFlag': True,
+    'fullyQualifiedDomainName': 'testtest1.test.sftlyr.ws',
+    'processorPhysicalCoreAmount': 4,
+    'memoryCapacity': 4,
+    'primaryBackendIpAddress': '10.4.4.4',
+    'networkManagementIpAddress': '10.4.4.4',
+    'hardwareStatus': {'status': 'ACTIVE'},
+    'primaryNetworkComponent': {'maxSpeed': 1000, 'speed': 1000},
+    'provisionDate': '2020-08-01 15:23:45',
+    'notes': 'NOTES NOTES NOTES',
+    'operatingSystem': {
+        'softwareLicense': {
+            'softwareDescription': {
+                'referenceCode': 'UBUNTU_20_64',
+                'name': 'Ubuntu',
+                'version': 'Ubuntu 20.04 LTS',
+            }
+        },
+        'passwords': [
+            {'username': 'root', 'password': 'xxxxxxxxxxxx'}
+        ],
+    },
+    'remoteManagementAccounts': [
+        {'username': 'root', 'password': 'zzzzzzzzzzzzzz'}
+    ],
+    'networkVlans': [
+        {
+            'networkSpace': 'PRIVATE',
+            'vlanNumber': 1234,
+            'id': 11111
+        },
+    ],
+    'tagReferences': [
+        {'tag': {'name': 'a tag'}}
+    ],
+}
+
+allowAccessToNetworkStorageList = True
+
+getSensorData = [
+    {
+        "lowerCritical": "5.000",
+        "lowerNonCritical": "10.000",
+        "sensorId": "Ambient 1 Temperature",
+        "sensorReading": "25.000",
+        "sensorUnits": "degrees C",
+        "status": "ok",
+        "upperCritical": "43.000",
+        "upperNonCritical": "41.000",
+        "upperNonRecoverable": "46.000"
+    },
+    {
+        "lowerCritical": "3500.000",
+        "lowerNonCritical": "3700.000",
+        "sensorId": "Fan 1 Tach",
+        "sensorReading": "6580.000",
+        "sensorUnits": "RPM",
+        "status": "ok",
+        "upperCritical": "25400.000",
+        "upperNonCritical": "25300.000",
+    }, {
+        "sensorId": "IPMI Watchdog",
+        "sensorReading": "0x0",
+        "sensorUnits": "discrete",
+        "status": "0x0080"
+    }, {
+        "sensorId": "Avg Power",
+        "sensorReading": "70.000",
+        "sensorUnits": "Watts",
+        "status": "ok"
+    },
+    {
+        "lowerCritical": "10.536",
+        "lowerNonCritical": "10.780",
+        "lowerNonRecoverable": "10.170",
+        "sensorId": "12V",
+        "sensorReading": "12.305",
+        "sensorUnits": "Volts",
+        "status": "ok",
+        "upperCritical": "13.281",
+        "upperNonCritical": "12.915",
+        "upperNonRecoverable": "13.403"
+    }]
