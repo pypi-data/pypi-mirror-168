3f9ea4673127090f42964a3718426c24039c0b79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:51 +0000

    Add package AnyRobot/1.4.3/AnyRobot-1.4.3.tar.gz

---
+++ packages/AnyRobot/AnyRobot-1.4.3.tar.gz/AnyRobot-1.4.3/aishu/datafaker/profession/entity/testMail.py
@@ -0,0 +1,12 @@
+
+class date(object):
+    def getMailUser(self):
+        return 'test@aishu.tech'
+
+    def getMailPass(self):
+        return 'niNPLwLP'
+
+    def getADUser(self):
+        return 'ar.test@aishu.cn'
+    def getADPass(self):
+        return 'artest`123'
