715b8beceb2630b2a08acbdb3d8d745447cba098
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:58:16 +0000

    Add package aicns-univariate-analyzer/0.2.0/aicns-univariate-analyzer-0.2.0.tar.gz

---
+++ packages/aicns-univariate-analyzer/aicns-univariate-analyzer-0.2.0.tar.gz/aicns-univariate-analyzer-0.2.0/univariate/analyzer/__init__.py
@@ -0,0 +1,4 @@
+from univariate.analyzer.analysis_report import AnalysisReport
+from univariate.analyzer.analyzer import Analyzer
+from univariate.analyzer.regularity_analyzer import RegularityAnalyzer
+from univariate.analyzer.descriptive_stat_analyzer import DescriptiveStatAnalyzer
