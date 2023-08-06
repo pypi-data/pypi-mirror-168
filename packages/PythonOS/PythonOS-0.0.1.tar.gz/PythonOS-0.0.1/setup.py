996638fda6f6bdde98291186957556684644f936
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:37 +0000

    Add package PythonOS/0.0.1/PythonOS-0.0.1.tar.gz

---
+++ packages/PythonOS/PythonOS-0.0.1.tar.gz/PythonOS-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PythonOS",
+    version="0.0.1",
+    description="A simple OS package that can be used to build a Python OS.",
+    long_description="A simple OS package that can be used to build a Python OS simply using our package. \n Credit to Eric for creating and caring for this package.",
+    author="Eric",
+    author_email="email@devwatch.epizy.com",
+    packages=find_packages(),
+    license="MIT",
+    keywords=["PythonOS"],
+    requires=[]
+)
