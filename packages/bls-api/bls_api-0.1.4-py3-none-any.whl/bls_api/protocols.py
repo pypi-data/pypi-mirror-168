647640e08b68cdf391819d4fa6fca4b4eb548fb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:17 +0000

    Add package bls-api/0.1.4/bls_api-0.1.4-py3-none-any.whl

---
+++ packages/bls-api/bls_api-0.1.4-py3-none-any.whl/bls_api/protocols.py
@@ -0,0 +1,36 @@
+from pydantic import BaseModel,StrictStr
+from typing import List, Union,Any
+
+class series_id(BaseModel):
+    id: Union[List[StrictStr],StrictStr]
+
+
+
+
+
+class catalog(BaseModel):
+    series_id:Any=None
+    series_title:Any=None
+    seasonality:Any = None
+    survey_name:Any=None
+    survey_abbreviation:Any=None
+    measure_data_type: Any=None
+    area:Any=None
+    area_type:Any=None
+    industry:Any =None
+    supersector:Any =None
+
+class series_data(BaseModel):
+    year:Any=None
+    period:Any=None
+    value:Any=None
+    periodName:Any=None
+
+class series_data_type(BaseModel):
+    data_type:Any=None
+
+
+
+
+
+
