16bb2436602cdf7bd2cbe5086e4155380c7e54f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:51 +0000

    Add package redframes/1.0/redframes-1.0.tar.gz

---
+++ packages/redframes/redframes-1.0.tar.gz/redframes-1.0/redframes/verbs/select.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+from ..checks import _check_type
+from ..types import LazyColumns, PandasDataFrame
+
+
+def select(df: PandasDataFrame, columns: LazyColumns) -> PandasDataFrame:
+    _check_type(columns, {list, str})
+    columns = [columns] if isinstance(columns, str) else columns
+    bad_columns = list(set(columns) - set(df.columns))
+    if bad_columns and len(bad_columns) == 1:
+        raise KeyError(f"column key: {bad_columns} is invalid")
+    if bad_columns and len(bad_columns) > 1:
+        raise KeyError(f"column keys: {bad_columns} are invalid")
+    df = df[columns]
+    return df
