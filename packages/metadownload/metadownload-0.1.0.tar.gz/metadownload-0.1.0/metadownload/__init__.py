f7c6a5de170b2025a6e6f22b42b29da3c88b3b70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:06 +0000

    Add package metadownload/0.1.0/metadownload-0.1.0.tar.gz

---
+++ packages/metadownload/metadownload-0.1.0.tar.gz/metadownload-0.1.0/metadownload/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+
+__title__ = 'metadownload'
+__description__ = 'download metainfo based on biosample accession number'
+__url__ = 'https://github.com/hbucqp/metadownload'
+__version__ = "0.1.0"
+__author__ = 'Qingpo Cui'
+__author_email__ = 'cqp@cau.edu.cn'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2022 Qingpo Cui'
