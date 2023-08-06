86799ef64be48603f8b5f5a6aaef20bb9962f84d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:05 +0000

    Add package lhcb-ftcalib/1.2.8/lhcb_ftcalib-1.2.8.tar.gz

---
+++ packages/lhcb-ftcalib/lhcb_ftcalib-1.2.8.tar.gz/lhcb_ftcalib-1.2.8/tests/epm_reference_data/run_epm.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+EPM=$1
+set -e
+
+function calibrate {
+    cd $1
+    $EPM calibrate.py |& tee calibrate.log
+    mv EspressoCalibratedPerformanceSummary.csv EspressoCalibratedPerformanceSummary.csv.orig
+    mv EspressoPerformanceSummary.csv EspressoPerformanceSummary.csv.orig
+    $EPM write.py |& tee write.log
+    cd ..
+}
+
+calibrate Bd_bspline4_logit
+calibrate Bd_bspline5_mistag
+calibrate Bd_nspline3_mistag
+calibrate Bd_nspline4_logit
+calibrate Bd_poly1_logit
+calibrate Bd_poly1_mistag
+calibrate Bd_poly2_mistag
+calibrate Bs_poly1_mistag
+calibrate Bu_poly1_mistag
+calibrate Bd_poly1_mistag_sweights
+
+cd combination_test
+$EPM combine.py |& tee combine.log
+cd ..
