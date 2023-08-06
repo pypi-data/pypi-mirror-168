0f8c999139d844a3c54680f327f05b0916676231
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:01 +0000

    Add package clayrs/0.2.1/clayrs-0.2.1.tar.gz

---
+++ packages/clayrs/clayrs-0.2.1.tar.gz/clayrs-0.2.1/clayrs/recsys/__init__.py
@@ -0,0 +1,10 @@
+from . import content_based_algorithm
+from . import graph_based_algorithm
+from . import graphs
+
+from .content_based_algorithm import *
+from .graph_based_algorithm import *
+from .graphs import *
+from .recsys import ContentBasedRS, GraphBasedRS
+from .partitioning import KFoldPartitioning, HoldOutPartitioning, Split, BootstrapPartitioning
+from .methodology import TestRatingsMethodology, TestItemsMethodology, TrainingItemsMethodology, AllItemsMethodology
