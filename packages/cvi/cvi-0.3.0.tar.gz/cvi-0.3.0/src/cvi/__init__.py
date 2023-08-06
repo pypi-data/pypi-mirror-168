a2aec29f44a0a5e74a9d8d6a4d2ccfc48564a442
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:36 +0000

    Add package cvi/0.3.0/cvi-0.3.0.tar.gz

---
+++ packages/cvi/cvi-0.3.0.tar.gz/cvi-0.3.0/src/cvi/__init__.py
@@ -0,0 +1,45 @@
+"""
+cvi - A Python library for both incremental and batch cluster validity indices.
+"""
+
+# Set the version variable of the package
+__version__ = "0.3.0"
+
+# Import CVI modules to the top level
+from .modules import (
+    CVI,
+    CH,
+    cSIL,
+    DB,
+    GD43,
+    GD53,
+    WB,
+    XB,
+)
+
+# import compat
+from . import compat
+
+# Set these names to be imported
+__all__ = [
+    "CVI",
+    "CH",
+    "cSIL",
+    "DB",
+    "GD43",
+    "GD53",
+    "WB",
+    "XB",
+    "compat",
+]
+
+# Convenience variable containing all implemented modules
+MODULES = [
+    CH,
+    cSIL,
+    DB,
+    GD43,
+    GD53,
+    WB,
+    XB,
+]
