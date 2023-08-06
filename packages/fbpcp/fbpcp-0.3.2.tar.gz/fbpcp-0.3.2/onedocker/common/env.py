0a08cf2be5d6e8bb3ae93b701f438b4870a323f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:11 +0000

    Add package fbpcp/0.3.2/fbpcp-0.3.2.tar.gz

---
+++ packages/fbpcp/fbpcp-0.3.2.tar.gz/fbpcp-0.3.2/onedocker/common/env.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+# This is the repository path that OneDocker downloads binaries from
+ONEDOCKER_REPOSITORY_PATH = "ONEDOCKER_REPOSITORY_PATH"
+
+# This is the repository path that OneDocker downloads binary checksums from
+ONEDOCKER_CHECKSUM_REPOSITORY_PATH = "ONEDOCKER_CHECKSUM_REPOSITORY_PATH"
+
+# This is the local path that the binaries reside
+ONEDOCKER_EXE_PATH = "ONEDOCKER_EXE_PATH"
+
+# This is the path user specified to upload the core dump file to
+CORE_DUMP_REPOSITORY_PATH = "CORE_DUMP_REPOSITORY_PATH"
+
+# This is the type of checksum we want to compare when running program
+ONEDOCKER_CHECKSUM_TYPE = "ONEDOCKER_CHECKSUM_TYPE"
