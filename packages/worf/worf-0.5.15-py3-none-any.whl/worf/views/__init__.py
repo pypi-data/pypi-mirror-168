59fc86978aca88d831d75e886ae50ab7beff0ead
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:58:06 +0000

    Add package worf/0.5.15/worf-0.5.15-py3-none-any.whl

---
+++ packages/worf/worf-0.5.15-py3-none-any.whl/worf/views/__init__.py
@@ -0,0 +1,6 @@
+from worf.views.base import AbstractBaseAPI, APIResponse  # noqa
+from worf.views.create import CreateAPI  # noqa
+from worf.views.delete import DeleteAPI  # noqa
+from worf.views.detail import DetailAPI, DetailUpdateAPI  # noqa
+from worf.views.list import ListAPI, ListCreateAPI  # noqa
+from worf.views.update import UpdateAPI  # noqa
