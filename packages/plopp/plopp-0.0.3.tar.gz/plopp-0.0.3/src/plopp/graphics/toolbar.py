0fa0e64aa8b1276c24996dab278d984501c31dc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:31 +0000

    Add package plopp/0.0.3/plopp-0.0.3.tar.gz

---
+++ packages/plopp/plopp-0.0.3.tar.gz/plopp-0.0.3/src/plopp/graphics/toolbar.py
@@ -0,0 +1,82 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
+
+from ipywidgets import Button, VBox
+from functools import partial
+from typing import Callable
+
+LAYOUT_STYLE = {"layout": {"width": "34px", "padding": "0px 0px 0px 0px"}}
+
+
+class ButtonTool:
+
+    def __init__(self, callback: Callable = None, **kwargs):
+        """
+        Create a new button with a callback that is called when the button is clicked.
+        """
+        self.widget = Button(**{**LAYOUT_STYLE, **kwargs})
+        self._callback = callback
+        self.widget.on_click(self)
+
+    def __call__(self, *args, **kwargs):
+        self._callback()
+
+
+class ToggleTool:
+
+    def __init__(self, callback: Callable, value: bool = False, **kwargs):
+        """
+        Create a toggle button with a callback that is called when the button is
+        toggled. We use a Button and handle the styling ourselves because in some
+        cases, we need to toggle the button color without triggering the callback
+        function.
+        """
+        self.widget = Button(**{**LAYOUT_STYLE, **kwargs})
+        self._callback = callback
+        self.widget.on_click(self)
+        self._value = value
+
+    def __call__(self, *args, **kwargs):
+        self._toggle()
+        self._callback()
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, val):
+        self._value = val
+        self._update_color()
+
+    def _update_color(self):
+        self.widget.button_style = 'info' if self._value else ''
+
+    def _toggle(self):
+        self._value = not self._value
+        self._update_color()
+
+
+TOOL_LIBRARY = {
+    'home': partial(ButtonTool, icon="home", tooltip="Autoscale view"),
+    'pan': partial(ToggleTool, icon="arrows", tooltip="Pan"),
+    'zoom': partial(ToggleTool, icon="search-plus", tooltip="Zoom"),
+    'logx': partial(ToggleTool, description="logx", tooltip="Toggle X axis scale"),
+    'logy': partial(ToggleTool, description="logy", tooltip="Toggle Y axis scale"),
+    'save': partial(ButtonTool, icon="save", tooltip="Save figure")
+}
+
+
+class Toolbar(VBox):
+    """
+    Custom toolbar with additional buttons for controlling log scales and
+    normalization, and with back/forward buttons removed.
+    """
+
+    def __init__(self, tools=None):
+        self._widgets = {}
+        for key, callback in tools.items():
+            tool = TOOL_LIBRARY[key](callback=callback)
+            setattr(self, key, tool)
+            self._widgets[key] = tool.widget
+        super().__init__(tuple(self._widgets.values()))
---
+++ packages/plopp/plopp-0.0.3.tar.gz/plopp-0.0.3/tests/kwargs_test.py
@@ -0,0 +1,144 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
+
+import numpy as np
+from scipp import scalar
+import plopp as pp
+from plopp.data import dense_data_array, dense_dataset
+
+
+def _get_child(plot, n=0):
+    children = plot._children
+    keys = list(children.keys())
+    return children[keys[n]]
+
+
+def _get_line(plot, n=0):
+    return _get_child(plot=plot, n=n)._line
+
+
+def _get_mesh(plot):
+    return _get_child(plot)._mesh
+
+
+def test_kwarg_linecolor():
+    da = dense_data_array(ndim=1)
+    p = pp.plot(da, color='r')
+    assert _get_line(p).get_color() == 'r'
+    p = pp.plot(da, c='b')
+    assert _get_line(p).get_color() == 'b'
+
+
+def test_kwarg_linestyle():
+    da = dense_data_array(ndim=1)
+    p = pp.plot(da, linestyle='solid')
+    assert _get_line(p).get_linestyle() == '-'
+    p = pp.plot(da, ls='dashed')
+    assert _get_line(p).get_linestyle() == '--'
+
+
+def test_kwarg_linewidth():
+    da = dense_data_array(ndim=1)
+    p = pp.plot(da, linewidth=3)
+    assert _get_line(p).get_linewidth() == 3
+    p = pp.plot(da, lw=5)
+    assert _get_line(p).get_linewidth() == 5
+
+
+def test_kwarg_marker():
+    da = dense_data_array(ndim=1)
+    p = pp.plot(da, marker='+')
+    assert _get_line(p).get_marker() == '+'
+
+
+def test_kwarg_norm():
+    da = dense_data_array(ndim=1)
+    p = pp.plot(da, norm='log')
+    assert p._ax.get_yscale() == 'log'
+
+
+def test_kwarg_scale():
+    da = dense_data_array(ndim=1)
+    p = pp.plot(da, scale={'xx': 'log'})
+    assert p._ax.get_xscale() == 'log'
+    assert p._ax.get_yscale() == 'linear'
+
+
+def test_kwarg_cmap():
+    da = dense_data_array(ndim=2)
+    p = pp.plot(da, cmap='magma')
+    assert _get_mesh(p).get_cmap().name == 'magma'
+
+
+def test_kwarg_scale_2d():
+    da = dense_data_array(ndim=2)
+    p = pp.plot(da, scale={'xx': 'log', 'yy': 'log'})
+    assert p._ax.get_xscale() == 'log'
+    assert p._ax.get_yscale() == 'log'
+
+
+def test_kwarg_crop_1d_min_max():
+    da = dense_data_array(ndim=1)
+    p = pp.plot(da,
+                crop={'xx': {
+                    'min': scalar(20, unit='m'),
+                    'max': scalar(40, unit='m')
+                }})
+    assert np.array_equal(p._ax.get_xlim(), [20, 40])
+
+
+def test_kwarg_crop_1d_min_only():
+    da = dense_data_array(ndim=1)
+    p = pp.plot(da, crop={'xx': {'min': scalar(20, unit='m')}})
+    assert p._ax.get_xlim()[0] == 20
+
+
+def test_kwarg_crop_1d_min_conversion():
+    da = dense_data_array(ndim=1)
+    p = pp.plot(da, crop={'xx': {'min': scalar(200, unit='cm')}})
+    assert p._ax.get_xlim()[0] == 2
+
+
+def test_kwarg_crop_1d_with_no_unit():
+    da = dense_data_array(ndim=1)
+    del da.coords['xx']
+    p = pp.plot(da, crop={'xx': {'min': scalar(20, unit=None)}})
+    assert p._ax.get_xlim()[0] == 20
+    p = pp.plot(da, crop={'xx': {'min': 20}})
+    assert p._ax.get_xlim()[0] == 20
+    p = pp.plot(da, crop={'xx': {'min': 20.5}})
+    assert p._ax.get_xlim()[0] == 20.5
+
+
+def test_kwarg_crop_2d():
+    da = dense_data_array(ndim=2)
+    p = pp.plot(da,
+                crop={
+                    'xx': {
+                        'min': scalar(20, unit='m')
+                    },
+                    'yy': {
+                        'min': scalar(10, unit='m'),
+                        'max': scalar(4000, unit='cm')
+                    }
+                })
+    assert p._ax.get_xlim()[0] == 20
+    assert np.array_equal(p._ax.get_ylim(), [10, 40])
+
+
+def test_kwarg_for_two_lines():
+    ds = dense_dataset(ndim=1)
+    p = pp.plot(ds, color='r')
+    assert _get_line(p, 0).get_color() == 'r'
+    assert _get_line(p, 1).get_color() == 'r'
+
+
+def test_kwarg_as_dict():
+    ds = dense_dataset(ndim=1)
+    p = pp.plot(ds, color={'a': 'red', 'b': 'black'})
+    assert _get_line(p, 0).get_color() == 'red'
+    assert _get_line(p, 1).get_color() == 'black'
+    da = dense_data_array(ndim=1)
+    p = pp.plot({'a': da, 'b': 0.2 * da}, color={'a': 'red', 'b': 'black'})
+    assert _get_line(p, 0).get_color() == 'red'
+    assert _get_line(p, 1).get_color() == 'black'
