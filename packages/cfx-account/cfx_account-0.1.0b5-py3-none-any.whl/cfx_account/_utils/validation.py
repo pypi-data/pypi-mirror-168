f19ef663e94f1473f8022d142e85a4d462aaf7b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:13 +0000

    Add package cfx-account/0.1.0b5/cfx_account-0.1.0b5-py3-none-any.whl

---
+++ packages/cfx-account/cfx_account-0.1.0b5-py3-none-any.whl/cfx_account/_utils/validation.py
@@ -0,0 +1,10 @@
+# def validate_network_id(chain_id):
+#     if isinstance(chain_id, int) and chain_id > 0:
+#         return
+#     if isinstance(chain_id, str):
+#         if int(chain_id, 16) > 0:
+#             return
+        
+#     raise ValueError("Expected a positive integer, "
+#                      f"receives {chain_id}")
+        
