3c5ff72ac55e28888e5b0e2967d809840341fee9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:26 +0000

    Add package sysnet-esmlouvy/0.0.28/sysnet_esmlouvy-0.0.28-py3-none-any.whl

---
+++ packages/sysnet-esmlouvy/sysnet_esmlouvy-0.0.28-py3-none-any.whl/esmlouvy-connector/api/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client.api.admins_api import AdminsApi
+from swagger_client.api.attachments_api import AttachmentsApi
+from swagger_client.api.developers_api import DevelopersApi
+from swagger_client.api.public_api import PublicApi
