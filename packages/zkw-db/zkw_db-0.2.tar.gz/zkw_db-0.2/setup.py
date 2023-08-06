a13327b29b8af606067db9a572506e0eaf3eb3e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:27 +0000

    Add package zkw-db/0.2/zkw_db-0.2.tar.gz

---
+++ packages/zkw-db/zkw_db-0.2.tar.gz/zkw_db-0.2/setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup,find_packages
+
+setup(name='zkw_db',
+      version='0.2',
+      description='string to date',
+      # url="https://github.com/18832823200/pip_model",
+      aauthor="sbdsyf",
+      author_email="zkw8119@163.com",
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      platforms="any",
+      install_requires=[],
+
+      )
---
+++ packages/zkw-db/zkw_db-0.2.tar.gz/zkw_db-0.2/zkw_db/__init__.py
@@ -0,0 +1,2 @@
+from . import create_table
+from . import my_class
