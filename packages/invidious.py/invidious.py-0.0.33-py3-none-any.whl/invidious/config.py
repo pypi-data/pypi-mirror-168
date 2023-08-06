3449712ddda0e5de016336f0fd5411f300ab0e3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:24 +0000

    Add package invidious.py/0.0.33/invidious.py-0.0.33-py3-none-any.whl

---
+++ packages/invidious.py/invidious.py-0.0.33-py3-none-any.whl/invidious/config.py
@@ -0,0 +1,14 @@
+
+
+HEADERS = ({
+        "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) Chrome"
+        "/75.0.3770.100 Safari/537.36"
+    })
+
+MIRRORS = [
+    "https://invidious.snopyta.org",
+    "https://vid.puffyan.us",
+    "https://yewtu.be"
+]
