83b2faab4ebb35c2db02a3f53a05dbc05fbac4cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:45 +0000

    Add package mlflow-mlserver-docker/0.1.0/mlflow-mlserver-docker-0.1.0.tar.gz

---
+++ packages/mlflow-mlserver-docker/mlflow-mlserver-docker-0.1.0.tar.gz/mlflow-mlserver-docker-0.1.0/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['mlflow_mlserver_docker']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['boto3>=1.24.76,<2.0.0',
+ 'mlflow-skinny>=1.28.0,<2.0.0',
+ 'python-dotenv>=0.21.0,<0.22.0',
+ 'python-on-whales>=0.52.0,<0.53.0',
+ 'typer[all]>=0.6.1,<0.7.0']
+
+entry_points = \
+{'console_scripts': ['mlflow-mlserver-docker = mlflow_mlserver_docker.cli:app']}
+
+setup_kwargs = {
+    'name': 'mlflow-mlserver-docker',
+    'version': '0.1.0',
+    'description': 'Package mlflow models as mlserver docker images',
+    'long_description': 'None',
+    'author': 'Martin Morset',
+    'author_email': 'mmorset@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
