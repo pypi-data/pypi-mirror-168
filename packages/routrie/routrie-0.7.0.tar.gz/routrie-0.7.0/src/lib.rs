8ca24e1c8f8deb86d3a156aba6f2b9008b3fa370
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:29 +0000

    Add package routrie/0.7.0/routrie-0.7.0.tar.gz

---
+++ packages/routrie/routrie-0.7.0.tar.gz/routrie-0.7.0/src/lib.rs
@@ -0,0 +1,32 @@
+use path_tree::PathTree;
+use pyo3::prelude::*;
+
+#[pyclass(module = "routrie._routrie")]
+struct Router {
+    router: PathTree<Py<PyAny>>,
+}
+
+#[pymethods]
+impl Router {
+    #[new]
+    fn new() -> Self {
+        Router {
+            router: PathTree::new(),
+        }
+    }
+    fn insert(&mut self, path: &str, data: &PyAny, py: Python) -> () {
+        self.router.insert(path, data.into_py(py));
+    }
+    fn find<'m>(&'m self, path: &'m str) -> Option<(&'m Py<PyAny>, Vec<(&'m str, &'m str)>)> {
+        match self.router.find(path) {
+            None => None,
+            Some(path) => Some((path.value, path.params())),
+        }
+    }
+}
+
+#[pymodule]
+fn _routrie(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<Router>()?;
+    Ok(())
+}
