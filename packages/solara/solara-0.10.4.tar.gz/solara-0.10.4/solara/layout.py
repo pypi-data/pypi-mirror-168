2b754aeaf02aff78e02e46d5b92b328a2460a5a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:42 +0000

    Add package solara/0.10.4/solara-0.10.4.tar.gz

---
+++ packages/solara/solara-0.10.4.tar.gz/solara-0.10.4/solara/layout.py
@@ -0,0 +1,47 @@
+import react_ipywidgets as react
+import react_ipywidgets.ipyvuetify as v
+
+from solara.kitchensink import vue
+
+
+@react.component
+def AppIcon(open=False, on_click=None):
+    def click(*ignore):
+        print("click")
+        on_click()
+
+    icon = v.AppBarNavIcon()
+    v.use_event(icon, "click", click)
+    return icon
+
+
+@react.component
+def LayoutApp(children=[], left=None, right=None, open_left=False, open_right=False, title="Solara"):
+    open_left, set_open_left = react.use_state(open_left)
+    open_right, set_open_right = react.use_state(open_right)
+    print(open_left)
+    with v.Html(tag="div") as main:
+        if left:
+            with v.NavigationDrawer(absolute=True, right=False, width="min-content", v_model=open_left):
+                AppIcon(open_left, on_click=lambda: set_open_left(not open_left))
+                with v.Html(tag="div", children=[left]):
+                    pass
+        with v.Toolbar(dense=True, class_="elevation-3", dark=False):
+            if left:
+                AppIcon(open_left, on_click=lambda: set_open_left(not open_left))
+            v.ToolbarTitle(children=[title])
+            v.Spacer()
+            if right:
+                with v.Btn(icon=True) as btn:
+                    vue.use_event(btn, "click", lambda *_ignore: set_open_right(not open_right))
+                    v.Icon(children=["mdi-settings"])
+        with v.Row():
+            v.Col(cols=12, children=[*children])
+        if right:
+            with v.NavigationDrawer(absolute=True, right=True, width="min-content", v_model=open_right):
+                with v.Btn(icon=True) as btn:
+                    vue.use_event(btn, "click", lambda *_ignore: set_open_right(not open_right))
+                    v.Icon(children=["mdi-settings"])
+                with v.Html(tag="div", children=[right]):
+                    pass
+    return main
