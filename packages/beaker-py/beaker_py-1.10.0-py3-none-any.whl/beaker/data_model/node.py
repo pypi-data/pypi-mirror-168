3f176954c59858d14bed48de638b648958ccc73a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:40 +0000

    Add package beaker-py/1.10.0/beaker_py-1.10.0-py3-none-any.whl

---
+++ packages/beaker-py/beaker_py-1.10.0-py3-none-any.whl/beaker/data_model/node.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from typing import Optional
+
+from .base import BaseModel
+
+__all__ = ["NodeResources", "Node", "NodeUtilization"]
+
+
+class NodeResources(BaseModel):
+    cpu_count: Optional[float] = None
+    memory: Optional[str] = None
+    gpu_count: Optional[int] = None
+    gpu_type: Optional[str] = None
+
+
+class Node(BaseModel):
+    id: str
+    hostname: str
+    created: datetime
+    expiry: datetime
+    limits: NodeResources
+
+
+class NodeUtilization(BaseModel):
+    id: str
+    hostname: str
+    limits: NodeResources
+    running_jobs: int
+    used: NodeResources
+    free: NodeResources
