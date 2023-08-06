d919ba760077e44a35faf4e17eeabb741978420b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:12 +0000

    Add package bloxflippredictor/3.1.4/bloxflippredictor-3.1.4-py3-none-any.whl

---
+++ packages/bloxflippredictor/bloxflippredictor-3.1.4-py3-none-any.whl/bloxflippredictor/__init__.py
@@ -0,0 +1,4 @@
+from bloxflippredictor.crash import crashpredictor
+from bloxflippredictor.towers import towerspredictor
+from bloxflippredictor.mines import minespredictor
+from bloxflippredictor.roulette import roulettepredictor
