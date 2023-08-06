844b198ba8ef58f97f39ee3459ce4c50e8ea613d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:39 +0000

    Add package calici-pk/0.0.1/calici_pk-0.0.1.tar.gz

---
+++ packages/calici-pk/calici_pk-0.0.1.tar.gz/calici_pk-0.0.1/calici_pk/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 15 09:19:01 2022
+
+@author: PAVAN KUMAR S
+"""
+
+def add(a,b):
+    return(a+b)
+def sub(a,b):
+    return(a-b)
+def mul(a,b):
+    return(a*b)
+def div(a,b):
+    return(a/b)
---
+++ packages/calici-pk/calici_pk-0.0.1.tar.gz/calici_pk-0.0.1/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 15 09:28:08 2022
+
+@author: PAVAN KUMAR S
+"""
+
+
+from setuptools import setup
+classifiers = [
+   'Development Status:: 5 - Production/Stable',
+   'Intended Audience :: Education',
+   'Operating System :: Microsoft :: Windows :: Windows 11',
+   'Topic :: Utilities',
+   'License :: OSI Approved :: MIT License',
+   'Programming Language :: Python 3.0.9',
+]
+setup(
+    name='calici_pk',
+    version='0.0.1',
+    description='this is a trial package used for calculation.',
+    url='',
+    author='pavankumar.s',
+    author_email='pavankumar.sa02@gmail.com',
+    licence='MIT',
+    packages=['calici_pk'],
+    install_requires=[]
+)
