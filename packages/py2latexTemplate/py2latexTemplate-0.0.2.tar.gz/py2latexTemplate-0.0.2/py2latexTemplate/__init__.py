000d6c8a0d4f717b20811d6cf438ac53320d5f19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:22 +0000

    Add package py2latexTemplate/0.0.2/py2latexTemplate-0.0.2.tar.gz

---
+++ packages/py2latexTemplate/py2latexTemplate-0.0.2.tar.gz/py2latexTemplate-0.0.2/py2latexTemplate/__init__.py
@@ -0,0 +1,1 @@
+from py2latexTemplate import LatexCommands, generateLatex
---
+++ packages/py2latexTemplate/py2latexTemplate-0.0.2.tar.gz/py2latexTemplate-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+			
+with open('README.md', 'r') as fh:
+	long_description = fh.read()
+
+setup(
+	name="py2latexTemplate",
+	version="0.0.2",
+	author="Gabrielle Ohlson",
+	description="Code for producing LaTeX files with python scripts (heavily dependent on sympy module); makes it simpler to format elegantly and include python math/code blocks in your LaTeX.",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/gabrielle-ohlson/py2latexTemplate",
+	packages=['py2latexTemplate'],
+	install_requires=['sympy']
+)
