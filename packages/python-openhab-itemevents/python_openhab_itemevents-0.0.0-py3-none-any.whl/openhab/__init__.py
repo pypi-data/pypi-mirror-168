817ba4546b3216d0b1f0ce154a5d258b3bd7d15a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:47 +0000

    Add package python-openhab-itemevents/0.0.0/python_openhab_itemevents-0.0.0-py3-none-any.whl

---
+++ packages/python-openhab-itemevents/python_openhab_itemevents-0.0.0-py3-none-any.whl/openhab/__init__.py
@@ -0,0 +1,8 @@
+"""Module entry point."""
+
+from .events import ItemEvent
+
+__all__ = ['ItemEvent']
+__version__ = "0.1.0"
+__author__ = 'Michael Dörflinger'
+__credits__ = 'Furtwangen University'
