d5b9fb6e5655e3855c23d4c7e6ca48fcf413b4b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:35 +0000

    Add package defgraficos/1.0/defgraficos-1.0.tar.gz

---
+++ packages/defgraficos/defgraficos-1.0.tar.gz/defgraficos-1.0/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from setuptools import setup
+setup(
+    name="defgraficos",
+    version="1.0",
+    description="Funciones para Graficos",
+    author="Jorge Guerra Guerra",
+    author_email="jguerra91@gmail.com",
+    url="https://dina.concytec.gob.pe/appDirectorioCTI/VerDatosInvestigador.do?id_investigador=12581",
+    packages=['defgraficos'],
+    scripts=[]
+)
+
+
+# In[ ]:
+
+
+
+
