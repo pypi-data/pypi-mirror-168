adb406efb350d2837154bb867a8d2452a0f3e3ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:07 +0000

    Add package python-enigma/1.1.2/python_enigma-1.1.2.tar.gz

---
+++ packages/python-enigma/python_enigma-1.1.2.tar.gz/python_enigma-1.1.2/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python_enigma",
+    version="1.1.2",
+    author="Zachary Adam-MacEwen",
+    author_email="zadammac@arcanalabs.ca",
+    description="A simple module which adds Enigma Machine-emulating functionality to your python projects.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    url="https://github.com/ZAdamMac/python-enigma",
+    packages=setuptools.find_packages()
+)
